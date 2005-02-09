48d6098256b520e3e34049394bdd0d8d2b2c054c
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-02-09 14:46:25 +0000

    headers for libjson
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@35 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ OpenSRF/include/libjson/json.h
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ *
+ * Copyright Metaparadigm Pte. Ltd. 2004.
+ * Michael Clark <michael@metaparadigm.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public (LGPL)
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details: http://www.gnu.org/
+ *
+ */
+
+#ifndef _json_h_
+#define _json_h_
+
+#include "libjson/bits.h"
+#include "libjson/debug.h"
+#include "libjson/linkhash.h"
+#include "libjson/arraylist.h"
+#include "libjson/json_util.h"
+#include "libjson/json_object.h"
+#include "libjson/json_tokener.h"
+
+#endif
