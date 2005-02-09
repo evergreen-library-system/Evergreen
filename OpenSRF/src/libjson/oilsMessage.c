0bbb2e0738a302b736113cae448bd9a774bf486f
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-02-09 14:45:55 +0000

    LGPL'd json C library.  Slightly modified to build a .so file so we can link it into our code.
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@34 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ OpenSRF/src/libjson/oilsMessage.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+#include "libjson/json.h"
+
+
+struct oils_method_struct {
+	char* method_name;
+	struct oils_param_struct* params;
+	char (*to_json_string) (struct oils_method_struct*);
+};
+typedef struct oils_method_struct oils_method;
+
+struct oils_param_struct {
+	struct oils_param_struct* next;
+	void* param;
+	char (*to_json_string) (struct oils_param_struct*);
+};
+typedef struct oils_param_struct oils_param;
+
+
+oils_method* oils_method_init( char* name );
+int oils_method_add( oils_method*, oils_param* param );
+oils_param* oils_param_init( void* param );
+char* oils_method_to_json( oils_method* );
+char* oils_param_to_json( oils_param* );
+int main();
+
+
+
+oils_method* oils_method_init( char* name ) {
+
+	if( name == NULL )
+		perror( "null method name" );
+
+	oils_method* method = (oils_method*) malloc(sizeof(oils_method));
+	memset( method, 0, sizeof(oils_method));
+
+	method->method_name = strdup( name );
+
+	return method;
+}
+
+int oils_method_add( oils_method* method, oils_param* param ) {
+
+	if( method->params == NULL ) {
+		method->params = param;
+		return 1;
+	}
+
+	while(1) {
+		if( method->params->next == NULL ) {
+			method->params->next = param;
+			return 1;
+		}
+		method->params = method->params->next;
+	}
+
+	return 0;
+}
+
+oils_param* oils_param_init( void* param ) {
+
+	if( param == NULL )
+		perror( "Null param" );
+
+	oils_param* par = (oils_param*) malloc(sizeof(oils_param));
+	memset( par, 0, sizeof(oils_param));
+
+	par->param = param;
+	return par;
+}
+
+
+json_object* oils_method_to_json( oils_method* method ) {
+  struct json_object *method_obj;
+
+}
+
+
+json_object* oils_param_to_json( oils_param* param ) {
+
+}
+
+int main() {
+	printf( "Hello world\n" );
+
+}
