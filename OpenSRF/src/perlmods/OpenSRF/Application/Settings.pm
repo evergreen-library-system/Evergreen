fd98c9f4253daa86a9aac039f6e3466ab4d065bf
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-02-16 17:24:50 +0000

    first round of the settings app
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@73 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ OpenSRF/src/perlmods/OpenSRF/Application/Settings.pm
@@ -0,0 +1,25 @@
+package OpenSRF::Application::Settings;
+use OpenSRF::Application;
+use OpenSRF::Utils::SettingsParser;
+use base 'OpenSRF::Application';
+
+
+
+__PACKAGE__->register_method( method => 'get_host_config', api_name => 'opensrf.settings.host_config.get' );
+sub get_host_config {
+	my( $self, $client, $host ) = @_;
+	my $parser = OpenSRF::Utils::SettingsParser->new();
+	return $parser->get_server_config($host);
+}
+
+
+
+
+__PACKAGE__->register_method( method => 'xpath_get', api_name => 'opensrf.settings.xpath.get' );
+sub xpath_get {
+	my($self, $client, $xpath) = @_;
+	return  OpenSRF::Utils::SettingsParser->new()->_get_all( $xpath );
+}
+
+
+1;
