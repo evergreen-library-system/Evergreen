0612feca3861993c2776702e63f336140c444a67
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-03-02 23:23:46 +0000

    use transaction interface
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@196 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/extras/import/import_clean_marc.pl
+++ Open-ILS/src/extras/import/import_clean_marc.pl
@@ -51,55 +51,72 @@ while ( my $xml = <> ) {
 	$ns->xml_to_nodeset;
 	#next;
 
-	my $req = $st_server->request(
-		'open-ils.storage.biblio.record_entry.create',
-		{	creator		=> $userid,
-			editor		=> $userid,
-			source		=> $sourceid,
-			tcn_value	=> $tcn,
-		},
-	);
+	my $xact = $st_server->request( 'open-ils.storage.transaction.begin' );
+	$xact->wait_complete;
+	warn "  ==> Starting transaction\n";
 
-	$req->wait_complete;
-
-	my $resp = $req->recv;
-	unless( $resp && $resp->can('content') ) {
-		throw OpenSRF::EX::ERROR ("Failed to create record for TCN [$tcn]!! -- $resp");
-	}
-
-	my $new_id = $resp->content;
-
-	$req->finish;
-
-	if ($new_id) {
-		my $nodeset = $ns->nodeset;
-		
-		$_->{owner_doc} = $new_id for (@$nodeset);
-		
-		$req = $st_server->request(
-			'open-ils.storage.biblio.record_node.batch.create',
-			@$nodeset,
+	try {
+		my $req = $st_server->request(
+			'open-ils.storage.biblio.record_entry.create',
+			{	creator		=> $userid,
+				editor		=> $userid,
+				source		=> $sourceid,
+				tcn_value	=> $tcn,
+			},
 		);
 
 		$req->wait_complete;
 
-		$resp = $req->recv;
+		my $resp = $req->recv;
 		unless( $resp && $resp->can('content') ) {
-			throw OpenSRF::EX::ERROR
-				("Failed to create record_nodes for TCN [$tcn]!! -- $resp");
+			throw OpenSRF::EX::ERROR ("Failed to create record for TCN [$tcn]!! -- $resp");
 		}
 
-
-		if ($wormize) {
-			my $worm_req = $worm_server->request(
-				'open-ils.worm.record_data.digest',
-				$new_id,
-			);
-		}
+		my $new_id = $resp->content;
 
 		$req->finish;
-	} else {
-		throw OpenSRF::EX::ERROR ("Failed to create record for TCN [$tcn]!! -- $resp");
+
+		if ($new_id) {
+			my $nodeset = $ns->nodeset;
+		
+			$_->{owner_doc} = $new_id for (@$nodeset);
+		
+			$req = $st_server->request(
+				'open-ils.storage.biblio.record_node.batch.create',
+				@$nodeset,
+			);
+
+			$req->wait_complete;
+
+			$resp = $req->recv;
+			unless( $resp && $resp->can('content') ) {
+				throw OpenSRF::EX::ERROR
+					("Failed to create record_nodes for TCN [$tcn]!! -- $resp");
+			}
+
+
+			if ($wormize) {
+				my $worm_req = $worm_server->request(
+					'open-ils.worm.record_data.digest',
+					$new_id,
+				);
+			}
+
+			$req->finish;
+		} else {
+			throw OpenSRF::EX::ERROR ("Failed to create record for TCN [$tcn]!! -- $resp");
+		}
+	} catch Error with {
+	warn "  !!> Rolling back transaction\n";
+		$xact = $st_server->request( 'open-ils.storage.transaction.rollback' );
+		$xact->wait_complete;
+		$xact = undef;
+	};
+
+	if ($xact) {
+		warn "  ==>Commiting addition of $tcn\n";
+		$xact = $st_server->request( 'open-ils.storage.transaction.commit' );
+		$xact->wait_complete;
 	}
 }
 
