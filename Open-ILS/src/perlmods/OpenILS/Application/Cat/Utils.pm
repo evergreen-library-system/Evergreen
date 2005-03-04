483c14e17df7e83ef2f7a0ca4aea0e213cd737ab
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-03-04 17:32:53 +0000

    added method that will convert a tree back to a nodelist
    added method that will walk a nodelist and perform appropriate actions
    on each node it encounters
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@217 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Cat/Utils.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Cat/Utils.pm
@@ -7,6 +7,8 @@ use constant TYPE			=> 4;
 use constant NAME			=> 5;
 use constant VALUE		=> 6;
 use constant CHILDREN	=> 7;
+use constant ALTERED		=> 8;
+use constant DELETED		=> 9;
 
 
 # Converts an XML nodeset into a tree
@@ -27,9 +29,76 @@ sub nodeset2tree {
 }
 
 
+my @_nodelist = ();
 sub tree2nodeset {
+	my($self, $node) = @_;
 
+	if(!defined($node->[PARENT])) {
+		@_nodelist = ();
+	}
+
+	push( @_nodelist, $node );
+	for my $child (@{$node->[CHILDREN]}) {
+
+		if(!defined($child->[PARENT])) {
+			$child->[PARENT] = $node->[INTRA_DOC];
+			$child->[ALTERED] = 1; #just to be sure
+		}
+
+		$self->tree2nodeset( $child );
+	}
+	$node->[CHILDREN] = undef;
+	return \@_nodelist;
+}
+
+sub commit_nodeset {
+	my($self, $nodeset) = @_;
+
+	my $size = @$nodeset;
+	my $offset = 0;
+
+	for my $index (0..$size) {
+
+		my $pos = $index + $offset;
+		my $node = $nodeset->[$index];
+		next unless $node;
+
+		if( $node->[DELETED] ) {
+			$offset--;
+			return 0 unless _deletenode($node);
+			next;
+		}
+
+		if(!defined($node->[INTRA_DOC])) {
+			$node->[INTRA_DOC] = $pos;
+			return 0 unless _addnode($node);
+			next;
+		}
+
+		if( $node->[INTRA_DOC] != $pos ||
+			 $node->[ALTERED] ) {
+
+			$node->[INTRA_DOC] = $pos;
+			return 0 unless _updatenode($node);
+			next;
+		}
+	}
+	return 1;
+}
+
+sub _updatenode {
+	my $node = shift;
+	return 1;
+}
+
+sub _addnode {
+	my $node = shift;
+	return 1;
+}
+
+sub _deletenode {
+	my $node = shift;
+	return 1;
 }
  
-
 1;
