95f07948b21daf34b60d0ce7275dbc5b76553018
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-03-07 20:29:43 +0000

    added a method lookup in child init to speed up the cat server.
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@252 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Cat.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Cat.pm
@@ -3,24 +3,32 @@ package OpenILS::Application::Cat;
 use OpenSRF::Application;
 use OpenILS::Application::Cat::Utils;
 use base qw/OpenSRF::Application/;
+use Time::HiRes qw(time);
+use JSON;
+use OpenILS::Utils::Fieldmapper;
 
 my $utils = "OpenILS::Application::Cat::Utils";
 
 
+sub child_init {
+	OpenSRF::Application->method_lookup( "blah" );
+}
 
 
 __PACKAGE__->register_method(
-	method	=> "record_tree_retrieve",
-	api_name	=> "open-ils.cat.record.tree.retrieve",
+	method	=> "biblio_record_tree_retrieve",
+	api_name	=> "open-ils.cat.biblio.record.tree.retrieve",
 	argc		=> 1, 
 	note		=> "Returns the tree associated with the nodeset of the given doc id"
 );
 
-sub record_tree_retrieve {
+sub biblio_record_tree_retrieve {
 	my( $self, $client, $recordid ) = @_;
 
+	warn "Starting Retrieve: " . time() . "\n";
 	my $name = "open-ils.storage.biblio.record_entry.nodeset.retrieve";
 	my $method = $self->method_lookup($name);
+	warn "Looked Up Method: " . time() . "\n";
 
 	unless($method) {
 		throw OpenSRF::EX::PANIC ("Could not lookup method $name");
@@ -34,24 +42,28 @@ sub record_tree_retrieve {
 
 	return undef unless $nodes;
 
-	return $utils->nodeset2tree( $nodes );
+	warn "Starting Tree Builder: " . time() . "\n";
+	my $tree = $utils->nodeset2tree( $nodes );
+	warn "Returning Tree: " . time() . "\n";
+	return $tree;
 }
 
 
 __PACKAGE__->register_method(
-	method	=> "record_tree_commit",
-	api_name	=> "open-ils.cat.record.tree.commit",
+	method	=> "biblio_record_tree_commit",
+	api_name	=> "open-ils.cat.biblio.record.tree.commit",
 	argc		=> 1, 
 	note		=> "Walks the tree and commits any changed nodes " .
 					"adds any new nodes, and deletes any deleted nodes",
 );
 
-sub record_tree_commit {
+sub biblio_record_tree_commit {
 	my( $self, $client, $tree ) = @_;
 	my $nodeset = $utils->tree2nodeset($tree);
 	$utils->commit_nodeset( $nodeset );
 	return $nodeset;
 }
+
 
 
 
