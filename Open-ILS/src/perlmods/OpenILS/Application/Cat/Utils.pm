cd36d026a7fb99d537ba50d029de4710b35157f9
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-03-29 17:36:58 +0000

    moved the mods code to the new Utils/ModsParser package
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@407 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Cat/Utils.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Cat/Utils.pm
@@ -6,25 +6,15 @@ use XML::LibXML;
 use XML::LibXSLT;
 use OpenSRF::Utils::SettingsParser;
 use OpenILS::Utils::FlatXML;
-use OpenILS::Application::WORM;
+use OpenILS::Utils::ModsParser;
 
-my $utils = OpenILS::Application::Cat::Utils->new();
+my $mods_utils = OpenILS::Utils::ModsParser->new();
+
 
 my $parser		= XML::LibXML->new();
 my $xslt			= XML::LibXSLT->new();
 my $xslt_doc	= $parser->parse_file( "/pines/cvs/ILS/Open-ILS/xsl/MARC21slim2MODS.xsl" );
 my $mods_sheet = $xslt->parse_stylesheet( $xslt_doc );
-
-my $isbn_xpath			= "//mods:mods/mods:identifier[\@type='isbn']";
-
-my $resource_xpath	= "//mods:mods/mods:typeOfResource";
-
-my $pub_xpath			= "//mods:mods/mods:originInfo//mods:dateIssued[\@encoding='marc']|" . 
-								"//mods:mods/mods:originInfo//mods:dateIssued[1]";
-
-my $tcn_xpath			= "//mods:mods/mods:recordInfo/mods:recordIdentifier";
-my $publisher_xpath	= "//mods:mods/mods:originInfo//mods:publisher[1]";
-
 
 sub new {
 	my($class) = @_;
@@ -100,128 +90,6 @@ sub clean_nodeset {
 }
 
 
-
-
-# ---------------------------------------------------------------------------
-# Grabs the data 'we want' from the MODS doc and returns it in hash form
-# ---------------------------------------------------------------------------
-sub mods_values_to_mods_slim {
-	my( $self, $modsperl ) = @_;
-
-	my $title = "";
-	my $author = "";
-	my $subject = [];
-
-	my $tmp = $modsperl->{title};
-
-	if(!$tmp) { $title = ""; }
-	else {
-		($title = $tmp->{proper}) ||
-		($title = $tmp->{translated}) ||
-		($title = $tmp->{abbreviated}) ||
-		($title = $tmp->{uniform});
-	}
-
-	$tmp = $modsperl->{author};
-	if(!$tmp) { $author = ""; }
-	else {
-		($author = $tmp->{personal}) ||
-		($author = $tmp->{other}) ||
-		($author = $tmp->{corporate}) ||
-		($author = $tmp->{conference}); 
-	}
-
-	$tmp = $modsperl->{subject};
-	if(!$tmp) { $subject = []; } 
-	else {
-		for my $key( keys %{$tmp}) {
-			push(@$subject, $tmp->{$key}) if $tmp->{$key};
-		}
-	}
-
-	return { title => $title, author => $author, subject => $subject };
-
-}
-
-sub _marcxml_to_perl {
-	my($self, $marcxml) = @_;
-	my $xmldoc = $parser->parse_string( $marcxml );
-	my $mods = $mods_sheet->transform($xmldoc);
-	my $perl = OpenSRF::Utils::SettingsParser::XML2perl( $mods->documentElement );
-	return $perl->{mods} if exists($perl->{mods});
-	return $perl;
-}
-
-
-# ---------------------------------------------------------------------------
-# Initializes a MARC -> Unified MODS batch process
-# ---------------------------------------------------------------------------
-
-sub start_mods_batch {
-
-	my( $self, $master_doc ) = @_;
-	if(!ref($self)) { 
-		$self = new OpenILS::Application::Cat::Utils; 
-	}
-
-	my $xmldoc = $parser->parse_string($master_doc);
-	my $mods = $mods_sheet->transform($xmldoc);
-
-	$self->{master_doc} = OpenILS::Application::WORM->modsdoc_to_values( $mods );
-	$self->{master_doc} = $utils->mods_values_to_mods_slim( $self->{master_doc} );
-
-	$self->{master_doc}->{isbn} = 
-		OpenILS::Application::WORM::_get_field_value( $mods, $isbn_xpath );
-
-	$self->{master_doc}->{type_of_resource} = 
-		[ OpenILS::Application::WORM::_get_field_value( $mods, $resource_xpath ) ];
-
-	$self->{master_doc}->{tcn} = 
-		OpenILS::Application::WORM::_get_field_value( $mods, $tcn_xpath );
-
-	$self->{master_doc}->{pubdate} = 
-		OpenILS::Application::WORM::_get_field_value( $mods, $pub_xpath );
-
-	$self->{master_doc}->{publisher} = 
-		OpenILS::Application::WORM::_get_field_value( $mods, $publisher_xpath );
-
-}
-
-# ---------------------------------------------------------------------------
-# Takes a MARCXML string and adds it to the growing MODS doc
-# ---------------------------------------------------------------------------
-sub push_mods_batch {
-	my( $self, $marcxml ) = @_;
-
-	my $xmldoc = $parser->parse_string($marcxml);
-	my $mods = $mods_sheet->transform($xmldoc);
-
-	my $xmlperl = OpenILS::Application::WORM->modsdoc_to_values( $mods );
-	$xmlperl = $utils->mods_values_to_mods_slim( $xmlperl );
-
-	for my $subject( @{$xmlperl->{subject}} ) {
-		push @{$self->{master_doc}->{subject}}, $subject;
-	}
-
-	push( @{$self->{master_doc}->{type_of_resource}}, 
-		OpenILS::Application::WORM::_get_field_value( $mods, $resource_xpath ));
-
-	if(!($self->{master_doc}->{isbn}) ) {
-		$self->{master_doc}->{isbn} = 
-			OpenILS::Application::WORM::_get_field_value( $mods, $isbn_xpath );
-	}
-}
-
-
-# ---------------------------------------------------------------------------
-# Completes a MARC -> Unified MODS batch process and returns the perl hash
-# ---------------------------------------------------------------------------
-sub finish_mods_batch {
-	my $self = shift;
-	my $perl = $self->{master_doc};
-	$self->{master_doc} = undef;
-	return $perl
-}
 
 
 1;
