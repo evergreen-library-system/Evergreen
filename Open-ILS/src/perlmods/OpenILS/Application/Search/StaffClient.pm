c1175786363885be731429f6a70927ecee8b357f
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-03-29 17:33:03 +0000

    breaking out searches specific to staff client web to their own packages
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@404 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/perlmods/OpenILS/Application/Search/StaffClient.pm
@@ -0,0 +1,184 @@
+package OpenILS::Application::Search::StaffClient;
+use base qw/OpenSRF::Application/;
+use strict; use warnings;
+
+# Searches specific to the staff client code
+
+# used for cat search classes
+my $cat_search_hash =  {
+
+	author => [ 
+		{ tag => "100", subfield => "a"} ,
+		{ tag => "700", subfield => "a"}, 
+	],
+
+	title => [ 
+		{ tag => "245", subfield => "a"},
+		{ tag => "242", subfield => "a"}, 
+		{ tag => "240", subfield => "a"},
+		{ tag => "210", subfield => "a"},
+	],
+
+	subject => [ 
+		{ tag => "650", subfield => "_" }, 
+	],
+
+	tcn	=> [
+		{ tag => "035", subfield => "_" },
+	],
+
+};
+
+
+__PACKAGE__->register_method(
+	method	=> "cat_biblio_search_tcn",
+	api_name	=> "open-ils.search.cat.biblio.tcn",
+	argc		=> 3, 
+	note		=> "Searches biblio information by search class",
+);
+
+sub cat_biblio_search_tcn {
+
+	my( $self, $client, $org_id, $tcn ) = @_;
+
+	$tcn =~ s/.*?(\w+)\s*$/$1/o;
+	warn "Searching TCN $tcn\n";
+
+	my $session = OpenSRF::AppSession->create( "open-ils.storage" );
+	my $request = $session->request( 
+			"open-ils.storage.biblio.record_entry.search.tcn_value", $tcn );
+	my $response = $request->recv();
+
+
+	unless ($response) { return []; }
+
+	if($response->isa("OpenSRF::EX")) {
+		throw $response ($response->stringify);
+	}
+
+	my $record_entry = $response->content;
+	my @ids;
+	for my $record (@$record_entry) {
+		push @ids, $record->id;
+	}
+
+
+	my $record_list = _records_to_mods( @ids );
+
+	for my $rec (@$record_list) {
+		$client->respond($rec);
+	}
+
+	return undef;
+
+}
+
+__PACKAGE__->register_method(
+	method	=> "cat_biblio_search_class",
+	api_name	=> "open-ils.search.cat.biblio.class",
+	argc		=> 3, 
+	note		=> "Searches biblio information by search class",
+);
+
+sub cat_biblio_search_class {
+
+	my( $self, $client, $org_id, $class, $sort, $string ) = @_;
+
+	throw OpenSRF::EX::InvalidArg 
+		("Not enough args to open-ils.search.cat.biblio.class")
+			unless( defined($org_id) and $class and $sort and $string );
+
+
+	my $search_hash;
+
+	my $method = $self->method_lookup("open-ils.search.biblio.marc");
+	if(!$method) {
+		throw OpenSRF::EX::PANIC 
+			("Can't lookup method 'open-ils.search.biblio.marc'");
+	}
+
+	my ($records) = $method->run( $cat_search_hash->{$class}, $string );
+
+	my @ids;
+	for my $i (@$records) { push @ids, $i->[0]; }
+
+	my $mods_list = _records_to_mods( @ids );
+
+	# ---------------------------------------------------------------
+	# append copy count information to the mods objects
+	my $session = OpenSRF::AppSession->create("open-ils.storage");
+
+	my $request = $session->request(
+		"open-ils.storage.biblio.record_copy_count.batch",  $org_id, @ids );
+
+	for my $id (@ids) {
+
+		warn "receiving copy counts for doc $id\n";
+
+		my $response = $request->recv();
+		next unless $response;
+
+		if( $response and UNIVERSAL::isa($response, "Error")) {
+			throw $response ($response->stringify);
+		}
+
+		my $count = $response->content;
+		my $mods_obj = undef;
+		for my $m (@$mods_list) {
+			$mods_obj = $m if ($m->{doc_id} == $id)
+		}
+		if($mods_obj) {
+			$mods_obj->{copy_count} = $count;
+		}
+
+		$client->respond( $mods_obj );
+
+	}	
+	$request->finish();
+
+	$session->finish();
+	$session->disconnect();
+	$session->kill_me();
+	# ---------------------------------------------------------------
+
+	return undef;
+}
+
+
+
+__PACKAGE__->register_method(
+	method	=> "cat_biblio_search_class_id",
+	api_name	=> "open-ils.search.cat.biblio.class.id",
+	argc		=> 3, 
+	note		=> "Searches biblio information by search class and returns the IDs",
+);
+
+sub cat_biblio_search_class_id {
+
+	my( $self, $client, $org_id, $class, $sort, $string ) = @_;
+
+	throw OpenSRF::EX::InvalidArg 
+		("Not enough args to open-ils.search.cat.biblio.class")
+			unless( defined($org_id) and $class and $sort and $string );
+
+
+	my $search_hash;
+
+	my $method = $self->method_lookup("open-ils.search.biblio.marc");
+	if(!$method) {
+		throw OpenSRF::EX::PANIC 
+			("Can't lookup method 'open-ils.search.biblio.marc'");
+	}
+
+	my ($records) = $method->run( $cat_search_hash->{$class}, $string );
+
+	my @ids;
+	for my $i (@$records) { push @ids, $i->[0]; }
+
+	return \@ids;
+
+}
+
+
+
+1;
---
+++ Open-ILS/src/perlmods/OpenILS/Application/Search/Web.pm
@@ -0,0 +1,11 @@
+package OpenILS::Application::Search::Web;
+use strict; use warnings;
+use base qw/OpenSRF::Application/;
+
+
+
+
+
+
+
+1;
