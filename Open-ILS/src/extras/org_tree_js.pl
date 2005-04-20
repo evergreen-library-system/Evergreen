3e8562eefb39799dc9dd11ff78c9d2f207e1a504
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-04-20 20:50:34 +0000

    script for generating the org tree as javascript
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@536 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/extras/org_tree_js.pl
@@ -0,0 +1,32 @@
+
+# turns the orgTree and orgTypes into js files
+
+use OpenSRF::AppSession;
+use OpenSRF::System;
+use JSON;
+OpenSRF::System->bootstrap_client(config_file => "/pines/conf/bootstrap.conf");
+
+
+my $ses = OpenSRF::AppSession->create("open-ils.search");
+my $req = $ses->request("open-ils.search.actor.org_tree.slim.retrieve");
+my $tree = $req->gather(1);
+
+my $ses2 = OpenSRF::AppSession->create("open-ils.storage");
+my $req2 = $ses2->request("open-ils.storage.direct.actor.org_unit_type.retrieve.all");
+my $types = $req2->gather(1);
+
+
+my $tree_string = JSON->perl2JSON($tree);
+my $types_string = JSON->perl2JSON($types);
+
+$tree_string =~ s/\"/\\\"/g;
+$types_string =~ s/\"/\\\"/g;
+
+$tree_string = "var globalOrgTree = JSON2js(\"$tree_string\");";
+$types_string = "var globalOrgTypes = JSON2js(\"$types_string\");";
+
+print "$tree_string\n\n$types_string\n";
+
+
+$ses->disconnect();
+$ses2->disconnect();
