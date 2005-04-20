89a875ecaf90da65be900359b6ed5fc186193e6a
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-04-20 21:40:08 +0000

    redo ALL metarecords with the quick-loader
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@541 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Evergreen/src/extras/import/quick_metarecord_map.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+ALTER TABLE metabib.metarecord_source_map DROP CONSTRAINT metabib_metarecord_source_map_metarecord_fkey;
+
+TRUNCATE metabib.metarecord;
+TRUNCATE metabib.metarecord_source_map;
+
+INSERT INTO metabib.metarecord (fingerprint,master_record)
+	SELECT	fingerprint,min(id)
+	  FROM	biblio.record_entry
+	  WHERE	fingerprint IS NOT NULL
+	  GROUP BY 1;
+
+INSERT INTO metabib.metarecord_source_map (metarecord,source)
+	SELECT	m.id, b.id
+	  FROM	biblio.record_entry b
+	  	JOIN metabib.metarecord m ON (m.fingerprint = b.fingerprint);
+
+ALTER TABLE metabib.metarecord_source_map ADD CONSTRAINT metabib_metarecord_source_map_metarecord_fkey FOREIGN KEY (metarecord) REFERENCES metabib.metarecord (id);
+
+COMMIT;
+
+VACUUM FULL ANALYZE VERBOSE metabib.metarecord;
+VACUUM FULL ANALYZE VERBOSE metabib.metarecord_source_map;
+
