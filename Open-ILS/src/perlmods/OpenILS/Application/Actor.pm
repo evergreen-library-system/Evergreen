1ce1bda79822422220e86fb7b862a04a5cb17abd
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-04-21 16:24:16 +0000

    moving as much 'actor' code to here as i can for maintainability
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@544 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/perlmods/OpenILS/Application/Actor.pm
@@ -0,0 +1,574 @@
+package OpenILS::Application::Actor;
+use base qw/OpenSRF::Application/;
+use strict; use warnings;
+use Data::Dumper;
+use OpenSRF::EX qw(:try);
+use OpenILS::Application::AppUtils;
+use OpenILS::Utils::Fieldmapper;
+use OpenILS::Application::Search::Actor;
+
+my $apputils = "OpenILS::Application::AppUtils";
+sub _d { warn "Patron:\n" . Dumper(shift()); }
+my $cache_client = OpenSRF::Utils::Cache->new( "global", 0 );
+
+
+__PACKAGE__->register_method(
+	method	=> "update_patron",
+	api_name	=> "open-ils.actor.patron.create",
+);
+
+__PACKAGE__->register_method(
+	method	=> "update_patron",
+	api_name	=> "open-ils.actor.patron.update",
+);
+
+
+sub update_patron {
+	my( $self, $client, $patron ) = @_;
+
+	my $session = $apputils->start_db_session();
+	my $err = undef;
+
+
+	# $new_patron is the patron in progress.  $patron is the original patron
+	# passed in with the method.  new_patron will change as the components
+	# of patron are added/updated.
+
+	my $new_patron;
+
+	try {
+		# create/update the patron first so we can use his id
+		if( $self->api_name =~ /create/ ) {
+			$new_patron = _add_patron(
+					$session, _clone_patron($patron));
+		} else { 
+			$new_patron = $patron; 
+		}
+
+		$new_patron = _add_update_addresses($session, $patron, $new_patron);
+		$new_patron = _add_update_cards($session, $patron, $new_patron);
+
+		# re-update the patron if anything has happened to him during this process
+		if($new_patron->ischanged()) {
+			$new_patron = _update_patron($session, $new_patron);
+		}
+		$apputils->commit_db_session($session);
+
+	} catch Error with { 
+		my $e = shift;
+		$err =  "-*- Failure adding user: $e";
+		$apputils->rollback_db_session($session);
+		warn $err;
+	};
+
+	if($err) { throw OpenSRF::EX::ERROR ($err); }
+	warn "Patron Update/Create complete\n";
+	return flesh_user($new_patron->id());
+}
+
+
+sub flesh_user {
+	my $id = shift;
+	my $session = shift;
+	my $kill = 0;
+
+	if(!$session) {
+		$session = OpenSRF::AppSession->create("open-ils.storage");
+		$kill = 1;
+	}
+
+	# grab the user with the given card
+	my $ureq = $session->request(
+			"open-ils.storage.direct.actor.user.retrieve",
+			$id);
+	my $user = $ureq->gather(1);
+
+	# grab the cards
+	my $cards_req = $session->request(
+			"open-ils.storage.direct.actor.card.search.usr",
+			$user->id() );
+	$user->cards( $cards_req->gather(1) );
+
+	my $add_req = $session->request(
+			"open-ils.storage.direct.actor.user_address.search.usr",
+			$user->id() );
+	$user->addresses( $add_req->gather(1) );
+
+	if($kill) { $session->disconnect(); }
+
+	return $user;
+
+}
+
+
+# clone and clear stuff that would break the database
+sub _clone_patron {
+	my $patron = shift;
+
+	my $new_patron = Fieldmapper::actor::user->new();
+
+	my $fmap = $Fieldmapper::fieldmap;
+	no strict; # shallow clone, may be useful in the fieldmapper
+	for my $field 
+		(keys %{$fmap->{"Fieldmapper::actor::user"}->{'fields'}}) {
+			$new_patron->$field( $patron->$field() );
+	}
+	use strict;
+
+	# clear these
+	$new_patron->clear_billing_address();
+	$new_patron->clear_mailing_address();
+	$new_patron->clear_addresses();
+	$new_patron->clear_card();
+	$new_patron->clear_cards();
+	$new_patron->clear_id();
+	$new_patron->clear_isnew();
+	$new_patron->clear_changed();
+	$new_patron->clear_deleted();
+
+	return $new_patron;
+}
+
+
+sub _add_patron {
+	my $session		= shift;
+	my $patron		= shift;
+
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.user.create",$patron);
+	my $id = $req->gather(1);
+	if(!$id) { throw OpenSRF::EX::ERROR ("Unable to create new user"); }
+	warn "Created new patron with id $id\n";
+	$patron->id($id);
+
+	return $patron;
+}
+
+
+sub _update_patron {
+	my( $session, $patron) = @_;
+
+	warn "updating patron " . $patron->usrname() . "\n";
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.user.update",$patron );
+	my $status = $req->gather(1);
+	if(!defined($status)) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error updating patron"); 
+	}
+	return $patron;
+}
+
+
+sub _add_update_addresses {
+	my $session = shift;
+	my $patron = shift;
+	my $new_patron = shift;
+
+	my $current_id; # id of the address before creation
+
+	for my $address (@{$patron->addresses()}) {
+
+		$address->usr($new_patron->id());
+
+		if(ref($address) and $address->isnew()) {
+			warn "Adding new address at street " . $address->street1() . "\n";
+
+			$current_id = $address->id();
+			$address = _add_address($session,$address);
+
+			if( $patron->billing_address() == $current_id ) {
+				$new_patron->billing_address($address->id());
+				$new_patron->ischanged(1);
+			}
+
+			if( $patron->mailing_address() == $current_id ) {
+				$new_patron->mailing_address($address->id());
+				$new_patron->ischanged(1);
+			}
+
+		} elsif( ref($address) and $address->ischanged() ) {
+			warn "Updating address at street " . $address->street1();
+			_update_address($session,$address);
+
+		} elsif( ref($address) and $address->isdeleted() ) {
+			warn "Deleting address at street " . $address->street1();
+			_delete_address($session,$address);
+		}
+	}
+
+	return $new_patron;
+}
+
+
+# adds an address to the db and returns the address with new id
+sub _add_address {
+	my($session, $address) = @_;
+	$address->clear_id();
+
+	# put the address into the database
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.user_address.create",
+		$address );
+
+	#update the id
+	my $id = $req->gather(1);
+	if(!$id) { 
+		throw OpenSRF::EX::ERROR 
+			("Unable to create new user address"); 
+	}
+
+	warn "Created address with id $id\n";
+
+	# update all the necessary id's
+	$address->id( $id );
+	return $address;
+}
+
+
+sub _update_address {
+	my( $session, $address ) = @_;
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.user_address.update",
+		$address );
+	my $status = $req->gather(1);
+	if(!defined($status)) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error updating address"); 
+	}
+	return $address;
+}
+
+
+
+sub _add_update_cards {
+
+	my $session = shift;
+	my $patron = shift;
+	my $new_patron = shift;
+
+	my $virtual_id; #id of the card before creation
+	for my $card (@{$patron->cards()}) {
+
+		$card->usr($new_patron->id());
+
+		if(ref($card) and $card->isnew()) {
+
+			$virtual_id = $card->id();
+			$card = _add_card($session,$card);
+
+			if($patron->card() == $virtual_id) {
+				$new_patron->card($card->id());
+				$new_patron->ischanged(1);
+			}
+
+		} elsif( ref($card) and $card->ischanged() ) {
+			_update_card($session, $card);
+		}
+	}
+	return $new_patron;
+}
+
+
+# adds an card to the db and returns the card with new id
+sub _add_card {
+	my( $session, $card ) = @_;
+	$card->clear_id();
+
+	warn "Adding card with barcode " . $card->barcode() . "\n";
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.card.create",
+		$card );
+
+	my $id = $req->gather(1);
+	if(!$id) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error creating card"); 
+	}
+
+	$card->id($id);
+	warn "Created patron card with id $id\n";
+	return $card;
+}
+
+
+sub _update_card {
+	my( $session, $card ) = @_;
+	warn Dumper $card;
+
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.card.update",
+		$card );
+	my $status = $req->gather(1);
+	if(!defined($status)) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error updating card"); 
+	}
+	return $card;
+}
+
+
+
+
+sub _delete_address {
+	my( $session, $address ) = @_;
+
+	warn "Deleting address " . $address->street1() . "\n";
+
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.user_address.delete",
+		$address );
+	my $status = $req->gather(1);
+	if(!defined($status)) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error updating address"); 
+	}
+}
+
+
+
+
+__PACKAGE__->register_method(
+	method	=> "search_username",
+	api_name	=> "open-ils.actor.user.search.username",
+);
+
+sub search_username {
+	my($self, $client, $username) = @_;
+	my $users = OpenILS::Application::AppUtils->simple_scalar_request(
+			"open-ils.storage", 
+			"open-ils.storage.direct.actor.user.search.usrname",
+			$username );
+	return $users;
+}
+
+
+__PACKAGE__->register_method(
+	method	=> "user_retrieve_by_barcode",
+	api_name	=> "open-ils.actor.user.fleshed.retrieve_by_barcode",
+);
+
+sub user_retrieve_by_barcode {
+	my($self, $client, $barcode) = @_;
+	warn "Searching for user with barcode $barcode\n";
+
+	my $session = OpenSRF::AppSession->create("open-ils.storage");
+
+	# find the card with the given barcode
+	my $creq	= $session->request(
+			"open-ils.storage.direct.actor.card.search.barcode",
+			$barcode );
+	my $card = $creq->gather(1);
+	$card = $card->[0];
+	my $user = flesh_user($card->usr(), $session);
+	$session->disconnect();
+	return $user;
+
+}
+
+
+
+
+__PACKAGE__->register_method(
+	method	=> "get_org_types",
+	api_name	=> "open-ils.actor.org_types.retrieve",
+);
+sub get_org_types {
+	my($self, $client) = @_;
+
+	 my $org_typelist = OpenILS::Application::AppUtils->simple_scalar_request(
+		"open-ils.storage",
+		"open-ils.storage.direct.actor.org_unit_type.retrieve.all.atomic" );
+
+	 return $org_typelist;
+}
+
+
+__PACKAGE__->register_method(
+	method	=> "get_user_profiles",
+	api_name	=> "open-ils.actor.user.profiles.retrieve",
+);
+sub get_user_profiles {
+	return OpenILS::Application::AppUtils->simple_scalar_request(
+			"open-ils.storage",
+			"open-ils.storage.direct.actor.profile.retrieve.all.atomic",
+			( "1", "2", "3" ) );
+}
+
+
+
+__PACKAGE__->register_method(
+	method	=> "get_user_ident_types",
+	api_name	=> "open-ils.actor.user.ident_types.retrieve",
+);
+sub get_user_ident_types {
+	return OpenILS::Application::AppUtils->simple_scalar_request(
+			"open-ils.storage",
+			"open-ils.storage.direct.config.identification_type.retrieve.all.atomic" );
+}
+
+
+
+
+__PACKAGE__->register_method(
+	method	=> "get_org_unit",
+	api_name	=> "open-ils.actor.org_unit.retrieve",
+	argc		=> 1, 
+	note		=> "Returns the entire org tree structure",
+);
+
+sub get_org_unit {
+
+	my( $self, $client, $user_session ) = @_;
+
+	my $user_obj = 
+		OpenILS::Application::AppUtils->check_user_session( $user_session ); #throws EX on error
+
+	my $home_ou = OpenILS::Application::AppUtils->simple_scalar_request(
+		"open-ils.storage",
+		"open-ils.storage.direct.actor.org_unit.retrieve", 
+		$user_obj->home_ou );
+
+	return $home_ou;
+}
+
+
+# build the org tree
+
+__PACKAGE__->register_method(
+	method	=> "get_org_tree",
+	api_name	=> "open-ils.actor.org_tree.retrieve",
+	argc		=> 1, 
+	note		=> "Returns the entire org tree structure",
+);
+
+sub get_org_tree {
+	my( $self, $client) = @_;
+
+	# see if it's in the cache
+	warn "Getting ORG Tree\n";
+	my $tree = $cache_client->get_cache('orgtree');
+	if($tree) { 
+		warn "Found orgtree in cache. returning...\n";
+		return $tree; 
+	}
+
+	my $orglist = $apputils->simple_scalar_request( 
+		"open-ils.storage", 
+		"open-ils.storage.direct.actor.org_unit.retrieve.all.atomic" );
+
+	$tree = $self->build_org_tree($orglist);
+	$cache_client->put_cache('orgtree', $tree);
+
+	return $tree;
+
+}
+
+sub build_org_tree {
+
+	my( $self, $orglist) = @_;
+
+	return $orglist unless ( 
+			ref($orglist) and @$orglist > 1 );
+
+	my @list = sort { 
+		$a->ou_type <=> $b->ou_type ||
+		$a->name cmp $b->name } @$orglist;
+
+	for my $org (@list) {
+
+		next unless ($org and defined($org->parent_ou));
+		my ($parent) = grep { $_->id == $org->parent_ou } @list;
+		next unless $parent;
+
+		$parent->children([]) unless defined($parent->children); 
+		push( @{$parent->children}, $org );
+	}
+
+	return $list[0];
+
+}
+
+
+
+
+
+1;
+
+
+
+
+
+
+__END__
+
+
+sub _delete_card {
+	my( $session, $card ) = @_;
+
+	warn "Deleting card with barcode " . $card->barcode() . "\n";
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.card.delete",
+		$card );
+	my $status = $req->gather(1);
+	if(!defined($status)) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error updating card"); 
+	}
+}
+
+
+
+# deletes the patron and any attached addresses and cards
+__PACKAGE__->register_method(
+	method	=> "delete_patron",
+	api_name	=> "open-ils.actor.patron.delete",
+);
+
+sub delete_patron {
+
+	my( $self, $client, $patron ) = @_;
+	my $session = $apputils->start_db_session();
+	my $err = undef;
+
+	try {
+
+		$patron->clear_mailing_address();
+		$patron->clear_billing_address();
+		$patron->ischanged(1);
+
+		_update_patron($session, $patron);
+		_delete_address($session,$_) for (@{$patron->addresses()});
+		_delete_card($session,$_) for (@{$patron->cards()});
+		_delete_patron($session,$patron);
+		$apputils->commit_db_session($session);
+
+	} catch Error with {
+		my $e = shift;
+		$err =  "-*- Failure deleting user: $e";
+		$apputils->rollback_db_session($session);
+		warn $err;
+	};
+
+	if($err) { throw OpenSRF::EX::ERROR ($err); }
+	warn "Patron Delete complete\n";
+	return 1;
+}
+
+
+
+sub _delete_patron {
+	my( $session, $patron ) = @_;
+
+	warn "Deleting patron " . $patron->usrname() . "\n";
+
+	my $req = $session->request(
+		"open-ils.storage.direct.actor.user.delete",
+		$patron );
+	my $status = $req->gather(1);
+	if(!defined($status)) { 
+		throw OpenSRF::EX::ERROR 
+			("Unknown error updating patron"); 
+	}
+}
+
