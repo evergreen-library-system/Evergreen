54d14b9e5ed7215ecf90fbb83c78f22ba4e43958
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-04-25 22:10:29 +0000

    testing fix for fieldmapper
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@565 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/biblio.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/biblio.pm
@@ -9,57 +9,9 @@ package biblio::record_entry;
 use base qw/biblio/;
 
 biblio::record_entry->table( 'biblio_record_entry' );
-biblio::record_entry->columns( Primary		=> 'id' );
-biblio::record_entry->columns( Essential	=> qw/tcn_source tcn_value creator editor
-						      create_date edit_date source active
-						      deleted marc last_xact_id/ );
-biblio::record_entry->columns( Others		=> qw/fingerprint/ );
-
-#-------------------------------------------------------------------------------
-#package biblio::record_node::subnode;
-#sub _load {
-#	my $intra_doc_id = shift;
-#	my $owner_doc = shift()->owner_doc;
-#	return (biblio::record_node->search(
-#			owner_doc	=> $owner_doc,
-#			intra_doc_id	=> $intra_doc_id
-#		)
-#	)[0];
-#}
-#
-#package biblio::record_node;
-#use base qw/biblio/;
-#
-#biblio::record_node->table( 'biblio_record_data' );
-#biblio::record_node->columns( All => qw/id owner_doc intra_doc_id
-#					parent_node node_type
-#					namespace_uri name value last_xact_id/ );
-#
-#biblio::record_node->has_a(
-#	parent_node	=> 'biblio::record_node::subnode',
-#	inflate		=> sub {
-#				return biblio::record_node::subnode::_load(@_)
-#			},
-#);
-
-
-#-------------------------------------------------------------------------------
-#package biblio::record_marc;
-#use base qw/biblio/;
-
-#biblio::record_marc->table( 'biblio_record_marc' );
-#biblio::record_marc->columns( All => qw/id marc last_xact_id/ );
-#biblio::record_marc->columns( Stringify => qw/marc/ );
-#biblio::record_marc->is_a( id => qw/biblio::record_entry/ );
-
-#-------------------------------------------------------------------------------
-#package biblio::record_mods;
-#use base qw/biblio/;
-
-#biblio::record_mods->table( 'biblio_record_mods' );
-#biblio::record_mods->columns( All => qw/id mods/ );
-#biblio::record_mods->columns( Stringify => qw/mods/ );
-#biblio::record_mods->is_a( id => qw/biblio::record_entry/ );
+biblio::record_entry->columns( All => qw/id tcn_source tcn_value creator editor
+				      create_date edit_date source active
+				      deleted marc last_xact_id fingerprint/ );
 
 #-------------------------------------------------------------------------------
 package biblio::record_note;
@@ -68,9 +20,6 @@ use base qw/biblio/;
 biblio::record_note->table( 'biblio_record_note' );
 biblio::record_note->columns( All => qw/id record value creator
 					editor create_date edit_date/ );
-#biblio::record_note->columns( Stringify => qw/value/ );
-#biblio::record_note->is_a( record => qw/biblio::record_entry/ );
-
 #-------------------------------------------------------------------------------
 
 1;
--- Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/metabib.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/metabib.pm
@@ -10,7 +10,7 @@ use base qw/metabib/;
 
 metabib::metarecord->table( 'metabib_metarecord' );
 metabib::metarecord->columns( Primary => qw/id/ );
-metabib::metarecord->columns( Others => qw/fingerprint master_record mods/ );
+metabib::metarecord->columns( Essential => qw/fingerprint master_record mods/ );
 
 #-------------------------------------------------------------------------------
 package metabib::title_field_entry;
@@ -18,7 +18,7 @@ use base qw/metabib/;
 
 metabib::title_field_entry->table( 'metabib_title_field_entry' );
 metabib::title_field_entry->columns( Primary => qw/id/ );
-metabib::title_field_entry->columns( Others => qw/field value source/ );
+metabib::title_field_entry->columns( Essential => qw/field value source/ );
 
 
 #-------------------------------------------------------------------------------
@@ -27,7 +27,7 @@ use base qw/metabib/;
 
 metabib::author_field_entry->table( 'metabib_author_field_entry' );
 metabib::author_field_entry->columns( Primary => qw/id/ );
-metabib::author_field_entry->columns( Others => qw/field value source/ );
+metabib::author_field_entry->columns( Essential => qw/field value source/ );
 
 
 #-------------------------------------------------------------------------------
@@ -36,7 +36,7 @@ use base qw/metabib/;
 
 metabib::subject_field_entry->table( 'metabib_subject_field_entry' );
 metabib::subject_field_entry->columns( Primary => qw/id/ );
-metabib::subject_field_entry->columns( Others => qw/field value source/ );
+metabib::subject_field_entry->columns( Essential => qw/field value source/ );
 
 
 #-------------------------------------------------------------------------------
@@ -45,7 +45,7 @@ use base qw/metabib/;
 
 metabib::keyword_field_entry->table( 'metabib_keyword_field_entry' );
 metabib::keyword_field_entry->columns( Primary => qw/id/ );
-metabib::keyword_field_entry->columns( Others => qw/field value source/ );
+metabib::keyword_field_entry->columns( Essential => qw/field value source/ );
 
 #-------------------------------------------------------------------------------
 package metabib::metarecord_source_map;
@@ -53,7 +53,7 @@ use base qw/metabib/;
 
 metabib::metarecord_source_map->table( 'metabib_metarecord_source_map' );
 metabib::metarecord_source_map->columns( Primary => qw/id/ );
-metabib::metarecord_source_map->columns( Others => qw/metarecord source/ );
+metabib::metarecord_source_map->columns( Essential => qw/metarecord source/ );
 
 #-------------------------------------------------------------------------------
 package metabib::full_rec;
@@ -61,7 +61,7 @@ use base qw/metabib/;
 
 metabib::full_rec->table( 'metabib_full_rec' );
 metabib::full_rec->columns( Primary => qw/id/ );
-metabib::full_rec->columns( Others => qw/record tag ind1 ind2 subfield value/ );
+metabib::full_rec->columns( Essential => qw/record tag ind1 ind2 subfield value/ );
 
 #-------------------------------------------------------------------------------
 package metabib::record_descriptor;
@@ -70,7 +70,7 @@ use base qw/metabib/;
 
 metabib::record_descriptor->table( 'metabib_rec_descriptor' );
 metabib::record_descriptor->columns( Primary => qw/id/ );
-metabib::record_descriptor->columns( Others => qw/record item_type item_form bib_level
+metabib::record_descriptor->columns( Essential => qw/record item_type item_form bib_level
 					 control_type char_encoding enc_level
 					 cat_form pub_status item_lang audience/ );
 
