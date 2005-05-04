4181c8c63749ad554c3f9ab826448a702d393308
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-05-04 02:29:17 +0000

    added instanceOf function
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@626 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/javascript/util/webutils.js
+++ Open-ILS/src/javascript/util/webutils.js
@@ -1,49 +1,4 @@
 
-var isMac
-var NS;
-var NS4;
-var NS6;
-var IE;
-var IE4;
-var IE4mac;
-var IE4plus;
-var IE5;
-var IE5plus;
-var IE6;
-var IEMajor;
-var ver4;
-
-function detect_browser() {       
-
-	isMac = (navigator.appVersion.indexOf("Mac")!=-1) ? true : false;
-	NS = (navigator.appName == "Netscape") ? true : false;
-	NS4 = (document.layers) ? true : false;
-	IE = (navigator.appName == "Microsoft Internet Explorer") ? true : false;
-	IEmac = ((document.all)&&(isMac)) ? true : false;
-	IE4plus = (document.all) ? true : false;
-	IE4 = ((document.all)&&(navigator.appVersion.indexOf("MSIE 4.")!=-1)) ? true : false;
-	IE5 = ((document.all)&&(navigator.appVersion.indexOf("MSIE 5.")!=-1)) ? true : false;
-	IE6 = ((document.all)&&(navigator.appVersion.indexOf("MSIE 6.")!=-1)) ? true : false;
-	ver4 = (NS4 || IE4plus) ? true : false;
-	NS6 = (!document.layers) && (navigator.userAgent.indexOf('Netscape')!=-1)?true:false;
-
-	IE5plus = IE5 || IE6;
-	IEMajor = 0;
-
-	if (IE4plus) {
-		var start = navigator.appVersion.indexOf("MSIE");
-		var end = navigator.appVersion.indexOf(".",start);
-		IEMajor = parseInt(navigator.appVersion.substring(start+5,end));
-		IE5plus = (IEMajor>=5) ? true : false;
-	}
-}
-
-detect_browser();
-
-
-
-
-
 function hideMe(obj) {
 	if(!obj)  return;
 	add_css_class( obj, "hide_me");
@@ -56,7 +11,7 @@ function showMe(obj) {
 	remove_css_class( obj, "hide_me");
 }
 
-function grabCharCode( evt ) {
+function grabCharCode(evt) {
 	evt = (evt) ? evt : ((window.event) ? event : null); /* for mozilla and IE */
 	if( evt ) {
 		return (evt.charCode ? evt.charCode : 
@@ -583,6 +538,21 @@ function buildFunction(string) {
 }
 
 
+function instanceOf(object, constructorFunction) {
 
-
+	if(!IE) {
+		while (object != null) {
+			if (object == constructorFunction.prototype)
+				return true;
+			object = object.__proto__;
+		}
+	} else {
+		while(object != null) {
+			if( object instanceof constructorFunction )
+				return true;
+			object = object.__proto__;
+		}
+	}
+	return false;
+}
 
