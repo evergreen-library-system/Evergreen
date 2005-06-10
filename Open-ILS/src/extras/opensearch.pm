87f9709a981b80d8b66eb8b9b50a57ce92fe74cd
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-06-10 22:45:21 +0000

    better support for ranking and OpenILL integration
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@799 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/extras/opensearch.pm
+++ Open-ILS/src/extras/opensearch.pm
@@ -9,6 +9,7 @@ use APR::Const    -compile => qw(:error SUCCESS);
 use Apache::RequestRec ();
 use Apache::RequestIO ();
 use Apache::RequestUtil;
+use LWP::UserAgent;
 
 use CGI ();
 use Template qw(:template);
@@ -21,36 +22,42 @@ sub handler {
 	my $apache = shift;
 	print "Content-type: text/xml; charset=utf-8\n\n";
 
-	my $template = Template->new( { 
-		OUTPUT			=> $apache, 
-		ABSOLUTE			=> 1, 
-		RELATIVE			=> 1,
-		PLUGIN_BASE		=> 'OpenILS::Template::Plugin',
-		INCLUDE_PATH	=> ['/pines/cvs/ILS/Open-ILS/src/extras'], 
-		PRE_CHOMP		=> 1,
-		POST_CHOMP		=> 1,
-		} 
-	);
+	my $cgi = new CGI;
+	
+	if (my $fetch = $cgi->param('fetch')) {
 
-	try {
+		print LWP::UserAgent->new->get($fetch)->content;
 
-		if( ! $template->process( 'opensearch.ttk' ) ) { 
-			warn "Error processing template opensearch.ttk\n";	
-			warn  "Error Occured: " . $template->error();
-			my $err = $template->error();
-			$err =~ s/\n/\<br\/\>/g;
-			print "<br><b>Unable to process template:<br/><br/> " . $err . "!!!</b>";
-		}
+	} else {
 
-	} catch Error with {
-		my $e = shift;
-		warn "Error processing template opensearch.ttk:  $e - $@ \n";	
-		print "<center><br/><br/><b>Error<br/><br/> $e <br/><br/> $@ </b><br/></center>";
-		return;
-	};
+		my $template = Template->new( { 
+			OUTPUT			=> $apache, 
+			ABSOLUTE			=> 1, 
+			RELATIVE			=> 1,
+			PLUGIN_BASE		=> 'OpenILS::Template::Plugin',
+			INCLUDE_PATH	=> ['/pines/cvs/ILS/Open-ILS/src/extras'], 
+			PRE_CHOMP		=> 1,
+			POST_CHOMP		=> 1,
+			} 
+		);
 
+		try {
+	
+			if( ! $template->process( 'opensearch.ttk' ) ) { 
+				warn "Error processing template opensearch.ttk\n";	
+				warn  "Error Occured: " . $template->error();
+				my $err = $template->error();
+				$err =~ s/\n/\<br\/\>/g;
+				print "<br><b>Unable to process template:<br/><br/> " . $err . "!!!</b>";
+			}
 
-
+		} catch Error with {
+			my $e = shift;
+			warn "Error processing template opensearch.ttk:  $e - $@ \n";	
+			print "<center><br/><br/><b>Error<br/><br/> $e <br/><br/> $@ </b><br/></center>";
+			return;
+		};
+	}
 	return Apache::OK;
 }
 
