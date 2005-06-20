459c13eb75ec3c553953fe729f12ecbd74e3dfed
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-06-20 16:39:16 +0000

    name tweak
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@877 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/Open-ILS/OpenILS.js
+++ Evergreen/staff_client/chrome/content/evergreen/Open-ILS/OpenILS.js
@@ -2,8 +2,8 @@ function OpenILS_init(screen) {
 	sdump('D_TRACE','Entering OpenILS_init with ' + screen + '\n');
 
 	switch(screen) {
-		case 'auth' : auth_init(); break;
-		case 'app_shell' : app_shell_init(); break;
+		case 'Auth' : auth_init(); break;
+		case 'AppShell' : app_shell_init(); break;
 	}
 
 	sdump('D_TRACE','Exiting OpenILS_init\n');
