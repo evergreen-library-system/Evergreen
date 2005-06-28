9427ed78d6464fe6699f8bbdbb542847093c6763
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-06-28 15:56:04 +0000

    opac screen.  currently broke
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@950 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/Open-ILS/OpenILS.js
+++ Evergreen/staff_client/chrome/content/evergreen/Open-ILS/OpenILS.js
@@ -4,6 +4,7 @@ function OpenILS_init(screen,params) {
 	switch(screen) {
 		case 'Auth' : auth_init(params); break;
 		case 'AppShell' : app_shell_init(params); break;
+		case 'Opac' : opac_init(params); break;
 	}
 
 	sdump('D_TRACE','Exiting OpenILS_init\n');
