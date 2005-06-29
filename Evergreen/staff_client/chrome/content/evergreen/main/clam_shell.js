de691b15f73ebf4684eaae23d276a5ebe10effb4
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-06-29 22:30:53 +0000

    typo.  Made no difference.  hrmm
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@990 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/main/clam_shell.js
+++ Evergreen/staff_client/chrome/content/evergreen/main/clam_shell.js
@@ -3,7 +3,7 @@ sdump('D_TRACE','Loading clam_shell.js\n');
 function clam_shell_init(p) {
 	dump("TESTING: clam_shell.js: " + mw.G['main_test_variable'] + '\n');
 	if (d.params && d.params.vertical) {
-		p.d.setAttribute('orient','verticle');
+		p.d.setAttribute('orient','vertical');
 	}
 }
 
