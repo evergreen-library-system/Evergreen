6728bcc956717957a491495d0294ab6be35347ce
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-06-29 22:23:14 +0000

    change in onload and onunload handlers
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@984 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/Open-ILS/OpenILS.js
+++ Evergreen/staff_client/chrome/content/evergreen/Open-ILS/OpenILS.js
@@ -1,12 +1,37 @@
-function OpenILS_init(screen,params) { 
+function OpenILS_init(params) { 
 	sdump('D_TRACE','Entering OpenILS_init with ' + screen + '\n');
 
-	switch(screen) {
-		case 'Auth' : auth_init(params); break;
-		case 'AppShell' : app_shell_init(params); break;
-		case 'Opac' : opac_init(params); break;
-	}
+	try {
+
+		switch(params.app) {
+			case 'Auth' : auth_init(params); break;
+			case 'AppShell' : app_shell_init(params); break;
+			case 'ClamShell' : clam_shell_init(params); break;
+			case 'Opac' : opac_init(params); break;
+		}
+
+		register_document(params.d);
+
+	} catch(E) {}
 
 	sdump('D_TRACE','Exiting OpenILS_init\n');
 }
 
+function OpenILS_exit(params) {
+	sdump('D_TRACE','Entering OpenILS_exit with ' + screen + '\n');
+
+	try {
+	
+		switch(params.app) {
+			case 'Auth' : auth_exit(params); break;
+			case 'AppShell' : app_shell_exit(params); break;
+			case 'ClamShell' : clam_shell_exit(params); break;
+			case 'Opac' : opac_exit(params); break;
+		}
+
+		unregister_document(params.d);
+
+	} catch(E) {}
+
+	sdump('D_TRACE','Exiting OpenILS_exit\n');
+}
