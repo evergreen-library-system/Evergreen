b46665c2549d7840d561b94fec8d2dc6be22e1a8
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-06-30 02:40:04 +0000

    parameter passing works.  Let clam_shell default to a left pane and a right pane
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@996 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/main/clam_shell.js
+++ Evergreen/staff_client/chrome/content/evergreen/main/clam_shell.js
@@ -2,8 +2,12 @@ sdump('D_TRACE','Loading clam_shell.js\n');
 
 function clam_shell_init(p) {
 	dump("TESTING: clam_shell.js: " + mw.G['main_test_variable'] + '\n');
-	if (d.params && d.params.vertical) {
-		p.d.setAttribute('orient','vertical');
+	if (p) {
+		if (p.horizontal) {
+			get_widget(p.d,'ClamShell_main').orient = 'horizontal';
+		} else if (p.vertical) {
+			get_widget(p.d,'ClamShell_main').orient = 'vertical';
+		}
 	}
 }
 
