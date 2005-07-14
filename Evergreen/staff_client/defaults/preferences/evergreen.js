88d4d1ddf3514430b61a0a63ed01333bf725685d
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-07-14 07:08:34 +0000

    is it POEM if the end result is still slow? :)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1175 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/defaults/preferences/evergreen.js
+++ Evergreen/staff_client/defaults/preferences/evergreen.js
@@ -7,3 +7,7 @@ pref("toolkit.defaultChromeURI", "chrome://evergreen/content/auth/auth.xul");
 
 // This one just makes things speedier.  We use a lot of XMLHttpRequest
 pref("network.http.max-persistent-connections-per-server",8);
+
+// This stops the unresponsive script warning, but the code is still too slow for some reason.
+// However, it's better than POEM, which I wasted a day on :)
+pref("dom.max_script_run_time",60);
