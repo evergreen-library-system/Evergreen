c5fae57cf49bf70f30c06a9d1bcba60e9935e8f1
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-07-20 16:25:24 +0000

    fleshed
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1311 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/circ/hold_capture.js
+++ Evergreen/staff_client/chrome/content/evergreen/circ/hold_capture.js
@@ -51,7 +51,7 @@ function hold_capture_hold_capture_items_init(p) {
 	p.attempt_hold_capture = function(barcode) {
 		try {
 			//if (! is_barcode_valid(barcode) ) throw('Invalid Barcode');
-			var check = hold_capture_by_copy_barcode( barcode );
+			var check = hold_capture_by_copy_barcode( barcode, true );
 			if (check) {
 				sdump('D_HOLD_CAPTURE','check = ' + check + '\n' + pretty_print( js2JSON( check ) ) + '\n');
 
