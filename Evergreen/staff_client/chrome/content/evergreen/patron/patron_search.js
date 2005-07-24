87465de98cafef9945c25918d3d3cc275c2e4b3d
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-07-24 15:25:09 +0000

    catch harmless error on empty searches
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1378 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/patron/patron_search.js
+++ Evergreen/staff_client/chrome/content/evergreen/patron/patron_search.js
@@ -114,17 +114,21 @@ function patron_search_patron_search_results_init(p) {
 	p.search_results.register_patron_select_callback(
 		function (ev) {
 			sdump('D_PATRON_SEARCH','Firing patron_select_callback\n');
-			/* Clear Current Patron */
 			p.retrieve_button.disabled = true; 
-			p._patron = fake_patron();
-			render_fm( p.w.document, { 'au' : p._patron } );
-			/* Get selection */
-			var patrons = get_list_from_tree_selection( p.search_results.paged_tree.tree );
-			/* Get patron and render status */
-			retrieve_patron_by_id( 
-				patrons[ patrons.length - 1 ].getAttribute('record_id'),
-				gen_patron_select_async_function( ++patron_select_async_count )
-			);
+			try {
+				/* Get selection */
+				var patrons = get_list_from_tree_selection( p.search_results.paged_tree.tree );
+				/* Clear Current Patron */
+				p._patron = fake_patron();
+				render_fm( p.w.document, { 'au' : p._patron } );
+				/* Get patron and render status */
+				retrieve_patron_by_id( 
+					patrons[ patrons.length - 1 ].getAttribute('record_id'),
+					gen_patron_select_async_function( ++patron_select_async_count )
+				);
+			} catch(E) {
+				sdump('D_ERROR_EXPECTED',js2JSON(E) + '\n');
+			}
 		}
 	);
 	p.search_results.register_flesh_patron_function(
