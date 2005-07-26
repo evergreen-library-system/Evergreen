21776c3df65f38c66463d2455006925fd49cc0f2
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-07-26 16:08:58 +0000

    adding description fields
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1483 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/permission.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/permission.pm
@@ -8,12 +8,12 @@ use base qw/OpenILS::Application::Storage::CDBI/;
 package permission::perm_list;
 use base qw/permission/;
 __PACKAGE__->table('permission_perm_list');
-__PACKAGE__->columns(All => qw/id code/);
+__PACKAGE__->columns(All => qw/id code description/);
 #-------------------------------------------------------------------------------
 package permission::grp_tree;
 use base qw/permission/;
 __PACKAGE__->table('permission_grp_tree');
-__PACKAGE__->columns(All => qw/id name parent/);
+__PACKAGE__->columns(All => qw/id name parent description/);
 #-------------------------------------------------------------------------------
 package permission::usr_grp_map;
 use base qw/permission/;
