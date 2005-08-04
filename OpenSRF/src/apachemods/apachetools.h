23a75889b96b507643cc2c2e633aa4ba06bc8511
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-08-04 22:13:11 +0000

    apachetools is some generic apache module helper functions xmltools are some generic xml helper functions mod_xmltools is the beginning of our XML monster for processing DTDs, xincludes, and potentially xslt's
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1610 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ OpenSRF/src/apachemods/apachetools.h
@@ -0,0 +1,37 @@
+#include "httpd.h"
+#include "http_config.h"
+#include "http_core.h"
+#include "http_protocol.h"
+#include "apr_compat.h"
+#include "apr_strings.h"
+
+#include "string_array.h"
+#include "utils.h"
+
+#ifndef APACHE_TOOLS_H
+#define APACHE_TOOLS_H
+
+
+/* parses apache URL params (GET and POST).  
+	Returns a string_array of the form [ key, val, key, val, ...]
+	Returns NULL if there are no params */
+string_array* apacheParseParms(request_rec* r);
+
+/* provide the params string array, and this will generate a 
+	string of array of param keys 
+	the returned string_array most be freed by the caller
+	*/
+string_array* apacheGetParamKeys(string_array* params);
+
+/* provide the params string array and a key name, and 
+	this will provide the value found for that key 
+	the returned string_array most be freed by the caller
+	*/
+string_array* apacheGetParamValues(string_array* params, char* key);
+
+/* returns the first value found for the given param.  
+	char* must be freed by the caller */
+char* apacheGetFirstParamValue(string_array* params, char* key);
+
+
+#endif
