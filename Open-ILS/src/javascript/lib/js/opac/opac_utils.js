f6c3dc8501ff653666e0ed7b451f12d835a78ac0
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-08-08 16:03:15 +0000

    moved org_utils into opac_utils
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1625 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/javascript/lib/js/opac/opac_utils.js
+++ Open-ILS/src/javascript/lib/js/opac/opac_utils.js
@@ -311,8 +311,52 @@ function orgSelect(id) {
 }
 
 
+/* ------------------------------------------------------------------------------------------------------ */
+/* org tree utilities */
+/* ------------------------------------------------------------------------------------------------------ */
 
-/* ----------------------------------------------------------------------- */
+/* takes an org unit or id and return the numeric depth */
+function findOrgDepth(org_id_or_node) {
+	return findOrgType(findOrgUnit(org_id_or_node).ou_type()).depth();
+}
+
+/* takes the org type id from orgunit.ou_type() field and returns
+	the org type object */
+function findOrgType(type_id) {
+	if(typeof type_id == 'object') return type_id;
+	for(var type in globalOrgTypes) {
+		var t =globalOrgTypes[type]; 
+		if( t.id() == type_id || t.id() == parseInt(type_id) ) 
+			return t;
+	}
+	return null;
+}
+
+
+/* returns an org unit by id.  if an object is passed in as the id,
+	then the object is assumed to be an org unit and is returned */
+function findOrgUnit(org_id) {
+	return (typeof org_id == 'object') ? org_id : orgArraySearcher[org_id];
+}
+
+
+/* builds a trail from the top of the org tree to the node provide.
+	basically fleshes out 'my orgs' 
+	Returns an array of [org0, org1, ..., myorg] */
+function orgNodeTrail(node) {
+	var na = new Array();
+	while( node ) {
+		na.push(node);
+		node = findOrgUnit(node.parent_ou());
+	}
+	return na.reverse();
+}
+
+
+/* returns an array of sibling org units */
+function findSiblingOrgs(node) { return findOrgUnit(node.parent_ou()).children(); }
+/* ------------------------------------------------------------------------------------------------------ */
+
 
 
 
