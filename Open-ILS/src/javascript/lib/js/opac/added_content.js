5af8f483ef5f29a177adad6428c642276ef41b23
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-08-21 19:24:54 +0000

    removing all the old web stuff, css, images, templates, etc. This all lives under Open-ILS/web/opac/* now
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1684 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/javascript/lib/js/opac/added_content.js
+++
@@ -1,3 +0,0 @@
-function buildISBNSrc(isbn) {
-	return "http://www.thecontentserver.com/bin/cntsvr.dll?GetImage&SysID=Content&CustID=Cafe&Return=1&Type=S&Key=" + isbn ;
-}      
--- Open-ILS/src/javascript/opac/AboutPage.js
+++
@@ -1,8 +0,0 @@
-AboutPage.prototype					= new Page();
-AboutPage.prototype.constructor	= AboutPage;
-AboutPage.baseClass					= Page.constructor;
-
-function AboutPage() {
-	this.searchBarForm	= new SearchBarFormChunk();
-	this.searchBar			= new SearchBarChunk();
-}
