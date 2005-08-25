1e9ae743c46afa3fc6b8153c541485b6cc7d62fc
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-08-25 19:10:31 +0000

    cleanup
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@1741 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/cat/marc_validate.js
+++ Evergreen/staff_client/chrome/content/evergreen/cat/marc_validate.js
@@ -46,106 +46,62 @@ function legend_marc() {
 function handle_tag_change(ev) {
 	mw.sdump('D_CAT','Entering handle_tag_change: ' + timer_elapsed('cat') + '\n');
 	try {
-dump('1\n');
 	rule_warnings = []; disable_widgets('explain_marc');
-dump('2\n');
 	tag_seen = {};
-dump('3\n');
 	var rows = document.getElementById('ctrl_rows').childNodes;
-dump('4\n');
 	for (var r in rows) {
-dump('5\n');
 		if ((typeof(rows[r]) == 'object')&&(rows[r].tagName == 'row')) {
-dump('6\n');
 			test_tagnumber_rule(rows[r]);
-dump('7\n');
 		}
-dump('8\n');
 	}
-dump('9\n');
 	rows = document.getElementById('data_rows').childNodes;
-dump('10\n');
 	for (var r in rows) {
-dump('11\n');
 		if (	(typeof(rows[r]) == 'object') &&
 			(rows[r].tagName == 'row') &&
 			(rows[r].getAttribute('hidden') != 'true')
 		) {
-dump('12\n');
 			test_tagnumber_rule(rows[r]);
-dump('13\n');
 			test_ind1_rule(rows[r]);
-dump('14\n');
 			test_ind2_rule(rows[r]);
-dump('15\n');
 			test_subfield_rule(rows[r]);
-dump('16\n');
 		}
-dump('17\n');
 	}
-dump('18\n');
 	if (rule_warnings.length > 0) { 
-dump('19\n');
 		enable_widgets('explain_marc'); }
 	} catch(E) {
-dump('20\n');
 		handle_error(E);
 	}
-dump('21\n');
 	mw.sdump('D_CAT','Exiting handle_tag_change: ' + timer_elapsed('cat') + '\n');
 }
 
 function test_tagnumber_rule(r) {
 	// rows (rows) -> row (r) -> wrapper (w) -> textbox (t)
-dump('t1\n');
 	try {
-dump('t2\n');
 	var t = r.firstChild.firstChild;
-dump('t3\n');
 	if (t.value.length > 3) { 
-dump('t4\n');
 		t.value = t.value.substr(0,3); 
-dump('t5\n');
 	} else if (t.value.length < 3) {
-dump('t6\n');
 		switch(t.value.length) {
 			case 2: t.value = '0' + t.value; break;
 			case 1: t.value = '00' + t.value; break;
 			case 0: t.value = '000'; break;
 		}
-dump('t7\n');
 	}
-dump('t8\n');
 	removeCSSClass(t,'invalid');
-dump('t9\n');
 	if (marc_rules[t.value]) {
-dump('t10\n');
 		var rule = marc_rules[t.value];
-dump('t11\n');
 		if (tag_seen[t.value]) { tag_seen[t.value]++; } else { tag_seen[t.value] = 1; }
-dump('t12\n');
 		if ( (rule.repeat == 'NR') && (tag_seen[t.value] > 1) ) {
-dump('t13\n');
 			addCSSClass(t,'invalid');
-dump('t14\n');
 			var s = 'Tag ' + t.value + ' should be Non-Repeating\n';
-dump('t15\n');
 			rule_warnings.push(s); mw.sdump('D_CAT',s);
-dump('t16\n');
 		}
-dump('t17\n');
 	} else if (t.value != 'LDR') {
-dump('t18\n');
 		addCSSClass(t,'invalid');
-dump('t19\n');
 		var s = 'Tag ' + t.value + ' is unknown.\n';
-dump('t20\n');
 		rule_warnings.push(s); mw.sdump('D_CAT',s);
-dump('t21\n');
 	}
-dump('t22\n');
 	} catch(E) {
-		dump('t23\n');
 		handle_error();
 	}
 }
