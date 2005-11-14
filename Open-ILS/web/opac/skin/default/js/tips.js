fb487bee6d77e16bdaf0c4ecad985e4744b43fe8
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-11-14 03:28:11 +0000

    dtd-izing the tips stuff (and the "of" on the results pages)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2015 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/opac/skin/default/js/tips.js
+++ Open-ILS/web/opac/skin/default/js/tips.js
@@ -1,13 +1,18 @@
 attachEvt('common', 'run', insertTip);
 
+/*
 var user_tips = [
 	'Click on a folder icon in the sidebar to access related quick searches',
 	"If you don't find what you want try expanding your search using the range selector at the right of the search bar"
 ];
+*/
 
 function insertTip () {
 	var tip_div = document.getElementById('tips');
-	if (tip_div)
-		tip_div.appendChild( text( user_tips[ Math.floor(Math.random() * user_tips.length) ] ) );
+	if (tip_div) {
+		var tips = tip_div.getElementsByTagName('div')[0].getElementsByTagName('span');
+		var index = Math.floor(Math.random() * tips.length);
+		tip_div.appendChild( tips[index] );
+	}
 }
 
