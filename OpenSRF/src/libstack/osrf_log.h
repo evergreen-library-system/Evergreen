8a295db2de5eeb180b51c5467b7a20a3cd8104a5
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-11-30 18:38:16 +0000

    removed these since the C code is now using the unified log code in utils/log.*
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2134 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- OpenSRF/src/libstack/osrf_log.h
+++
@@ -1,9 +0,0 @@
-#include "opensrf/utils.h"
-#include "osrf_settings.h"
-#include "osrfConfig.h"
-
-enum OSRF_LOG_LEVEL { OSRF_ERROR = 1, OSRF_WARN = 2, OSRF_INFO = 3, OSRF_DEBUG = 4 };
-
-int osrfLogInit(char* appname);
-void osrfLog( enum OSRF_LOG_LEVEL, char* msg, ... );
-
