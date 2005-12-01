35adab7f519b6114fda7db110549d92f61a2a8c3
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-01 17:50:23 +0000

    toward patron display
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2145 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/main/menu.js
+++ Open-ILS/xul/staff_client/server/main/menu.js
@@ -11,48 +11,71 @@ main.menu = function () {
 
 main.menu.prototype = {
 
-	'init' : function() {
+	'init' : function( params ) {
+
+		var session = params['session'];
 
 		var obj = this;
+		obj.view = {};
 
-		obj.tabbox = obj.w.document.getElementById('main_tabbox');
-		obj.tabs = obj.tabbox.firstChild;
-		obj.panels = obj.tabbox.lastChild;
+		obj.view.tabbox = obj.w.document.getElementById('main_tabbox');
+		obj.view.tabs = obj.view.tabbox.firstChild;
+		obj.view.panels = obj.view.tabbox.lastChild;
 
 		var cmd_map = {
-			'cmd_close_window' : function() { obj.w.close(); },
-			'cmd_new_window' : function() {
-				obj.window.open('/xul/server/main/menu_frame.xul','test' + 
-					obj.window.appshell_name_increment++ ,'chrome'); 
-			},
-			'cmd_new_tab' : function() { obj.new_tab(true); },
-			'cmd_close_tab' : function() { obj.close_tab(); },
-			'cmd_broken' : function() { alert('Not Yet Implemented'); },
-			'cmd_circ_checkout' : function() { 
-				obj.set_tab('/xul/server/patron/patron_barcode_entry.xul');
-			}
+			'cmd_close_window' : [ 
+				['command','keypress'], 
+				function() { obj.w.close(); } 
+			],
+			'cmd_new_window' : [
+				['command','keypress'],
+				function() {
+					obj.window.open('/xul/server/main/menu_frame.xul','test' + 
+						obj.window.appshell_name_increment++ ,'chrome'); 
+				}
+			],
+			'cmd_new_tab' : [
+				['command','keypress'],
+				function() { obj.new_tab(true); }
+			],
+			'cmd_close_tab' : [
+				['command','keypress'],
+				function() { obj.close_tab(); }
+			],
+			'cmd_broken' : [
+				['command','keypress'],
+				function() { alert('Not Yet Implemented'); }
+			],
+			'cmd_circ_checkout' : [
+				['command','keypress'],
+				function() { 
+					obj.set_tab('/xul/server/patron/patron_barcode_entry.xul?session='+obj.w.escape(session));
+				}
+			]
 		};
 
 		for (var i in cmd_map) {
 			var cmd = this.w.document.getElementById(i);
 			if (cmd) {
-				cmd.addEventListener('command',cmd_map[i],false);
-				cmd.addEventListener('keypress',cmd_map[i],false);
+				for (var j in cmd_map[i][0]) {
+					cmd.addEventListener(cmd_map[i][0][j],cmd_map[i][1],false);
+				}
 			}
+			obj.view[i] = cmd;
 		}
 
 		obj.new_tab(true);
 	},
 
 	'close_tab' : function () {
-		var idx = this.tabs.selectedIndex;
+		var idx = this.view.tabs.selectedIndex;
 		if (idx == 0) {
 			try {
-				this.tabs.advanceSelectedTab(+1);
+				this.view.tabs.advanceSelectedTab(+1);
 			} catch(E) {
 				this.error.sdump('D_TAB','failed tabs.advanceSelectedTab(+1):'+js2JSON(E) + '\n');
 				try {
-					this.tabs.advanceSelectedTab(-1);
+					this.view.tabs.advanceSelectedTab(-1);
 				} catch(E) {
 					this.error.sdump('D_TAB','failed again tabs.advanceSelectedTab(-1):'+
 						js2JSON(E) + '\n');
@@ -60,11 +83,11 @@ main.menu.prototype = {
 			}
 		} else {
 			try {
-				this.tabs.advanceSelectedTab(-1);
+				this.view.tabs.advanceSelectedTab(-1);
 			} catch(E) {
 				this.error.sdump('D_TAB','failed tabs.advanceSelectedTab(-1):'+js2JSON(E) + '\n');
 				try {
-					this.tabs.advanceSelectedTab(+1);
+					this.view.tabs.advanceSelectedTab(+1);
 				} catch(E) {
 					this.error.sdump('D_TAB','failed again tabs.advanceSelectedTab(+1):'+
 						js2JSON(E) + '\n');
@@ -73,15 +96,15 @@ main.menu.prototype = {
 
 		}
 		
-		this.error.sdump('D_TAB','\tnew tabbox.selectedIndex = ' + this.tabbox.selectedIndex + '\n');
+		this.error.sdump('D_TAB','\tnew tabbox.selectedIndex = ' + this.view.tabbox.selectedIndex + '\n');
 
-		this.tabs.childNodes[ idx ].hidden = true;
+		this.view.tabs.childNodes[ idx ].hidden = true;
 		this.error.sdump('D_TAB','tabs.childNodes[ ' + idx + ' ].hidden = true;\n');
 
 		// Make sure we keep at least one tab open.
 		var tab_flag = true;
-		for (var i = 0; i < this.tabs.childNodes.length; i++) {
-			var tab = this.tabs.childNodes[i];
+		for (var i = 0; i < this.view.tabs.childNodes.length; i++) {
+			var tab = this.view.tabs.childNodes[i];
 			if (!tab.hidden)
 				tab_flag = false;
 		}
@@ -90,23 +113,23 @@ main.menu.prototype = {
 
 	'find_free_tab' : function() {
 		var last_not_hidden = -1;
-		for (var i = 0; i<this.tabs.childNodes.length; i++) {
-			var tab = this.tabs.childNodes[i];
+		for (var i = 0; i<this.view.tabs.childNodes.length; i++) {
+			var tab = this.view.tabs.childNodes[i];
 			if (!tab.hidden)
 				last_not_hidden = i;
 		}
-		if (last_not_hidden == this.tabs.childNodes.length - 1)
+		if (last_not_hidden == this.view.tabs.childNodes.length - 1)
 			last_not_hidden = -1;
 		// If the one next to last_not_hidden is hidden, we want it.
 		// Basically, we fill in tabs after existing tabs for as 
 		// long as possible.
 		var idx = last_not_hidden + 1;
-		var candidate = this.tabs.childNodes[ idx ];
+		var candidate = this.view.tabs.childNodes[ idx ];
 		if (candidate.hidden)
 			return idx;
 		// Alright, find the first hidden then
-		for (var i = 0; i<this.tabs.childNodes.length; i++) {
-			var tab = this.tabs.childNodes[i];
+		for (var i = 0; i<this.view.tabs.childNodes.length; i++) {
+			var tab = this.view.tabs.childNodes[i];
 			if (tab.hidden)
 				return i;
 		}
@@ -116,11 +139,11 @@ main.menu.prototype = {
 	'new_tab' : function(focus) {
 		var tc = this.find_free_tab();
 		if (tc == -1) { return null; } // 9 tabs max
-		var tab = this.tabs.childNodes[ tc ];
+		var tab = this.view.tabs.childNodes[ tc ];
 		//tab.setAttribute('label','Tab ' + (tc + 1) );
 		tab.hidden = false;
 		try {
-			if (focus) this.tabs.selectedIndex = tc;
+			if (focus) this.view.tabs.selectedIndex = tc;
 			this.set_tab('data:text/html,<h1>Hello World</h1>',tc);
 		} catch(E) {
 			this.error.sdump('D_ERROR',E);
@@ -128,9 +151,9 @@ main.menu.prototype = {
 	},
 
 	'set_tab' : function(url,idx) {
-		if (!idx) idx = this.tabs.selectedIndex;
-		var tab = this.tabs.childNodes[ idx ];
-		var panel = this.panels.childNodes[ idx ];
+		if (!idx) idx = this.view.tabs.selectedIndex;
+		var tab = this.view.tabs.childNodes[ idx ];
+		var panel = this.view.panels.childNodes[ idx ];
 		while ( panel.lastChild ) panel.removeChild( panel.lastChild );
 		var frame = this.w.document.createElement('iframe');
 		frame.setAttribute('flex','1');
