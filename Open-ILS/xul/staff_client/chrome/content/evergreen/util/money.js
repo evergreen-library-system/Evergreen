9d889031bb6aa450a6b0aa38d589b4761da330ae
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-04 23:05:28 +0000

    util.money
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2191 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/xul/staff_client/chrome/content/evergreen/util/money.js
@@ -0,0 +1,63 @@
+dump('entering util/money.js\n');
+
+if (typeof util == 'undefined') var util = {};
+util.money = {};
+
+util.money.EXPORT_OK	= [ 
+	'dollars_float_to_cents_integer', 'cents_as_dollars'
+];
+util.money.EXPORT_TAGS	= { ':all' : util.money.EXPORT_OK };
+
+util.money.dollars_float_to_cents_integer = function( money ) {
+	// careful to avoid fractions of pennies
+	var money_s = money.toString();
+	// FIXME: strip miscellaneous characters
+	var marray = money_s.split(".");
+	var dollars = marray[0];
+	var cents = marray[1];
+	try {
+		if (cents.length < 2) {
+			cents = cents + '0';
+		}
+	} catch(E) {
+		// I am not sure why these are getting thrown, especially with the code still working
+		dump("util.money: cents.length? " + E + "\n");
+	}
+	try {
+		if (cents.length > 2) {
+			dump("util.money: We don't round money\n");
+			cents = cents.substr(0,2);
+		}
+	} catch(E) {
+		dump("util.money:2: cents.length? " + E + "\n");
+	}
+	var total = 0;
+	try {
+		if (parseInt(cents)) total += parseInt(cents);
+	} catch(E) {
+		dump("util.money: parseInt(cents)? " + E + "\n");
+	}
+	try {
+		if (parseInt(dollars)) total += (parseInt(dollars) * 100);
+	} catch(E) {
+		dump("util.money: parseInt(dollars)? " + E + "\n");
+	}
+	return total;	
+}
+
+util.money.cents_as_dollars = function( cents ) {
+	cents = cents.toString(); 
+	// FIXME: strip miscellaneous characters
+	try {
+		switch( cents.length ) {
+			case 0: cents = '000'; break;
+			case 1: cents = '00' + cents; break;
+		}
+	} catch(E) {
+		dump("util.money: cents_as_dollars: cents.length? " + E + "\n");
+	}
+	return cents.substr(0,cents.length-2) + '.' + cents.substr(cents.length - 2);
+}
+
+
+dump('exiting util/money.js\n');
