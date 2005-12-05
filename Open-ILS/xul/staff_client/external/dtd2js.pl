19602f45cb9a60c3120bcaaabf446ea5984f793e
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-05 18:55:37 +0000

    feed lang.dtd to this to get some js
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2220 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/xul/staff_client/external/dtd2js.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#<!ENTITY common.title "Title">
+#<!ENTITY common.author "Author">
+#<!ENTITY common.subject "Subject">
+#<!ENTITY common.series "Series">
+#<!ENTITY common.keyword "Keyword">
+#<!ENTITY common.type "Type">
+
+print "var entities = {};";
+while( $line = <> ) {
+
+	if ($line =~ /<!ENTITY\s+(\S+)\s+(".+")\s*>/) {
+		print "entities['$1'] = $2;\n";	
+	}
+}
