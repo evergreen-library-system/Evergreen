b2dba8c7b0ed01b9a0f8757e1d9bfff7b0d4a490
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-06 05:11:54 +0000

    staff. prefix
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2236 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/circ/checkout.js
+++ Open-ILS/xul/staff_client/server/circ/checkout.js
@@ -30,91 +30,91 @@ circ.checkout.prototype = {
 			{
 				'columns' : [
 					{
-						'id' : 'acp_id', 'label' : getString('acp_label_id'), 'flex' : 1,
+						'id' : 'acp_id', 'label' : getString('staff.acp_label_id'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.id()'
 					},
 					{
-						'id' : 'circ_id', 'label' : getString('circ_label_id'), 'flex' : 1,
+						'id' : 'circ_id', 'label' : getString('staff.circ_label_id'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.circ.id()'
 					},
 					{
-						'id' : 'mvr_doc_id', 'label' : getString('mvr_label_doc_id'), 'flex' : 1,
+						'id' : 'mvr_doc_id', 'label' : getString('staff.mvr_label_doc_id'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.mvr.doc_id()'
 					},
 					{
-						'id' : 'barcode', 'label' : getString('acp_label_barcode'), 'flex' : 1,
+						'id' : 'barcode', 'label' : getString('staff.acp_label_barcode'), 'flex' : 1,
 						'primary' : false, 'hidden' : false, 'render' : 'my.acp.barcode()'
 					},
 					{
-						'id' : 'call_number', 'label' : getString('acp_label_call_number'), 'flex' : 1,
+						'id' : 'call_number', 'label' : getString('staff.acp_label_call_number'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.call_number()'
 					},
 					{
-						'id' : 'copy_number', 'label' : getString('acp_label_copy_number'), 'flex' : 1,
+						'id' : 'copy_number', 'label' : getString('staff.acp_label_copy_number'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.copy_number()'
 					},
 					{
-						'id' : 'location', 'label' : getString('acp_label_location'), 'flex' : 1,
+						'id' : 'location', 'label' : getString('staff.acp_label_location'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.location()'
 					},
 					{
-						'id' : 'loan_duration', 'label' : getString('acp_label_loan_duration'), 'flex' : 1,
+						'id' : 'loan_duration', 'label' : getString('staff.acp_label_loan_duration'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.loan_duration()'
 					},
 					{
-						'id' : 'circ_lib', 'label' : getString('acp_label_circ_lib'), 'flex' : 1,
+						'id' : 'circ_lib', 'label' : getString('staff.acp_label_circ_lib'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.circ_lib()'
 					},
 					{
-						'id' : 'fine_level', 'label' : getString('acp_label_fine_level'), 'flex' : 1,
+						'id' : 'fine_level', 'label' : getString('staff.acp_label_fine_level'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.fine_level()'
 					},
 					{
-						'id' : 'deposit', 'label' : getString('acp_label_deposit'), 'flex' : 1,
+						'id' : 'deposit', 'label' : getString('staff.acp_label_deposit'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.deposit()'
 					},
 					{
-						'id' : 'deposit_amount', 'label' : getString('acp_label_deposit_amount'), 'flex' : 1,
+						'id' : 'deposit_amount', 'label' : getString('staff.acp_label_deposit_amount'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.deposit_amount()'
 					},
 					{
-						'id' : 'price', 'label' : getString('acp_label_price'), 'flex' : 1,
+						'id' : 'price', 'label' : getString('staff.acp_label_price'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.price()'
 					},
 					{
-						'id' : 'circ_as_type', 'label' : getString('acp_label_circ_as_type'), 'flex' : 1,
+						'id' : 'circ_as_type', 'label' : getString('staff.acp_label_circ_as_type'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.circ_as_type()'
 					},
 					{
-						'id' : 'circ_modifier', 'label' : getString('acp_label_circ_modifier'), 'flex' : 1,
+						'id' : 'circ_modifier', 'label' : getString('staff.acp_label_circ_modifier'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.acp.circ_modifier()'
 					},
 					{
-						'id' : 'xact_start', 'label' : getString('circ_label_xact_start'), 'flex' : 1,
+						'id' : 'xact_start', 'label' : getString('staff.circ_label_xact_start'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.circ.xact_start()'
 					},
 					{
-						'id' : 'xact_finish', 'label' : getString('circ_label_xact_finish'), 'flex' : 1,
+						'id' : 'xact_finish', 'label' : getString('staff.circ_label_xact_finish'), 'flex' : 1,
 						'primary' : false, 'hidden' : true, 'render' : 'my.circ.xact_finish()'
 					},
 					{
-						'id' : 'due_date', 'label' : getString('circ_label_due_date'), 'flex' : 1,
+						'id' : 'due_date', 'label' : getString('staff.circ_label_due_date'), 'flex' : 1,
 						'primary' : false, 'hidden' : false, 'render' : 'my.circ.due_date().substr(0,10)'
 					},
 					{
-						'id' : 'title', 'label' : getString('mvr_label_title'), 'flex' : 2,
+						'id' : 'title', 'label' : getString('staff.mvr_label_title'), 'flex' : 2,
 						'primary' : false, 'hidden' : false, 'render' : 'my.mvr.title()'
 					},
 					{
-						'id' : 'author', 'label' : getString('mvr_label_author'), 'flex' : 1,
+						'id' : 'author', 'label' : getString('staff.mvr_label_author'), 'flex' : 1,
 						'primary' : false, 'hidden' : false, 'render' : 'my.mvr.author()'
 					},
 					{
-						'id' : 'renewal_remaining', 'label' : getString('circ_label_renewal_remaining'), 'flex' : 0,
+						'id' : 'renewal_remaining', 'label' : getString('staff.circ_label_renewal_remaining'), 'flex' : 0,
 						'primary' : false, 'hidden' : false, 'render' : 'my.circ.renewal_remaining()'
 					},
 					{
-						'id' : 'status', 'label' : getString('acp_label_status'), 'flex' : 1,
+						'id' : 'status', 'label' : getString('staff.acp_label_status'), 'flex' : 1,
 						'primary' : false, 'hidden' : false, 'render' : 'stash.data.hash.acp[ my.acp.status() ].name()'
 					}
 				],
