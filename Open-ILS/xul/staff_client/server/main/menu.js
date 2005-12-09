65b9922dddaf19629e06b3454a0f5b04e61b396b
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-09 21:42:22 +0000

    *** empty log message ***
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2326 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/main/menu.js
+++ Open-ILS/xul/staff_client/server/main/menu.js
@@ -57,8 +57,8 @@ main.menu.prototype = {
 				['command','keypress'],
 				function() {
 					var content_params = { 'authtoken' : session, 'authtime' : authtime };
-					obj.error.sdump('D_MENU','session = ' + session));
-					obj.error.sdump('D_MENU','authtime = ' + authtime));
+					obj.error.sdump('D_MENU','session = ' + session);
+					obj.error.sdump('D_MENU','authtime = ' + authtime);
 					obj.error.sdump('D_MENU','content_params = ' + js2JSON(content_params));
 					obj.set_tab(urls.xul_opac_wrapper,{},content_params);
 				}
