e9be87cb5b7b1a3983ed228a2eca667c3022d99e
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-11 07:33:22 +0000

    dev tools
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2332 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/xul/staff_client/chrome/content/evergreen/util/xuledit.js
@@ -0,0 +1,65 @@
+// From Ted's Mozilla page: http://ted.mielczarek.org/code/mozilla/index.html 
+// Modified by Jason for Evergreen to push in the main (auth) window reference
+var old = '';
+var timeout = -1;
+var xwin = null;
+var newwin = false;
+
+function init()
+{
+  if(xwin)  // for some reason onload gets called when the browser refreshes???
+    return;
+
+  update();
+  document.getElementById('ta').select();
+}
+
+function openwin()
+{
+  toggleBrowser(false);
+  xwin = window.open('about:blank', 'xulwin', 'chrome,all,resizable=yes,width=400,height=400');
+  newwin = true;
+  update();
+}
+
+function toggleBrowser(show)
+{
+  document.getElementById("split").collapsed = !show;
+  document.getElementById("content").collapsed = !show;
+  document.getElementById("open").collapsed = !show;
+}
+
+function update()
+{
+  var textarea = document.getElementById("ta");
+
+  // either this is the first time, or
+  // they closed the window
+  if(xwin == null || (xwin instanceof Window && xwin.document == null)) {
+    toggleBrowser(true);
+    xwin = document.getElementById("content");
+    newwin = true;
+  }
+
+  if (old != textarea.value || newwin) {
+    old = textarea.value;
+    newwin = false;
+    var dataURI = "data:application/vnd.mozilla.xul+xml," + encodeURIComponent(old);
+    if(xwin instanceof Window)
+      xwin.document.location = dataURI;
+    else
+      xwin.setAttribute("src",dataURI);
+  }
+  try { xwin.contentWindow.mw = mw; } catch(E) {}
+  try { xwin.mw = mw; } catch(E) {}
+
+  timeout = window.setTimeout(update, 500);
+}
+
+function resetTimeout()
+{
+  if(timeout != -1)
+    window.clearTimeout(timeout);
+
+  timeout = window.setTimeout(update, 500);
+}
