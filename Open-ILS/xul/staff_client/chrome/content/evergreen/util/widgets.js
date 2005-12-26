3f40ee3843e4f346d904b412fc374733fc606b37
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2005-12-26 23:23:44 +0000

    skipping this.  Other things to try.. re-ordering the children in the deck.. or just not have conflicting accesskeys in a document at all
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2528 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/evergreen/util/widgets.js
+++ Open-ILS/xul/staff_client/chrome/content/evergreen/util/widgets.js
@@ -10,23 +10,26 @@ util.widgets.EXPORT_OK	= [
 util.widgets.EXPORT_TAGS	= { ':all' : util.widgets.EXPORT_OK };
 
 util.widgets.disable_accesskeys_in_node_and_children = function( node ) {
-	if (node.getAttribute('accesskeys')) {
-		node.setAttribute('oldaccesskeys', node.getAttribute('accesskeys'));
-		node.setAttribute('accesskeys','');
+	if (node.getAttribute('accesskey')) {
+		node.setAttribute('oldaccesskey', node.getAttribute('accesskey'));
+		node.setAttribute('accesskey',''); node.accessKey = '';
 	}
 	for (var i = 0; i < node.childNodes.length; i++) {
 		util.widgets.disable_accesskeys_in_node_and_children( node.childNodes[i] );
 	}
+	dump('- node = <' + node.id + '> accesskey = <' + node.accessKey + '> accesskey = <' + node.getAttribute('accesskey') + '> oldaccesskey = <' + node.getAttribute('oldaccesskey') + '>\n');
 }
 
 util.widgets.enable_accesskeys_in_node_and_children = function( node ) {
-	if (node.getAttribute('oldaccesskeys')) {
-		node.setAttribute('accesskeys', node.getAttribute('oldaccesskeys'));
-		node.setAttribute('oldaccesskeys','');
+	if (node.getAttribute('oldaccesskey')) {
+		node.setAttribute('accesskey', node.getAttribute('oldaccesskey'));
+		node.accessKey = node.getAttribute('oldaccesskey'); 
+		node.setAttribute('oldaccesskey','');
 	}
 	for (var i = 0; i < node.childNodes.length; i++) {
 		util.widgets.enable_accesskeys_in_node_and_children( node.childNodes[i] );
 	}
+	dump('+ node = <' + node.id + '> accesskey = <' + node.accessKey + '> accesskey = <' + node.getAttribute('accesskey') + '> oldaccesskey = <' + node.getAttribute('oldaccesskey') + '>\n');
 }
 
 dump('exiting util/widgets.js\n');
