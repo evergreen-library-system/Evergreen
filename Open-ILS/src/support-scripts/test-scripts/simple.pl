8bda9723e81b67957cc7f2fec6afb278fbc7ab5f
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-05 15:39:59 +0000

    simple example script for talking to opensrf
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2618 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/support-scripts/test-scripts/simple.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+#----------------------------------------------------------------
+# Simple example
+#----------------------------------------------------------------
+
+require '../oils_header.pl';
+use vars qw/ $apputils $memcache $user $authtoken $authtime 
+	$AUTH $STORAGE $SEARCH $CIRC $CAT $MATH $SETTINGS $ACTOR /;
+use strict; use warnings;
+
+my $config = $ARGV[0];
+err( "usage: $0 <bootstrap_config>" ) unless $config;
+osrf_connect($config);
+
+my( $user, $evt ) = simplereq($STORAGE, 'open-ils.storage.direct.actor.user.retrieve', 1 );
+oils_event_die($evt); # this user was not found / not all methods return events..
+print debug($user);
+
