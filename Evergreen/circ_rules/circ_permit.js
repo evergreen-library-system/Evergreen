da5519fdec7b2bec6403773cf677cbb379ce2a11
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-09 15:26:50 +0000

    added reference check to permit script
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2661 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/circ_rules/circ_permit.js
+++ Evergreen/circ_rules/circ_permit.js
@@ -12,6 +12,7 @@ log_debug('Checking permit circ on ' +
 	'');
 
 
+
 /* Patron checks --------------------------------------------- */
 if( ! patron.standing.match(/good/i) ) 
 	return result.event = 'PATRON_BAD_STANDING';
@@ -22,12 +23,18 @@ if( patron.profile.match(/patrons/i) && patron_info.items_out > 10 )
 if( patron.profile.match(/staff/i) && patron_info.items_out > 30 )
 	return result.event = 'PATRON_EXCEEDS_CHECKOUT_COUNT';
 
+
+
 /* Copy checks ------------------------------------------------ */
 if( is_false( copy.circulate ) ) 
 	return result.event = 'COPY_CIRC_NOT_ALLOWED';
 
+if( is_true( copy.ref ) ) 
+	return result.event = 'COPY_IS_REFERENCE';
+
 if( !copy.status.match(/available/i) && !copy.status.match(/on holds shelf/i) )
 	return result.event = 'COPY_NOT_AVAILABLE';
+
 
 /* check for holds -------------------------------------------- */
 fetch_hold_by_copy( copy.id );
