9c9f874bf981a06ef07f10e144786ab0007f270d
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-11 19:47:13 +0000

    skeleton files
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2689 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/xul/staff_client/external/template.js
@@ -0,0 +1,42 @@
+dump('entering example.template.js\n');
+
+if (typeof example == 'undefined') example = {};
+example.template = function (params) {
+	try {
+		netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+		JSAN.use('util.error'); this.error = new util.error();
+	} catch(E) {
+		dump('example.template: ' + E + '\n');
+	}
+}
+
+example.template.prototype = {
+
+	'init' : function( params ) {
+
+		try {
+			netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+
+			var obj = this;
+
+			obj.session = params['session'];
+
+			JSAN.use('util.controller'); obj.controller = new util.controller();
+			obj.controller.init(
+				{
+					control_map : {
+						'cmd_broken' : [
+							['command'],
+							function() { alert('Not Yet Implemented'); }
+						],
+					}
+				}
+			);
+
+		} catch(E) {
+			this.error.sdump('D_ERROR','example.template.init: ' + E + '\n');
+		}
+	},
+}
+
+dump('exiting example.template.js\n');
