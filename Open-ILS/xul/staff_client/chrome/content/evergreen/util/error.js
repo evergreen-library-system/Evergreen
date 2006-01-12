5b04eb9f8b765918feb94ede8964b89dc4d0f722
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-12 17:03:47 +0000

    debugging
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2699 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/evergreen/util/error.js
+++ Open-ILS/xul/staff_client/chrome/content/evergreen/util/error.js
@@ -40,7 +40,7 @@ util.error.prototype = {
 		'D_CLAM' : false, 'D_PAGED_TREE' : false, 'D_GRID_LIST' : false, 'D_HTML_TABLE' : false,
 		'D_TAB' : false, 'D_LIST' : true,
 
-		'D_AUTH' : true, 'D_OPAC' : true, 'D_CAT' : false,
+		'D_AUTH' : true, 'D_OPAC' : true, 'D_CAT' : true,
 
 		'D_PATRON_SEARCH' : false, 'D_PATRON_SEARCH_FORM' : false, 'D_PATRON_SEARCH_RESULTS' : false,
 
