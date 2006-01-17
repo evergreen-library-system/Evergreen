07fb68a238535dc3f0e13b0ebd6c68ea49d6808c
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-17 01:34:08 +0000

    didn't mean to remove these
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2721 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/evergreen/util/widgets.js
+++ Open-ILS/xul/staff_client/chrome/content/evergreen/util/widgets.js
@@ -4,7 +4,9 @@ if (typeof util == 'undefined') var util = {};
 util.widgets = {};
 
 util.widgets.EXPORT_OK	= [ 
-	'get_list_from_tree_selection'
+	'get_list_from_tree_selection',
+	'disable_accesskeys_in_node_and_children', 
+	'enable_accesskeys_in_node_and_children', 
 ];
 util.widgets.EXPORT_TAGS	= { ':all' : util.widgets.EXPORT_OK };
 
@@ -27,6 +29,29 @@ util.widgets.get_list_from_tree_selection = function(tree_w) {
 		}
 	}
 	return list;
+}
+
+util.widgets.disable_accesskeys_in_node_and_children = function( node ) {
+	if (node.getAttribute('accesskey')) {
+		node.setAttribute('oldaccesskey', node.getAttribute('accesskey'));
+		node.setAttribute('accesskey',''); node.accessKey = '';
+	}
+	for (var i = 0; i < node.childNodes.length; i++) {
+		util.widgets.disable_accesskeys_in_node_and_children( node.childNodes[i] );
+	}
+	dump('- node = <' + node.id + '> accesskey = <' + node.accessKey + '> accesskey = <' + node.getAttribute('accesskey') + '> oldaccesskey = <' + node.getAttribute('oldaccesskey') + '>\n');
+}
+
+util.widgets.enable_accesskeys_in_node_and_children = function( node ) {
+	if (node.getAttribute('oldaccesskey')) {
+		node.setAttribute('accesskey', node.getAttribute('oldaccesskey'));
+		node.accessKey = node.getAttribute('oldaccesskey'); 
+		node.setAttribute('oldaccesskey','');
+	}
+	for (var i = 0; i < node.childNodes.length; i++) {
+		util.widgets.enable_accesskeys_in_node_and_children( node.childNodes[i] );
+	}
+	dump('+ node = <' + node.id + '> accesskey = <' + node.accessKey + '> accesskey = <' + node.getAttribute('accesskey') + '> oldaccesskey = <' + node.getAttribute('oldaccesskey') + '>\n');
 }
 
 dump('exiting util/widgets.js\n');
