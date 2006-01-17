39fe56b16e6f7a0a2efd3d23df0fc7f614f6afbf
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-17 00:37:24 +0000

    useful
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2718 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/evergreen/util/widgets.js
+++ Open-ILS/xul/staff_client/chrome/content/evergreen/util/widgets.js
@@ -4,32 +4,29 @@ if (typeof util == 'undefined') var util = {};
 util.widgets = {};
 
 util.widgets.EXPORT_OK	= [ 
-	'disable_accesskeys_in_node_and_children', 
-	'enable_accesskeys_in_node_and_children', 
+	'get_list_from_tree_selection'
 ];
 util.widgets.EXPORT_TAGS	= { ':all' : util.widgets.EXPORT_OK };
 
-util.widgets.disable_accesskeys_in_node_and_children = function( node ) {
-	if (node.getAttribute('accesskey')) {
-		node.setAttribute('oldaccesskey', node.getAttribute('accesskey'));
-		node.setAttribute('accesskey',''); node.accessKey = '';
+util.widget.get_list_from_tree_selection = function(tree_w) {
+	var hitlist;
+	if (typeof(tree_w) != 'object') {
+		tree = document.getElementById(tree_w);
+	} else {
+		tree = tree_w;
 	}
-	for (var i = 0; i < node.childNodes.length; i++) {
-		util.widgets.disable_accesskeys_in_node_and_children( node.childNodes[i] );
+	var list = [];
+	var start = new Object();
+	var end = new Object();
+	var numRanges = tree.view.selection.getRangeCount();
+	for (var t=0; t<numRanges; t++){
+		tree.view.selection.getRangeAt(t,start,end);
+		for (var v=start.value; v<=end.value; v++){
+			var i = tree.contentView.getItemAtIndex(v);
+			list.push( i );
+		}
 	}
-	dump('- node = <' + node.id + '> accesskey = <' + node.accessKey + '> accesskey = <' + node.getAttribute('accesskey') + '> oldaccesskey = <' + node.getAttribute('oldaccesskey') + '>\n');
-}
-
-util.widgets.enable_accesskeys_in_node_and_children = function( node ) {
-	if (node.getAttribute('oldaccesskey')) {
-		node.setAttribute('accesskey', node.getAttribute('oldaccesskey'));
-		node.accessKey = node.getAttribute('oldaccesskey'); 
-		node.setAttribute('oldaccesskey','');
-	}
-	for (var i = 0; i < node.childNodes.length; i++) {
-		util.widgets.enable_accesskeys_in_node_and_children( node.childNodes[i] );
-	}
-	dump('+ node = <' + node.id + '> accesskey = <' + node.accessKey + '> accesskey = <' + node.getAttribute('accesskey') + '> oldaccesskey = <' + node.getAttribute('oldaccesskey') + '>\n');
+	return list;
 }
 
 dump('exiting util/widgets.js\n');
