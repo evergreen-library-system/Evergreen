774bf9d043bf5941e4401f8652e001cc12377f13
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-01-25 20:18:48 +0000

    another one for Kados ;)    trying to remove reference to Evergreen in Open-ILS part of tree
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@2837 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/external/dtd2js.sh
+++ Open-ILS/xul/staff_client/external/dtd2js.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./dtd2js.pl ~/ILS/Open-ILS/web/opac/locale/en-US/lang.dtd > ../chrome/content/evergreen/main/lang.js
+./dtd2js.pl ~/ILS/Open-ILS/web/opac/locale/en-US/lang.dtd > ../chrome/content/OPEN_ILS_STAFF_CLIENT/main/lang.js
