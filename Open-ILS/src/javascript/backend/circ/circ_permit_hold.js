36f12c67189c69fc29043601cf33c302f3dd93a1
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-02-10 22:54:55 +0000

    adding initial permit script for copy level holds
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3027 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/javascript/backend/circ/circ_permit_hold.js
@@ -0,0 +1,15 @@
+function go() {
+
+var patron				= environment.patron;
+var title				= environment.title;
+var copy					= environment.copy;
+var requestor			= environment.requestor;
+var requestLib			= environment.requestLib;
+var titleDescriptor	= environment.titleDescriptor;
+
+
+return result.event = 'SUCCESS';
+
+
+} go();
+
