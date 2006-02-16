e9416d4aaeb42ce1ba4c075a79d929e28f74ddac
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-02-16 09:05:02 +0000

    list dump for trees
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3093 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/util/list.js
+++ Open-ILS/xul/staff_client/chrome/content/util/list.js
@@ -309,5 +309,26 @@ util.list.prototype = {
 		}
 		return list;
 	},
+
+	'dump' : function(params) {
+		switch(this.node.nodeName) {
+			case 'tree' : return this._dump_tree(params); break;
+			default: throw('NYI: Need ._init() for ' + this.node.nodeName); break;
+		}
+	},
+
+	'_dump_tree' : function(params) {
+		var dump = [];
+		for (var i = 0; i < this.treechildren.childNodes.length; i++) {
+			var row = [];
+			var treeitem = this.treechildren.childNodes[i];
+			var treerow = treeitem.firstChild;
+			for (var j = 0; j < treerow.childNodes.length; j++) {
+				row.push( treerow.childNodes[j].getAttribute('label') );
+			}
+			dump.push( row );
+		}
+		return dump;
+	}
 }
 dump('exiting util.list.js\n');
