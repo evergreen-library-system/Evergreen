a0905fed6bf61c8b5eece9d9125bba2b5b98646b
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-01 21:30:35 +0000

    getting closer
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3504 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/offline/offline.pl
+++ Open-ILS/src/offline/offline.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl
 use strict; use warnings;
-use DBI;
 use CGI;
 use OpenSRF::System;
 use OpenSRF::Utils::Logger qw/$logger/;
@@ -13,12 +12,19 @@ use OpenILS::Utils::Fieldmapper;
 use Digest::MD5 qw/md5_hex/;
 use OpenSRF::Utils qw/:daemon/;
 use OpenILS::Utils::OfflineStore;
+
+use DBI;
 $DBI::trace = 1;
+
+#use Class::DBI;
+#Class::DBI->autoupdate(1);
+#local $OpenILS::Utils::OfflineStore::NO_TRIGGERS = 1;
 
 my $U = "OpenILS::Application::AppUtils";
 my $DB = "OpenILS::Utils::OfflineStore";
 my $SES = "${DB}::Session";
 my $SCRIPT = "OpenILS::Utils::OfflineStore::Script";
+#$DB->autoupdate(1);
 
 our %config;
 
@@ -53,13 +59,19 @@ my $evt;
 # this is moved to mod_perl
 # --------------------------------------------------------------------
 sub ol_init {
-
-	my $s = "";
-	$s .= "$_=" . $cgi->param($_) . "\n" for $cgi->param;
-	warn '-'x60 ."\n$s\n" . '-'x60 ."\n";
-
+	_ol_debug_params();
 	$DB->DBFile($config{db});
 	OpenSRF::System->bootstrap_client(config_file => $config{bootstrap} ); 
+}
+
+
+sub _ol_debug_params {
+	my $s = "";
+	my @params = $cgi->param;
+	@params = sort { $a cmp $b } @params;
+	$s .= "$_=" . $cgi->param($_) . "\n" for @params;
+	$s =~ s/\n$//o;
+	warn '-'x60 ."\n$s\n";
 }
 
 
@@ -125,18 +137,19 @@ sub ol_do_action {
 sub ol_create_session {
 
 	my $desc = $cgi->param('desc') || "";
-	$seskey ||=  time . "_${$}_" . int(rand() * 100);
+	$seskey = time . "_${$}_" . int(rand() * 1000);
 
 	$logger->debug("offline: user ".$requestor->id.
 		" creating new session with key $seskey and description $desc");
 
 	$SES->create(
 		{	
-			key			=> $seskey,
-			org			=> $org,
-			description => $desc,
-			creator		=> $requestor->id,
-			create_time => CORE::time(), 
+			key				=> $seskey,
+			org				=> $org,
+			description		=> $desc,
+			creator			=> $requestor->id,
+			create_time		=> CORE::time(), 
+			num_complete	=> 0,
 		} 
 	);
 
@@ -195,6 +208,7 @@ sub ol_load {
 
 	ol_handl_event('OFFLINE_SESSION_FILE_EXISTS') if ol_find_script();
 	ol_handle_event('OFFLINE_SESSION_ACTIVE') if $session->in_process;
+	ol_handle_event('OFFLINE_SESSION_COMPLETE') if $session->end_time;
 
 	qx/mkdir -p $outdir/;
 	my $x = 0;
@@ -265,10 +279,16 @@ sub ol_status {
 
 	my $type = $cgi->param('status_type') || "scripts";
 
+	# --------------------------------------------------------------------
+	# Returns info on every script upload attached to the current session
+	# --------------------------------------------------------------------
 	if( $type eq 'scripts' ) {
 		my $session = ol_find_session();
 		ol_handle_result(ol_flesh_session($session));
 
+	# --------------------------------------------------------------------
+	# Returns all scripts and sessions for the given org
+	# --------------------------------------------------------------------
 	} elsif( $type eq 'sessions' ) {
 		my @sessions = $SES->search( org => $org );
 
@@ -277,6 +297,20 @@ sub ol_status {
 		my @data;
 		push( @data, ol_flesh_session($_) ) for @sessions;
 		ol_handle_result(\@data);
+
+	# --------------------------------------------------------------------
+	# Returns total commands and completed commands counts
+	# --------------------------------------------------------------------
+	} elsif( $type eq 'summary' ) {
+		my $session = ol_find_session();
+
+		$logger->debug("offline: retrieving summary info ".
+			"for session ".$session->key." with completed=".$session->num_complete);
+
+		my $count = 0;
+		$count += $_->count for ($session->scripts);
+		ol_handle_result(
+			{ total => $count, num_complete => $session->num_complete });
 	}
 }
 
@@ -298,14 +332,20 @@ sub ol_fetch_workstation {
 # --------------------------------------------------------------------
 sub ol_execute {
 
+	my $session = ol_find_session();
+	ol_handle_event('OFFLINE_SESSION_ACTIVE') if $session->in_process;
+	ol_handle_event('OFFLINE_SESSION_COMPLETE') if $session->end_time;
+
 	my $commands = ol_collect_commands();
-	
+
 	# --------------------------------------------------------------------
 	# Note that we must disconnect from opensrf before forking or the 
 	# connection will be borked...
 	# --------------------------------------------------------------------
-	my $con = OpenSRF::Transport::PeerHandle->retrieve;
-	$con->disconnect if $con;
+	OpenSRF::Transport::PeerHandle->retrieve->disconnect;
+	#$session->in_process("1");
+	#$session->description("test ".CORE::time());
+	#$DB->disconnect;
 
 
 	if( safe_fork() ) {
@@ -332,7 +372,12 @@ sub ol_execute {
 		OpenSRF::System->bootstrap_client(config_file => $config{bootstrap});
 	
 		try {
-			ol_process_commands( $commands );
+
+			#use Class::DBI
+			#Class::DBI->autoupdate(1);
+			my $sesion = ol_find_session();
+			$session->in_process(1);
+			ol_process_commands($session, $commands);
 			ol_archive_files();
 
 		} catch Error with {
@@ -369,18 +414,303 @@ sub ol_collect_commands {
 		}
 	}
 
+	# make sure thera are no blank commands
+	@commands = grep { ($_ and $_->{type}) } @commands;
+
 	# sort on realtime
 	@commands = sort { $a->{_realtime} <=> $b->{_realtime} } @commands;
-	return \@commands;
+
+	# push user registrations to the front
+	my @regs		= grep { $_->{type} eq 'register' } @commands;
+	my @others	= grep { $_->{type} ne 'register' } @commands;
+
+	return [ @regs, @others ];
 }
 
-sub ol_process_commands {
-	my $commands = shift;
-	$logger->debug("offline: command = " . JSON->perl2JSON($_)) for @$commands;
+sub ol_date {
+	my (undef,undef, undef, $mday,$mon,$year) = localtime(time);
+	$mon++; $year	+= 1900;
+	$mday	= "0$mday" unless $mday =~ /\d{2}/o;
+	$mon	= "0$mon" unless $mon	=~ /\d{2}/o;
+	return ($year, $mon, $mday);
 }
 
+
+# --------------------------------------------------------------------
+# Moves all files from the pending directory to the archive directory
+# and removes the pending directory
+# --------------------------------------------------------------------
 sub ol_archive_files {
+	my ($y, $m, $d) = ol_date();
+
+	my $dir = "$basedir/pending/$org/$seskey";
+	my $archdir = "$basedir/archive/$org/${y}_${m}_${d}/$seskey";
+	$logger->debug("offline: archiving files to $archdir");
+
+	qx/mkdir -p $archdir/;
+	qx/mv $_ $archdir/ for <$dir/*>;
+	qx/rmdir $dir/;
 }
+
+
+# --------------------------------------------------------------------
+# Appends results to the results file.
+# --------------------------------------------------------------------
+my $rhandle;
+sub ol_append_result {
+
+	my $obj	= shift;
+	my $last = shift;
+
+	$obj = JSON->perl2JSON($obj);
+
+	if(!$rhandle) {
+		open($rhandle, ">>$basedir/pending/$org/$seskey/results") 
+			or ol_handle_event('OFFLINE_FILE_ERROR');
+	}
+
+	print $rhandle "$obj\n";
+	close($rhandle) if $last;
+}
+
+
+
+# --------------------------------------------------------------------
+# Runs the commands and returns the list of errors
+# --------------------------------------------------------------------
+sub ol_process_commands {
+
+	my $session	 = shift;
+	my $commands = shift;
+	my $x        = 0;
+
+	$session->start_time(CORE::time);
+
+	for my $d ( @$commands ) {
+
+		my $t		= $d->{type};
+		my $last = ($x++ == scalar(@$commands) - 1) ? 1 : 0;
+		my $res	= { command => $d };
+
+		$res->{event} = ol_handle_checkin($d)	if $t eq 'checkin';
+		$res->{event} = ol_handle_inhouse($d)	if $t eq 'in_house_use';
+		$res->{event} = ol_handle_checkout($d) if $t eq 'checkout';
+		$res->{event} = ol_handle_renew($d)		if $t eq 'renew';
+		$res->{event} = ol_handle_register($d) if $t eq 'register';
+
+
+		ol_append_result($res, $last);
+		$session->num_complete( $session->num_complete + 1 );
+
+		$logger->debug("offline: set session [".$session->key."] num_complete to ".$session->num_complete);
+	}
+
+	$session->end_time(CORE::time);
+	$session->in_process(0);
+}
+
+
+# --------------------------------------------------------------------
+# Runs an in_house_use action
+# --------------------------------------------------------------------
+sub ol_handle_inhouse {
+
+	my $command		= shift;
+	my $realtime	= $command->{_realtime};
+	my $ws			= $command->{_workstation};
+	my $barcode		= $command->{barcode};
+	my $count		= $command->{count} || 1;
+	my $use_time	= $command->{use_time} || "";
+
+	$logger->activity("offline: in_house_use : requestor=". $requestor->id.", realtime=$realtime, ".  
+		"workstation=$ws, barcode=$barcode, count=$count, use_time=$use_time");
+
+	my $ids = $U->simplereq(
+		'open-ils.circ', 
+		'open-ils.circ.in_house_use.create', $authtoken, 
+		{ barcode => $barcode, count => $count, location => &offline_org, use_time => $use_time } );
+	
+	return OpenILS::Event->new('SUCCESS', payload => $ids) if( ref($ids) eq 'ARRAY' );
+	return $ids;
+}
+
+
+
+# --------------------------------------------------------------------
+# Pulls the relevant circ args from the command, fetches data where 
+# necessary
+# --------------------------------------------------------------------
+sub ol_circ_args_from_command {
+	my $command = shift;
+
+	my $type			= $command->{type};
+	my $realtime	= $command->{_realtime};
+	my $ws			= $command->{_workstation};
+	my $barcode		= $command->{barcode} || "";
+	my $cotime		= $command->{checkout_time} || "";
+	my $pbc			= $command->{patron_barcode};
+	my $due_date	= $command->{due_date} || "";
+	my $noncat		= ($command->{noncat}) ? "yes" : "no"; # for logging
+
+	$logger->activity("offline: $type : requestor=". $requestor->id.
+		", realtime=$realtime, workstation=$ws, checkout_time=$cotime, ".
+		"patron=$pbc, due_date=$due_date, noncat=$noncat");
+
+	my $args = { 
+		permit_override	=> 1, 
+		barcode				=> $barcode,		
+		checkout_time		=> $cotime, 
+		patron_barcode		=> $pbc,
+		due_date				=> $due_date };
+
+	if( $command->{noncat} ) {
+		$args->{noncat} = 1;
+		$args->{noncat_type} = $command->{noncat_type};
+		$args->{noncat_count} = $command->{noncat_count};
+	}
+
+	return $args;
+}
+
+
+
+# --------------------------------------------------------------------
+# Performs a checkout action
+# --------------------------------------------------------------------
+sub ol_handle_checkout {
+	my $command	= shift;
+	my $args = ol_circ_args_from_command($command);
+	return $U->simplereq(
+		'open-ils.circ', 'open-ils.circ.checkout', $authtoken, $args );
+}
+
+
+# --------------------------------------------------------------------
+# Performs the renewal action
+# --------------------------------------------------------------------
+sub ol_handle_renew {
+	my $command = shift;
+	my $args = ol_circ_args_from_command($command);
+	my $t = time;
+	return $U->simplereq(
+		'open-ils.circ', 'open-ils.circ.renew', $authtoken, $args );
+}
+
+
+# --------------------------------------------------------------------
+# Runs a checkin action
+# --------------------------------------------------------------------
+sub ol_handle_checkin {
+
+	my $command		= shift;
+	my $realtime	= $command->{_realtime};
+	my $ws			= $command->{_workstation};
+	my $barcode		= $command->{barcode};
+	my $backdate	= $command->{backdate} || "";
+
+	$logger->activity("offline: checkin : requestor=". $requestor->id.
+		", realtime=$realtime, ".  "workstation=$ws, barcode=$barcode, backdate=$backdate");
+
+	return $U->simplereq(
+		'open-ils.circ', 
+		'open-ils.circ.checkin', $authtoken,
+		{ barcode => $barcode, backdate => $backdate } );
+}
+
+
+
+# --------------------------------------------------------------------
+# Registers a new patron
+# --------------------------------------------------------------------
+sub ol_handle_register {
+	my $command = shift;
+
+	my $barcode = $command->{user}->{card}->{barcode};
+	delete $command->{user}->{card}; 
+
+	$logger->info("offline: creating new user with barcode $barcode");
+
+	# now, create the user
+	my $actor	= Fieldmapper::actor::user->new;
+	my $card		= Fieldmapper::actor::card->new;
+
+
+	# username defaults to the barcode
+	$actor->usrname( ($actor->usrname) ? $actor->usrname : $barcode );
+
+	# Set up all of the virtual IDs, isnew, etc.
+	$actor->isnew(1);
+	$actor->id(-1);
+	$actor->card(-1);
+	$actor->cards([$card]);
+
+	$card->isnew(1);
+	$card->id(-1);
+	$card->usr(-1);
+	$card->barcode($barcode);
+
+	my $billing_address;
+	my $mailing_address;
+
+	my @sresp;
+	for my $resp (@{$command->{user}->{survey_responses}}) {
+		my $sr = Fieldmapper::action::survey_response->new;
+		$sr->$_( $resp->{$_} ) for keys %$resp;
+		$sr->isnew(1);
+		$sr->usr(-1);
+		push(@sresp, $sr);
+		$logger->debug("offline: created new survey response for survey ".$sr->survey);
+	}
+	delete $command->{user}->{survey_responses};
+	$actor->survey_responses(\@sresp) if @sresp;
+
+	# extract the billing address
+	if( my $addr = $command->{user}->{billing_address} ) {
+		$billing_address = Fieldmapper::actor::user_address->new;
+		$billing_address->$_($addr->{$_}) for keys %$addr;
+		$billing_address->isnew(1);
+		$billing_address->id(-1);
+		$billing_address->usr(-1);
+		delete $command->{user}->{billing_address};
+		$logger->debug("offline: read billing address ".$billing_address->street1);
+	}
+
+	# extract the mailing address
+	if( my $addr = $command->{user}->{mailing_address} ) {
+		$mailing_address = Fieldmapper::actor::user_address->new;
+		$mailing_address->$_($addr->{$_}) for keys %$addr;
+		$mailing_address->isnew(1);
+		$mailing_address->id(-2);
+		$mailing_address->usr(-1);
+		delete $command->{user}->{mailing_address};
+		$logger->debug("offline: read mailing address ".$mailing_address->street1);
+	}
+
+	# make sure we have values for both
+	$billing_address ||= $mailing_address;
+	$mailing_address ||= $billing_address;
+
+	$actor->billing_address($billing_address->id);
+	$actor->mailing_address($mailing_address->id);
+	$actor->addresses([$mailing_address]);
+
+	push( @{$actor->addresses}, $billing_address ) 
+		unless $billing_address->id eq $mailing_address->id;
+	
+	# pull all of the rest of the data from the command blob
+	$actor->$_( $command->{user}->{$_} ) for keys %{$command->{user}};
+
+	$logger->debug("offline: creating user object...");
+	$actor = $U->simplereq(
+		'open-ils.actor', 
+		'open-ils.actor.patron.update', $authtoken, $actor);
+
+	return $actor if(ref($actor) eq 'HASH'); # an event occurred
+
+	return OpenILS::Event->new('SUCCESS', payload => $actor);
+}
+
+
 
 
 
