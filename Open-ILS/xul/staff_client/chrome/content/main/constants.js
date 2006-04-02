f44a0ae0549133c0d1f7f0669408234dbc0c22d3
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-02 15:49:26 +0000

    *** empty log message ***
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3512 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/main/constants.js
+++ Open-ILS/xul/staff_client/chrome/content/main/constants.js
@@ -103,7 +103,7 @@ var urls = {
 	'XUL_NON_CAT_LABEL_EDIT' : '/xul/server/admin/non_cat_types.xhtml',
 	'XUL_OFFLINE_UPLOAD_XACTS' : '/xul/server/admin/upload_xacts.xhtml',
 	'XUL_OFFLINE_MANAGE_XACTS' : '/xul/server/admin/offline_manage_xacts.xul',
-	'XUL_OFFLINE_MANAGE_XACTS_CGI' : '/~erickson/offline/offline-jason.pl',
+	'XUL_OFFLINE_MANAGE_XACTS_CGI' : '/~erickson/offline/offline.pl',
 	'XUL_OPAC_WRAPPER' : 'chrome://open_ils_staff_client/content/cat/opac.xul',
 	'XUL_PATRON_BARCODE_ENTRY' : '/xul/server/patron/barcode_entry.xul',
 	'XUL_PATRON_BILLS' : '/xul/server/patron/bills.xul',
