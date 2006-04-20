ae6cb9af3841253198e98076ac2e0e8c17c16549
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-20 03:56:42 +0000

    adding auditing for user addresses, org units and call numbers
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3764 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/900.audit-tables.sql
+++ Open-ILS/src/sql/Pg/900.audit-tables.sql
@@ -44,7 +44,10 @@ END;
 $creator$ LANGUAGE 'plpgsql';
 
 SELECT auditor.create_auditor ( 'actor', 'usr' );
+SELECT auditor.create_auditor ( 'actor', 'usr_address' );
+SELECT auditor.create_auditor ( 'actor', 'org_unit' );
 SELECT auditor.create_auditor ( 'biblio', 'record_entry' );
+SELECT auditor.create_auditor ( 'asset', 'call_number' );
 SELECT auditor.create_auditor ( 'asset', 'copy' );
 
 COMMIT;
