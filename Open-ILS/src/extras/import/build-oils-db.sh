b4ee9a4cee432321fbc8960dfb47ad57f2a8f202
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-20 03:57:55 +0000

    adding db port to build/install
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3766 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/extras/import/build-oils-db.sh
+++ Open-ILS/src/extras/import/build-oils-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ "_$3" == "_" ]; then
+if [ "_$4" == "_" ]; then
 	echo "Usage:"
-	echo "	$0 {Open-ILS-driver} {db-host} {db-name} {db-user} {db-password}"
+	echo "	$0 {Open-ILS-driver} {db-host} {db-port} {db-name} {db-user} {db-password}"
 	exit 1;
 fi
 
@@ -12,5 +12,5 @@ WD=`dirname $0`
 	echo "cd $WD/../../sql/$1/;"
 	cd $WD/../../sql/$1/;
 	pwd
-	./build-db.sh $2 $3 $4 $5
+	./build-db.sh $2 $3 $4 $5 $6
 )
--- Open-ILS/src/sql/Pg/build-db.sh
+++ Open-ILS/src/sql/Pg/build-db.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-# args: {db-host} {db-name} {db-user} {db-password}
+# args: {db-host} {db-port} {db-name} {db-user} {db-password}
 
 echo "You may be prompted several times for your database password..."
 
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 002.schema.config.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 005.schema.actors.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 006.schema.permissions.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 010.schema.biblio.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 011.schema.authority.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 020.schema.functions.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 030.schema.metabib.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 040.schema.asset.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 070.schema.container.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 080.schema.money.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 090.schema.action.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 002.schema.config.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 005.schema.actors.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 006.schema.permissions.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 010.schema.biblio.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 011.schema.authority.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 020.schema.functions.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 030.schema.metabib.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 040.schema.asset.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 070.schema.container.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 080.schema.money.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 090.schema.action.sql
 
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 800.fkeys.sql
-PGUSER=$3 PGHOST=$1 PGDATABASE=$2 psql -f 900.audit-tables.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 800.fkeys.sql
+PGUSER=$4 PGHOST=$1 PGPORT=$2 PGDATABASE=$3 psql -f 900.audit-tables.sql
