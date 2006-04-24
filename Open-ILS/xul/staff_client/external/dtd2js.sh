77b98afec5b1e8f331a16d7550728d0988b70f92
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-24 18:17:29 +0000

    correct path
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3849 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/external/dtd2js.sh
+++ Open-ILS/xul/staff_client/external/dtd2js.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./dtd2js.pl ~/ILS/Open-ILS/web/opac/locale/en-US/lang.dtd > ../chrome/content/open_ils_staff_client/main/lang.js
+./dtd2js.pl ~/ILS/Open-ILS/web/opac/locale/en-US/lang.dtd > ../chrome/content/main/lang.js
