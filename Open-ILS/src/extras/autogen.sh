4c9fd170970b67b15ccd1894b335895c4167ec5c
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-27 03:23:29 +0000

    not generating unused extcore file
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3921 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/extras/autogen.sh
+++ Open-ILS/src/extras/autogen.sh
@@ -4,7 +4,6 @@ CONFIG="$1";
 
 [ -z "$CONFIG" ] && echo "usage: $0 <bootstrap_config>" && exit;
 
-
 JSDIR="/openils/var/web/opac/common/js/";
 
 echo "Updating fieldmapper";
@@ -12,9 +11,6 @@ perl fieldmapper.pl		> "$JSDIR/fmall.js";
 
 echo "Updating web_fieldmapper";
 perl fieldmapper.pl "web_core"	> "$JSDIR/fmcore.js";
-
-echo "Updating web_fieldmapper";
-perl fieldmapper.pl "web"	> "$JSDIR/fmextcore.js";
 
 echo "Updating OrgTree";
 perl org_tree_js.pl "$CONFIG" > "$JSDIR/OrgTree.js";
