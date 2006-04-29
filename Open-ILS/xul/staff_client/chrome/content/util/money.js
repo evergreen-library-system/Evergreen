fe0b3af895698fddc646587a660da6c7139ad741
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-04-29 04:55:26 +0000

    allow negatives
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@3964 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/util/money.js
+++ Open-ILS/xul/staff_client/chrome/content/util/money.js
@@ -9,47 +9,57 @@ util.money.EXPORT_OK	= [
 util.money.EXPORT_TAGS	= { ':all' : util.money.EXPORT_OK };
 
 util.money.dollars_float_to_cents_integer = function( money ) {
-	// careful to avoid fractions of pennies
-	var money_s = money.toString().replace(/[^\.\d]/g, '');
-	var marray = money_s.split(".");
-	var dollars = marray[0];
-	var cents = marray[1];
 	try {
-		if (cents.length < 2) {
-			cents = cents + '0';
+		// careful to avoid fractions of pennies
+		var negative; negative = money.toString().match(/-/) ? -1 : 1;
+		var money_s = money.toString().replace(/[^\.\d]/g, '');
+		var marray = money_s.split(".");
+		var dollars = marray[0];
+		var cents = marray[1];
+		try {
+			if (cents.length < 2) {
+				cents = cents + '0';
+			}
+		} catch(E) {
 		}
-	} catch(E) {
-	}
-	try {
-		if (cents.length > 2) {
-			dump("util.money: We don't round money\n");
-			cents = cents.substr(0,2);
+		try {
+			if (cents.length > 2) {
+				dump("util.money: We don't round money\n");
+				cents = cents.substr(0,2);
+			}
+		} catch(E) {
 		}
+		var total = 0;
+		try {
+			if (parseInt(cents)) total += parseInt(cents);
+		} catch(E) {
+		}
+		try {
+			if (parseInt(dollars)) total += (parseInt(dollars) * 100);
+		} catch(E) {
+		}
+		return total * negative;	
 	} catch(E) {
+		alert('util.money.dollars_float_to_cents_integer:\n' + E);
 	}
-	var total = 0;
-	try {
-		if (parseInt(cents)) total += parseInt(cents);
-	} catch(E) {
-	}
-	try {
-		if (parseInt(dollars)) total += (parseInt(dollars) * 100);
-	} catch(E) {
-	}
-	return total;	
 }
 
 util.money.cents_as_dollars = function( cents ) {
-	cents = cents.toString().replace(/[^\.\d]/g, ''); 
-	if (cents.match(/\./)) cents = util.money.dollars_float_to_cents_integer( cents ).toString();
 	try {
-		switch( cents.length ) {
-			case 0: cents = '000'; break;
-			case 1: cents = '00' + cents; break;
+		var negative; negative = cents.toString().match(/-/) ? '-' : '';
+		cents = cents.toString().replace(/[^\.\d]/g, ''); 
+		if (cents.match(/\./)) cents = util.money.dollars_float_to_cents_integer( cents ).toString();
+		try {
+			switch( cents.length ) {
+				case 0: cents = '000'; break;
+				case 1: cents = '00' + cents; break;
+			}
+		} catch(E) {
 		}
+		return negative + cents.substr(0,cents.length-2) + '.' + cents.substr(cents.length - 2);
 	} catch(E) {
+		alert('util.money.cents_as_dollars:\n' + E);
 	}
-	return cents.substr(0,cents.length-2) + '.' + cents.substr(cents.length - 2);
 }
 
 util.money.sanitize = function( money ) {
