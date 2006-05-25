77650700b486d2454d19699de8181d29d23d3480
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-05-25 13:18:50 +0000

    added usergroup.new method
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@4401 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Actor/UserGroups.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Actor/UserGroups.pm
@@ -2,7 +2,7 @@ package OpenILS::Application::Actor::UserGroups;
 use base 'OpenSRF::Application';
 use strict; use warnings;
 use OpenILS::Application::AppUtils;
-use OpenILS::Utils::Editor;
+use OpenILS::Utils::Editor q/:funcs/;
 use OpenSRF::Utils::Logger q/$logger/;
 use OpenSRF::EX qw(:try);
 my $U = "OpenILS::Application::AppUtils";
@@ -20,7 +20,7 @@ __PACKAGE__->register_method(
 
 sub get_users_from_usergroup {
 	my( $self, $conn, $auth, $usergroup ) = @_;
-	my $e = OpenILS::Utils::Editor->new(authtoken=>$auth);
+	my $e = new_editor(authtoken=>$auth);
 	return $e->event unless $e->checkauth;
 	return $e->event unless $e->allowed('VIEW_USER'); # XXX reley on editor perm
 	return $e->search_actor_user({usrgroup => $usergroup}, {idlist => 1});
@@ -38,7 +38,7 @@ __PACKAGE__->register_method(
 
 sub get_leaders_from_usergroup {
 	my( $self, $conn, $auth, $usergroup ) = @_;
-	my $e = OpenILS::Utils::Editor->new(authtoken=>$auth);
+	my $e = new_editor(authtoken=>$auth);
 	return $e->event unless $e->checkauth;
 	return $e->event unless $e->allowed('VIEW_USER'); # XXX reley on editor perm
 	my $users = $e->search_actor_user({usrgroup => $usergroup})
@@ -67,7 +67,7 @@ __PACKAGE__->register_method(
 sub get_address_members {
 	my( $self, $conn, $auth, $addrid ) = @_;
 
-	my $e = OpenILS::Utils::Editor->new(authtoken=>$auth);
+	my $e = new_editor(authtoken=>$auth);
 	return $e->event unless $e->checkauth;
 	return $e->event unless $e->allowed('VIEW_USER'); # XXX reley on editor perm
 
@@ -79,5 +79,41 @@ sub get_address_members {
 	my %dedup = map { $_ => 1 } @list;
 	return [ keys %dedup ];
 }
+
+
+
+__PACKAGE__->register_method(
+	method	=> 'reset_group',
+	api_name	=> 'open-ils.actor.usergroup.new',
+	signature	=> q/
+		Gives the requested user a new empty usergroup.  
+		@param auth The auth token
+		@param userid The id of the user who needs the new usergroup
+		@param leader If true, this user will be marked as the group leader
+	/
+);
+
+sub reset_group {
+	my( $self, $conn, $auth, $userid, $leader ) = @_;
+
+	my $e = new_editor(authtoken=>$auth, xact=>1);
+	return $e->event unless $e->checkauth;
+	return $e->event unless $e->allowed('UPDATE_USER'); # XXX reley on editor perm
+
+	my $user = $e->retrieve_actor_user($userid) or return $e->event;
+
+	# ask for a new group id
+	my $groupid = $e->request('open-ils.storage.actor.user.group_id.new');
+
+	$user->usrgroup($groupid);
+	$user->master_account('t') if $leader;
+
+	$e->update_actor_user($user) or return $e->event;
+	$e->commit;
+	return $groupid;
+}
+
+
+
 
 1;
