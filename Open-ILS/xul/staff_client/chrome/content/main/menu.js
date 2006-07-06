ed40d3e229a517e8ad6f37c7e89155a4055361fa
pines <pines@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-07-06 19:34:12 +0000

    prompt
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@4916 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/main/menu.js
+++ Open-ILS/xul/staff_client/chrome/content/main/menu.js
@@ -513,16 +513,16 @@ main.menu.prototype = {
 			'cmd_shutdown' : [
 				['oncommand'],
 				function() {
-					var windowManager = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService();
-					var windowManagerInterface = windowManager.QueryInterface(Components.interfaces.nsIWindowMediator);
-					var enumerator = windowManagerInterface.getEnumerator(null);
-
-					var w; // close all other windows
-					while ( w = enumerator.getNext() ) {
-						if (w != window) w.close();
-					
+					if (window.confirm('Exit Evergreen completely?')) {
+						var windowManager = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService();
+						var windowManagerInterface = windowManager.QueryInterface(Components.interfaces.nsIWindowMediator);
+						var enumerator = windowManagerInterface.getEnumerator(null);
+						var w; // close all other windows
+						while ( w = enumerator.getNext() ) {
+							if (w != window) w.close();
+						}
+						window.close();
 					}
-					window.close();
 				}
 			],
 		};
