dd7d51f64bb94e3870b6d92fd7130140f38125b6
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-04 10:03:13 +0000

    clipboard function; using it for patron barcode and record tcn
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5291 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/OpenILS/global_util.js
+++ Open-ILS/xul/staff_client/server/OpenILS/global_util.js
@@ -27,4 +27,24 @@
 		return 'f';
 	}
 
+	function copy_to_clipboard(ev) {
+		try {
+			netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+			var text;
+			if (typeof ev == 'object') {
+				if (typeof ev.target != 'undefined') {
+					if (typeof ev.target.textContent != 'undefined') if (ev.target.textContent) text = ev.target.textContent;
+					if (typeof ev.target.value != 'undefined') if (ev.target.value) text = ev.target.value;
+				}
+			} else if (typeof ev == 'string') {
+				text = ev;
+			}
+			const gClipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+				.getService(Components.interfaces.nsIClipboardHelper);
+			gClipboardHelper.copyString(text);
+			alert('Copied "'+text+'" to clipboard.');
+		} catch(E) {
+			alert('Clipboard action failed: ' + E);	
+		}
+	}
 
