64b181d405fd97922f843658bec5112ea31b9a9f
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-07 20:19:26 +0000

    added check for bestsellernh
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5345 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/javascript/backend/circ/circ_permit_hold.js
+++ Open-ILS/src/javascript/backend/circ/circ_permit_hold.js
@@ -6,9 +6,13 @@ log_vars('circ_permit_hold');
 if( isTrue(patron.barred) ) 
 	result.events.push('PATRON_BARRED');
 
+if( copy.circ_modifier == 'bestsellernh' )
+	result.events.push('ITEM_NOT_HOLDABLE');
+
 /* projected medium 
 	this needs to be expanded to check circ_modifiers as well
 */
+
 if( getMARCItemType() == 'g' &&
 		!isOrgDescendent(copy.circ_lib.shortname, patron.home_ou.id) )
 	result.events.push('CIRC_EXCEEDS_COPY_RANGE');
