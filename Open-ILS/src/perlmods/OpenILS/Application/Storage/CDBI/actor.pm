6f3bb75596f396c994e7ef6ba253e13bafef6aaa
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-18 13:15:16 +0000

    adding phone and email to aou
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5583 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/actor.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/CDBI/actor.pm
@@ -74,7 +74,7 @@ use base qw/actor/;
 __PACKAGE__->table( 'actor_org_unit' );
 __PACKAGE__->columns( Primary => qw/id/);
 __PACKAGE__->columns( Essential => qw/parent_ou ou_type mailing_address billing_address
-				ill_address holds_address shortname name/);
+				ill_address holds_address shortname name email phone/);
 
 #-------------------------------------------------------------------------------
 package actor::org_unit::hours_of_operation;
