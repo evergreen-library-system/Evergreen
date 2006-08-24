f59b34e4e76b7c991e262a0d39edae72b6b2a6f9
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-24 21:02:21 +0000

    minor fixups
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5665 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/030.schema.metabib.sql
+++ Open-ILS/src/sql/Pg/030.schema.metabib.sql
@@ -108,10 +108,12 @@ CREATE INDEX metabib_rec_descriptor_bib_level_idx ON metabib.rec_descriptor (bib
 CREATE INDEX metabib_rec_descriptor_control_type_idx ON metabib.rec_descriptor (control_type);
 CREATE INDEX metabib_rec_descriptor_char_encoding_idx ON metabib.rec_descriptor (char_encoding);
 CREATE INDEX metabib_rec_descriptor_enc_level_idx ON metabib.rec_descriptor (enc_level);
+CREATE INDEX metabib_rec_descriptor_audience_idx ON metabib.rec_descriptor (audience);
+CREATE INDEX metabib_rec_descriptor_lit_form_idx ON metabib.rec_descriptor (lit_form);
 CREATE INDEX metabib_rec_descriptor_cat_form_idx ON metabib.rec_descriptor (cat_form);
 CREATE INDEX metabib_rec_descriptor_pub_status_idx ON metabib.rec_descriptor (pub_status);
 CREATE INDEX metabib_rec_descriptor_item_lang_idx ON metabib.rec_descriptor (item_lang);
-CREATE INDEX metabib_rec_descriptor_audience_idx ON metabib.rec_descriptor (audience);
+CREATE INDEX metabib_rec_descriptor_vr_format_idx ON metabib.rec_descriptor (vr_format);
 
 */
 
