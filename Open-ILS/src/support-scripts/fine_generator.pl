7d76c08b3f3ee6b990c9890315e85eace6d295c4
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-25 06:59:46 +0000

    perl based, so they are safer than srfsh and his puny 1M buffer ;)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5668 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/support-scripts/fine_generator.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+# ---------------------------------------------------------------------
+# Generic databse object dumper.
+# ./object_dumper.pl <bootstrap_config> <type>, <type>, ...
+# ./object_dumper.pl /openils/conf/bootstrap.conf permission.grp_tree
+# ---------------------------------------------------------------------
+
+use strict; 
+use warnings;
+use JSON;
+use OpenSRF::System;
+
+my $config = shift || die "bootstrap config required\n";
+
+OpenSRF::System->bootstrap_client( config_file => $config );
+
+my $r = OpenSRF::AppSession
+		->create( 'open-ils.storage' )
+		->request( 'open-ils.storage.action.circulation.overdue.generate_fines' => 1 );
+
+while (!$r->complete) { $r->recv };
+
