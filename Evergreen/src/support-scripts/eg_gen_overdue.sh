8016b0fb0a551bea52445f51157e7a30c3650422
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-30 15:28:30 +0000

    changed some paths
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5777 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/src/support-scripts/eg_gen_overdue.sh
+++ Evergreen/src/support-scripts/eg_gen_overdue.sh
@@ -6,9 +6,12 @@
 # ---------------------------------------------------------------
 
 
+
+
 DATE=$(date +%Y-%m-%d);
 DAY=$(date +%u);
 BSCONFIG="/openils/conf/bootstrap.conf"
+ODDIR="/openils/var/data/overdue";
 
 [ $(whoami) != "opensrf" ] && echo "Must be run as opensrf" && exit 1;
 source /etc/profile;
@@ -17,6 +20,5 @@ ARGS="0"
 [ $DAY == 6 -o $DAY == 7 ] && exit 0; # don't run on saturday or sunday
 if [ $DAY == 1 ]; then ARGS="2 1 0"; fi; # If today is monday, run for sat/sun/mon
 
-./eg_gen_overdue.pl $BSCONFIG $ARGS > "/openils/var/web/tmp/overdue/EG_overdue.$DATE.xml"
-
+./eg_gen_overdue.pl $BSCONFIG $ARGS > "$ODDIR/overdue.$DATE.xml"
 
