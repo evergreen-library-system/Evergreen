dd05460476ebe764a28d1122c6f714733fe6d380
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-08-31 19:51:23 +0000

    added constanst for collect fee billing type
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5811 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Const.pm
+++ Open-ILS/src/perlmods/OpenILS/Const.pm
@@ -83,6 +83,7 @@ econst OILS_HOLD_TYPE_METARECORD  => 'M';
 
 
 econst OILS_BILLING_TYPE_OVERDUE_MATERIALS => 'Overdue materials';
+econst OILS_BILLING_TYPE_COLLECTION_FEE => 'Long Overdue Collection Fee';
 
 
 
