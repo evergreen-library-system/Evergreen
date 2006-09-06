9c415a0fcd754771de78ee0c1ec83b8fa81c17e8
pines <pines@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-09-06 03:32:17 +0000

    have patron summary kludge handle object or id for bill summary
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@5967 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/patron/summary.js
+++ Open-ILS/xul/staff_client/server/patron/summary.js
@@ -138,7 +138,7 @@ patron.summary.prototype = {
 										}
 										var sum = 0;
 										for (var i = 0; i < list.length; i++) {
-											var robj = obj.network.simple_request('FM_MBTS_RETRIEVE',[ses(),list[i]]);
+											var robj = typeof list[i] == 'object' ? list[i] : obj.network.simple_request('FM_MBTS_RETRIEVE',[ses(),list[i]]);
 											sum += robj.balance_owed();
 										}
 										JSAN.use('util.money');
