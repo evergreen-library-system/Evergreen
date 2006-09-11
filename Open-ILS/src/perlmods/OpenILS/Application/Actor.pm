e5d9c00bc9fb2c4f4e89fa9463bf8a60b5ace64e
pines <pines@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-09-11 14:04:44 +0000

    typo
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@6062 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Actor.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Actor.pm
@@ -1919,7 +1919,7 @@ sub user_transaction_history {
 	return $e->event unless $e->allowed('VIEW_USER_TRANSACTIONS');
 
 	my $api = $self->api_name;
-	my @xact_finish  = (xact_finish => undef ) if ($api /history.still_open$/);
+	my @xact_finish  = (xact_finish => undef ) if ($api =~ /history.still_open$/);
 
 	my @xacts = @{ $e->search_money_billable_transaction(
 		[	{ usr => $userid, @xact_finish },
