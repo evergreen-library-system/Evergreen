98262c0c7a28406688983ed8f8caec532d9cbe9a
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-09-14 14:50:08 +0000

    allowing -'s in barcode
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@6095 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/xul/staff_client/server/patron/ue_config.js
+++ Evergreen/xul/staff_client/server/patron/ue_config.js
@@ -39,7 +39,7 @@ var myPerms = [
 
 var dataFields;
 const numRegex		= /^\d+$/;
-const wordRegex	= /^\w+$/;
+const wordRegex	= /^[\w-]+$/;
 const unameRegex	= /^\w[\.\w\@-]*$/;
 const ssnRegex		= /^\d{3}-\d{2}-\d{4}$/;
 const dlRegex		= /^[a-zA-Z]{2}-\w+/; /* driver's license */
