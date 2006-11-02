0e7e99d6061f404fc868fdb79d604297fb7b7df1
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-11-02 17:26:06 +0000

    added special case for LEE videos
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@6547 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/javascript/backend/circ/circ_duration.js
+++ Open-ILS/src/javascript/backend/circ/circ_duration.js
@@ -47,15 +47,18 @@ if( config ) {
 var circMod		= (copy.circ_modifier) ? copy.circ_modifier.toLowerCase() : '';
 
 /* statelib has some special circ rules */
+
+
 if( isOrgDescendent('STATELIB', copy.circ_lib.id) ) {
 	if(isTrue(copy.ref))
 		result.durationRule	= '14_days_2_renew';
-}
 
-
-/* Newton county gives their DVD's for $.10 a day */
-if( isOrgDescendent('NCLS', copy.circ_lib.id) && circMod == 'dvd' ) 
+} else if( isOrgDescendent('NCLS', copy.circ_lib.id) && circMod == 'dvd' ) 
 	result.recurringFinesRule	= '10_cent_per_day';
+
+else if( isOrgDescendent('LEE', copy.circ_lib.id) && circMod == 'video' ) 
+	result.recurringFinesRule	= '50_cent_per_day';
+	
 
 
 checkDurationExceptions();
