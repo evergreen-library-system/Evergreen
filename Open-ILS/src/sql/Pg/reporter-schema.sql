b59216618f152d4b7632ed5f24aaa929b30df472
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2006-12-01 18:44:02 +0000

    adding billing and payment totals views
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@6641 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/reporter-schema.sql
+++ Open-ILS/src/sql/Pg/reporter-schema.sql
@@ -174,5 +174,37 @@ SELECT	id,
 	END AS "type"
   FROM	action.circulation;
 
+CREATE OR REPLACE VIEW reporter.hold_request_record AS
+SELECT	id,
+	target,
+	hold_type,
+	CASE
+		WHEN hold_type = 'T'
+			THEN target
+		WHEN hold_type = 'V'
+			THEN (SELECT cn.record FROM asset.call_number cn WHERE cn.id = ahr.target)
+		WHEN hold_type = 'C'
+			THEN (SELECT cn.record FROM asset.call_number cn JOIN asset.copy cp ON (cn.id = cp.call_number) WHERE cp.id = ahr.target)
+		WHEN hold_type = 'M'
+			THEN (SELECT mr.master_record FROM metabib.metarecord mr WHERE mr.id = ahr.target)
+	END AS bib_record
+  FROM	action.hold_request ahr;
+
+CREATE OR REPLACE VIEW reporter.xact_billed_totals AS
+SELECT	b.xact,
+	SUM( CASE WHEN b.voided THEN 0 ELSE amount END ) as unvoided,
+	SUM( CASE WHEN b.voided THEN amount ELSE 0 END ) as voided,
+	SUM( amount ) as total
+  FROM	money.billing b
+  GROUP BY 1;
+
+CREATE OR REPLACE VIEW reporter.xact_paid_totals AS
+SELECT	b.xact,
+	SUM( CASE WHEN b.voided THEN 0 ELSE amount END ) as unvoided,
+	SUM( CASE WHEN b.voided THEN amount ELSE 0 END ) as voided,
+	SUM( amount ) as total
+  FROM	money.payment b
+  GROUP BY 1;
+
 COMMIT;
 
