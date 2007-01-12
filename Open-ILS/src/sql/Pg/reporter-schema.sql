340197a0b32a4b2efb294422672730548164593c
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-01-12 17:56:59 +0000

    get author from 110 or 111 failing 100
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@6763 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/reporter-schema.sql
+++ Open-ILS/src/sql/Pg/reporter-schema.sql
@@ -139,20 +139,20 @@ SELECT	r.id,
 	r.tcn_source,
 	r.tcn_value,
 	title.value AS title,
-	author.value AS author,
+	FIRST(author.value) AS author,
 	publisher.value AS publisher,
 	SUBSTRING(pubdate.value FROM $$\d+$$) AS pubdate,
 	ARRAY_ACCUM( SUBSTRING(isbn.value FROM $$^\S+$$) ) AS isbn,
 	ARRAY_ACCUM( SUBSTRING(issn.value FROM $$^\S+$$) ) AS issn
   FROM	biblio.record_entry r
 	LEFT JOIN metabib.full_rec title ON (r.id = title.record AND title.tag = '245' AND title.subfield = 'a')
-	LEFT JOIN metabib.full_rec author ON (r.id = author.record AND author.tag = '100' AND author.subfield = 'a')
+	LEFT JOIN metabib.full_rec author ON (r.id = author.record AND author.tag IN ('100','110','111') AND author.subfield = 'a')
 	LEFT JOIN metabib.full_rec publisher ON (r.id = publisher.record AND publisher.tag = '260' AND publisher.subfield = 'b')
 	LEFT JOIN metabib.full_rec pubdate ON (r.id = pubdate.record AND pubdate.tag = '260' AND pubdate.subfield = 'c')
 	LEFT JOIN metabib.full_rec isbn ON (r.id = isbn.record AND isbn.tag IN ('024', '020') AND isbn.subfield IN ('a','z'))
 	LEFT JOIN metabib.full_rec issn ON (r.id = issn.record AND issn.tag = '022' AND issn.subfield = 'a')
   WHERE	r.deleted IS FALSE
-  GROUP BY 1,2,3,4,5,6,7,8,9;
+  GROUP BY 1,2,3,4,5,6,8,9;
 
 CREATE OR REPLACE VIEW reporter.demographic AS
 SELECT	u.id,
