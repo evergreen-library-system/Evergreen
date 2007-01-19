1e787858d33e1ffc0463bd1f9f6024cb76a45f13
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-01-19 20:29:33 +0000

    hour truncation function fix
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_0@6794 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Reporter/SQLBuilder.pm
+++ Open-ILS/src/perlmods/OpenILS/Reporter/SQLBuilder.pm
@@ -728,7 +728,7 @@ package OpenILS::Reporter::SQLBuilder::Column::Transform::hour_trunc;
 
 sub toSQL {
 	my $self = shift;
-	return 'DATE_TRUNC("' . $self->{_relation} . '"."' . $self->name . '")';
+	return 'EXTRAC(HOUR FROM "' . $self->{_relation} . '"."' . $self->name . '")';
 }
 
 sub is_aggregate { return 0 }
