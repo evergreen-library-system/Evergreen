a99b91cddfb72bb3fb8ccc37790991274201ec20
pines <pines@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-01-30 16:33:46 +0000

    to make the download dialog work
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_0@6829 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/defaults/preferences/prefs.js
+++ Open-ILS/xul/staff_client/defaults/preferences/prefs.js
@@ -34,3 +34,15 @@ pref("nglayout.debug.disable_xul_cache",true);
 pref("nglayout.debug.disable_xul_fastload",true);
 pref("browser.xul.error_pages.enabled",true);
 
+pref("browser.download.useDownloadDir", true);
+pref("browser.download.folderList", 0);
+pref("browser.download.manager.showAlertOnComplete", true);
+pref("browser.download.manager.showAlertInterval", 2000);
+pref("browser.download.manager.retention", 2);
+pref("browser.download.manager.showWhenStarting", true);
+pref("browser.download.manager.useWindow", true);
+pref("browser.download.manager.closeWhenDone", false);
+pref("browser.download.manager.openDelay", 0);
+pref("browser.download.manager.focusWhenStarting", false);
+pref("browser.download.manager.flashCount", 2); 
+
