35e1b4a3dfbd78caea79c777befebe3ccf6d442e
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-02-05 20:59:45 +0000

    try this
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_0@6893 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Storage/Publisher/actor.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/Publisher/actor.pm
@@ -513,7 +513,7 @@ sub patron_search {
 	}
 
 	my $order_by = join ', ', map { 'LOWER(users.'. (split / /,$_)[0] . ') ' . (split / /,$_)[1] } @$sort;
-	my $distinct_by = join ', ', map { 'LOWER(users.'. (split / /,$_)[0] . ')' } @$sort;
+	my $distinct_list = join ', ', map { 'LOWER(users.'. (split / /,$_)[0] . ')' } @$sort;
 
 	if ($inactive) {
 		$inactive = '';
