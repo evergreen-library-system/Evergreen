fa187228c6a3ef38751f6a1d9c7cd20b34020a8c
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-03-12 13:27:07 +0000

    forcing removal of org tree from the cache before fetching the (potentially) updated org tree
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7061 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/extras/org_tree_js.pl
+++ Open-ILS/src/extras/org_tree_js.pl
@@ -6,11 +6,17 @@ use OpenSRF::System;
 use JSON;
 use OpenILS::Utils::Fieldmapper;
 use OpenSRF::Utils::SettingsClient;
+use OpenSRF::Utils::Cache;
 
 die "usage: perl org_tree_js.pl <bootstrap_config>" unless $ARGV[0];
 OpenSRF::System->bootstrap_client(config_file => $ARGV[0]);
 
 Fieldmapper->import(IDL => OpenSRF::Utils::SettingsClient->new->config_value("IDL"));
+
+warn "removing OrgTree from the cache...\n";
+my $cache = OpenSRF::Utils::Cache->new;
+$cache->delete_cache('orgtree');
+
 
 my $ses = OpenSRF::AppSession->create("open-ils.storage");
 my $tree = $ses->request("open-ils.storage.direct.actor.org_unit.retrieve.all.atomic")->gather(1);
