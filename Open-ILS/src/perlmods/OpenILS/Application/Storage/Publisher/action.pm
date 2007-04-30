9b9151fd34b06565e664f61e00330a52035662d0
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-04-30 20:20:48 +0000

    sending pickup_lib into the permit script
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_0@7183 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Storage/Publisher/action.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/Publisher/action.pm
@@ -1224,6 +1224,7 @@ sub choose_nearest_copy {
 				  copy => $c->to_fieldmapper,
 				  requestor => $hold->requestor->to_fieldmapper,
 				  request_lib => $hold->request_lib->to_fieldmapper,
+				  pickup_lib => $hold->pickup_lib->id,
 				}
 			));
 
