db82c9f6d66c8cd38c17fb62fa42ea68f8d4b8bf
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-05-10 16:27:18 +0000

    removing the JSAN dep -- all we need is the $ function
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7233 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/reports/xul/utilities.js
+++ Open-ILS/web/reports/xul/utilities.js
@@ -1,3 +1,22 @@
+function $ () {
+	var elements = new Array();
+
+	for (var i = 0; i < arguments.length; i++) {
+		var element = arguments[i];
+
+		if (typeof element == 'string')
+			element = document.getElementById(element) || undefined;
+
+		if (arguments.length == 1)
+			return element;
+
+		elements.push( element );
+	}
+
+	return elements;
+}
+
+
 function map (func, list) {
         var ret = [];
         for (var i = 0; i < list.length; i++) ret.push(func(list[i]));
