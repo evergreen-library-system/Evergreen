d4ef8f542a03458598e5e85b9889254a45583143
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-06-11 20:52:55 +0000

    removing legacy staff client after having tested modern client for lingering dependencies. yay :)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7418 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/staff_client/chrome/content/evergreen/z39_50/z39_50.js
+++
@@ -1,7 +0,0 @@
-dump('Loading z39_50.js\n');
-
-function z39_50_init() {
-	mw.sdump('D_Z39_50','**** TESTING: z39_50.js: z39_50_init(): ' + mw.G.main_test_variable + '\n');
-	mw.focus_widget( 't1' );
-}
-
