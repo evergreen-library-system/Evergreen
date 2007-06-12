559ea1f11eceb14deaeb30b95362d8da11609565
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-06-12 18:05:45 +0000

    Changes needed to make things work after the cvs2svn migration
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_0@7420 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/external/prune_dirs.sh
+++ Open-ILS/xul/staff_client/external/prune_dirs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find $1 -type d -name CVS -exec rm -rf {} \; 2> /dev/null
+find $1 -type d -name .svn -exec rm -rf {} \; 2> /dev/null
 find $1 -type d -name OPEN_ILS_STAFF_CLIENT -exec rm -rf {} \; 2> /dev/null
 exit 0
