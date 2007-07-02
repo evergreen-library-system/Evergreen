7a520dd8c038f48944d4228aa7d1c2b8dc7cfaba
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-07-02 01:33:43 +0000

    changed bootstrap config to new opensrf_core.xml file
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7499 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/examples/apache/startup.pl
+++ Open-ILS/examples/apache/startup.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use lib qw( /openils/lib/perl5 ); 
-use OpenILS::WWW::SuperCat qw( /openils/conf/bootstrap.conf );
-use OpenILS::WWW::AddedContent qw( /openils/conf/bootstrap.conf );
-use OpenILS::Reporter::Proxy ('/openils/conf/bootstrap.conf');
+use OpenILS::WWW::SuperCat qw( /openils/conf/opensrf_core.xml );
+use OpenILS::WWW::AddedContent qw( /openils/conf/opensrf_core.xml );
+use OpenILS::Reporter::Proxy ('/openils/conf/opensrf_core.xml');
 
 
 
