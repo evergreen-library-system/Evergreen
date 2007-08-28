fd0bdb61874a539644100306278b158aed0459b7
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-08-28 15:52:33 +0000

    speed tweak
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7725 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/defaults/preferences/prefs.js
+++ Open-ILS/xul/staff_client/defaults/preferences/prefs.js
@@ -37,8 +37,8 @@ pref("privacy.popups.policy",0);
 pref("browser.dom.window.dump.enabled",true);
 pref("javascript.options.strict",false);
 pref("javascript.options.showInConsole",true);
-pref("nglayout.debug.disable_xul_cache",true);
-pref("nglayout.debug.disable_xul_fastload",true);
+pref("nglayout.debug.disable_xul_cache",false);
+pref("nglayout.debug.disable_xul_fastload",false);
 pref("browser.xul.error_pages.enabled",true);
 
 pref("browser.download.useDownloadDir", true);
