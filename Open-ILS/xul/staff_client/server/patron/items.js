e0417de3219e875a055d85dcfd116663c2199aec
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-10-22 05:42:02 +0000

    missing semi-colon; I don't care if it's optional in javascript :)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7904 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/patron/items.js
+++ Open-ILS/xul/staff_client/server/patron/items.js
@@ -306,7 +306,7 @@ patron.items.prototype = {
                                 // A renewed circ is a new circ, and has a new circ_id.
                                 obj.list_circ_map[ r[0].payload.circ.id() ] = obj.list_circ_map[ circ_id ];
                             } else {
-                                var msg = bc + ' not renewed.\n' + r[0].textcode + r[0].desc
+                                var msg = bc + ' not renewed.\n' + r[0].textcode + r[0].desc;
                                 l.setAttribute('value', msg);
                                 alert(msg);
                             }
