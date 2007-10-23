47ebca0ee7c6eb1d1f286f7bd55404d8eef219dd
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-10-23 15:37:33 +0000

    added example of building a new IDL object
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7921 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/java/org/open_ils/test/TestIDL.java
+++ Open-ILS/src/java/org/open_ils/test/TestIDL.java
@@ -1,11 +1,19 @@
 package org.open_ils.test;
 import org.open_ils.idl.*;
+import org.opensrf.*;
+import org.opensrf.util.*;
 
 public class TestIDL {
     public static void main(String args[]) throws Exception {
         String idlFile = args[0];
         IDLParser parser = new IDLParser(idlFile);
         parser.parse();
-        System.out.print(parser.toXML());
+        //System.out.print(parser.toXML());
+
+        /*
+        OSRFObject bre = new OSRFObject("bre");
+        bre.put("id", new Integer(1));
+        System.out.println(bre);
+        */
     }
 }
