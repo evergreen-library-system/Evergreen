1b4f3ebf6f01629cd74131f018d1f3e8d8b23023
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-11-04 00:03:40 +0000

    adding copy location checks to default item permit scripts
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@7980 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Evergreen/src/javascript/backend/circ/circ_permit_copy.js
+++ Evergreen/src/javascript/backend/circ/circ_permit_copy.js
@@ -5,7 +5,7 @@ load_lib('circ/circ_lib.js');
 log_vars('circ_permit_copy');
 
 
-if( ! isTrue(copy.circulate) ) 
+if( ! isTrue(copy.circulate) || !isTrue(copy.location.circulate) ) 
 	result.events.push('COPY_CIRC_NOT_ALLOWED');
 
 
--- Open-ILS/src/javascript/backend/circ/circ_permit_copy.js
+++ Open-ILS/src/javascript/backend/circ/circ_permit_copy.js
@@ -6,7 +6,7 @@ load_lib('JSON_v1.js');
 log_vars('circ_permit_copy');
 
 
-if( ! isTrue(copy.circulate) ) 
+if( ! isTrue(copy.circulate) || !isTrue(copy.location.circulate) ) 
 	result.events.push('COPY_CIRC_NOT_ALLOWED');
 
 
