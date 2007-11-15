678242548f7bc091eceb1a42d8cd6a6f60feaf1d
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2007-11-15 04:17:58 +0000

    make the version number a - separated string to avoid floatiness (1.300000, etc)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@8065 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application.pm
+++ Open-ILS/src/perlmods/OpenILS/Application.pm
@@ -3,7 +3,8 @@ use OpenSRF::Application;
 use base qw/OpenSRF::Application/;
 
 sub ils_version {
-    return "1.3";
+    # version format is "x-y-z-p", for example "1-2-1-0" for Evergreen 1.2.1.0
+    return "1-3";
 }
 
 __PACKAGE__->register_method(
