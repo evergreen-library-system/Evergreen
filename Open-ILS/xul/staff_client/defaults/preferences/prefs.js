826cc08b2e973770dd61e0342a91d18195bc75e1
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-01-03 16:36:22 +0000

    experiment with svn:keywords
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@8298 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/defaults/preferences/prefs.js
+++ Open-ILS/xul/staff_client/defaults/preferences/prefs.js
@@ -5,6 +5,13 @@
 // This one is required for XUL Runner
 pref("toolkit.defaultChromeURI", "chrome://open_ils_staff_client/content/main/main.xul");
 
+// We'll use this one to help brand some build information into the client, and rely on subversion keywords
+pref("open-ils.repository.headURL","$HeadURL$");
+pref("open-ils.repository.author","$Author$");
+pref("open-ils.repository.revision","$Revision$");
+pref("open-ils.repository.date","$HeadURL$");
+pref("open-ils.repository.id","$Id$");
+
 // We need something like this to get window.open to work in some places (where it complains about
 // navigator.xul not being registered.  But is about:blank the best value to use here?
 pref("browser.chromeURL","about:blank");
