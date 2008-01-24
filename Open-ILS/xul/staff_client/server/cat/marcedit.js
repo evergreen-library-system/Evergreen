eeacbc12ae9e19a17bffaee474fa7f2de95b238d
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-01-24 16:25:30 +0000

    fixing authority validation with the new input/output methods for the gateway
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_2_1@8490 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/cat/marcedit.js
+++ Open-ILS/xul/staff_client/server/cat/marcedit.js
@@ -1842,12 +1842,11 @@ function validateAuthority (button) {
 
 
 function searchAuthority (term, tag, sf, limit) {
-	var url = "/gateway?format=xml&service=open-ils.search&method=open-ils.search.authority.fts";
+	var url = "/gateway?input_format=json&format=xml&service=open-ils.search&method=open-ils.search.authority.fts";
 	url += '&param="term"&param="' + term + '"';
 	url += '&param="limit"&param=' + limit;
 	url += '&param="tag"&param=' + tag;
 	url += '&param="subfield"&param="' + sf + '"';
-
 
 	var req = new XMLHttpRequest();
 	req.open('GET',url,false);
