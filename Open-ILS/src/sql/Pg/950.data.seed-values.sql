59aebdf25846d341518e2c6c1104b04ade5b38b9
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-02-13 18:02:35 +0000

    added admin/manage perms for provider
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@8744 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -833,7 +833,9 @@ INSERT INTO permission.perm_list VALUES
     (171, 'ADMIN_FUND', oils_i18n_gettext('Allow a user to create/view/update/delete a fund')),
     (172, 'MANAGE_FUNDING_SOURCE', oils_i18n_gettext('Allow a user to view/credit/debit a funding source')),
     (173, 'MANAGE_FUND', oils_i18n_gettext('Allow a user to view/credit/debit a fund')),
-    (174, 'CREATE_PICKLIST', oils_i18n_gettext('Allows a user to create a picklist'));
+    (174, 'CREATE_PICKLIST', oils_i18n_gettext('Allows a user to create a picklist')),
+    (175, 'ADMIN_PROVIDER', oils_i18n_gettext('Allow a user to create/view/update/delete a provider')),
+    (176, 'MANAGE_PROVIDER', oils_i18n_gettext('Allow a user to view and purchase from a provider'));
 
 INSERT INTO permission.grp_tree (id, name, parent, description, perm_interval, usergroup, application_perm) VALUES
 	(1, 'Users', NULL, NULL, '3 years', FALSE, 'group_application.user'),
