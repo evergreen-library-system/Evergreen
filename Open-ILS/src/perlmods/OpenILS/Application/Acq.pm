c730aa6e79a6ead3d53d38dccc69fb3a2a9d0f3c
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-02-19 19:59:19 +0000

    removed some unnecessary imports
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@8780 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Acq.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Acq.pm
@@ -2,9 +2,6 @@ package OpenILS::Application::Acq;
 use base qw/OpenILS::Application/;
 use strict; use warnings;
 
-use OpenSRF::Utils::Logger qw(:logger);
-use OpenILS::Utils::CStoreEditor q/:funcs/;
-
 use OpenILS::Application::Acq::Picklist;
 use OpenILS::Application::Acq::Financials;
 use OpenILS::Application::Acq::Provider;
