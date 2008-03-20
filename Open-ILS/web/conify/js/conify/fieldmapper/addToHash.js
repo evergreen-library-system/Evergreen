a44baff9f9ef7ffe4aade8a97ec6cfba61551076
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-03-20 03:11:56 +0000

    getting closer ...
    git-svn-id: svn://svn.open-ils.org/ILS/branches/dojo-admin@9096 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/conify/js/conify/fieldmapper/addToHash.js
@@ -0,0 +1,10 @@
+
+function _toHash () {
+	var _hash = {};
+	var _fields = fmclasses[this.classname];
+	for ( var i=0; i < _fields.length; i++) _hash[_fields[i]] = '' + this[_fields[i]]();
+	return _hash;
+}
+
+for (var i in fmclasses) window[i].prototype.toHash = _toHash;
+
--- Open-ILS/web/conify/js/fm_list2dojo.data.js
+++
@@ -1,9 +0,0 @@
-
-function _toHash () {
-	var _hash = {};
-	for ( var i in fmclasses['aou']) _hash[i] = this[i]();
-	return _hash;
-}
-	
-aou.prototype.toHash = _toHash;
-
