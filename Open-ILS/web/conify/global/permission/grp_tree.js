0ba8c921b9fe91812e1b3919a4f06052df043d11
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-03-28 16:14:33 +0000

    perm group interface -- will get dropdown for application permission when the perms interface is added
    git-svn-id: svn://svn.open-ils.org/ILS/branches/dojo-admin@9156 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/conify/global/permission/grp_tree.js
@@ -0,0 +1,68 @@
+dojo.require('conify.fieldmapper.addToHash', true);
+dojo.require('conify.fieldmapper.addFromHash', true);
+dojo.require('conify.fieldmapper.addToStoreData', true);
+dojo.require('conify.fieldmapper.addFromStoreItem', true);
+dojo.require('dojo.parser');
+dojo.require('dojo.data.ItemFileWriteStore');
+dojo.require('dojo.date.stamp');
+dojo.require('dijit.form.NumberSpinner');
+dojo.require('dijit.form.TextBox');
+dojo.require('dijit.form.TimeTextBox');
+dojo.require('dijit.form.ValidationTextBox');
+dojo.require('dijit.form.CheckBox');
+dojo.require('dijit.form.FilteringSelect');
+dojo.require('dijit.form.Textarea');
+dojo.require('dijit.Tree');
+dojo.require('dijit.layout.ContentPane');
+dojo.require('dijit.layout.TabContainer');
+dojo.require('dijit.layout.LayoutContainer');
+dojo.require('dijit.layout.SplitContainer');
+dojo.require('dojox.widget.Toaster');
+dojo.require('dojox.fx');
+
+// some handy globals
+var cgi = new CGI();
+var cookieManager = new HTTP.Cookies();
+var ses = cookieManager.read('ses') || cgi.param('ses');
+var pCRUD = new OpenSRF.ClientSession('open-ils.permacrud');
+
+var current_group;
+var virgin_out_id = -1;
+
+var highlighter = {};
+
+function status_update (markup) {
+	if (parent !== window && parent.status_update) parent.status_update( markup );
+}
+
+function save_group () {
+
+	var modified_pgt = new pgt().fromStoreItem( current_group );
+	modified_pgt.ischanged( 1 );
+
+	new_kid_button.disabled = false;
+	save_out_button.disabled = false;
+	delete_out_button.disabled = false;
+
+	pCRUD.request({
+		method : 'open-ils.permacrud.update.pgt',
+		timeout : 10,
+		params : [ ses, modified_pgt ],
+		onerror : function (r) {
+			highlighter.editor_pane.red.play();
+			status_update( 'Problem saving data for ' + ou_group_store.getValue( current_group, 'name' ) );
+		},
+		oncomplete : function (r) {
+			var res = r.recv();
+			if ( res && res.content() ) {
+				ou_group_store.setValue( current_group, 'ischanged', 0 );
+				highlighter.editor_pane.green.play();
+				status_update( 'Saved changes to ' + ou_group_store.getValue( current_group, 'name' ) );
+			} else {
+				highlighter.editor_pane.red.play();
+				status_update( 'Problem saving data for ' + ou_group_store.getValue( current_group, 'name' ) );
+			}
+		},
+	}).send();
+}
+
