8e06afcf0d82d20dfef16b6f6916c127c1020af8
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-04-11 03:36:57 +0000

    pull in lassos
    git-svn-id: svn://svn.open-ils.org/ILS/branches/dojo-admin@9308 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/fieldmapper/OrgUtils.js
+++ Open-ILS/web/js/fieldmapper/OrgUtils.js
@@ -21,6 +21,7 @@ if(!dojo._hasResource["fieldmapper.OrgUtils"]){
 	dojo.provide("fieldmapper.OrgUtils");
 	dojo.require("fieldmapper.Fieldmapper");
 	dojo.require("fieldmapper.OrgTree", true);
+	dojo.require("fieldmapper.OrgLasso", true);
 
 	fieldmapper.aou.globalOrgTree = {};
 	fieldmapper.aou.OrgCache = {};
