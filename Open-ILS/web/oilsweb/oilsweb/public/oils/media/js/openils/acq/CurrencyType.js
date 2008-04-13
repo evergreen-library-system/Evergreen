5cdba9c5ceebab4be8b9111d7a5c9a9c0a9d6011
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-04-13 13:23:54 +0000

    added some currency_type handling code.  added some copyright headers. moving away from legacy org_utils.js code
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@9324 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/oilsweb/oilsweb/public/oils/media/js/openils/acq/CurrencyType.js
@@ -0,0 +1,43 @@
+/* ---------------------------------------------------------------------------
+ * Copyright (C) 2008  Georgia Public Library Service
+ * Bill Erickson <erickson@esilibrary.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ---------------------------------------------------------------------------
+ */
+
+if(!dojo._hasResource["openils.acq.CurrencyType"]) {
+
+    dojo._hasResource["openils.acq.CurrencyType"] = true;
+    dojo.provide("openils.acq.CurrencyType");
+    dojo.declare('openils.acq.CurrencyType', null, {
+    });
+
+    openils.acq.CurrencyType.cache = {};
+
+    /**
+     * Retrieves all of the currency types
+     */
+    openils.acq.CurrencyType.fetchAll = function(onComplete) {
+        var req = new OpenSRF.ClientSession('open-ils.acq').request(
+            'open-ils.acq.currency_type.all.retrieve', oilsAuthtoken);
+
+        req.oncomplete = function(r) {
+            var msg = r.recv();
+            var types = msg.content();
+            for(var i in types) 
+                openils.acq.CurrencyType.cache[types[i].code()] = types[i];
+            onComplete(types);
+        }
+        req.send();
+    }
+}
+
--- Open-ILS/web/oilsweb/oilsweb/public/oils/media/js/openils/acq/Provider.js
+++ Open-ILS/web/oilsweb/oilsweb/public/oils/media/js/openils/acq/Provider.js
@@ -1,3 +1,19 @@
+/* ---------------------------------------------------------------------------
+ * Copyright (C) 2008  Georgia Public Library Service
+ * Bill Erickson <erickson@esilibrary.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ---------------------------------------------------------------------------
+ */
+
 if(!dojo._hasResource['openils.acq.Provider']) {
 dojo._hasResource['openils.acq.Provider'] = true;
 dojo.provide('openils.acq.Provider');
