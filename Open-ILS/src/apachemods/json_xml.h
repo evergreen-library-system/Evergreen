733d5c9b2693ffe88dade90541eed255bb21fb5e
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-04-23 15:13:45 +0000

    Removing dead code and their makefile counterparts
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@9441 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/apachemods/json_xml.h
+++
@@ -1,9 +0,0 @@
-
-#include <string.h>
-#include <stdio.h>
-
-/* the JSON parser, so we can read the response we're XMLizing */
-#include <opensrf/osrf_json.h>
-#include <opensrf/utils.h>
-
-char* json_string_to_xml(char*);
