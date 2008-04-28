994d4ca4499f902465d4e7a1f5ed3bdd58c7b29b
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-04-28 07:42:25 +0000

    fixing translator for grids
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@9474 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/conify/global/permission/perm_list.js
+++ Open-ILS/web/conify/global/permission/perm_list.js
@@ -16,7 +16,7 @@
 */
 
 dojo.require('fieldmapper.dojoData');
-dojo.require('openils.I18N');
+dojo.require('openils.widget.TranslatorPopup');
 dojo.require('dojo.parser');
 dojo.require('dojo.string');
 dojo.require('dojo.data.ItemFileWriteStore');
