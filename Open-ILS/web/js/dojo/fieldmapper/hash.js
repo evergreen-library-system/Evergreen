e21db473a775133bad91d562250be024719aaa00
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-05-13 02:42:09 +0000

    Merged revisions 9568 via svnmerge from svn://svn.open-ils.org/ILS/trunk
    ........
      r9568 | miker | 2008-05-12 22:41:36 -0400 (Mon, 12 May 2008) | 1 line
      
      store actual nulls instead of stringified "null"
    ........
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@9569 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/dojo/fieldmapper/hash.js
+++ Open-ILS/web/js/dojo/fieldmapper/hash.js
@@ -32,8 +32,11 @@ if(!dojo._hasResource['fieldmapper.hash']){
 	function _toHash (includeNulls) {
 		var _hash = {};
 		for ( var i=0; i < this._fields.length; i++) {
-			if (includeNulls || this[this._fields[i]]() != null)
-				_hash[this._fields[i]] = '' + this[this._fields[i]]();
+			if (includeNulls || this[this._fields[i]]() != null) {
+				if (this[this._fields[i]]() == null)
+                    _hash[this._fields[i]] = null;
+                else
+				    _hash[this._fields[i]] = '' + this[this._fields[i]]();
 		}
 		return _hash;
 	}
