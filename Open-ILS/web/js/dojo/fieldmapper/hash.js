c557d2e12e6dbeedef4b91b018c340821749c951
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-05-13 02:23:47 +0000

    Merged revisions 9564 via svnmerge from svn://svn.open-ils.org/ILS/trunk
    ........
      r9564 | miker | 2008-05-12 22:22:32 -0400 (Mon, 12 May 2008) | 1 line
      
      allow null properties coming from objects
    ........
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@9565 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/dojo/fieldmapper/hash.js
+++ Open-ILS/web/js/dojo/fieldmapper/hash.js
@@ -29,10 +29,10 @@ if(!dojo._hasResource['fieldmapper.hash']){
 		return this;
 	}
 
-	function _toHash () {
+	function _toHash (includeNulls) {
 		var _hash = {};
 		for ( var i=0; i < this._fields.length; i++) {
-			if (this[this._fields[i]]() != null)
+			if (includeNulls || this[this._fields[i]]() != null)
 				_hash[this._fields[i]] = '' + this[this._fields[i]]();
 		}
 		return _hash;
