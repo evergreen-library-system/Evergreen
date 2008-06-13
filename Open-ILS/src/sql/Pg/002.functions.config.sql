7e644d80bec25cbabe4d29e86b0267a53b84bd6b
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-06-13 17:31:13 +0000

    backporting reporter extention schema and upgrade script; backporting copyright notices
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_2@9830 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.functions.config.sql
+++ Open-ILS/src/sql/Pg/002.functions.config.sql
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2004-2008  Georgia Public Library Service
+ * Copyright (C) 2008  Equinox Software, Inc.
+ * Mike Rylander <miker@esilibrary.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
 BEGIN;
 
 CREATE OR REPLACE FUNCTION oils_xml_transform ( TEXT, TEXT ) RETURNS TEXT AS $_$
