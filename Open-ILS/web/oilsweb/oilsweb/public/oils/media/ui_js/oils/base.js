e903ed20d42e3e53f41cad55e908b0f49cc6aed6
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-06-25 18:25:42 +0000

    moved the logged in user fetching to an external JS file.  shoved into onload instead of running at render time
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@9930 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/oilsweb/oilsweb/public/oils/media/ui_js/oils/base.js
@@ -0,0 +1,42 @@
+function loadUser(username, password) {
+    dojo.require('dojo.cookie');
+    dojo.require('openils.CGI');
+    dojo.require("openils.User");
+
+    openils.User.authcookie = 'ses';
+    openils.User.authtoken = dojo.cookie('ses') || new openils.CGI().param('ses');
+    // cache the user object as a cookie?
+    //openils.User.user = JSON2js(dojo.cookie('user'));
+
+    if(!username) return;
+
+    dojo.require('openils.Event');
+
+    function dologin() {
+        openils.User.authtoken = null;
+        user = new openils.User();
+        user.login({
+            login_type:'staff', 
+            username:username, 
+            passwd:password, 
+            login:true
+        });
+        user.getBySession();
+        openils.User.authtoken = user.authtoken;
+        openils.User.user = user.user;
+        alert('user = ' + openils.User.user);
+        //dojo.cookie('user', js2JSON(openils.User.user),{path:'/'});
+    }
+
+    if(!openils.User.user) {
+        if(openils.User.authtoken) {
+            user = new openils.User();
+            openils.User.user = user.user;
+            if(openils.Event.parse(user.user)) // session timed out
+                dologin();
+        } else {
+            dologin();
+        }
+    }
+}
+
