8d94bbbd64ff9ab6e75913e7183f5a7a470d9dcd
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-08-03 14:20:53 +0000

    started plugging in ident search jubgrid, which I'm not sure is the best route here, is not displaying right, but it's probably just a missing bit of css
    git-svn-id: svn://svn.open-ils.org/ILS/branches/acq-experiment@10239 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/oilsweb/oilsweb/public/oils/media/ui_js/oils/default/acq/receiving/process.js
@@ -0,0 +1,59 @@
+dojo.require('fieldmapper.Fieldmapper');
+dojo.require('dijit.ProgressBar');
+dojo.require('dijit.form.Form');
+dojo.require('dijit.form.TextBox');
+dojo.require('dijit.form.CheckBox');
+dojo.require('dijit.form.FilteringSelect');
+dojo.require('dijit.form.Button');
+dojo.require("dijit.Dialog");
+dojo.require('openils.Event');
+dojo.require('openils.acq.Lineitem');
+dojo.require('openils.widget.OrgUnitFilteringSelect');
+
+var lineitems = [];
+
+function drawForm() {
+}
+
+var liReceived;
+function doSearch(values) {
+    var search = {
+        attr_values : [values.identifier],
+        po_agencies : (values.ordering_agency) ? [values.ordering_agency] : null,
+    };
+
+    options = {clear_marc:1, flesh_attrs:1};
+    liReceived = 0;
+    dojo.style('searchProgress', 'visibility', 'visible');
+
+    fieldmapper.standardRequest(
+        ['open-ils.acq', 'open-ils.acq.lineitem.search.ident'],
+        {   async: true,
+            params: [openils.User.authtoken, search, options],
+            onresponse: handleResult,
+            oncomplete: viewList
+        }
+    );
+}
+
+var searchLimit = 10; // ?
+function handleResult(r) {
+    var result = r.recv().content();
+    searchProgress.update({maximum: searchLimit, progress: ++liReceived});
+    lineitems.push(result);
+}
+
+function viewList() {
+    dojo.style('searchProgress', 'visibility', 'hidden');
+    dojo.style('oils-acq-li-recv-grid', 'visibility', 'visible');
+    dojo.style('oils-acq-li-recv-grid', 'display', 'block');
+    var store = new dojo.data.ItemFileWriteStore(
+        {data:jub.toStoreData(lineitems, null, 
+            {virtualFields:['estimated_price', 'actual_price']})});
+    var model = new dojox.grid.data.DojoData(
+        null, store, {rowsPerPage: 20, clientSort: true, query:{id:'*'}});
+    JUBGrid.populate(liGrid, model, lineitems);
+}
+
+
+
