f3a9e18f31891e599865330a60937dd2a2952345
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-10-11 02:43:13 +0000

    Fix up dojo.requires for copy status
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@10821 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/conify/global/config/copy_status.js
+++ Open-ILS/web/conify/global/config/copy_status.js
@@ -29,8 +29,8 @@ dojo.require('dijit.layout.BorderContainer');
 dojo.require('dojox.widget.Toaster');
 dojo.require('dojox.fx');
 dojo.require('dojox.grid.Grid');
-require("dojox.grid.compat._data.editors");
-
+dojo.require('dojox.grid.compat._data.model');
+dojo.require("dojox.grid.compat._data.editors");
 
 // some handy globals
 var cgi = new CGI();
