edeef7ae3376b838a1ecad9123a1669b0d1e914b
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-10-24 17:17:52 +0000

    commenting out unused locales
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_4@10900 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -729,12 +729,15 @@ INSERT INTO config.i18n_locale (code,marc_code,name,description)
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
     VALUES ('fr-CA', 'fre', oils_i18n_gettext('fr-CA', 'French (Canada)', 'i18n_l', 'name'),
 	oils_i18n_gettext('fr-CA', 'Canadian French', 'i18n_l', 'description'));
+/*
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
     VALUES ('es-US', 'spa', oils_i18n_gettext('es-US', 'Spanish (US)', 'i18n_l', 'name'),
 	oils_i18n_gettext('es-US', 'American Spanish', 'i18n_l', 'description'));
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
     VALUES ('es-MX', 'spa', oils_i18n_gettext('es-MX', 'Spanish (Mexico)', 'i18n_l', 'name'),
 	oils_i18n_gettext('es-MX', 'Mexican Spanish', 'i18n_l', 'description'));
+*/
+
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
     VALUES ('hy-AM', 'arm', oils_i18n_gettext('hy-AM', 'Armenian', 'i18n_l', 'name'),
 	oils_i18n_gettext('hy-AM', 'Armenian', 'i18n_l', 'description'));
