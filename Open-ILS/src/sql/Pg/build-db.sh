b33d7d1190c11b2cd0bcab3c9b04b99e4af89641
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-10-25 13:31:59 +0000

    wrong method name
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@10907 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/build-db.sh
+++ Open-ILS/src/sql/Pg/build-db.sh
@@ -15,7 +15,8 @@ export PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD
 # ---------------------------------------------------------------------------
 # Lookup the database version from the PostgreSQL server.
 # ---------------------------------------------------------------------------
-DB_VERSION=`psql -qtc 'show server_version;' | xargs | cut -c1,3`
+#DB_VERSION=`psql -qtc 'show server_version;' | xargs | cut -c1,3`
+DB_VERSION=81
 if [ -z "$DB_VERSION" ] || [ `echo $DB_VERSION | grep -c '[^0-9]'` != 0 ]; then
   cat <<EOM
 ********************************************************************************
