008d41b71338455c389df831c6cb175233588dde
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-11-13 22:30:06 +0000

    In the interest of moving into the TT hierarchy, I started a config interface for billing types. It's part of conify, but it's living where the rest of conify will (presumably) be migrated for 2.0.
    Clearly, we need to have some discussion of how we want to lay out the dojo menu.
    
    Also, assuming trunk/2.0 will use dojo 1.2, using new-style dojo grid
    
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11177 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/js/ui/default/conify/global/config/billing_type.js
@@ -0,0 +1,28 @@
+dojo.require('dojox.grid.DataGrid');
+dojo.require('dojo.data.ItemFileReadStore');
+dojo.require('fieldmapper.OrgUtils');
+
+var btContextOrg;
+var btList;
+
+function buildBTGrid() {
+    if(btContextOrg == null)
+       btContextOrg = openils.User.user.ws_ou();
+    fieldmapper.standardRequest(
+        ['open-ils.circ', 'open-ils.circ.billing_type.ranged.retrieve.all'],
+        {   async: true,
+            params: [openils.User.authtoken, btContextOrg, fieldmapper.aou.findOrgDepth(btContextOrg)],
+            oncomplete: function(r) {
+                if(btList = openils.Util.readResponse(r)) {
+                    var store = new dojo.data.ItemFileReadStore({data:cbt.toStoreData(btList)});
+                    btGrid.setStore(store);
+                    btGrid.render();
+                }
+            }
+        }
+    );
+}
+
+openils.Util.addOnLoad(buildBTGrid);
+
+
