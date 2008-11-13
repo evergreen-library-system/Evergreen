91c9ca1a73709c67289d51bb1bf65d85267648fe
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-11-13 19:59:23 +0000

    slight cleanup and re-org
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11168 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/base.js
+++ Open-ILS/web/js/ui/base.js
@@ -1,22 +1,28 @@
 dojo.require('dijit.Dialog');
+dojo.require('dojo.cookie');
 dojo.require('fieldmapper.dojoData');
 dojo.require('openils.User');
-dojo.require('dojo.cookie');
 dojo.require('openils.CGI');
 dojo.require('openils.Event');
+dojo.require('openils.Util');
 
 function oilsSetupUser() {
     var authtoken = new openils.CGI().param('ses') || dojo.cookie('ses');
+    var workstation = dojo.cookie('oils.ws');
     var user;
     if(authtoken) user = new openils.User({authtoken:authtoken});
     if(!authtoken || openils.Event.parse(user.user)) {
-        dojo.cookie('ses', openils.User.authtoken, {expires:-1, path:'/'});
+        dojo.cookie('ses', openils.User.authtoken, {expires:-1, path:'/'}); // remove the cookie
         openils.User.authtoken = null;
-        dojo.addOnLoad(function(){oilsLoginDialog.show();});
+        dojo.addOnLoad(function(){
+            oilsLoginDialog.show();
+            dojo.byId('oils-login-workstation').innerHTML = workstation || '';
+        });
         return;
     }
     dojo.cookie('ses', authtoken, {path : oilsCookieBase});
     openils.User.authtoken = authtoken;
+    openils.User.workstation = dojo.cookie('oils.ws');
 }
 
 function oilsDoLogin() {
@@ -27,7 +33,8 @@ function oilsDoLogin() {
         type: 'staff' // hardcode for now
     });
     dojo.cookie('ses', user.authtoken, {path : oilsCookieBase});
-    return true;
+    location.href = location.href;
+    return false;
 }
 
 oilsSetupUser();
