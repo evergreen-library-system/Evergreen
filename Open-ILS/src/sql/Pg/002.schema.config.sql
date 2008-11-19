68c096ced1f4875b53a41f542e5db4a1b8852c85
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-11-19 20:34:40 +0000

    adding config.standing_penalty, and links to that from actor.usr_standing_penalty
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11270 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -111,6 +111,18 @@ COMMENT ON TABLE config.standing IS $$
  */
 $$;
 
+CREATE TABLE config.standing_penalty (
+	id		SERIAL	PRIMARY KEY,
+	name		TEXT	NOT NULL UNIQUE,
+	label		TEXT	NOT NULL,
+	block_list	TEXT
+);
+INSERT INTO config.standing_penalty (id,name,label,block_list)
+	VALUES (1,'PATRON_EXCEEDS_FINES','Patron exceeds fine threshold','CIRC|HOLD|RENEW');
+INSERT INTO config.standing_penalty (id,name,label,block_list)
+	VALUES (2,'PATRON_EXCEEDS_OVERDUE_COUNT','Patron exceeds max overdue item threshold','CIRC|HOLD|RENEW');
+SELECT SETVAL('config.standing_penalty_id_seq', 100);
+
 CREATE TABLE config.xml_transform (
 	name		TEXT	PRIMARY KEY,
 	namespace_uri	TEXT	NOT NULL,
