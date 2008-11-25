57671ab276ee5fcd26d7ed7537d3272cd71717e0
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-11-25 15:51:37 +0000

    created general perm group object and using that in the perm filtering select
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11341 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/dojo/openils/widget/PermGrpFilteringSelect.js
+++ Open-ILS/web/js/dojo/openils/widget/PermGrpFilteringSelect.js
@@ -1,48 +1,21 @@
 if(!dojo._hasResource["openils.widget.PermGrpFilteringSelect"]){
-    dojo._hasResource["openils.widget.PermGrpFilteringSelect"] = true;
     dojo.provide("openils.widget.PermGrpFilteringSelect");
     dojo.require("dijit.form.FilteringSelect");
     dojo.require('dojo.data.ItemFileReadStore');
     dojo.require('openils.Util');
+    dojo.require('openils.PermGrp');
 
     dojo.declare(
         "openils.widget.PermGrpFilteringSelect", [dijit.form.FilteringSelect], 
         {
-            fetchGroups : function(onload) {
-
-                if(this.groupTree) 
-                    return onload();
-                var self = this;
-
-                fieldmapper.standardRequest(
-                    ['open-ils.actor', 'open-ils.actor.groups.tree.retrieve'],
-                    {   async: true,
-                        oncomplete: function(r) {
-                            self.groupTree = openils.Util.readResponse(r);
-                            onload();
-                        }
-                    }
-                );
-            },
-
-            flatten : function(node) {
-                if(!node) {
-                    node = this.groupTree;
-                    this.groupMap = {};
-                }
-                this.groupMap[node.id()] = node;
-                for(var idx in node.children())
-                    this.flatten(node.children()[idx]);
-            },
-            
             drawGroups : function() {
                 var self = this;
-                this.fetchGroups(function(){self._drawGroups()});
+                openils.PermGrp.fetchGroupTree(function(){self._drawGroups()});
             },
 
             _drawGroups : function(node, depth, list) {
                 if(!node) { 
-                    node = this.groupTree; 
+                    node = openils.PermGrp.groupTree;
                     list = []; 
                     depth = 0; 
                 }
