684b762e50d0f8601e8c3c44a3827f4dd1b5aac6
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-12-01 23:42:30 +0000

    added create functionality
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11376 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/financial/list_currency_types.js
+++ Open-ILS/web/js/ui/default/acq/financial/list_currency_types.js
@@ -21,5 +21,22 @@ function loadCTypesGrid() {
     );
 }
 
+function createCT(args) {
+    if(!(args.code && args.label)) return;
+    var ct = new acqct();
+    ct.code(args.code);
+    ct.label(args.label);
+    fieldmapper.standardRequest(
+        ['open-ils.permacrud', 'open-ils.permacrud.create.acqct'],
+        {   async: true,
+            params: [openils.User.authtoken, ct],
+            oncomplete: function(r) {
+                if(new String(openils.Util.readResponse(r)) != '0')
+                    loadCTypesGrid();
+            }
+        }
+    );
+}
+
 
 openils.Util.addOnLoad(loadCTypesGrid);
