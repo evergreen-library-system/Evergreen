80de48fa9f80ebc9d21dc07f12621bce5138c4dd
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-12-04 21:11:24 +0000

    typo in reporter view
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_2_3@11409 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/extend-reporter.sql
+++ Open-ILS/src/sql/Pg/extend-reporter.sql
@@ -29,7 +29,7 @@ CREATE VIEW extend_reporter.full_circ_count AS
  SELECT cp.id, COALESCE(sum(c.circ_count), 0::bigint) + COALESCE(count(circ.id), 0::bigint) AS circ_count
    FROM asset."copy" cp
    LEFT JOIN extend_reporter.legacy_circ_count c USING (id)
-   LEFT JOIN "action".circulation circ ON circ.target_copy = c.id
+   LEFT JOIN "action".circulation circ ON circ.target_copy = cp.id
   GROUP BY cp.id;
 
 COMMIT;
