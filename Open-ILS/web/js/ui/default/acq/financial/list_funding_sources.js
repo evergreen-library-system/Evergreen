2ef7a75f092374550c97211996f11f9a2e3d0b67
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-12-09 23:07:48 +0000

    moved to 1.2-style dojo grid
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11483 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/financial/list_funding_sources.js
+++ Open-ILS/web/js/ui/default/acq/financial/list_funding_sources.js
@@ -4,30 +4,30 @@ dojo.require('openils.acq.FundingSource');
 dojo.require('openils.acq.CurrencyType');
 dojo.require('openils.widget.OrgUnitFilteringSelect');
 dojo.require('dijit.form.Button');
-dojo.require('dojox.grid.Grid');
+dojo.require('dojo.data.ItemFileReadStore');
+dojo.require('dojox.grid.DataGrid');
 dojo.require('openils.Event');
 dojo.require('openils.Util');
 
-function getOrgInfo(rowIndex) {
-    data = fundingSourceListGrid.model.getRow(rowIndex);
-    if(!data) return;
-    return fieldmapper.aou.findOrgUnit(data.owner).shortname();
+function getOrgInfo(rowIndex, item) {
+    if(!item) return ''; 
+    var owner = this.grid.store.getValue(item, 'owner'); 
+    return fieldmapper.aou.findOrgUnit(owner).shortname();
+
 }
 
-function getBalanceInfo(rowIndex) {
-    data = fundingSourceListGrid.model.getRow(rowIndex);
-    if(!data) return;
-    return new String(openils.acq.FundingSource.cache[data.id].summary().balance);
+function getBalanceInfo(rowIndex, item) {
+    if(!item) return '';
+    var data = this.grid.store.getValue( item, 'id');   
+    return new String(openils.acq.FundingSource.cache[data].summary().balance);
 }
 
 function loadFSGrid() {
     openils.acq.FundingSource.createStore(
         function(storeData) {
             var store = new dojo.data.ItemFileReadStore({data:storeData});
-            var model = new dojox.grid.data.DojoData(null, store, 
-                {rowsPerPage: 20, clientSort: true, query:{id:'*'}});
-            fundingSourceListGrid.setModel(model);
-            fundingSourceListGrid.update();
+            fundingSourceListGrid.setStore(store);
+            fundingSourceListGrid.render();
         }
     );
 }
