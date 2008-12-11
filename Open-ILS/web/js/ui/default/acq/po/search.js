8450d947ef306d7e676aac4790fab9f7212117a5
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-12-11 15:51:51 +0000

    move to markup grid
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11508 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/po/search.js
+++ Open-ILS/web/js/ui/default/acq/po/search.js
@@ -2,7 +2,7 @@ dojo.require('dijit.form.Form');
 dojo.require('dijit.form.Button');
 dojo.require('dijit.form.FilteringSelect');
 dojo.require('dijit.form.NumberTextBox');
-dojo.require('dojox.grid.Grid');
+dojo.require('dojox.grid.DataGrid');
 dojo.require('openils.acq.Provider');
 dojo.require('fieldmapper.OrgUtils');
 dojo.require('dojo.date.locale');
@@ -11,28 +11,29 @@ dojo.require('openils.User');
 dojo.require('openils.Util');
 dojo.require('openils.widget.OrgUnitFilteringSelect');
 
-function getOrgInfo(rowIndex) {
-    data = poGrid.model.getRow(rowIndex);
-    if(!data) return;
-    return fieldmapper.aou.findOrgUnit(data.owner).shortname();
+
+function getOrgInfo(rowIndex, item) {
+    if(!item) return '';
+    var data = this.grid.store.getValue(item, 'ordering_agency')
+    return fieldmapper.aou.findOrgUnit(data).shortname();
 }
 
-function getProvider(rowIndex) {
-    data = poGrid.model.getRow(rowIndex);
-    if(!data) return;
-    return openils.acq.Provider.retrieve(data.provider).name();
+function getProvider(rowIndex, item) {
+    if(!item) return '';
+    var data = this.grid.store.getValue(item, 'provider');
+    return openils.acq.Provider.retrieve(data).code();
 }
 
-function getPOOwner(rowIndex) {
-    data = poGrid.model.getRow(rowIndex);
-    if(!data) return;
-    return new openils.User({id:data.owner}).user.usrname();
+function getPOOwner(rowIndex, item) {
+    if(!item) return '';
+    var data = this.grid.store.getValue(item, 'owner');
+    return new openils.User({id:data}).user.usrname();
 }
 
-function getDateTimeField(rowIndex) {
-    data = poGrid.model.getRow(rowIndex);
-    if(!data) return;
-    var date = dojo.date.stamp.fromISOString(data[this.field]);
+function getDateTimeField(rowIndex, item) {
+    if(!item) return '';
+    var data = this.grid.store.getValue(item, this.field);
+    var date = dojo.date.stamp.fromISOString(data);
     return dojo.date.locale.format(date, {formatLength:'medium'});
 }
 
@@ -55,10 +56,8 @@ function doSearch(fields) {
             oncomplete : function(r) {
                 dojo.style('po-grid', 'visibility', 'visible');
                 var store = new dojo.data.ItemFileReadStore({data:acqpo.toStoreData(itemList)});
-                var model = new dojox.grid.data.DojoData(null, store,
-                    {rowsPerPage: 20, clientSort: true, query:{id:'*'}});
-                poGrid.setModel(model);
-                poGrid.update();
+                poGrid.setStore(store);
+                poGrid.render();
             },
         }
     );
