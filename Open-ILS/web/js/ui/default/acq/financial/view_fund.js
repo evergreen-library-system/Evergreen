5fe08d37b46817ef88f4306b1051b5e9d18d5abd
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2008-12-11 16:12:54 +0000

    move to 1.2 markup grid
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11523 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/financial/view_fund.js
+++ Open-ILS/web/js/ui/default/acq/financial/view_fund.js
@@ -2,8 +2,9 @@ dojo.require("dijit.Dialog");
 dojo.require('dijit.form.FilteringSelect');
 dojo.require('dijit.layout.TabContainer');
 dojo.require('dijit.layout.ContentPane');
-dojo.require('dojox.grid.Grid');
-
+dojo.require('dojox.grid.DataGrid');
+dojo.require('dijit.form.CurrencyTextBox');
+dojo.require('dojo.data.ItemFileReadStore');
 dojo.require("fieldmapper.OrgUtils");
 dojo.require('openils.acq.Fund');
 dojo.require('openils.acq.FundingSource');
@@ -13,7 +14,8 @@ dojo.require('openils.Util');
 
 var fund = null;
 
-function getSummaryInfo(rowIndex) {
+function getSummaryInfo(rowIndex, item) {
+    if(!item) return'';
     return new String(fund.summary()[this.field]);
 }
 
@@ -21,48 +23,42 @@ function createAllocation(fields) {
     fields.fund = fundID;
     if(isNaN(fields.percent)) fields.percent = null;
     if(isNaN(fields.amount)) fields.amount = null;
-    //openils.acq.Fund.createAllocation(fields, resetPage);
     openils.acq.Fund.createAllocation(fields, 
         function(r){location.href = location.href;});
 }
+function getOrgInfo(rowIndex, item) {
+    if(!item) return ''; 
+    var owner = this.grid.store.getValue(item, 'org'); 
+    return fieldmapper.aou.findOrgUnit(owner).shortname();
 
-function getOrgInfo(rowIndex) {
-    data = fundGrid.model.getRow(rowIndex);
-    if(!data) return;
-    return fieldmapper.aou.findOrgUnit(data.org).shortname();
 }
 
-function getXferDest(rowIndex) {
-    data = this.grid.model.getRow(rowIndex);
-    if(!(data && data.xfer_destination)) return '';
-    return data.xfer_destination;
+function getXferDest(rowIndex, item) {
+    if(!item) return '';
+    var xfer_destination = this.grid.store.getValue(item, 'xfer_destination');
+    if(!(item && xfer_destination)) return '';
+    return xfer_destination;
 }
 
 function loadFundGrid() {
     var store = new dojo.data.ItemFileReadStore({data:acqf.toStoreData([fund])});
-    var model = new dojox.grid.data.DojoData(
-        null, store, {rowsPerPage: 20, clientSort: true, query:{id:'*'}});
-    fundGrid.setModel(model);
-    fundGrid.update();
+    fundGrid.setStore(store);
+    fundGrid.render();
 }
 
 function loadAllocationGrid() {
     if(fundAllocationGrid.isLoaded) return;
     var store = new dojo.data.ItemFileReadStore({data:acqfa.toStoreData(fund.allocations())});
-    var model = new dojox.grid.data.DojoData(
-        null, store, {rowsPerPage: 20, clientSort: true, query:{id:'*'}});
-    fundAllocationGrid.setModel(model);
-    fundAllocationGrid.update();
+    fundAllocationGrid.setStore(store);
+    fundAllocationGrid.render();
     fundAllocationGrid.isLoaded = true;
 }
 
 function loadDebitGrid() {
     if(fundDebitGrid.isLoaded) return;
     var store = new dojo.data.ItemFileReadStore({data:acqfa.toStoreData(fund.debits())});
-    var model = new dojox.grid.data.DojoData(
-        null, store, {rowsPerPage: 20, clientSort: true, query:{id:'*'}});
-    fundDebitGrid.setModel(model);
-    fundDebitGrid.update();
+    fundDebitGrid.setStore(store);
+    fundDebitGrid.render();
     fundDebitGrid.isLoaded = true;
 }
 
