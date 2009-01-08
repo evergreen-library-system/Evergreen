991eb909ac6e2bd1139dd5bef078a2a1f4a7014e
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-01-08 06:05:47 +0000

    Add compilation guard; prepare for #inclusion in C++.
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11766 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/include/openils/oils_constants.h
+++ Open-ILS/include/openils/oils_constants.h
@@ -1,4 +1,9 @@
+#ifndef OILS_CONSTANTS_H
+#define OILS_CONSTANTS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Settings ------------------------------------------------------ */
 #define OILS_ORG_SETTING_OPAC_TIMEOUT "auth.opac_timeout"
@@ -12,3 +17,8 @@
 #define OILS_EVENT_PERM_FAILURE "PERM_FAILURE"
 #define OILS_EVENT_NO_SESSION "NO_SESSION"
 
+#ifdef __cplusplus
+}
+#endif
+
+#endif
