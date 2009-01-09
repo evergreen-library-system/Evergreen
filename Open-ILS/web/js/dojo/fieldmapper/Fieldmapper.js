49dfeff60f01138272e6887208c02dd6d84f7c4f
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-01-09 14:37:01 +0000

    thinko ... reverse the array_position test
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@11781 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/dojo/fieldmapper/Fieldmapper.js
+++ Open-ILS/web/js/dojo/fieldmapper/Fieldmapper.js
@@ -150,7 +150,7 @@ if(!dojo._hasResource["fieldmapper.Fieldmapper"]){
                     for (var f in fieldmapper.IDL.fmclasses[this.classname].fields) {
                         var field = fieldmapper.IDL.fmclasses[this.classname].fields[f];
                         var p = field.array_position;
-                        if (p > 2) continue;
+                        if (p <= 2) continue;
 
         				this._fields.push( field.name );
     					this[field.name]=new Function('n', 'if(arguments.length==1)this.a['+p+']=n;return this.a['+p+'];');
