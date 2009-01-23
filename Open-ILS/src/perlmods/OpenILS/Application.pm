5dc7c29e84b0eeb6c3bf9fd0279d68c3e9cdd006
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-01-23 15:35:56 +0000

    version bump and (minimal) db upgrade script for 1.4.0.1
    git-svn-id: svn://svn.open-ils.org/ILS/tags/rel_1_4_0_1@11929 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application.pm
+++ Open-ILS/src/perlmods/OpenILS/Application.pm
@@ -5,7 +5,7 @@ use base qw/OpenSRF::Application/;
 
 sub ils_version {
     # version format is "x-y-z-p", for example "1-2-1-0" for Evergreen 1.2.1.0
-    return "1-4-0";
+    return "1-4-0-1";
 }
 
 __PACKAGE__->register_method(
