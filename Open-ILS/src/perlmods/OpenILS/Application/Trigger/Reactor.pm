ee9df4e2ee3fcf342614e9a33d197ffcecc683ac
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-02-25 03:36:08 +0000

    basic TT processor
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12294 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Trigger/Reactor.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Trigger/Reactor.pm
@@ -1,5 +1,25 @@
 package OpenILS::Application::Trigger::Reactor;
+use Template;
+use OpenSRF::Utils::Logger qw(:logger);
+
 sub fourty_two { return 42 }
 sub NOOP_True { return 1 }
 sub NOOP_False { return 0 }
+
+
+# processes templates.  Returns template output on success, undef on error
+sub run_TT {
+    my $self = shift;
+    my $env = shift;
+    return '' unless $env->{template};
+
+    my $output = '';
+    my $tt = Template->new;
+
+    $tt->process($env->{template}, $env, \$output) or 
+        $logger->error("Error processing Trigger template: " . $tt->error);
+
+    return $output;
+}
+
 1;
