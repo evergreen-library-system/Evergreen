bf935d973141e7f6f464683f080b286115b3c127
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-03 02:39:34 +0000

    provide a cstore object
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12367 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Ingest.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Ingest.pm
@@ -400,7 +400,10 @@ sub ro_biblio_ingest_single_object {
 	my $bib = shift;
 	my $xml = OpenILS::Application::Ingest::entityize($bib->marc);
 
+	my $cstore = OpenSRF::AppSession->connect( 'open-ils.cstore' );
 	my $cn = $cstore->request( 'open-ils.cstore.direct.asset.call_number.search' => { id => { '!=' => undef }, { limit => 1 } } )->gather(1);
+    $cstore->disconnect;
+
     my $max_cn = int($cn->id) + 1000;
 
 	my $document = $parser->parse_string($xml);
