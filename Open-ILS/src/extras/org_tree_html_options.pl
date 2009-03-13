af5bf36656e3d61edd1b1eb81a3f35232c7cf5aa
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-13 01:42:15 +0000

    Backport r12490 from trunk: Enable slimpac and dynamic OPAC to have localized org trees
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_4_0@12504 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/extras/org_tree_html_options.pl
+++ Open-ILS/src/extras/org_tree_html_options.pl
@@ -1,56 +1,64 @@
 #!/usr/bin/perl
-# turns the orgTree and orgTypes into a static HTML option list
+# for each supported locale, turn the orgTree and orgTypes into a static HTML option list
 
 use OpenSRF::AppSession;
 use OpenSRF::System;
 use OpenILS::Utils::Fieldmapper;
 use OpenSRF::Utils::SettingsClient;
+use OpenILS::Application::AppUtils;
 use Unicode::Normalize;
 use Data::Dumper;
+use File::Spec;
 
-die "usage: perl org_tree_html_options.pl <bootstrap_config> <output_file>" unless $ARGV[1];
+die "usage: perl org_tree_html_options.pl <bootstrap_config> <output_path> <output_file>" unless $ARGV[2];
 OpenSRF::System->bootstrap_client(config_file => $ARGV[0]);
 
-open FILE, ">$ARGV[1]";
+my $path = $ARGV[1];
+my $filename = $ARGV[2];
+
+my @types;
 
 Fieldmapper->import(IDL => OpenSRF::Utils::SettingsClient->new->config_value("IDL"));
 
-my $ses = OpenSRF::AppSession->create("open-ils.actor");
-my $tree = $ses->request("open-ils.actor.org_tree.retrieve")->gather(1);
+#Get our list of locales
+my $session = OpenSRF::AppSession->create("open-ils.cstore");
+my $locales = $session->request("open-ils.cstore.direct.config.i18n_locale.search.atomic", {"code" => {"!=" => undef}}, {"order_by" => {"i18n_l" => "name"}})->gather();
+$session->disconnect();
 
-my @types;
-my $aout = $ses->request("open-ils.actor.org_types.retrieve")->gather(1);
-foreach my $type (@$aout) {
-	$types[int($type->id)] = $type;
+foreach my $locale (@$locales) {
+	my $ses = OpenSRF::AppSession->create("open-ils.actor");
+	$ses->session_locale($locale->code);
+	my $tree = $ses->request("open-ils.actor.org_tree.retrieve")->gather(1);
+
+	my $aout = $ses->request("open-ils.actor.org_types.retrieve")->gather(1);
+	foreach my $type (@$aout) {
+		$types[int($type->id)] = $type;
+	}
+	my $dir = File::Spec->catdir($path, $locale->code);
+	if (!-d $dir) {
+		mkdir($dir) or die "Could not create output directory: $dir $!\n";
+	}
+
+	my @org_tree_html;
+	print_option($tree, \@org_tree_html);
+	$ses->disconnect();
+	open(FILE, '>', File::Spec->catfile($dir, $filename)) or die $!;
+	print FILE @org_tree_html;
+	close FILE;
 }
-
-print_option($tree);
-
-$ses->disconnect();
-close FILE;
-
-
 
 sub print_option {
 	my $node = shift;
+	my $org_tree_html = shift;
+
 	return unless ($node->opac_visible =~ /^[y1t]+/i);
 
 	my $depth = $types[$node->ou_type]->depth;
-	my $sname = entityize($node->shortname);
-	my $name = entityize($node->name);
+	my $sname = OpenILS::Application::AppUtils->entityize($node->shortname);
+	my $name = OpenILS::Application::AppUtils->entityize($node->name);
 	my $kids = $node->children;
 
-	print FILE "<option value='$sname'>" . '&#160;&#160;&#160;'x$depth . "$name</option>\n";
-	print_option($_) for (@$kids);
-}
-
-sub entityize {
-        my $stuff = shift || return "";
-        $stuff =~ s/\</&lt;/og;
-        $stuff =~ s/\>/&gt;/og;
-        $stuff =~ s/\&/&amp;/og;
-        $stuff = NFD($stuff);
-        $stuff =~ s/([\x{0080}-\x{fffd}])/sprintf('&#x%X;',ord($1))/sgoe;
-        return $stuff;
+	push @$org_tree_html, "<option value='$sname'>" . '&#160;&#160;&#160;'x$depth . "$name</option>\n";
+	print_option($_, $org_tree_html) for (@$kids);
 }
 
