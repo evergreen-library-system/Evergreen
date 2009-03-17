5ccaf598233059434f9083aeaeb4c8a4d59592ca
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-17 15:32:12 +0000

    plug in li table to picklist display
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12560 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/js/ui/default/acq/picklist/view.js
@@ -0,0 +1,61 @@
+dojo.require('dojo.date.stamp');
+dojo.require('dojo.date.locale');
+dojo.require('openils.User');
+dojo.require('openils.Util');
+dojo.require('dijit.layout.ContentPane');
+
+var plist;
+var plOffset = 0;
+var plLimit = 20;
+var liTable;
+
+
+function load() {
+    liTable = new AcqLiTable();
+    fieldmapper.standardRequest(
+        ['open-ils.acq', 'open-ils.acq.picklist.retrieve'],
+        {   async: true,
+            params: [openils.User.authtoken, plId, 
+                {flesh_lineitem_count:true, flesh_owner:true}],
+            oncomplete: function(r) {
+                plist = openils.Util.readResponse(r);
+                drawPl(plist);
+            }
+        }
+    );
+
+}
+
+function drawPl() {
+
+    dojo.byId("oils-acq-picklist-name").innerHTML = plist.name();
+    dojo.byId("oils-acq-picklist-attr-owner").innerHTML = plist.owner().usrname();
+
+    dojo.byId("oils-acq-picklist-attr-cdate").innerHTML =
+         dojo.date.locale.format(
+            dojo.date.stamp.fromISOString(plist.create_time()), 
+            {selector:'date'}
+        );
+
+    dojo.byId("oils-acq-picklist-attr-edate").innerHTML = 
+         dojo.date.locale.format(
+            dojo.date.stamp.fromISOString(plist.edit_time()), 
+            {selector:'date'}
+        );
+
+    fieldmapper.standardRequest(
+        ['open-ils.acq', 'open-ils.acq.lineitem.picklist.retrieve'],
+        {   async: true,
+            params: [openils.User.authtoken, plId, 
+                {flesh_attrs:true, clear_marc:true, offset:plOffset, limit:plLimit}],
+            onresponse: function(r) {
+                liTable.showTable();
+                liTable.addLineitem(openils.Util.readResponse(r));
+            }
+        }
+    );
+}
+
+openils.Util.addOnLoad(load);
+
+
