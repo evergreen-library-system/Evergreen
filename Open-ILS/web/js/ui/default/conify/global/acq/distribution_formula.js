3a51885efbf1bf90d8506bde3a28da19d8ef4877
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-25 19:09:58 +0000

    added distribution formula admin UI
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12670 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/js/ui/default/conify/global/acq/distribution_formula.js
@@ -0,0 +1,68 @@
+dojo.require('openils.widget.AutoGrid');
+dojo.require('dijit.form.FilteringSelect');
+dojo.require('openils.PermaCrud');
+var formula;
+var formCache = [];
+function draw() {
+    if(formulaId) {
+        openils.Util.hide('formula-list-div');
+        drawFormulaSummary();
+    } else {
+        openils.Util.hide('formula-entry-div');
+        fListGrid.onPostCreate = function(fmObject) {
+            location.href = location.href + '/' + fmObject.id();
+        }
+ fieldmapper.standardRequest(
+                ['open-ils.acq', 'open-ils.acq.distribution_formula.ranged.retrieve'],
+                {   async: true,
+                    params: [openils.User.authtoken],
+                    onresponse: function (r) { 
+                        var form = openils.Util.readResponse(r);
+                        formCache[form.id()] = form;
+                        fListGrid.store.newItem(form.toStoreItem());
+                        }
+                    }
+                             );
+     
+    }
+}
+openils.Util.addOnLoad(draw);
+
+/*function getFormulaId (rowIndex, item) {
+    if(!item) return '';
+    var pcrud = new openils.PermaCrud;
+    var formulaName = pcrud.retrieve('acqdf', formulaId);
+    return formulaName.name();
+    
+
+}
+*/
+function drawFormulaSummary() {
+    openils.Util.show('formula-entry-div');
+    dfeListGrid.overrideEditWidgets.formula = new
+        dijit.form.TextBox({style:'display:none', value: formulaId});
+    dfeListGrid.loadAll({order_by:{acqdfe : 'formula'}}, {formula : formulaId});
+    var pcrud = new openils.PermaCrud;
+    var formulaName = pcrud.retrieve('acqdf', formulaId);
+    dojo.byId('formula_head').innerHTML = formulaName.name();
+}
+
+function getItemCount(rowIndex, item) {
+    if(!item) return '';
+    var form = formCache[this.grid.store.getValue(item, "id")];
+    var count = 0;
+    dojo.forEach(form.entries(),
+                 function(e) {
+                     count = count + e.item_count();
+                 }
+                 );
+    return count;
+}
+
+function getFormulaName(rowIndex, item) {
+    if(!item) return '';
+    return '<a href="' + location.href + '/' +
+        this.grid.store.getValue(item, 'id') + '">' +
+        this.grid.store.getValue(item, 'name') + '</a>';
+}
+
