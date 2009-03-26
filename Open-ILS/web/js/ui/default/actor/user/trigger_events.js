6a2ef1093e8065e70827879182def55aecdf3d5c
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-26 16:10:40 +0000

    initial interface for managing pending notification events
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12688 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/js/ui/default/actor/user/trigger_events.js
@@ -0,0 +1,65 @@
+dojo.require('dojox.grid.DataGrid');
+dojo.require('dojo.data.ItemFileWriteStore');
+dojo.require('openils.Util');
+dojo.require('openils.User');
+
+var evtCache = {};
+
+function init() {
+    var store = new dojo.data.ItemFileWriteStore({data:acqf.initStoreData()});
+    evtGrid.setStore(store);
+    evtGrid.render();
+
+    function onResponse(r) {
+        var evt = openils.Util.readResponse(r);
+        evtCache[evt.id()] = evt;
+        evtGrid.store.newItem(evt.toStoreItem()); 
+    }
+
+    fieldmapper.standardRequest(
+        ['open-ils.actor', 'open-ils.actor.user.events.circ'],
+        {   async: true,
+            params: [openils.User.authtoken, patronId],
+            onresponse : onResponse
+        }
+    );
+
+    fieldmapper.standardRequest(
+        ['open-ils.actor', 'open-ils.actor.user.events.ahr'],
+        {   async: true,
+            params: [openils.User.authtoken, patronId],
+            onresponse : onResponse
+        }
+    );
+}
+
+function getField(rowIdx, item) {
+    if(!item) return '';
+    var evt = evtCache[this.grid.store.getValue(item, 'id')];
+
+    switch(this.field) {
+        case 'reactor':
+            return evt.event_def().reactor().module();
+        case 'validator':
+            return evt.event_def().validator().module();
+        case 'hook':
+            return evt.event_def().hook();
+        case 'target':
+            switch(evt.target().classname) {
+                case 'circ':
+                    return evt.target().target_copy().barcode();
+                case 'ahr':
+                    if(evt.target().currrent_copy())
+                        return evt.target().currrent_copy().barcode();
+            }
+            
+    }
+
+    return this.grid.store.getValue(item, this.field) || '';
+}
+
+function evtCancelSelected() {
+}
+
+openils.Util.addOnLoad(init);
+
