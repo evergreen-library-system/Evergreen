f41efe49556eda1f7f231a3a60c9b35c3efa1690
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-27 19:59:57 +0000

    plugged in intial lineitem table implementation
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12706 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/po/view_po.js
+++ Open-ILS/web/js/ui/default/acq/po/view_po.js
@@ -1,81 +1,36 @@
-dojo.require("dijit.Dialog");
-dojo.require('dijit.form.FilteringSelect');
-dojo.require('dijit.layout.TabContainer');
 dojo.require('dijit.layout.ContentPane');
-dojo.require('dojox.grid.DataGrid');
-dojo.require('dojo.data.ItemFileReadStore');
-dojo.require('openils.acq.PO');
-dojo.require('openils.Event');
 dojo.require('openils.User');
 dojo.require('openils.Util');
-dojo.require('fieldmapper.OrgUtils');
-dojo.require('openils.acq.Provider');
-dojo.require('openils.acq.Lineitem');
-dojo.require('dojo.date.locale');
-dojo.require('dojo.date.stamp');
 
 var PO = null;
-var lineitems = [];
+var liTable;
 
-function getOrgInfo(rowIndex, item) {
-    if(!item) return '';
-    var data = this.grid.store.getValue(item , 'ordering_agency')
-    return fieldmapper.aou.findOrgUnit(data).shortname();
-}
+function init() {
+    liTable = new AcqLiTable();
+    liTable.reset();
+    liTable.isPO = true;
 
-function getProvider(rowIndex, item) {
-    if(!item) return '';
-    var data = this.grid.store.getValue(item, 'provider');
-    return openils.acq.Provider.retrieve(data).code();
-}
+    fieldmapper.standardRequest(
+        ['open-ils.acq', 'open-ils.acq.purchase_order.retrieve'],
+        {   async: true,
+            params: [openils.User.authtoken, poId],
+            oncomplete: function(r) {
+                PO = openils.Util.readResponse(r);
+                console.log('got PO');
+            }
+        }
+    );
 
-function getPOOwner(rowIndex, item) {
-    if(!item) return '';
-    var data = this.grid.store.getValue(item, 'owner');
-    return new openils.User({id:data}).user.usrname();
-}
-
-function getDateTimeField(rowIndex, item) {
-    if(!item) return '';
-    var data = this.grid.store.getValue(item, this.field);
-    var date = dojo.date.stamp.fromISOString(data);
-    return dojo.date.locale.format(date, {formatLength:'medium'});
-}
-
-function loadPOGrid() {
-    if(!PO) return '';
-    var store = new dojo.data.ItemFileReadStore({data:acqpo.toStoreData([PO])});
-    poGrid.setStore(store);
-    poGrid.render();
-}
-
-function loadLIGrid() {
-    if(liGrid.isLoaded) return;
-
-    function load(po) {
-        lineitems = po.lineitems();
-        var store = new dojo.data.ItemFileReadStore({data:jub.toStoreData(lineitems)});
-        var model = new dojox.grid.data.DojoData(
-            null, store, {rowsPerPage: 20, clientSort: true, query:{id:'*'}}); 
-        JUBGrid.populate(liGrid, model, lineitems)
-    }
-
-    openils.acq.PO.retrieve(poId, load, {flesh_lineitems:1, clear_marc:1});
-    liGrid.isLoaded = true;
-}
-
-function loadPage() {
-    fetchPO();
-}
-
-function fetchPO() {
-    openils.acq.PO.retrieve(poId, 
-        function(po) {
-            PO = po;
-            loadPOGrid();
-        },
-        {flesh_lineitem_count:1}
+    fieldmapper.standardRequest(
+        ['open-ils.acq', 'open-ils.acq.lineitem.search'],
+        {   async: true,
+            params: [openils.User.authtoken, {purchase_order:poId}, {flesh_attrs:true}],
+            onresponse: function(r) {
+                liTable.show('list');
+                liTable.addLineitem(openils.Util.readResponse(r));
+            }
+        }
     );
 }
 
-openils.Util.addOnLoad(loadPage);
+openils.Util.addOnLoad(init);
