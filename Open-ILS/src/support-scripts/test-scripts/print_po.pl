ab860e93d7a00f0d748a09eb2e67242267c3bbd2
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-03-31 14:25:59 +0000

    some formatting
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12733 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/support-scripts/test-scripts/print_po.pl
+++ Open-ILS/src/support-scripts/test-scripts/print_po.pl
@@ -37,9 +37,8 @@ die "No PO with id $po_id\n" unless $po;
 print 'PO ID: ' . $po->id . "\n";
 print 'Ordering Agency: ' . $po->ordering_agency->shortname . "\n";
 
-print "\n";
 if(my $addr = $po->ordering_agency->mailing_address) {
-    print "  Mailing Address: \n";
+    print "Mailing Address: \n";
     print '  street1: ' . $addr->street1 . "\n";
     print '  street2: ' . $addr->street2 . "\n";
     print '  city: ' . $addr->city . "\n";
@@ -49,9 +48,8 @@ if(my $addr = $po->ordering_agency->mailing_address) {
     print '  post_code: ' . $addr->post_code . "\n";
 }
 
-print "\n";
 if(my $addr = $po->ordering_agency->billing_address) {
-    print "  Billing Address: \n";
+    print "Billing Address: \n";
     print '  street1: ' . $addr->street1 . "\n";
     print '  street2: ' . $addr->street2 . "\n";
     print '  city: ' . $addr->city . "\n";
@@ -61,9 +59,9 @@ if(my $addr = $po->ordering_agency->billing_address) {
     print '  post_code: ' . $addr->post_code . "\n";
 }
 
-print "\n";
 print 'Provider: ' . $po->provider->code . "\n";
 if(my $addr = $po->provider->addresses->[0]) {
+    print "Provider Address:\n";
     print '  street1: ' . $addr->street1 . "\n";
     print '  street2: ' . $addr->street2 . "\n";
     print '  city: ' . $addr->city . "\n";
@@ -72,17 +70,16 @@ if(my $addr = $po->provider->addresses->[0]) {
     print '  country: ' . $addr->country . "\n";
     print '  post_code: ' . $addr->post_code . "\n";
 }
-print "\n";
 
 for my $li (@{$po->lineitems}) {
 
-    print "  Lineitem:------------------\n";
+    print "Lineitem:------------------\n";
     for my $li_attr (@{$li->attributes}) {
         print "  " . $li_attr->attr_name . ': ' . $li_attr->attr_value . "\n";
     }
 
     for my $li_det (@{$li->lineitem_details}) {
-        print "    Copy----------------------\n";
+        print "  Copy----------------------\n";
         print "    Owning Lib: " . $li_det->owning_lib->shortname . "\n";
         print "    Fund: " . $li_det->fund->code . "\n";
         print "    Location: " . $li_det->location->name . "\n";
