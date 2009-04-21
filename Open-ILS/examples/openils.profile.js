400bdbed9cd9d739ef9dabd746e15c21a367a076
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-04-21 15:13:18 +0000

    adding Dojo build profile (v1) for the OPAC (all thanks go to lmcfarland!)
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@12936 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/examples/openils.profile.js
@@ -0,0 +1,49 @@
+dependencies ={
+    layers: [
+{
+    name: "openils_dojo.js",
+    dependencies: [
+                   "dijit.form.CurrencyTextBox",
+                   "dijit.Menu",
+                   "dijit.form.FilteringSelect",
+                   "dojox.grid.compat._data.dijitEditors",
+                   "dojox.jsonPath",
+                   "dijit.ProgressBar",
+                   "dijit.Toolbar",
+                   "dijit.form.TextBox",
+                   "dojox.grid.DataGrid",
+                   "dojox.fx",
+                   "dijit.layout.BorderContainer",
+                   "dojox.grid.Grid",
+                   "dijit.form.ValidationTextBox",
+                   "dijit.form.ComboBox",
+                   "dijit.layout.LayoutContainer",
+                   "dijit.Tree",
+                   "dijit.form.NumberSpinner",
+                   "dijit.Tooltip",
+                   "dojox.widget.PlaceholderMenuItem",
+                   "dijit.form.Form",
+                   "dijit.form.Textarea",
+                   "dojo.data.ItemFileReadStore",
+                   "dojo.data.ItemFileWriteStore",
+                   "dijit.form.Button",
+                   "dijit.form.DateTextBox",
+                   "dijit.layout.ContentPane",
+                   "dijit.Dialog",
+                   "dojox.widget.Toaster",
+                   "dijit.form.NumberTextBox",
+                   "dijit.layout.SplitContainer",
+                   "dojox.form.CheckedMultiSelect",
+                   "dijit.form.TimeTextBox",
+                   "dijit.form.CheckBox",
+                   "dijit.layout.TabContainer"
+                   ]
+}
+
+             ],
+    prefixes: [
+               [ "dijit", "../dijit" ],
+               [ "dojox", "../dojox" ]
+               ]
+};
+               
