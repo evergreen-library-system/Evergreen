b158370a9c2eb5353451252869e6ff53411de861
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-07-05 19:38:38 +0000

    Merge r13500, r13503 from trunk to fix database i18n script and tests
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6@13504 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- build/i18n/tests/data/sqlsource.sql
+++ build/i18n/tests/data/sqlsource.sql
@@ -1,49 +1,105 @@
 --002.schema.config.sql:
-INSERT INTO config.bib_source (quality, source, transcendant) VALUES 
-    (90, oils_i18n_gettext('oclc'), FALSE),
-    (10, oils_i18n_gettext('System Local'), FALSE),
-    (1, oils_i18n_gettext('Project Gutenberg'), TRUE);
+INSERT INTO config.bib_source (id, quality, source, transcendant) VALUES 
+    (1, 90, oils_i18n_gettext(1, 'oclc', 'cbs', 'source'), FALSE);
+INSERT INTO config.bib_source (id, quality, source, transcendant) VALUES 
+    (2, 10, oils_i18n_gettext(2, 'System Local', 'cbs', 'source'), FALSE);
+INSERT INTO config.bib_source (id, quality, source, transcendant) VALUES 
+    (3, 1, oils_i18n_gettext(3, 'Project Gutenberg', 'cbs', 'source'), TRUE);
 
-INSERT INTO config.standing (value) VALUES (oils_i18n_gettext('Good'));
-INSERT INTO config.standing (value) VALUES (oils_i18n_gettext('Barred'));
+INSERT INTO config.standing (id, value) VALUES (1, oils_i18n_gettext(1, 'Good', 'cst', 'value'));
+INSERT INTO config.standing (id, value) VALUES (2, oils_i18n_gettext(2, 'Barred', 'cst', 'value'));
 
-INSERT INTO config.xml_transform VALUES ( 'marcxml', 'http://www.loc.gov/MARC21/slim', 'marc', '---' );
-INSERT INTO config.xml_transform VALUES ( 'mods', 'http://www.loc.gov/mods/', 'mods', '/home/miker/MARC21slim2MODS.xsl' );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'series', 'seriestitle', 'mods32', $$//mods32:mods/mods32:relatedItem[@type="series"]/mods32:titleInfo$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'title', 'abbreviated', 'mods32', $$//mods32:mods/mods32:titleInfo[mods32:title and (@type='abbreviated')]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'title', 'translated', 'mods32', $$//mods32:mods/mods32:titleInfo[mods32:title and (@type='translated')]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'title', 'alternative', 'mods32', $$//mods32:mods/mods32:titleInfo[mods32:title and (@type='alternative')]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'title', 'uniform', 'mods32', $$//mods32:mods/mods32:titleInfo[mods32:title and (@type='uniform')]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'title', 'proper', 'mods32', $$//mods32:mods/mods32:titleInfo[mods32:title and not (@type)]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'author', 'corporate', 'mods32', $$//mods32:mods/mods32:name[@type='corporate']/mods32:namePart[../mods32:role/mods32:roleTerm[text()='creator']]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'author', 'personal', 'mods32', $$//mods32:mods/mods32:name[@type='personal']/mods32:namePart[../mods32:role/mods32:roleTerm[text()='creator']]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'author', 'conference', 'mods32', $$//mods32:mods/mods32:name[@type='conference']/mods32:namePart[../mods32:role/mods32:roleTerm[text()='creator']]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'author', 'other', 'mods32', $$//mods32:mods/mods32:name[@type='personal']/mods32:namePart[not(../mods32:role)]$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'subject', 'geographic', 'mods32', $$//mods32:mods/mods32:subject/mods32:geographic$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'subject', 'name', 'mods32', $$//mods32:mods/mods32:subject/mods32:name$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'subject', 'temporal', 'mods32', $$//mods32:mods/mods32:subject/mods32:temporal$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'subject', 'topic', 'mods32', $$//mods32:mods/mods32:subject/mods32:topic$$ );
+--INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+--  ( field_class, name, xpath ) VALUES ( 'subject', 'genre', 'mods32', $$//mods32:mods/mods32:genre$$ );
+INSERT INTO config.metabib_field ( field_class, name, format, xpath ) VALUES 
+    ( 'keyword', 'keyword', 'mods32', $$//mods32:mods/*[not(local-name()='originInfo')]$$ ); -- /* to fool vim */;
 
-INSERT INTO config.metabib_field ( field_class, name, xpath ) VALUES 
-    ( 'series', 'seriestitle', $$//mods:mods/mods:relatedItem[@type="series"]/mods:titleInfo$$ ),
-    ( 'title', 'abbreviated', $$//mods:mods/mods:titleInfo[mods:title and (@type='abbreviated')]$$ ),
-    ( 'title', 'translated', $$//mods:mods/mods:titleInfo[mods:title and (@type='translated')]$$ ),
-    ( 'title', 'uniform', $$//mods:mods/mods:titleInfo[mods:title and (@type='uniform')]$$ ),
-    ( 'title', 'proper', $$//mods:mods/mods:titleInfo[mods:title and not (@type)]$$ ),
-    ( 'author', 'corporate', $$//mods:mods/mods:name[@type='corporate']/mods:namePart[../mods:role/mods:text[text()='creator']]$$ ),
-    ( 'author', 'personal', $$//mods:mods/mods:name[@type='personal']/mods:namePart[../mods:role/mods:text[text()='creator']]$$ ),
-    ( 'author', 'conference', $$//mods:mods/mods:name[@type='conference']/mods:namePart[../mods:role/mods:text[text()='creator']]$$ ),
-    ( 'author', 'other', $$//mods:mods/mods:name[@type='personal']/mods:namePart[not(../mods:role)]$$ ),
-    ( 'subject', 'geographic', $$//mods:mods/mods:subject/mods:geographic$$ ),
-    ( 'subject', 'name', $$//mods:mods/mods:subject/mods:name$$ ),
-    ( 'subject', 'temporal', $$//mods:mods/mods:subject/mods:temporal$$ ),
-    ( 'subject', 'topic', $$//mods:mods/mods:subject/mods:topic$$ ),
---  ( field_class, name, xpath ) VALUES ( 'subject', 'genre', $$//mods:mods/mods:genre$$ ),
-    ( 'keyword', 'keyword', $$//mods:mods/*[not(local-name()='originInfo')]$$ ); -- /* to fool vim */
+INSERT INTO config.non_cataloged_type ( id, owning_lib, name ) VALUES ( 1, 1, oils_i18n_gettext(1, 'Paperback Book', 'cnct', 'name') );
+SELECT SETVAL('config.non_cataloged_type_id_seq'::TEXT, 100);
 
-INSERT INTO config.audience_map (code, value, description) VALUES 
-    ('', oils_i18n_gettext('Unknown or unspecified'), oils_i18n_gettext('The target audience for the item not known or not specified.')),
-    ('a', oils_i18n_gettext('Preschool'), oils_i18n_gettext('The item is intended for children, approximate ages 0-5 years.')),
-    ('b', oils_i18n_gettext('Primary'), oils_i18n_gettext('The item is intended for children, approximate ages 6-8 years.')),
-    ('c', oils_i18n_gettext('Pre-adolescent'), oils_i18n_gettext('The item is intended for young people, approximate ages 9-13 years.')),
-    ('d', oils_i18n_gettext('Adolescent'), oils_i18n_gettext('The item is intended for young people, approximate ages 14-17 years.')),
-    ('e', oils_i18n_gettext('Adult'), oils_i18n_gettext('The item is intended for adults.')),
-    ('f', oils_i18n_gettext('Specialized'), oils_i18n_gettext('The item is aimed at a particular audience and the nature of the presentation makes the item of little interest to another audience.')),
-    ('g', oils_i18n_gettext('General'), oils_i18n_gettext('The item is of general interest and not aimed at an audience of a particular intellectual level.')),
-    ('j', oils_i18n_gettext('Juvenile'), oils_i18n_gettext('The item is intended for children and young people, approximate ages 0-15 years.'));
+INSERT INTO config.identification_type ( id, name ) VALUES 
+    ( 1, oils_i18n_gettext(1, 'Drivers License', 'cit', 'name') );
+INSERT INTO config.identification_type ( id, name ) VALUES 
+    ( 2, oils_i18n_gettext(2, 'SSN', 'cit', 'name') );
+INSERT INTO config.identification_type ( id, name ) VALUES 
+    ( 3, oils_i18n_gettext(3, 'Other', 'cit', 'name') );
+SELECT SETVAL('config.identification_type_id_seq'::TEXT, 100);
 
--- Admin user
-INSERT INTO permission.usr_perm_map (usr,perm,depth) VALUES (1,-1,0);
-
---010.schema.biblio.sql:
-INSERT INTO biblio.record_entry VALUES (-1,1,1,1,-1,NOW(),NOW(),FALSE,FALSE,'','AUTOGEN','-1','','FOO');
+INSERT INTO config.rule_circ_duration VALUES 
+    (1, oils_i18n_gettext(1, '7_days_0_renew', 'crcd', 'name'), '7 days', '7 days', '7 days', 0);
+INSERT INTO config.rule_circ_duration VALUES 
+    (2, oils_i18n_gettext(2, '28_days_2_renew', 'crcd', 'name'), '28 days', '28 days', '28 days', 2);
+INSERT INTO config.rule_circ_duration VALUES 
+    (3, oils_i18n_gettext(3, '3_months_0_renew', 'crcd', 'name'), '3 months', '3 months', '3 months', 0);
+INSERT INTO config.rule_circ_duration VALUES 
+    (4, oils_i18n_gettext(4, '3_days_1_renew', 'crcd', 'name'), '3 days', '3 days', '3 days', 1);
+INSERT INTO config.rule_circ_duration VALUES 
+    (5, oils_i18n_gettext(5, '2_months_2_renew', 'crcd', 'name'), '2 months', '2 months', '2 months', 2);
+INSERT INTO config.rule_circ_duration VALUES 
+    (6, oils_i18n_gettext(6, '35_days_1_renew', 'crcd', 'name'), '35 days', '35 days', '35 days', 1);
+INSERT INTO config.rule_circ_duration VALUES 
+    (7, oils_i18n_gettext(7, '7_days_2_renew', 'crcd', 'name'), '7 days', '7 days', '7 days', 2);
 
 --040.schema.asset.sql:
-INSERT INTO asset.copy_location (name,owning_lib) VALUES (oils_i18n_gettext('Stacks'),1);
-INSERT INTO asset.call_number VALUES (-1,1,NOW(),1,NOW(),-1,1,'UNCATALOGED');
+INSERT INTO asset.copy_location (id, name,owning_lib) VALUES (1, oils_i18n_gettext(1, 'Stacks', 'acpl', 'name'),1);
+
+-- Vandelay (for importing and exporting records) 012.schema.vandelay.sql 
+INSERT INTO vandelay.bib_attr_definition ( id, code, description, xpath ) VALUES (1, 'title', oils_i18n_gettext(1, 'Title of work', 'vqbrad', 'description'),'//*[@tag="245"]/*[contains("abcmnopr",@code)]');
+INSERT INTO vandelay.bib_attr_definition ( id, code, description, xpath ) VALUES (2, 'author', oils_i18n_gettext(1, 'Author of work', 'vqbrad', 'description'),'//*[@tag="100" or @tag="110" or @tag="113"]/*[contains("ad",@code)]');
+INSERT INTO vandelay.bib_attr_definition ( id, code, description, xpath ) VALUES (3, 'language', oils_i18n_gettext(3, 'Language of work', 'vqbrad', 'description'),'//*[@tag="240"]/*[@code="l"][1]');
+INSERT INTO vandelay.bib_attr_definition ( id, code, description, xpath ) VALUES (4, 'pagination', oils_i18n_gettext(4, 'Pagination', 'vqbrad', 'description'),'//*[@tag="300"]/*[@code="a"][1]');
+
+INSERT INTO config.bib_level_map (code, value) VALUES ('b', oils_i18n_gettext('b', 'Serial component part', 'cblvl', 'value'));
+INSERT INTO config.bib_level_map (code, value) VALUES ('c', oils_i18n_gettext('c', 'Collection', 'cblvl', 'value'));
+INSERT INTO config.bib_level_map (code, value) VALUES ('d', oils_i18n_gettext('d', 'Subunit', 'cblvl', 'value'));
+INSERT INTO config.bib_level_map (code, value) VALUES ('i', oils_i18n_gettext('i', 'Integrating resource', 'cblvl', 'value'));
+INSERT INTO config.bib_level_map (code, value) VALUES ('m', oils_i18n_gettext('m', 'Monograph/Item', 'cblvl', 'value'));
+INSERT INTO config.bib_level_map (code, value) VALUES ('s', oils_i18n_gettext('s', 'Serial', 'cblvl', 'value'));
+
+-- available locales
+INSERT INTO config.i18n_locale (code,marc_code,name,description)
+    VALUES ('en-US', 'eng', oils_i18n_gettext('en-US', 'English (US)', 'i18n_l', 'name'),
+	oils_i18n_gettext('en-US', 'American English', 'i18n_l', 'description'));
+INSERT INTO config.i18n_locale (code,marc_code,name,description)
+    VALUES ('en-CA', 'eng', oils_i18n_gettext('en-CA', 'English (Canada)', 'i18n_l', 'name'),
+	oils_i18n_gettext('en-CA', 'Canadian English', 'i18n_l', 'description'));
+INSERT INTO config.i18n_locale (code,marc_code,name,description)
+    VALUES ('fr-CA', 'fre', oils_i18n_gettext('fr-CA', 'French (Canada)', 'i18n_l', 'name'),
+	oils_i18n_gettext('fr-CA', 'Canadian French', 'i18n_l', 'description'));
+INSERT INTO config.i18n_locale (code,marc_code,name,description)
+    VALUES ('es-US', 'spa', oils_i18n_gettext('es-US', 'Spanish (US)', 'i18n_l', 'name'),
+	oils_i18n_gettext('es-US', 'American Spanish', 'i18n_l', 'description'));
+
+INSERT INTO container.copy_bucket_type (code,label) VALUES ('misc', oils_i18n_gettext('misc', 'Miscellaneous', 'ccpbt', 'label'));
+INSERT INTO container.copy_bucket_type (code,label) VALUES ('staff_client', oils_i18n_gettext('staff_client', 'General Staff Client container', 'ccpbt', 'label'));
+INSERT INTO container.call_number_bucket_type (code,label) VALUES ('misc', oils_i18n_gettext('misc', 'Miscellaneous', 'ccnbt', 'label'));
+INSERT INTO container.biblio_record_entry_bucket_type (code,label) VALUES ('misc', oils_i18n_gettext('misc', 'Miscellaneous', 'cbrebt', 'label'));
+INSERT INTO container.biblio_record_entry_bucket_type (code,label) VALUES ('staff_client', oils_i18n_gettext('staff_client', 'General Staff Client container', 'cbrebt', 'label'));
