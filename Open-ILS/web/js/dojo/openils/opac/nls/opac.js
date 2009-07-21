13b44d8c444549c44fc89a901f54f38a74b3d2c3
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-07-21 05:31:25 +0000

    Add a basic UI for creating and deleting MFHD records to the record display in the staff client
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@13649 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/dojo/openils/opac/nls/opac.js
+++ Open-ILS/web/js/dojo/openils/opac/nls/opac.js
@@ -1,5 +1,10 @@
 {
-	"EDIT_LABEL": "Edit",
+	"CREATE_MFHD": "Add MFHD record",
+	"CREATED_MFHD_RECORD": "Created MFHD record for ${0}",
+	"DELETED_MFHD_RECORD": "Deleted MFHD record ${0}",
+	"EDIT_MFHD_RECORD": "Edit MFHD Record",
+	"EDIT_MFHD_MENU": "Edit Holdings",
+	"EDIT_PROPERTIES": "Edit Propeties",
 	"HOLDINGS": "Previous volumes",
 	"INDEXES": "Previous indexes",
 	"CURRENT_HOLDINGS": "Current volume",
