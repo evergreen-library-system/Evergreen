88661b116c4078770ca1e5d5f111358ce8c9d73a
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-08-20 20:56:39 +0000

    New column: acq.hold_request.shelf_time
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6@13908 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/090.schema.action.sql
+++ Open-ILS/src/sql/Pg/090.schema.action.sql
@@ -286,7 +286,8 @@ CREATE TABLE action.hold_request (
 	phone_notify		TEXT,
 	email_notify		BOOL				NOT NULL DEFAULT TRUE,
 	frozen			BOOL				NOT NULL DEFAULT FALSE,
-	thaw_date		TIMESTAMP WITH TIME ZONE
+	thaw_date		TIMESTAMP WITH TIME ZONE,
+	shelf_time		TIMESTAMP WITH TIME ZONE
 );
 
 CREATE INDEX hold_request_target_idx ON action.hold_request (target);
