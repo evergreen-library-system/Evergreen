2a39b0ac22e4db0e76d223af1ba6f4a841e15c73
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-04 15:24:09 +0000

    expose PO ID and PO name.  plugged in option to change the PO name
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6@13960 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/po/view_po.js
+++ Open-ILS/web/js/ui/default/acq/po/view_po.js
@@ -1,6 +1,7 @@
 dojo.require('dijit.layout.ContentPane');
 dojo.require('openils.User');
 dojo.require('openils.Util');
+dojo.require('openils.PermaCrud');
 
 var PO = null;
 var liTable;
@@ -16,6 +17,8 @@ function init() {
             params: [openils.User.authtoken, poId, {flesh_price_summary:true, flesh_lineitem_count:true}],
             oncomplete: function(r) {
                 PO = openils.Util.readResponse(r);
+                dojo.byId('acq-po-view-id').innerHTML = PO.id();
+                dojo.byId('acq-po-view-name').innerHTML = PO.name();
                 dojo.byId('acq-po-view-total-li').innerHTML = PO.lineitem_count();
                 dojo.byId('acq-po-view-total-enc').innerHTML = PO.amount_encumbered();
                 dojo.byId('acq-po-view-total-spent').innerHTML = PO.amount_spent();
@@ -33,6 +36,20 @@ function init() {
             }
         }
     );
+}
+
+function updatePoName() {
+    var value = prompt('Enter new purchase order name:', PO.name()); // TODO i18n
+    if(!value || value == PO.name()) return;
+    PO.name(value);
+    var pcrud = new openils.PermaCrud();
+    pcrud.update(PO, {
+        oncomplete : function(r) {
+            var stat = openils.Util.readResponse(r);
+            if(stat) 
+                dojo.byId('acq-po-view-name').innerHTML = value;
+        }
+    });
 }
 
 openils.Util.addOnLoad(init);
