27394e58569de682108860bb57bf46afac82a015
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-15 16:53:44 +0000

    Initial stab at modeling database changes as atomic mini-scripts for ease of development and eventual automatic upgrades Created new upgrade directory for the 1.6 to trunk upgrade path.
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14025 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/sql/Pg/upgrade_1.6_trunk/0001.data.config.org_unit_setting_type.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+-- Org Unit Settings for configuring org unit weights and org unit max-loops for hold targeting
+
+INSERT INTO config.upgrade_log (version) VALUES ('0001.data.config.org_unit_setting_type.sql');
+
+INSERT INTO config.org_unit_setting_type (name, label, description, datatype) VALUES (
+    'circ.holds.org_unit_target_weight',
+    'Holds: Org Unit Target Weight',
+    'Org Units can be organized into hold target groups based on a weight.  Potential copies from org units with the same weight are chosen at random.',
+    'integer'
+);
+
+INSERT INTO config.org_unit_setting_type (name, label, description, datatype) VALUES (
+    'circ.holds.target_holds_by_org_unit_weight',
+    'Holds: Use weight-based hold targeting',
+    'Use library weight based hold targeting',
+    'bool'
+);
+
+INSERT INTO config.org_unit_setting_type (name, label, description, datatype) VALUES (
+    'circ.holds.max_org_unit_target_loops',
+    'Holds: Maximum library target attempts',
+    'When this value is set and greater than 0, the system will only attempt to find a copy at each possible branch the configured number of times',
+    'integer'
+);
+
+COMMIT;
+
