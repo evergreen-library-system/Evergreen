97bfd9e723241077fe88fbb01144599db4233844
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-21 14:57:17 +0000

    New org setting to turn on behind-the-desk hold pickup option for patrons
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14071 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -2220,3 +2220,11 @@ INSERT INTO config.org_unit_setting_type (name, label, description, datatype) VA
     'interval'
 );
 
+-- Setting for behind the desk hold pickups
+INSERT INTO config.org_unit_setting_type (name, label, description, datatype) VALUES (
+    'circ.holds.behind_desk_pickup_supported',
+    'Holds: Behind Desk Pickup Supported',
+    'If a branch supports both a public holds shelf and behind-the-desk pickups, set this value to true.  This gives the patron the option to enable behind-the-desk pickups for their holds',
+    'bool'
+);
+
---
+++ Open-ILS/src/sql/Pg/upgrade/0008.data.org-setting-lib-supports-behind-desk-holds.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+INSERT INTO config.upgrade_log (version) VALUES ('0008.data.org-setting-lib-supports-behind-desk-holds.sql');
+
+INSERT INTO config.org_unit_setting_type (name, label, description, datatype) VALUES (
+    'circ.holds.behind_desk_pickup_supported',
+    'Holds: Behind Desk Pickup Supported',
+    'If a branch supports both a public holds shelf and behind-the-desk pickups, set this value to true.  This gives the patron the option to enable behind-the-desk pickups for their holds',
+    'bool'
+);
+
+COMMIT;
+
