758ca6a17b1001c75037a3175b1be281b12e98f5
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-22 17:03:13 +0000

    Add upgrade script for changes previously committed, adding two columns to action.circulation.
    A    Open-ILS/src/sql/Pg/upgrade/0011.schema.circ-checkin-ws-and-scan-time.sql
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14086 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/sql/Pg/upgrade/0011.schema.circ-checkin-ws-and-scan-time.sql
@@ -0,0 +1,15 @@
+BEGIN
+
+INSERT INTO config.upgrade_log (version) VALUES ('0011.schema.circ-checkin-ws-and-scan-time.sql');
+
+ALTER TABLE action.circulation
+ADD COLUMN checkin_workstation INT
+	REFERENCES actor.workstation(id)
+	ON DELETE SET NULL
+	DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE action.circulation
+ADD COLUMN checkin_scan_time TIMESTAMPTZ;
+
+COMMIT;
+
