5604939b01de8c75c1ccf557238a7196f9d32ad6
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-23 18:37:08 +0000

    Correct a typo: a redundant ON
    M    upgrade/0017.schema.circ-target-copy-indexes.sql
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14132 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0017.schema.circ-target-copy-indexes.sql
+++ Open-ILS/src/sql/Pg/upgrade/0017.schema.circ-target-copy-indexes.sql
@@ -5,7 +5,7 @@ INSERT INTO config.upgrade_log (version) VALUES ('0017');
 CREATE INDEX action_circulation_target_copy_idx
 ON action.circulation (target_copy);
 
-CREATE INDEX action_aged_circulation_target_copy_idx ON
+CREATE INDEX action_aged_circulation_target_copy_idx
 ON action.aged_circulation (target_copy);
 
 COMMIT;
