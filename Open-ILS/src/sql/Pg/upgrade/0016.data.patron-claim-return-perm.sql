c410f73cc135c163464f1fe239ffd23fb351c2fb
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-23 16:06:41 +0000

    fixed db upgrade script collision
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14117 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0016.data.patron-claim-return-perm.sql
+++ Open-ILS/src/sql/Pg/upgrade/0016.data.patron-claim-return-perm.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO config.upgrade_log (version) VALUES ('0014');
+INSERT INTO config.upgrade_log (version) VALUES ('0016');
 
 INSERT INTO permission.perm_list (code, description) VALUES (
     'UPDATE_PATRON_CLAIM_RETURN_COUNT',
