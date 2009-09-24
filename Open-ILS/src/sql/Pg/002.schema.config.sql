41dfb6b0f788def9b7d1a537cbf8d74ae6ce9722
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-24 19:26:24 +0000

    backporting r14159: Correcting reporting view definition with regard to ISBN and ISSN extraction
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6@14160 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -51,7 +51,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0015');
+INSERT INTO config.upgrade_log (version) VALUES ('0022');
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
