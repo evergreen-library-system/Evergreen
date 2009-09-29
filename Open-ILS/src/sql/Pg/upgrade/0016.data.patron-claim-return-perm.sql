cee29c5436fcd6b954c4930c4242e176fae0826a
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-09-29 15:03:36 +0000

    repaired syntax error in sql update
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14202 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0016.data.patron-claim-return-perm.sql
+++ Open-ILS/src/sql/Pg/upgrade/0016.data.patron-claim-return-perm.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 INSERT INTO config.upgrade_log (version) VALUES ('0016');
 
-INSERT INTO permission.perm_list VALUES (
+INSERT INTO permission.perm_list VALUES 
     (345,'UPDATE_PATRON_CLAIM_RETURN_COUNT', oils_i18n_gettext(345,'Allows staff to manually change a patron''s claims returned count', 'ppl', 'description'));
 
 COMMIT;
