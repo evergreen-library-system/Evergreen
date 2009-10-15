6b157bff57716d055f7071497a07b1427f0be407
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-10-15 16:46:02 +0000

    backporting trunk fix for pathological historical circ report query plan
    git-svn-id: svn://svn.open-ils.org/ILS/tags/rel_1_6_0_0@14450 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/1.4.0.5-1.6.0.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/1.4.0.5-1.6.0.0-upgrade-db.sql
@@ -3859,6 +3859,14 @@ $$;
 
 COMMIT;
 
+CREATE OR REPLACE VIEW extend_reporter.full_circ_count AS
+ SELECT cp.id, COALESCE(sum(c.circ_count), 0::bigint) + COALESCE(count(circ.id), 0::bigint) + COALESCE(count(acirc.id), 0::bigint) AS circ_count
+   FROM asset."copy" cp
+   LEFT JOIN extend_reporter.legacy_circ_count c USING (id)
+   LEFT JOIN "action".circulation circ ON circ.target_copy = cp.id
+   LEFT JOIN "action".aged_circulation acirc ON acirc.target_copy = cp.id
+  GROUP BY cp.id;
+
 SELECT reporter.disable_materialized_simple_record_trigger();
 
 CREATE OR REPLACE FUNCTION reporter.simple_rec_update (r_id BIGINT, deleted BOOL) RETURNS BOOL AS $$
