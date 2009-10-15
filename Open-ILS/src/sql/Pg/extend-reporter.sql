620336f79a1981cf1b097a73bbb2ab23b829d1e4
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-10-15 17:02:13 +0000

    backporting trunk fix for pathological historical circ report query plan
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_4@14451 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/extend-reporter.sql
+++ Open-ILS/src/sql/Pg/extend-reporter.sql
@@ -26,10 +26,11 @@ CREATE TABLE extend_reporter.legacy_circ_count (
 );
 
 CREATE VIEW extend_reporter.full_circ_count AS
- SELECT cp.id, COALESCE(sum(c.circ_count), 0::bigint) + COALESCE(count(circ.id), 0::bigint) AS circ_count
+ SELECT cp.id, COALESCE(sum(c.circ_count), 0::bigint) + COALESCE(count(circ.id), 0::bigint) + COALESCE(count(acirc.id), 0::bigint) AS circ_count
    FROM asset."copy" cp
    LEFT JOIN extend_reporter.legacy_circ_count c USING (id)
-   LEFT JOIN "action".all_circulation circ ON circ.target_copy = cp.id
+   LEFT JOIN "action".circulation circ ON circ.target_copy = cp.id
+   LEFT JOIN "action".aged_circulation acirc ON acirc.target_copy = cp.id
   GROUP BY cp.id;
 
 CREATE OR REPLACE VIEW extend_reporter.global_bibs_by_holding_update AS
