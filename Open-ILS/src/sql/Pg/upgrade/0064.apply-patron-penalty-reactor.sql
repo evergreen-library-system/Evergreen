36fcf11c4274b386ba5dffa9f2a9d009b81b45ba
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-10-30 18:06:35 +0000

    repaired upgrade script name
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14704 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0064.apply-patron-penalty-reactor.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
-INSERT INTO config.upgrade_log (version) VALUES ('0064');
-
-INSERT INTO action_trigger.reactor (module,description) VALUES ('ApplyPatronPenalty','Applies the conifigured penalty to a patron.  Required named environment variables are "user", which refers to the user object, and "context_org", which refers to the org_unit object that acts as the focus for the penalty.');
-
-COMMIT;
-
