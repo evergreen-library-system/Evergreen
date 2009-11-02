908044aed350a73839300a8067668257465d0ed4
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-11-02 22:46:38 +0000

    Change default to true
    M    upgrade/0065.schema.mint_condition.sql
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14725 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0065.schema.mint_condition.sql
+++ Open-ILS/src/sql/Pg/upgrade/0065.schema.mint_condition.sql
@@ -3,13 +3,13 @@ BEGIN;
 INSERT INTO config.upgrade_log (version) VALUES ('0065');
 
 ALTER TABLE asset.copy
-ADD COLUMN mint_condition boolean NOT NULL DEFAULT FALSE;
+ADD COLUMN mint_condition boolean NOT NULL DEFAULT TRUE;
 
 ALTER TABLE action.hold_request
-ADD COLUMN mint_condition boolean NOT NULL DEFAULT FALSE;
+ADD COLUMN mint_condition boolean NOT NULL DEFAULT TRUE;
 
 ALTER TABLE auditor.asset_copy_history
-ADD COLUMN mint_condition boolean NOT NULL DEFAULT FALSE;
+ADD COLUMN mint_condition boolean NOT NULL DEFAULT TRUE;
 
 COMMIT;
 
