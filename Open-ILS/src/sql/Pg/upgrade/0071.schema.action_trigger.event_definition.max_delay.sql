9966e98912137137604a598dec69021c7584d53e
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-11-04 19:51:01 +0000

    added missing semicolon
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@14773 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0071.schema.action_trigger.event_definition.max_delay.sql
+++ Open-ILS/src/sql/Pg/upgrade/0071.schema.action_trigger.event_definition.max_delay.sql
@@ -4,7 +4,7 @@ BEGIN;
 
 INSERT INTO config.upgrade_log (version) VALUES ('0071');
 
-ALTER TABLE action_trigger.event_definition ADD COLUMN max_delay INTERVAL
+ALTER TABLE action_trigger.event_definition ADD COLUMN max_delay INTERVAL;
 
 COMMIT;
 
