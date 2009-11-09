48e64ee3c389254bd20e44f8ac52539dd2bc1360
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-11-09 01:10:07 +0000

    Back-porting:
    support for min-passive-delay-age validation
    more detailed error logging
    fix to max_delay between filter
    no longer validate batch passive events during the grouping phase 
    
    
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/tags/rel_1_6_0_0@14829 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Trigger/Validator.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Trigger/Validator.pm
@@ -14,27 +14,27 @@ sub CircIsOpen {
     my $env = shift;
 
     return 0 if (defined($env->{target}->checkin_time));
-    return 0 if ($env->{params}->{max_delay_age} && !$self->MaxPassiveDelayAge($env));
+
+    if ($env->{params}->{min_target_age}) {
+        $env->{params}->{target_age_field} = 'xact_start';
+        return 0 if (!$self->MinPassiveTargetAge($env));
+    }
 
     return 1;
 }
 
-sub MaxPassiveDelayAge {
+sub MinPassiveTargetAge {
     my $self = shift;
     my $env = shift;
     my $target = $env->{target};
-    my $delay_field = $env->{event}->event_def->delay_field;
+    my $delay_field = $env->{params}->{target_age_field} || $env->{event}->event_def->delay_field;
 
     my $delay_field_ts = DateTime::Format::ISO8601->new->parse_datetime(clense_ISO8601($target->$delay_field()));
 
-    # the cutoff date is the target timestamp + the delay + the max_delay_age
-    # This is also true for negative delays. For example:
-    #    due_date + "-3 days" + "1 day" == -2 days old.
-    $delay_field_ts
-        ->add( seconds => interval_to_seconds( $env->{event}->event_def->delay ) )
-        ->add( seconds => interval_to_seconds( $env->{params}->{max_delay_age} ) );
+    # to get the minimum time that the target must have aged to, add the min age to the delay field
+    $delay_field_ts->add( seconds => interval_to_seconds( $env->{params}->{min_target_age} ) );
 
-    return 1 if $delay_field_ts > DateTime->now;
+    return 1 if $delay_field_ts <= DateTime->now;
     return 0;
 }
 
@@ -45,7 +45,11 @@ sub CircIsOverdue {
 
     return 0 if $circ->checkin_time;
     return 0 if $circ->stop_fines and not $circ->stop_fines =~ /MAXFINES|LONGOVERDUE/;
-    return 0 if ($env->{params}->{max_delay_age} && !$self->MaxPassiveDelayAge($env));
+
+    if ($env->{params}->{min_target_age}) {
+        $env->{params}->{target_age_field} = 'xact_start';
+        return 0 if (!$self->MinPassiveTargetAge($env));
+    }
 
     my $due_date = DateTime::Format::ISO8601->new->parse_datetime(clense_ISO8601($circ->due_date));
     return 0 if $due_date > DateTime->now;
