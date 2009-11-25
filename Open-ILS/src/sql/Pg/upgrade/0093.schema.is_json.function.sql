8f57f7ac9a62b404229ce8d2633a6436b91e6a2c
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-11-25 18:49:34 +0000

    add is_json() in the 0092 upgrade script, to avoid warnings ... and add it again in 0093 to fix things for folks that already updated with 0092
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15027 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0093.schema.is_json.function.sql
+++ Open-ILS/src/sql/Pg/upgrade/0093.schema.is_json.function.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO config.upgrade_log (version) VALUES ('0092'); -- miker
+INSERT INTO config.upgrade_log (version) VALUES ('0093'); -- miker
 
 CREATE OR REPLACE FUNCTION is_json (TEXT) RETURNS BOOL AS $func$
     use JSON::XS;
@@ -8,8 +8,6 @@ CREATE OR REPLACE FUNCTION is_json (TEXT) RETURNS BOOL AS $func$
     eval { decode_json( $json ) };
     return $@ ? 0 : 1;
 $func$ LANGUAGE PLPERLU;
-
-ALTER TABLE action_trigger.event ADD COLUMN user_data TEXT CHECK (user_data IS NULL OR is_json( user_data ));
 
 COMMIT;
 
