8d11e9235042f376e832771a8192c266cbc9472d
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-11-30 20:35:11 +0000

    Create a schema named "query" instead of "sql" (which is a reserved word).
    Note that two previous upgrade scripts are being changed.  If you have
    already run them, you can run them again after manually deleting two
    entries from config.upgrade.log:
    
    DELETE FROM config.upgrade.log
    WHERE version in ( '0097', '0098' );
    
    The 0097.schema.sql-datatype.sql will drop the misbegotten sql
    schema if it exists.
    
    A    Pg/008.schema.query.sql
    M    Pg/upgrade/0098.schema.sql-stored-query.sql
    M    Pg/upgrade/0097.schema.sql-datatype.sql
    D    Pg/008.schema.sql.sql
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15047 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0098.schema.sql-stored-query.sql
+++ Open-ILS/src/sql/Pg/upgrade/0098.schema.sql-stored-query.sql
@@ -2,33 +2,33 @@ BEGIN;
 
 INSERT INTO config.upgrade_log (version) VALUES ('0098'); -- Scott McKellar
 
-CREATE TABLE  sql.stored_query (
+CREATE TABLE  query.stored_query (
 	id            SERIAL         PRIMARY KEY,
 	type          TEXT           NOT NULL CONSTRAINT query_type CHECK
 	                             ( type IN ( 'SELECT', 'UNION', 'INTERSECT', 'EXCEPT' ) ),
 	use_all       BOOLEAN        NOT NULL DEFAULT FALSE,
 	use_distinct  BOOLEAN        NOT NULL DEFAULT FALSE,
-	from_clause   INT            NOT NULL , --REFERENCES sql.from_clause
-	where_clause  INT            , --REFERENCES sql.expression
+	from_clause   INT            NOT NULL , --REFERENCES query.from_clause
+	where_clause  INT            , --REFERENCES query.expression
 	                             --DEFERRABLE INITIALLY DEFERRED,
-	having_clause INT            --REFERENCES sql.expression
+	having_clause INT            --REFERENCES query.expression
 	                             --DEFERRABLE INITIALLY DEFERRED
 );
 
 -- (Foreign keys to be defined later after other tables are created)
 
-CREATE TABLE sql.query_sequence (
+CREATE TABLE query.query_sequence (
 	id              SERIAL            PRIMARY KEY,
 	parent_query    INT               NOT NULL
-	                                  REFERENCES sql.stored_query
+	                                  REFERENCES query.stored_query
 									  ON DELETE CASCADE
 									  DEFERRABLE INITIALLY DEFERRED,
 	seq_no          INT               NOT NULL,
 	child_query     INT               NOT NULL
-	                                  REFERENCES sql.stored_query
+	                                  REFERENCES query.stored_query
 									  ON DELETE CASCADE
 									  DEFERRABLE INITIALLY DEFERRED,
-	CONSTRAINT sql_query_seq UNIQUE( parent_query, seq_no )
+	CONSTRAINT query_query_seq UNIQUE( parent_query, seq_no )
 );
 
 COMMIT;
