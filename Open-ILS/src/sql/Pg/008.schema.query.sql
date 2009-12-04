7b3558ea2641e45e81661cea87048508b0f31ebb
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-12-04 16:10:24 +0000

    missing $$;
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15079 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/008.schema.query.sql
+++ Open-ILS/src/sql/Pg/008.schema.query.sql
@@ -26,7 +26,7 @@ COMMENT ON SCHEMA actor IS $$
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-
+$$;
 
 CREATE TABLE  query.stored_query (
 	id            SERIAL         PRIMARY KEY,
