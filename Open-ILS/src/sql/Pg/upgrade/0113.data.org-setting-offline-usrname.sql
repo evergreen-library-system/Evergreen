1615afc73d3d5bf55eed34afdcc37dfccf2d0a8e
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-12-09 18:34:28 +0000

    added org setting to allow patrons to identify themselves via username (in addition to barcode) during offline checkout
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15123 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/sql/Pg/upgrade/0113.data.org-setting-offline-usrname.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+INSERT INTO config.upgrade_log (version) VALUES ('0113');
+
+INSERT INTO config.org_unit_setting_type ( name, label, description, datatype )
+    VALUES ( 
+        'circ.offline.username_allowed',
+        oils_i18n_gettext('circ.offline.username_allowed', 'Selfcheck: Patron Usernames Allowed', 'coust', 'label'),
+        oils_i18n_gettext('circ.offline.username_allowed', 'During offline circulations, allow patrons to identify themselves with usernames in addition to barcode.  For this setting to work, a barcode format must also be defined', 'coust', 'description'),
+        'bool'
+    );
+
+COMMIT;
