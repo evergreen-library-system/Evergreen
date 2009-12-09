ac16756843502eecf5cc5f4725cd432c59ed476e
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2009-12-09 18:48:35 +0000

    repaired mislabeled setting s/selfcheck/offline/.  thanks, jason
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15124 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -2897,7 +2897,7 @@ INSERT INTO action_trigger.environment ( event_def, path) VALUES
 INSERT INTO config.org_unit_setting_type ( name, label, description, datatype )
     VALUES ( 
         'circ.offline.username_allowed',
-        oils_i18n_gettext('circ.offline.username_allowed', 'Selfcheck: Patron Usernames Allowed', 'coust', 'label'),
+        oils_i18n_gettext('circ.offline.username_allowed', 'Offline: Patron Usernames Allowed', 'coust', 'label'),
         oils_i18n_gettext('circ.offline.username_allowed', 'During offline circulations, allow patrons to identify themselves with usernames in addition to barcode.  For this setting to work, a barcode format must also be defined', 'coust', 'description'),
         'bool'
     );
