8102d6a5c41d4d972ee619fe386c07a95c0c1ad4
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-01-08 15:27:21 +0000

    Backport r15266 from trunk: Protect serials from being deleted with ye olde ON DELETE rule
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6@15282 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/sql/Pg/upgrade/0131.schema.serials.protect-delete.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+INSERT INTO config.upgrade_log (version) VALUES ('0130'); -- dbs
+
+CREATE RULE protect_mfhd_delete AS ON DELETE TO serial.record_entry DO INSTEAD UPDATE serial.record_entry SET deleted = true WHERE old.id = serial.record_entry.id;
+
+COMMIT;
