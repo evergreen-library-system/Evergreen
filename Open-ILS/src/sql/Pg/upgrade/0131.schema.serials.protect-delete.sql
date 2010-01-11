9fda59cb4b8920c8845d7bc67d84adc9b5699903
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-01-11 15:11:19 +0000

    Correct the value of upgrade_log.version to match file name
    M    upgrade/0131.schema.serials.protect-delete.sql
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15295 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0131.schema.serials.protect-delete.sql
+++ Open-ILS/src/sql/Pg/upgrade/0131.schema.serials.protect-delete.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO config.upgrade_log (version) VALUES ('0130'); -- dbs
+INSERT INTO config.upgrade_log (version) VALUES ('0131'); -- dbs
 
 CREATE RULE protect_mfhd_delete AS ON DELETE TO serial.record_entry DO INSTEAD UPDATE serial.record_entry SET deleted = true WHERE old.id = serial.record_entry.id;
 
