e37848f2a337f8753d05219999234db74ae8e46a
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-02-16 18:23:56 +0000

    these have been moved to the seed-data file
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15557 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/200.schema.acq.sql
+++ Open-ILS/src/sql/Pg/200.schema.acq.sql
@@ -706,9 +706,6 @@ CREATE TABLE acq.invoice_method (
     code    TEXT    PRIMARY KEY,
     name    TEXT    NOT NULL -- i18n-ize
 );
-INSERT INTO acq.invoice_method (code,name) VALUES ('EDI',oils_i18n_gettext('EDI', 'EDI', 'acqim', 'name'));
-INSERT INTO acq.invoice_method (code,name) VALUES ('PPR',oils_i18n_gettext('PPR', 'Paper', 'acqit', 'name'));
-
 
 CREATE TABLE acq.invoice (
     id          SERIAL      PRIMARY KEY,
@@ -738,11 +735,6 @@ CREATE TABLE acq.invoice_item_type (
     code    TEXT    PRIMARY KEY,
     name    TEXT    NOT NULL -- i18n-ize
 );
-INSERT INTO acq.invoice_item_type (code,name) VALUES ('TAX',oils_i18n_gettext('TAX', 'Tax', 'aiit', 'name'));
-INSERT INTO acq.invoice_item_type (code,name) VALUES ('PRO',oils_i18n_gettext('PRO', 'Processing Fee', 'aiit', 'name'));
-INSERT INTO acq.invoice_item_type (code,name) VALUES ('SHP',oils_i18n_gettext('SHP', 'Shipping Charge', 'aiit', 'name'));
-INSERT INTO acq.invoice_item_type (code,name) VALUES ('HND',oils_i18n_gettext('HND', 'Handling Charge', 'aiit', 'name'));
-INSERT INTO acq.invoice_item_type (code,name) VALUES ('ITM',oils_i18n_gettext('ITM', 'Non-library Item', 'aiit', 'name'));
 
 CREATE TABLE acq.invoice_item ( -- for invoice-only debits: taxes/fees/non-bib items/etc
     id              SERIAL      PRIMARY KEY,
