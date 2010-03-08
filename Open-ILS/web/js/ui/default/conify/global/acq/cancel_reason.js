1ff4f267dc6bf8d0d76f062cae071738b8fabb6d
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-03-08 19:50:45 +0000

    acq cancel reason administration interface.  TODO: add staff client menu entries
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15738 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/js/ui/default/conify/global/acq/cancel_reason.js
@@ -0,0 +1,39 @@
+dojo.require('openils.Util');
+dojo.require('openils.User');
+dojo.require('openils.widget.AutoGrid');
+dojo.require('fieldmapper.OrgUtils');
+dojo.require('openils.widget.OrgUnitFilteringSelect');
+
+var contextOrg;
+
+function setup() {
+    buildGrid();
+
+    var connect = function() {
+        dojo.connect(contextOrgSelector, 'onChange',
+            function() {
+                contextOrg = this.attr('value');
+                crGrid.resetStore();
+                buildGrid();
+            }
+        );
+    };
+
+    new openils.User().buildPermOrgSelector(
+        'ADMIN_ACQ_CANCEL_CAUSE', contextOrgSelector, null, connect);
+}
+
+function buildGrid() {
+
+    if(contextOrg == null)
+        contextOrg = openils.User.user.ws_ou();
+
+    crGrid.loadAll( 
+        {order_by : {acqcr : 'label'}}, 
+        {org_unit : fieldmapper.aou.fullPath(contextOrg, true)}
+    );
+}
+
+openils.Util.addOnLoad(setup);
+
+
