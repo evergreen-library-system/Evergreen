09259afcf68f7597de4fb888a10b5b44f5eb7d04
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-03-10 22:54:38 +0000

    beginning of patron request processing page
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15783 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/web/js/ui/default/acq/picklist/user_request.js
@@ -0,0 +1,44 @@
+dojo.require('openils.Util');
+dojo.require('openils.User');
+dojo.require('openils.widget.AutoGrid');
+dojo.require('fieldmapper.OrgUtils');
+dojo.require('openils.widget.OrgUnitFilteringSelect');
+
+var contextOrg;
+
+function setup() {
+    buildGrid();
+
+    var connect = function() {
+        dojo.connect(contextOrgSelector, 'onChange',
+            function() {
+                contextOrg = this.attr('value');
+                rGrid.resetStore();
+                buildGrid();
+            }
+        );
+    };
+
+    new openils.User().buildPermOrgSelector(
+        'CREATE_PICKLIST', contextOrgSelector, null, connect);
+}
+
+function buildGrid() {
+
+    if(contextOrg == null)
+        contextOrg = openils.User.user.ws_ou();
+
+    rGrid.loadAll(
+        {   order_by : {aur : 'request_date'},
+            join : 'au' 
+        },
+        {'+au' : {
+            home_ou : fieldmapper.aou.descendantNodeList(contextOrg).map(
+                function(item) { return item.id(); })
+        }}
+    );
+}
+
+openils.Util.addOnLoad(setup);
+
+
