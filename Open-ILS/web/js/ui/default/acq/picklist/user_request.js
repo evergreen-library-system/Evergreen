3365dc4e3c0be9eaa7bcd6ee310bdc09079f5c21
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-03-11 18:59:10 +0000

    trim the default display fields in the user req ui.  format the title as a link to load the request detail page, added some todo comments
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15802 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/picklist/user_request.js
+++ Open-ILS/web/js/ui/default/acq/picklist/user_request.js
@@ -7,6 +7,41 @@ dojo.require('openils.widget.OrgUnitFilteringSelect');
 var contextOrg;
 
 function setup() {
+
+    if(reqId) {
+        drawRequest();
+    } else {
+        drawList();
+    }
+}
+
+function drawRequest() {
+    // hide the grid and the context selector
+    // draw a detail page for a particular request
+    // including ability to add request to a picklist
+    // and to "reject" it (aka apply a cancel reason)
+}
+
+
+// format the title data as id:title
+function getTitle(idx, item) {
+    if(item) {
+        return this.grid.store.getValue(item, 'id') + ':' + 
+        this.grid.store.getValue(item, 'title');
+    }
+    return ''
+}
+
+// turn id:title into a url
+function formatTitle(value) {
+    if(value) {
+        var parts = value.split(/:/);
+        return '<a href="' + oilsBasePath + 
+            '/acq/picklist/user_request/' + parts[0] + '">' + parts[1] + '</a>';
+    }
+}
+
+function drawList() {
     buildGrid();
 
     var connect = function() {
