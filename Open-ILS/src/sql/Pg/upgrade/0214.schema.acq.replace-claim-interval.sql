90178d5dcd8c3606edfdebde44bf4e2152228b79
scottmk <scottmk@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-03-25 18:07:56 +0000

    Replace claim_interval column with claim_policy column, a foreign key pointing to acq.claim_policy.
    (...thus bringing the history table back into sync with its source table.)
    
    M    Open-ILS/src/sql/Pg/002.schema.config.sql
    A    Open-ILS/src/sql/Pg/upgrade/0214.schema.acq.replace-claim-interval.sql
    M    Open-ILS/examples/fm_IDL.xml
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@15980 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/sql/Pg/upgrade/0214.schema.acq.replace-claim-interval.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+INSERT INTO config.upgrade_log (version) VALUES ('0214'); -- Scott McKellar
+
+ALTER TABLE acq.acq_lineitem_history
+	DROP COLUMN claim_interval;
+
+ALTER TABLE acq.acq_lineitem_history
+	ADD COLUMN claim_policy INT
+		REFERENCES acq.claim_policy
+		DEFERRABLE INITIALLY DEFERRED;
+
+COMMIT;
