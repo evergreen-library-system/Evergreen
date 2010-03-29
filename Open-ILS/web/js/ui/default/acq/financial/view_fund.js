1412f9af8a8de067e371fb1291c02c27cfa1695d
senator <senator@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-03-29 16:41:08 +0000

    Acq: fund transfer UI
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16036 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/financial/view_fund.js
+++ Open-ILS/web/js/ui/default/acq/financial/view_fund.js
@@ -11,9 +11,11 @@ dojo.require('openils.acq.FundingSource');
 dojo.require('openils.Event');
 dojo.require('openils.User');
 dojo.require('openils.Util');
+dojo.require("openils.widget.AutoFieldWidget");
 
 var fund = null;
 var tagManager;
+var xferManager;
 
 function getSummaryInfo(rowIndex, item) {
     if(!item) return'';
@@ -80,9 +82,68 @@ function fetchFund() {
     );
 }
 
+function TransferManager() {
+    var self = this;
+
+    new openils.widget.AutoFieldWidget({
+        "fmField": "fund",
+        /* We're not really using LIDs here, we just need some class that has
+         * a fund field to take advantage of AutoFieldWidget's magic.
+         */
+        "fmClass": "acqlid",
+        "labelFormat": ["${0} (${1})", "code", "year"],
+        "searchFormat": ["${0} (${1})", "code", "year"],
+        "searchFilter": {"active": "t"}, /* consider making it possible to select inactive? */
+        "parentNode": dojo.byId("oils-acq-fund-xfer-d-selector"),
+        "orgLimitPerms": ["ADMIN_ACQ_FUND"], /* XXX is there a more appropriate permission for this? */
+        "dijitArgs": {"name": "d_fund"},
+        "forceSync": true
+    }).build(function(w, ww) { self.fundSelector = w; });
+
+    this.clearFundSelector = function() {
+        if (!this.fundSelector.attr("value"))
+            this.fundSelector.attr("value", "");
+    };
+
+    this.setFundName = function(fund) {
+        dojo.byId("oils-acq-fund-xfer-name-fund").innerHTML =
+            fund.code() + " (" + fund.year() + ") / " + fund.name();
+    };
+
+    this.submit = function() {
+        var values = xferDialog.getValues();
+        if (values.d_fund == fund.id()) {
+            alert(localeStrings.FUND_XFER_SAME_SOURCE_AND_DEST);
+            return false;
+        }
+        if (confirm(localeStrings.FUND_XFER_CONFIRM)) {
+            fieldmapper.standardRequest(
+                ["open-ils.acq", "open-ils.acq.funds.transfer_money"], {
+                    "params": [
+                        openils.User.authtoken,
+                        fund.id(), values.o_amount,
+                        values.d_fund, null,
+                        values.note
+                    ],
+                    "async": true,
+                    "oncomplete": function(r) {
+                        if (openils.Util.readResponse(r) == 1) {
+                            location.href = location.href;
+                        }
+                    }
+                }
+            );
+        }
+        return true;
+    };
+}
+
 function load() {
     tagManager = new TagManager(dojo.byId("oils-acq-tag-manager-display"));
     tagManager.prepareTagSelector(tagSelector);
+
+    xferManager = new TransferManager();
+
     fetchFund();
 }
 
