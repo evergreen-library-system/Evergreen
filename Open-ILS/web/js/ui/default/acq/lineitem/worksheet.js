1ec5a4210f949fcfdedb0f189ddda55bebbb5443
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-03-31 20:46:59 +0000

    show progress dialog while waiting for the lineitem worksheet to load
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16075 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/acq/lineitem/worksheet.js
+++ Open-ILS/web/js/ui/default/acq/lineitem/worksheet.js
@@ -1,13 +1,16 @@
+dojo.require('openils.widget.ProgressDialog');
 dojo.requireLocalization("openils.acq", "acq");
 var localeStrings = dojo.i18n.getLocalization("openils.acq", "acq");
 
 function load() {
+    progressDialog.show(true);
     fieldmapper.standardRequest(
         ["open-ils.acq", "open-ils.acq.lineitem.format"], {
             "params": [openils.User.authtoken, liId, "html"],
             "async": true,
             "oncomplete": function(r) {
                 r = openils.Util.readResponse(r);
+                progressDialog.hide();
                 var d = dojo.byId("acq-worksheet-contents");
                 if (r.template_output())
                     d.innerHTML = r.template_output().data();
