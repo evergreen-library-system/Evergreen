da460dac580424796affbcfbc4f905dc706c3653
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-04-16 21:08:57 +0000

    don't attempt to validate a readOnly widget
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16260 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/dojo/openils/widget/AutoFieldWidget.js
+++ Open-ILS/web/js/dojo/openils/widget/AutoFieldWidget.js
@@ -502,7 +502,7 @@ if(!dojo._hasResource['openils.widget.AutoFieldWidget']) {
             if(this.onload)
                 this.onload(this.widget, this);
 
-            if(this.dijitArgs && this.dijitArgs.required) {
+            if(!this.readOnly && this.dijitArgs && this.dijitArgs.required) {
                 // a required dijit is not given any styling to indicate the value
                 // is invalid until the user has focused the widget then left it with
                 // invalid data.  This change tells dojo to pretend this focusing has 
