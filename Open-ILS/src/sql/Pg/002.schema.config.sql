bac98fd5f43e1f6503f52f082bebeee1a9f79ae7
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-05-16 19:16:42 +0000

    allow multiple 260 tags in summary view
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16438 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -65,7 +65,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0262'); -- berick
+INSERT INTO config.upgrade_log (version) VALUES ('0263'); -- miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
