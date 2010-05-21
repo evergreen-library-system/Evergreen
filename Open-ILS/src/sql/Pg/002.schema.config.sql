9cd336e470045c83619afe3ca9c173d4d95c9404
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-05-21 15:24:39 +0000

    use proper JSON, re &apos; vs &quot;
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16472 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -65,7 +65,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0270'); -- Scott McKellar
+INSERT INTO config.upgrade_log (version) VALUES ('0271'); -- miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
@@ -737,43 +737,43 @@ CREATE TABLE config.metabib_field_index_norm_map (
 
 CREATE OR REPLACE FUNCTION oils_tsearch2 () RETURNS TRIGGER AS $$
 DECLARE
-        normalizer      RECORD;
-        value           TEXT := '';
+    normalizer      RECORD;
+    value           TEXT := '';
 BEGIN
-        IF NEW.index_vector = ''::tsvector THEN
-            RETURN NEW;
-        END IF;
-
-        value := NEW.value;
-
-        IF TG_TABLE_NAME::TEXT ~ 'field_entry$' THEN
-                FOR normalizer IN
-                        SELECT  n.func AS func,
-                                n.param_count AS param_count,
-                                m.params AS params
-                          FROM  config.index_normalizer n
-                                JOIN config.metabib_field_index_norm_map m ON (m.norm = n.id)
-                          WHERE field = NEW.field
-                          ORDER BY m.pos
-                LOOP
-                        EXECUTE 'SELECT ' || normalizer.func || '(' ||
-                                        quote_literal( value ) ||
-                                        CASE
-                                                WHEN normalizer.param_count > 0 THEN ',' || BTRIM(normalizer.params,'[]')
-                                                ELSE ''
-                                        END ||
-                                ')' INTO value;
-
-                END LOOP;
-        END IF;
-
-        IF REGEXP_REPLACE(VERSION(),E'^.+?(\\d+\\.\\d+).*?$',E'\\1')::FLOAT > 8.2 THEN
-                NEW.index_vector = to_tsvector((TG_ARGV[0])::regconfig, value);
-        ELSE
-                NEW.index_vector = to_tsvector(TG_ARGV[0], value);
-        END IF;
-
+    IF NEW.index_vector = ''::tsvector THEN
         RETURN NEW;
+    END IF;
+
+    value := NEW.value;
+
+    IF TG_TABLE_NAME::TEXT ~ 'field_entry$' THEN
+        FOR normalizer IN
+            SELECT  n.func AS func,
+                    n.param_count AS param_count,
+                    m.params AS params
+              FROM  config.index_normalizer n
+                    JOIN config.metabib_field_index_norm_map m ON (m.norm = n.id)
+              WHERE field = NEW.field
+              ORDER BY m.pos LOOP
+                EXECUTE 'SELECT ' || normalizer.func || '(' ||
+                    quote_literal( value ) ||
+                    CASE
+                        WHEN normalizer.param_count > 0
+                            THEN ',' || REPLACE(REPLACE(BTRIM(normalizer.params,'[]'),E'\'',E'\\\''),E'"',E'\'')
+                            ELSE ''
+                        END ||
+                    ')' INTO value;
+
+        END LOOP;
+    END IF;
+
+    IF REGEXP_REPLACE(VERSION(),E'^.+?(\\d+\\.\\d+).*?$',E'\\1')::FLOAT > 8.2 THEN
+        NEW.index_vector = to_tsvector((TG_ARGV[0])::regconfig, value);
+    ELSE
+        NEW.index_vector = to_tsvector(TG_ARGV[0], value);
+    END IF;
+
+    RETURN NEW;
 END;
 $$ LANGUAGE PLPGSQL;
 
