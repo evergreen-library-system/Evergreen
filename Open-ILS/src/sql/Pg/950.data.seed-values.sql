c658be1b76bc81dbffd606405a6608c61475e05b
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-05-27 20:16:33 +0000

    adding function to return holds visible to the user, based on their horizon settings; fixing logic reversal in circ version of same
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16525 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -4994,4 +4994,18 @@ INSERT INTO config.usr_setting_type (name,opac_visible,label,description,datatyp
         'date'
     );
 
+INSERT INTO config.usr_setting_type (name,opac_visible,label,description,datatype)
+    VALUES (
+        'history.hold.retention_age',
+        TRUE,
+        oils_i18n_gettext('history.hold.retention_age','Historical Hold Retention Age','cust','label'),
+        oils_i18n_gettext('history.hold.retention_age','Historical Hold Retention Age','cust','description'),
+        'interval'
+    ),(
+        'history.hold.retention_count',
+        TRUE,
+        oils_i18n_gettext('history.hold.retention_count','Historical Hold Retention Count','cust','label'),
+        oils_i18n_gettext('history.hold.retention_count','Historical Hold Retention Count','cust','description'),
+        'integer'
+    );
 
