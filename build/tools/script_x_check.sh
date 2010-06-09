307c28f2adca1127a7e5f4a0248247b9bbf197b5
atz <atz@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-06-09 21:44:00 +0000

    Some python code is non-executable (script_x_check updated)
    This corrects an overreach on the earlier patch.
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16644 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- build/tools/script_x_check.sh
+++ build/tools/script_x_check.sh
@@ -6,5 +6,31 @@
 # usage: run this from the base directory of your repo,
 #   or wherever you want to check, inclusive of subdirectories
 
-find . \( -name "*.pl" -o -name "*.sh" -o -name "*.py" \) ! -executable -ls
+find . \
+    \(  -path ./Open-ILS/src/python/oils -prune \
+     -o -path ./build/i18n/tests/testhelper.py -prune \
+     -o -path ./Open-ILS/src/extras/Evergreen.py -prune \
+    \) -o \
+\
+    \( \
+       -name "*.pl" \
+    -o -name "*.sh" \
+    -o -name "*.py" \
+    \) \
+    \( ! -executable \) \
+    -ls ;
 
+exit;
+
+
+########################################################
+These should be exceptions (non-executable python libs):
+./Open-ILS/src/python/oils/const.py
+./Open-ILS/src/python/oils/event.py
+./Open-ILS/src/python/oils/org.py
+./Open-ILS/src/python/oils/system.py
+./Open-ILS/src/python/oils/utils/utils.py
+./Open-ILS/src/python/oils/utils/csedit.py
+./Open-ILS/src/python/oils/utils/idl.py
+./Open-ILS/src/extras/Evergreen.py
+./build/i18n/tests/testhelper.py
