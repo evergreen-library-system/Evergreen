5bc6f480c150e47bd8c946feb373511cd595075d
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-06-10 20:03:25 +0000

    syntax
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16665 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0302.data.fifo-holds-ou-setting.sql
+++ Open-ILS/src/sql/Pg/upgrade/0302.data.fifo-holds-ou-setting.sql
@@ -3,7 +3,7 @@ BEGIN;
 INSERT INTO config.upgrade_log (version) VALUES ('0302'); --miker
 
 INSERT INTO config.org_unit_setting_type (name,label,description,datatype)
-    VALUES ('circ.holds_fifo', 'Holds: FIFO', 'Force holds to a more strict First-In, First-Out capture', 'bool' ),
+    VALUES ('circ.holds_fifo', 'Holds: FIFO', 'Force holds to a more strict First-In, First-Out capture', 'bool' );
 
 COMMIT;
 
