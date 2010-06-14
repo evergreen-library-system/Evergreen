0c67f763f316a3d8889b0dfab3efb5034d81b9b1
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-06-14 14:48:37 +0000

    db upgrade script comment and license
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6@16700 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/1.6.0.4-1.6.1.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/1.6.0.4-1.6.1.0-upgrade-db.sql
@@ -1,6 +1,23 @@
-BEGIN;
+/*
+ * Copyright (C) 2010 Laurentian University
+ * Dan Scott <dscott@laurentian.ca>
+ * Copyright (C) 2010  Equinox Software, Inc.
+ * Mike Rylander <miker@esilibrary.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
--- Insert booking schema upgrade here
+
+BEGIN;
 
 CREATE TABLE actor.usr_password_reset (
   id SERIAL PRIMARY KEY,
