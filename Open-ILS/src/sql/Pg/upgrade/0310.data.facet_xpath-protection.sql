9de844f3496a13094afc66259bf87c9d45503fba
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-06-17 18:08:02 +0000

    typo
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@16744 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0310.data.facet_xpath-protection.sql
+++ Open-ILS/src/sql/Pg/upgrade/0310.data.facet_xpath-protection.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 INSERT INTO config.upgrade_log (version) VALUES ('0310'); --miker
 
-UPDATE config.metabib_field SET facet_xpath = '//' || facet_xpath WHER facet_xpath IS NOT NULL;
+UPDATE config.metabib_field SET facet_xpath = '//' || facet_xpath WHERE facet_xpath IS NOT NULL;
 
 COMMIT;
 
