b3d7c90aa71daadcb6002e3a419791607b23365a
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-07-22 01:03:33 +0000

    upgrade script and version bump for r17007
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@17008 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -68,7 +68,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0348'); -- miker
+INSERT INTO config.upgrade_log (version) VALUES ('0349'); -- miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
