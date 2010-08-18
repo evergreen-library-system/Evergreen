45863974be07befc425f616fccc1d1ca7be860b8
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-08-18 16:37:43 +0000

    Backport of 17243: so, some datasets have records with multiple 245a subfields. yeah, really.
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6_1@17246 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/1.6.1.1-1.6.1.2-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/1.6.1.1-1.6.1.2-upgrade-db.sql
@@ -149,19 +149,18 @@ BEGIN
 END;
 $func$ LANGUAGE plpgsql;
 
--- allow more than one 260 in summary data
 CREATE OR REPLACE VIEW reporter.old_super_simple_record AS
 SELECT  r.id,
     r.fingerprint,
     r.quality,
     r.tcn_source,
     r.tcn_value,
-    title.value AS title,
+    FIRST(title.value) AS title,
     FIRST(author.value) AS author,
     ARRAY_TO_STRING(ARRAY_ACCUM( DISTINCT publisher.value), ', ') AS publisher,
     ARRAY_TO_STRING(ARRAY_ACCUM( DISTINCT SUBSTRING(pubdate.value FROM $$\d+$$) ), ', ') AS pubdate,
     ARRAY_ACCUM( DISTINCT SUBSTRING(isbn.value FROM $$^\S+$$) ) AS isbn,
-    ARRAY_ACCUM( DISTINCT SUBSTRING(REGEXP_REPLACE(issn.value, E'^\\s*(\\d{4})[-\\s](\\d{3,4}x?)\\s*', E'\\1 \\2') FROM 1 FOR 9) ) AS issn
+    ARRAY_ACCUM( DISTINCT SUBSTRING(issn.value FROM $$^\S+$$) ) AS issn
   FROM  biblio.record_entry r
     LEFT JOIN metabib.full_rec title ON (r.id = title.record AND title.tag = '245' AND title.subfield = 'a')
     LEFT JOIN metabib.full_rec author ON (r.id = author.record AND author.tag IN ('100','110','111') AND author.subfield = 'a')
@@ -169,7 +168,7 @@ SELECT  r.id,
     LEFT JOIN metabib.full_rec pubdate ON (r.id = pubdate.record AND pubdate.tag = '260' AND pubdate.subfield = 'c')
     LEFT JOIN metabib.full_rec isbn ON (r.id = isbn.record AND isbn.tag IN ('024', '020') AND isbn.subfield IN ('a','z'))
     LEFT JOIN metabib.full_rec issn ON (r.id = issn.record AND issn.tag = '022' AND issn.subfield = 'a')
-  GROUP BY 1,2,3,4,5,6;
+  GROUP BY 1,2,3,4,5;
 
 CREATE OR REPLACE FUNCTION asset.merge_record_assets( target_record BIGINT, source_record BIGINT ) RETURNS INT AS $func$
 DECLARE
