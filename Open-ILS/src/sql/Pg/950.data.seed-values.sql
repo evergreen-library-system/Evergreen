9542e39f4852fa4444de05dc2608c1f4274853f6
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-08-25 15:57:33 +0000

    circ.missing_pieces.copy_status org unit setting to support the Mark/Scan as Missing Pieces workflow.
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@17335 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -6647,3 +6647,20 @@ INSERT INTO config.org_unit_setting_type (name, label, description, datatype)
         'bool')
 ;
 
+-- 0379.data.org-setting-circ.missing_pieces.copy_status.sql
+INSERT INTO config.org_unit_setting_type ( name, label, description, datatype, fm_class ) VALUES (
+        'circ.missing_pieces.copy_status',
+        oils_i18n_gettext(
+            'circ.missing_pieces.copy_status',
+            'Circulation: Item Status for Missing Pieces', 
+            'coust', 
+            'label'),
+        oils_i18n_gettext(
+            'circ.missing_pieces.copy_status',
+            'This is the Item Status to use for items that have been marked or scanned as having Missing Pieces.  In absense of this setting, the Damaged status is used.',
+            'coust', 
+            'description'),
+        'link',
+        'ccs'
+);
+
