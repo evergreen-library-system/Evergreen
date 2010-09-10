09af486f24d696bddf01d786859288ef3f5666a6
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-09-10 18:17:20 +0000

    ui.patron.registration.require_address org unit setting and associated behavior for requiring at least one postal address during patron registration
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@17582 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -68,7 +68,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0391'); -- miker
+INSERT INTO config.upgrade_log (version) VALUES ('0392'); -- phasefx
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -6916,4 +6916,20 @@ INSERT INTO config.org_unit_setting_type ( name, label, description, datatype ) 
         'string'
 );
 
+-- 0392.data.org-setting-ui.patron.editor_address_requirement.sql
+
+INSERT INTO config.org_unit_setting_type ( name, label, description, datatype ) VALUES (
+        'ui.patron.registration.require_address',
+        oils_i18n_gettext(
+            'ui.patron.registration.require_address',
+            'GUI: Require at least one address for Patron Registration', 
+            'coust', 
+            'label'),
+        oils_i18n_gettext(
+            'ui.patron.registration.require_address',
+            'Enforces a requirement for having at least one address for a patron during registration.',
+            'coust', 
+            'description'),
+        'bool'
+);
 
