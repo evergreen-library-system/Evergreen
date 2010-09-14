d3c6ca7d853e34bde5672ac968547f38c56e117a
senator <senator@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-09-14 21:44:39 +0000

    OPAC display of fully compressed serials holding. Mostly miker's work except the OU setting part
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@17661 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -68,7 +68,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0396'); -- senator
+INSERT INTO config.upgrade_log (version) VALUES ('0397'); -- senator
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
