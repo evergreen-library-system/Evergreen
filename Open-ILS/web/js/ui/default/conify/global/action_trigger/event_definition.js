c8d3c24f5ccf6aeb0a03da0e7ab41071ed46536d
erickson <erickson@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-10-08 21:10:43 +0000

    mild fixes for a/t interface admin interface.  sort by def name instead of hook, which probably makes more sense to a human.  hide the opt-in and max-delay columns to free up some badly needed horizontal space.  use percentage width for name column, which acts a lot like 'auto', but allows the user to manually resize
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@18253 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/web/js/ui/default/conify/global/action_trigger/event_definition.js
+++ Open-ILS/web/js/ui/default/conify/global/action_trigger/event_definition.js
@@ -17,7 +17,7 @@ function loadEventDef() {
     eventDefGranularity.attr('value', null);
     edGrid.overrideEditWidgets.granularity = eventDefGranularity;
     edGrid.overrideEditWidgets.granularity.shove = {"create": ""};
-    edGrid.loadAll({order_by:{atevdef : 'hook'}}); 
+    edGrid.loadAll({order_by:{atevdef : 'name'}}); 
     openils.widget.Textarea.width = '600px';
     openils.widget.Textarea.height = '600px';
     edGrid.overrideEditWidgetClass.template = 'openils.widget.Textarea';
