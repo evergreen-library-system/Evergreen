c69672efc27764c6cf54cb3c71cb9ae5bf1754cd
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-10-11 18:14:04 +0000

    fix Actions for this Record -> Add Volumes.  backport was missing this
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6_1@18268 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/chrome/content/OpenILS/global_util.js
+++ Open-ILS/xul/staff_client/chrome/content/OpenILS/global_util.js
@@ -3,6 +3,9 @@
     function ses(a) {
         JSAN.use('OpenILS.data'); var data = new OpenILS.data(); data.init({'via':'stash'});
         switch(a) {
+            case 'staff' : return data.list.au[0]; break;
+            case 'staff_id' : return data.list.au[0].id(); break;
+            case 'staff_usrname' : return data.list.au[0].usrname(); break;
             case 'ws_ou' :
                 return data.list.au[0].ws_ou();
             break;
