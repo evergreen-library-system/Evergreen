67765bed1e03d7ab9aed7972255a242ddeeacfa4
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-10-14 20:04:05 +0000

    "as" not "to" ... you pointed that out the first time, miker, what is your deal?
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@18343 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0438.schema.bytea-index-label.sql
+++ Open-ILS/src/sql/Pg/upgrade/0438.schema.bytea-index-label.sql
@@ -4,6 +4,6 @@ BEGIN;
 INSERT INTO config.upgrade_log (version) VALUES ('0438'); -- miker
 
 DROP INDEX asset.asset_call_number_upper_label_id_owning_lib_idx;
-CREATE INDEX asset_call_number_upper_label_id_owning_lib_idx ON asset.call_number (cast(upper(label) to bytea),id,owning_lib);
+CREATE INDEX asset_call_number_upper_label_id_owning_lib_idx ON asset.call_number (cast(upper(label) as bytea),id,owning_lib);
 
 COMMIT;
