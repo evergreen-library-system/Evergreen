41dd157274aaaa9fb36f9f4f8b277b6bfcfdc2a7
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-10-14 19:34:07 +0000

    backward compat indexing for label instead of label_sortkey
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_0@18337 dcc99617-32d9-48b4-a31d-7c20da2025e4

---
+++ Open-ILS/src/sql/Pg/upgrade/0438.schema.bytea-index-label.sql
@@ -0,0 +1,9 @@
+
+BEGIN;
+
+INSERT INTO config.upgrade_log (version) VALUES ('0437'); -- miker
+
+DROP INDEX asset.asset_call_number_upper_label_id_owning_lib_idx;
+CREATE INDEX asset_call_number_upper_label_id_owning_lib_idx ON asset.call_number (cast(upper(label) to bytea),id,owning_lib);
+
+COMMIT;
