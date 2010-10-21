0e7bde163ebdf4873d566045343442427a971980
gmc <gmc@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-10-21 14:54:44 +0000

    improve call number sorting
    oils_text_as_bytea now does uppercasing in addition
    to converting strings to bytea, working around
    the limitation that json_query can't stack transforms
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6_1@18438 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/1.6.1.2-1.6.1.3-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/1.6.1.2-1.6.1.3-upgrade-db.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
 CREATE OR REPLACE FUNCTION oils_text_as_bytea (TEXT) RETURNS BYTEA AS $_$
-    SELECT CAST(REGEXP_REPLACE($1, $$\\$$, $$\\\\$$, 'g') AS BYTEA);
+    SELECT CAST(REGEXP_REPLACE(UPPER($1), $$\\$$, $$\\\\$$, 'g') AS BYTEA);
 $_$ LANGUAGE SQL IMMUTABLE;
 
 DROP INDEX asset.asset_call_number_upper_label_id_owning_lib_idx;
-CREATE INDEX asset_call_number_upper_label_id_owning_lib_idx ON asset.call_number (oils_text_as_bytea(upper(label)),id,owning_lib);
+CREATE INDEX asset_call_number_upper_label_id_owning_lib_idx ON asset.call_number (oils_text_as_bytea(label),id,owning_lib);
 
 COMMIT;
 
