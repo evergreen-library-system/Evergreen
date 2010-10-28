57f918024457c692f9d5629ac703f7349c5f81f2
gmc <gmc@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-10-28 16:12:51 +0000

    adjust user tests that get done when retargeting holds
    Fixes bug where a user having the maximum number of 
    active hold requests allowed to them by policy would
    prevent items from being targeted to fill their requests.
    
    Backport of r18507
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_1_6_0@18524 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/perlmods/OpenILS/Application/Storage/Publisher/action.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Storage/Publisher/action.pm
@@ -1456,6 +1456,7 @@ sub choose_nearest_copy {
 				  requestor => $hold->requestor->to_fieldmapper,
 				  request_lib => $hold->request_lib->to_fieldmapper,
 				  pickup_lib => $hold->pickup_lib->id,
+				  retarget => 1
 				}
 			));
 			$seen{$c->id}++;
