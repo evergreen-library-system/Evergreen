da886095307c352d461196a426d9a914971a1e86
gmc <gmc@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-11-05 18:58:53 +0000

    #671213: remove extraneous insert from upgrade script
    Somebody upgrading from 1.6.1 should already have this.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_0@18640 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/1.6.1-2.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/1.6.1-2.0-upgrade-db.sql
@@ -3651,33 +3651,6 @@ INSERT INTO action_trigger.environment (
         ( 19, 'cancel_reason' )
     ;
 
-INSERT INTO action_trigger.event_definition (id, active, owner, name, hook, validator, reactor, delay, template) 
-    VALUES (20, 'f', 1, 'Password reset request notification', 'password.reset_request', 'NOOP_True', 'SendEmail', '00:00:01',
-$$
-[%- USE date -%]
-[%- user = target.usr -%]
-To: [%- params.recipient_email || user.email %]
-From: [%- params.sender_email || user.home_ou.email || default_sender %]
-Subject: [% user.home_ou.name %]: library account password reset request
-  
-You have received this message because you, or somebody else, requested a reset
-of your library system password. If you did not request a reset of your library
-system password, just ignore this message and your current password will
-continue to work.
-
-If you did request a reset of your library system password, please perform
-the following steps to continue the process of resetting your password:
-
-1. Open the following link in a web browser: https://[% params.hostname %]/opac/password/[% params.locale || 'en-US' %]/[% target.uuid %]
-The browser displays a password reset form.
-
-2. Enter your new password in the password reset form in the browser. You must
-enter the password twice to ensure that you do not make a mistake. If the
-passwords match, you will then be able to log in to your library system account
-with the new password.
-
-$$);
-
 INSERT INTO action_trigger.hook (key, core_type, description, passive)
     VALUES (
         'format.acqcle.html',
