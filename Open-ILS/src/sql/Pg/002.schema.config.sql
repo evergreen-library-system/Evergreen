03ce73057c30df1d2381a13f44604adf2a5fc3e3
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-11-15 18:09:56 +0000

    bumping version numbers
    git-svn-id: svn://svn.open-ils.org/ILS/tags/rel_2_0_beta2@18743 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -70,7 +70,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0464'); -- dbs
+INSERT INTO config.upgrade_log (version) VALUES ('2.0-beta2');
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
