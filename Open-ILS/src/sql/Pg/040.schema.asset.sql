e4c5617449cf817080f3f9872bb7d95845ae0174
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2010-11-24 03:31:14 +0000

    Add an index on asset.copy(create_date) for item-age browse
    Running an item-age browse against a database with a reasonable
    number of entries in asset.copy is dog-slow without an index
    like this.
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@18842 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/040.schema.asset.sql
+++ Open-ILS/src/sql/Pg/040.schema.asset.sql
@@ -88,6 +88,7 @@ CREATE INDEX cp_cn_idx ON asset.copy (call_number);
 CREATE INDEX cp_avail_cn_idx ON asset.copy (call_number);
 CREATE INDEX cp_creator_idx  ON asset.copy ( creator );
 CREATE INDEX cp_editor_idx   ON asset.copy ( editor );
+CREATE INDEX cp_create_date  ON asset.copy (create_date);
 CREATE RULE protect_copy_delete AS ON DELETE TO asset.copy DO INSTEAD UPDATE asset.copy SET deleted = TRUE WHERE OLD.id = asset.copy.id;
 
 CREATE TABLE asset.opac_visible_copies (
