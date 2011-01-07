16474ec4e99963b3fa87ef2d9e13d5dbc9e9ad19
Robert Soulliere <rsoulliere@libdog.mohawkcollege.ca>
2011-01-07 13:35:37 -0500

    Add 2.0 convert to tools. Update uograding 1.6

--- tools/2.0Convert.sh
+++ tools/2.0Convert.sh
@@ -16,16 +16,16 @@ cd ~/Evergreen-DocBook/2.0/
 rm pdf/temp.fo
 
 #generate 1.6 draft html
- xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/1.6/draft/html/ ~/Evergreen-DocBook/stylesheets/evergreen_docbook_files/evergreen_xhtml.xsl ~/Evergreen-DocBook/1.6/root.xml
+# xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/1.6/draft/html/ ~/Evergreen-DocBook/stylesheets/evergreen_docbook_files/evergreen_xhtml.xsl ~/Evergreen-DocBook/1.6/root.xml
 
 
 #Generate 1.6 PDF via FO
- xsltproc --xinclude  --output ~/Evergreen-DocBook/1.6/pdf/temp.fo ~/Evergreen-DocBook/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen-DocBook/1.6/root.xml
+# xsltproc --xinclude  --output ~/Evergreen-DocBook/1.6/pdf/temp.fo ~/Evergreen-DocBook/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen-DocBook/1.6/root.xml
 
 # must run fop from same directory as root.xml
-cd ~/Evergreen-DocBook/1.6/
+#cd ~/Evergreen-DocBook/1.6/
 
- ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/1.6/draft/pdf/Evergreen_Documentation.pdf 
+# ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/1.6/draft/pdf/Evergreen_Documentation.pdf 
 
 # remove temporary .fo file
-rm pdf/temp.fo
+#rm pdf/temp.fo
