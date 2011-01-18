735357c5f5e8b55f5b5b8694664e83f71bd86c4b
dbwells <dbwells@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-01-18 18:15:29 +0000

    Auto-generation of placeholder barcodes approved for backport
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_0@19191 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/040.schema.asset.sql
+++ Open-ILS/src/sql/Pg/040.schema.asset.sql
@@ -682,5 +682,18 @@ BEGIN
 END;
 $f$ LANGUAGE PLPGSQL;
 
+CREATE OR REPLACE FUNCTION asset.autogenerate_placeholder_barcode ( ) RETURNS TRIGGER AS $f$
+BEGIN
+	IF NEW.barcode LIKE '@@%' THEN
+		NEW.barcode := '@@' || NEW.id;
+	END IF;
+	RETURN NEW;
+END;
+$f$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER autogenerate_placeholder_barcode
+	BEFORE INSERT OR UPDATE ON asset.copy
+	FOR EACH ROW EXECUTE PROCEDURE asset.autogenerate_placeholder_barcode();
+
 COMMIT;
 
