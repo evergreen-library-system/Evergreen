940bfbf183fa896c8d21444a7855a96c22b4448e
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-03-05 03:15:57 +0000

    Correct search index definitions for subject|complete and identifier|bibcn
    Dan Wells reported in LP# 729385 that the index definitions for
    subject|complete and identifier|bibcn were not working, and traced
    the problem to a change in how indexes are generated in 2.0+. Dropping
    the trailing '//text()' on these two indexes addresses the problem.
    
    Sites upgrading from a previous version of Evergreen 2.0 will want
    to refresh the indexes after applying this schema change to enable
    searches against subject|complete and identifier|bibcn to work.
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_1@19588 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -70,7 +70,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0493'); -- dbs
+INSERT INTO config.upgrade_log (version) VALUES ('0494'); -- dbs
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
