a4ddbbce73139e122087597433a3074e4026612e
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-03-16 17:51:21 +0000

    version bumps
    git-svn-id: svn://svn.open-ils.org/ILS/tags/rel_2_0_4@19780 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -70,6 +70,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
+INSERT INTO config.upgrade_log (version) VALUES ('2.0.4');
 INSERT INTO config.upgrade_log (version) VALUES ('0500'); -- miker for dbs
 
 CREATE TABLE config.bib_source (
