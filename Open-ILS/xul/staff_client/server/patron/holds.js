24e49468b5d0edc9a197232d096d2de25d46cfe9
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-03-21 16:47:01 +0000

    no, this was correct.  doh :)  the list version uses paging, but we're counting the entire set
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_0@19830 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/xul/staff_client/server/patron/holds.js
+++ Open-ILS/xul/staff_client/server/patron/holds.js
@@ -1492,7 +1492,7 @@ patron.holds.prototype = {
                     params.push( obj.pull_from_shelf_interface.current.limit ); params.push( obj.pull_from_shelf_interface.current.offset );
                     var x = document.getElementById('hold_count');
                     if (x) {
-                        obj.network.simple_request('FM_AHR_PULL_LIST_COUNT',params,function(req) {
+                        obj.network.simple_request('FM_AHR_PULL_LIST_COUNT',[ses()],function(req) {
                             var count = req.getResultObject();
                             if (typeof count.ilsevent == 'undefined' && count != null) {
                                 x.setAttribute('value', document.getElementById('patronStrings').getFormattedString(
