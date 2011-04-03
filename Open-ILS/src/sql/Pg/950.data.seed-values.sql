da36fd8365210436597c7499c44bf85e5a6c6379
Mike Rylander <mrylander@gmail.com>
2011-04-03 18:49:08 -0400

    Seed data for Authority Control Sets -- LoC

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -9392,4 +9392,119 @@ INSERT INTO action_trigger.environment ( event_def, path) VALUES (
     ,( 47, 'record.queue.owner')
 ;
 
+SELECT SETVAL('authority.control_set_id_seq'::TEXT, 100);
+SELECT SETVAL('authority.control_set_authority_field_id_seq'::TEXT, 1000);
+SELECT SETVAL('authority.control_set_bib_field_id_seq'::TEXT, 1000);
+
+INSERT INTO authority.control_set (id, name, description) VALUES (
+    1,
+    oils_i18n_gettext('1','LoC','acs','name'),
+    oils_i18n_gettext('1','Library of Congress standard authority record control semantics','acs','description')
+);
+
+INSERT INTO authority.control_set_authority_field (id, control_set, main_entry, tag, sf_list, name) VALUES
+
+-- Main entries
+    (1, 1, NULL, '100', 'abcdefklmnopqrstvxyz', oils_i18n_gettext('1','Heading -- Personal Name','acsaf','name')),
+    (2, 1, NULL, '110', 'abcdefgklmnoprstvxyz', oils_i18n_gettext('2','Heading -- Corporate Name','acsaf','name')),
+    (3, 1, NULL, '111', 'acdefgklnpqstvxyz', oils_i18n_gettext('3','Heading -- Meeting Name','acsaf','name')),
+    (4, 1, NULL, '130', 'adfgklmnoprstvxyz', oils_i18n_gettext('4','Heading -- Uniform Title','acsaf','name')),
+    (5, 1, NULL, '150', 'abvxyz', oils_i18n_gettext('5','Heading -- Topical Term','acsaf','name')),
+    (6, 1, NULL, '151', 'avxyz', oils_i18n_gettext('6','Heading -- Geographic Name','acsaf','name')),
+    (7, 1, NULL, '155', 'avxyz', oils_i18n_gettext('7','Heading -- Genre/Form Term','acsaf','name')),
+    (8, 1, NULL, '180', 'vxyz', oils_i18n_gettext('8','Heading -- General Subdivision','acsaf','name')),
+    (9, 1, NULL, '181', 'vxyz', oils_i18n_gettext('9','Heading -- Geographic Subdivision','acsaf','name')),
+    (10, 1, NULL, '182', 'vxyz', oils_i18n_gettext('10','Heading -- Chronological Subdivision','acsaf','name')),
+    (11, 1, NULL, '185', 'vxyz', oils_i18n_gettext('11','Heading -- Form Subdivision','acsaf','name')),
+    (12, 1, NULL, '148', 'avxyz', oils_i18n_gettext('12','Heading -- Chronological Term','acsaf','name')),
+
+-- See Also tracings
+    (21, 1, 1, '500', 'abcdefiklmnopqrstvwxyz4', oils_i18n_gettext('21','See Also From Tracing -- Personal Name','acsaf','name')),
+    (22, 1, 2, '510', 'abcdefgiklmnoprstvwxyz4', oils_i18n_gettext('22','See Also From Tracing -- Corporate Name','acsaf','name')),
+    (23, 1, 3, '511', 'acdefgiklnpqstvwxyz4', oils_i18n_gettext('23','See Also From Tracing -- Meeting Name','acsaf','name')),
+    (24, 1, 4, '530', 'adfgiklmnoprstvwxyz4', oils_i18n_gettext('24','See Also From Tracing -- Uniform Title','acsaf','name')),
+    (25, 1, 5, '550', 'abivwxyz4', oils_i18n_gettext('25','See Also From Tracing -- Topical Term','acsaf','name')),
+    (26, 1, 6, '551', 'aivwxyz4', oils_i18n_gettext('26','See Also From Tracing -- Geographic Name','acsaf','name')),
+    (27, 1, 7, '555', 'aivwxyz4', oils_i18n_gettext('27','See Also From Tracing -- Genre/Form Term','acsaf','name')),
+    (28, 1, 8, '580', 'ivwxyz4', oils_i18n_gettext('28','See Also From Tracing -- General Subdivision','acsaf','name')),
+    (29, 1, 9, '581', 'ivwxyz4', oils_i18n_gettext('29','See Also From Tracing -- Geographic Subdivision','acsaf','name')),
+    (30, 1, 10, '582', 'ivwxyz4', oils_i18n_gettext('30','See Also From Tracing -- Chronological Subdivision','acsaf','name')),
+    (31, 1, 11, '585', 'ivwxyz4', oils_i18n_gettext('31','See Also From Tracing -- Form Subdivision','acsaf','name')),
+    (32, 1, 12, '548', 'aivwxyz4', oils_i18n_gettext('32','See Also From Tracing -- Chronological Term','acsaf','name')),
+
+-- Linking entries
+    (41, 1, 1, '700', 'abcdefghjklmnopqrstvwxyz25', oils_i18n_gettext('41','Established Heading Linking Entry -- Personal Name','acsaf','name')),
+    (42, 1, 2, '710', 'abcdefghklmnoprstvwxyz25', oils_i18n_gettext('42','Established Heading Linking Entry -- Corporate Name','acsaf','name')),
+    (43, 1, 3, '711', 'acdefghklnpqstvwxyz25', oils_i18n_gettext('43','Established Heading Linking Entry -- Meeting Name','acsaf','name')),
+    (44, 1, 4, '730', 'adfghklmnoprstvwxyz25', oils_i18n_gettext('44','Established Heading Linking Entry -- Uniform Title','acsaf','name')),
+    (45, 1, 5, '750', 'abvwxyz25', oils_i18n_gettext('45','Established Heading Linking Entry -- Topical Term','acsaf','name')),
+    (46, 1, 6, '751', 'avwxyz25', oils_i18n_gettext('46','Established Heading Linking Entry -- Geographic Name','acsaf','name')),
+    (47, 1, 7, '755', 'avwxyz25', oils_i18n_gettext('47','Established Heading Linking Entry -- Genre/Form Term','acsaf','name')),
+    (48, 1, 8, '780', 'vwxyz25', oils_i18n_gettext('48','Subdivision Linking Entry -- General Subdivision','acsaf','name')),
+    (49, 1, 9, '781', 'vwxyz25', oils_i18n_gettext('49','Subdivision Linking Entry -- Geographic Subdivision','acsaf','name')),
+    (50, 1, 10, '782', 'vwxyz25', oils_i18n_gettext('50','Subdivision Linking Entry -- Chronological Subdivision','acsaf','name')),
+    (51, 1, 11, '785', 'vwxyz25', oils_i18n_gettext('51','Subdivision Linking Entry -- Form Subdivision','acsaf','name')),
+    (52, 1, 12, '748', 'avwxyz25', oils_i18n_gettext('52','Established Heading Linking Entry -- Chronological Term','acsaf','name'));
+
+INSERT INTO authority.control_set_bib_field (tag, authority_field) VALUES
+    SELECT '100', id FROM authority.control_set_authority_field WHERE tag IN ('100','500','700')
+        UNION
+    SELECT '600', id FROM authority.control_set_authority_field WHERE tag IN ('100','500','700','180','181','182','185','580','581','582','585','780','781','782','785')
+        UNION
+    SELECT '700', id FROM authority.control_set_authority_field WHERE tag IN ('100','500','700')
+        UNION
+    SELECT '800', id FROM authority.control_set_authority_field WHERE tag IN ('100','500','700')
+        UNION
+
+    SELECT '110', id FROM authority.control_set_authority_field WHERE tag IN ('110','510','710')
+        UNION
+    SELECT '610', id FROM authority.control_set_authority_field WHERE tag IN ('110','510','710')
+        UNION
+    SELECT '710', id FROM authority.control_set_authority_field WHERE tag IN ('110','510','710')
+        UNION
+    SELECT '810', id FROM authority.control_set_authority_field WHERE tag IN ('110','510','710')
+        UNION
+
+    SELECT '111', id FROM authority.control_set_authority_field WHERE tag IN ('111','511','711')
+        UNION
+    SELECT '611', id FROM authority.control_set_authority_field WHERE tag IN ('111','511','711')
+        UNION
+    SELECT '711', id FROM authority.control_set_authority_field WHERE tag IN ('111','511','711')
+        UNION
+    SELECT '811', id FROM authority.control_set_authority_field WHERE tag IN ('111','511','711')
+        UNION
+
+    SELECT '130', id FROM authority.control_set_authority_field WHERE tag IN ('130','530','730')
+        UNION
+    SELECT '240', id FROM authority.control_set_authority_field WHERE tag IN ('130','530','730')
+        UNION
+    SELECT '630', id FROM authority.control_set_authority_field WHERE tag IN ('130','530','730')
+        UNION
+    SELECT '730', id FROM authority.control_set_authority_field WHERE tag IN ('130','530','730')
+        UNION
+    SELECT '830', id FROM authority.control_set_authority_field WHERE tag IN ('130','530','730')
+        UNION
+
+    SELECT '648', id FROM authority.control_set_authority_field WHERE tag IN ('148','548','748')
+        UNION
+
+    SELECT '650', id FROM authority.control_set_authority_field WHERE tag IN ('150','550','750','180','181','182','185','580','581','582','585','780','781','782','785')
+        UNION
+    SELECT '651', id FROM authority.control_set_authority_field WHERE tag IN ('151','551','751','180','181','182','185','580','581','582','585','780','781','782','785')
+        UNION
+    SELECT '655', id FROM authority.control_set_authority_field WHERE tag IN ('155','555','755','180','181','182','185','580','581','582','585','780','781','782','785')
+;
+
+INSERT INTO authority.thesaurus (code, name, control_set) VALUES
+    ('a', oils_i18n_gettext('a','Library of Congress Subject Headings','at','name'), 1),
+    ('b', oils_i18n_gettext('b',$$LC subject headings for children's literature$$,'at','name'), 1), -- silly vim '
+    ('c', oils_i18n_gettext('c','Medical Subject Headings','at','name'), 1),
+    ('d', oils_i18n_gettext('d','National Agricultural Library subject authority file','at','name'), 1),
+    ('k', oils_i18n_gettext('k','Canadian Subject Headings','at','name'), 1),
+    ('n', oils_i18n_gettext('n','Not applicable','at','name'), 1),
+    ('r', oils_i18n_gettext('r','Art and Architecture Thesaurus','at','name'), 1),
+    ('s', oils_i18n_gettext('s','Sears List of Subject Headings','at','name'), 1),
+    ('v', oils_i18n_gettext('v','Repertoire de vedettes-matiere','at','name'), 1),
+    ('z', oils_i18n_gettext('z','Other','at','name') 1),
+    ('|', oils_i18n_gettext('|','No attempt to code','at','name') 1);
 
