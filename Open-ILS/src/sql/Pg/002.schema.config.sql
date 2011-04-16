7623febd2d5edcbf95d6766bb3ac9b9abce199a3
dbs <dbs@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-04-16 02:20:06 +0000

    Delete ##URI## call numbers and uri_call_number_map entries on bib reingest
    This approach will lead to some acn/auricnm ID inflation, but it works.
    
    Addresses LP# 761130 (immortal ##URI## entries in asset.call_number) reported
    by Ben Shum and LP# 761085 (cannot delete bib with ##URI## volumes) reported
    by Jason Etheridge.
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_0@20119 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -70,7 +70,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0517'); -- miker
+INSERT INTO config.upgrade_log (version) VALUES ('0519'); -- dbs
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
