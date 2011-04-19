49355fc8e10faf427060de52f80308a1f9d514ad
phasefx <phasefx@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-04-19 15:52:54 +0000

    lock in upgrade script version #, tweak org setting labels and descriptions
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>
    
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@20208 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0522.client_menus.sql
+++ Open-ILS/src/sql/Pg/upgrade/0522.client_menus.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
-INSERT INTO config.upgrade_log (version) VALUES ('XXXX');
+INSERT INTO config.upgrade_log (version) VALUES ('0522'); -- tsbere/phasefx
 
 UPDATE config.org_unit_setting_type SET datatype = 'string' WHERE name = 'ui.general.button_bar';
 
-INSERT INTO config.org_unit_setting_type ( name, label, description, datatype) VALUES ('ui.general.hotkeyset', 'Default Hotkeyset', 'Default Hotkeyset for clients (filename without the .keyset)', 'string');
+INSERT INTO config.org_unit_setting_type ( name, label, description, datatype) VALUES ('ui.general.hotkeyset', 'GUI: Default Hotkeyset', 'Default Hotkeyset for clients (filename without the .keyset).  Examples: Default, Minimal, and None', 'string');
 
 UPDATE actor.org_unit_setting SET value='"circ"' WHERE name = 'ui.general.button_bar' AND value='true';
 
