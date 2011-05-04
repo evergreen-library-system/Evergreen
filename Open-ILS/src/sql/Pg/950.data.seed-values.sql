c8a6390b4313d7075a3260eecc9957f5a3c7d7e8
miker <miker@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-05-04 05:18:18 +0000

    Collaboration patch from Joseph Lewis and Michael Peters addressing LP#758007, missing patron merge OU settings.
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_0@20409 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -7627,3 +7627,30 @@ INSERT INTO action_trigger.event_definition (
     )
 ;
 
+
+-- add patron merge org unit settings
+
+INSERT INTO config.org_unit_setting_type 
+( name, label, description, datatype ) VALUES 
+( 'circ.user_merge.delete_addresses', 
+  'Circ:  Patron Merge Address Delete', 
+  'Delete address(es) of subordinate user(s) in a patron merge', 
+   'bool'
+);
+
+INSERT INTO config.org_unit_setting_type 
+( name, label, description, datatype ) VALUES 
+( 'circ.user_merge.delete_cards', 
+  'Circ: Patron Merge Barcode Delete', 
+  'Delete barcode(s) of subordinate user(s) in a patron merge', 
+  'bool'
+);
+
+INSERT INTO config.org_unit_setting_type 
+( name, label, description, datatype ) VALUES 
+( 'circ.user_merge.deactivate_cards', 
+  'Circ:  Patron Merge Deactivate Card', 
+  'Mark barcode(s) of subordinate user(s) in a patron merge as inactive', 
+  'bool'
+);
+
