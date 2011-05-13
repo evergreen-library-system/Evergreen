3341913654605eff25ec1360fe15bff6a6a6f172
gmc <gmc@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-05-13 13:25:27 +0000

    for upgrade, let prefix and suffix in audit be null
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    
    git-svn-id: svn://svn.open-ils.org/ILS/trunk@20466 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/upgrade/0531.schema.auditor_affixes.sql
+++ Open-ILS/src/sql/Pg/upgrade/0531.schema.auditor_affixes.sql
@@ -3,11 +3,7 @@ BEGIN;
 INSERT INTO config.upgrade_log (version) VALUES ('0531'); --gmc
 
 ALTER TABLE auditor.asset_call_number_history
-    ADD COLUMN prefix INT NOT NULL DEFAULT -1,
-    ADD COLUMN suffix INT NOT NULL DEFAULT -1;
-
-ALTER TABLE auditor.asset_call_number_history
-    ALTER COLUMN prefix DROP DEFAULT,
-    ALTER COLUMN suffix DROP DEFAULT;
+    ADD COLUMN prefix INT,
+    ADD COLUMN suffix INT;
 
 COMMIT;
