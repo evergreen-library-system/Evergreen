9b049dd9712b55602c8977a5a5dfd1df52086f68
gmc <gmc@dcc99617-32d9-48b4-a31d-7c20da2025e4>
2011-05-13 13:09:54 +0000

    add affix columns to acn auditor table
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    
    git-svn-id: svn://svn.open-ils.org/ILS/branches/rel_2_1@20465 dcc99617-32d9-48b4-a31d-7c20da2025e4

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0530'); -- senator, for Niles Ingalls
+INSERT INTO config.upgrade_log (version) VALUES ('0531'); -- gmc
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
