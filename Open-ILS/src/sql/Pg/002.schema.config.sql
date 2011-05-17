d6f8124b33f738bd06e0e83f7b2ba815ad3165bf
Dan Scott <dan@coffeecode.net>
2011-05-17 00:29:47 -0400

    LP#783486: Improve authority lookup performance
    Testing showed that the two indexes added herein improve
    authority lookup performance radically; on a 16 GB RAM
    database server with 1.1M records, lookup times decreased
    from 5500 ms to 0.223 ms. Recommended, would EXPLAIN ANALYZE
    again.
    
    Signed-off-by: Dan Scott <dan@coffeecode.net>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -86,7 +86,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0534', :eg_version); -- gmc
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0535', :eg_version); -- dbs
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
