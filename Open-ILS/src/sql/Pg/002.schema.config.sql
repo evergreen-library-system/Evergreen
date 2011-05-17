f2a547a6c0bcf50e7df6dfa5bd5467c342115487
Galen Charlton <gmc@esilibrary.com>
2011-05-17 00:40:02 -0400

    stop hold permit check from crashing if age protection in effect
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0533'); -- gmc
+INSERT INTO config.upgrade_log (version) VALUES ('0534'); -- gmc
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
