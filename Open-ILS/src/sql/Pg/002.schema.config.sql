c4b9bc54d61fccb0c872b8fb62b3b553f27c40d6
Dan Scott <dan@coffeecode.net>
2011-06-07 13:52:22 -0400

    Use native unnest() db function instead of explode_array()
    Per LP 789747, the custom explode_array() database function
    was found to generate significantly slower results for searches than
    using the native (and equivalent) unnest() array function. Given that
    unnest() has been part of PostgreSQL since 8.4 and Evergreen 2.0
    requires a minimum of PostgreSQL 8.4, this patch removes the definition
    of the explode_array() functions and replaces its usage with unnest().
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0547'); -- dbwells
+INSERT INTO config.upgrade_log (version) VALUES ('0554'); -- dbs
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
---
+++ Open-ILS/src/sql/Pg/upgrade/0552.unnest_biblio_map_authority_linking.sql
@@ -0,0 +1,22 @@
+-- Evergreen DB patch 0552.unnest_biblio_map_authority_linking.sql
+--
+-- Replace usage of custom explode_array() function with native unnest()
+--
+BEGIN;
+
+INSERT INTO config.upgrade_log (version) VALUES ('0550'); -- dbs
+
+CREATE OR REPLACE FUNCTION biblio.map_authority_linking (bibid BIGINT, marc TEXT) RETURNS BIGINT AS $func$
+    DELETE FROM authority.bib_linking WHERE bib = $1;
+    INSERT INTO authority.bib_linking (bib, authority)
+        SELECT  y.bib,
+                y.authority
+          FROM (    SELECT  DISTINCT $1 AS bib,
+                            BTRIM(remove_paren_substring(txt))::BIGINT AS authority
+                      FROM  unnest(oils_xpath('//*[@code="0"]/text()',$2)) x(txt)
+                      WHERE BTRIM(remove_paren_substring(txt)) ~ $re$^\d+$$re$
+                ) y JOIN authority.record_entry r ON r.id = y.authority;
+    SELECT $1;
+$func$ LANGUAGE SQL;
+
+COMMIT;
