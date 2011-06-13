5b0ceba2004fba7d3b45e7c38402103045221fae
Mike Rylander <mrylander@gmail.com>
2011-06-13 10:45:20 -0400

    Thinko fix -- split upgrade script, forgot to inc the version in the second
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0558.schema.in-db_unapi_copy_related_visibility-2.sql
+++ Open-ILS/src/sql/Pg/upgrade/0558.schema.in-db_unapi_copy_related_visibility-2.sql
@@ -6,7 +6,7 @@ BEGIN;
 
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('0557', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0558', :eg_version);
 
 CREATE OR REPLACE FUNCTION unapi.ccs ( obj_id BIGINT, format TEXT,  ename TEXT, includes TEXT[], org TEXT, depth INT DEFAULT NULL, slimit INT DEFAULT NULL, soffset INT DEFAULT NULL, include_xmlns BOOL DEFAULT TRUE ) RETURNS XML AS $F$
     SELECT  XMLELEMENT(
