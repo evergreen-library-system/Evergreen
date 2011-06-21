94c4dfa33c61f11ec22c3901237da5333144ada7
Mike Rylander <mrylander@gmail.com>
2011-06-21 11:44:02 -0400

    Reserve 0561 (used in 2.0) to avoid confusion
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0560'); -- tsbere via gmc
+INSERT INTO config.upgrade_log (version) VALUES ('0561'); -- miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
