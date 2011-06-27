688c8ee6e76cb142fd26b16808a22c6ec85c3f2c
Mike Rylander <mrylander@gmail.com>
2011-06-27 18:54:13 -0400

    Backport of cache visbility trigger speedup
    Backporting:
     cdcbbbbc520e91f003b68d716dcd0d75a2b68971
     27bed378975a1164232d0b922cd03ce7493dee82
     f762b75ac430ed70365ebc0385d2c9d0da04ab26
    
    from master
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0566'); -- miker/berick
+INSERT INTO config.upgrade_log (version) VALUES ('0568'); -- miker for tsbere
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
