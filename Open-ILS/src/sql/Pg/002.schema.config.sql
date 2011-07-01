52402b4d28643ca013d83f87de6fecb8e0d81813
Mike Rylander <mrylander@gmail.com>
2011-07-01 11:50:25 -0400

    Normalize facet values
    Spotted by Lebbeous Fogle-Weekley, facet values were not being normalized.  This is due to changes in how facets are stored in 2.1+.  This alternate implementation mirrors the way search_field index definitions work, which is a bit cleaner than inserting normalization into the field extraction process.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -86,7 +86,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0570', :eg_version); -- senator
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0571', :eg_version); -- miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
