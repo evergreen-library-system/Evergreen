01cfea29c585e566f76bfdcf9f6724d48a302f33
Thomas Berezansky <tsbere@mvlc.org>
2011-07-05 12:15:19 -0400

    Unwrapped upgrade script
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.opac_renewal_reuse_circ_lib.sql
@@ -0,0 +1,11 @@
+INSERT INTO config.global_flag (name, label, enabled)
+    VALUES (
+        'circ.opac_renewal.use_original_circ_lib',
+        oils_i18n_gettext(
+            'circ.opac_renewal.use_original_circ_lib',
+            'Circ: Use original circulation library on opac renewal instead of user home library',
+            'cgf',
+            'label'
+        ),
+        TRUE
+    );
