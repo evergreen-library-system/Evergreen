a27f472feb7653988a42ff450d9ba1be4fc698bf
Mike Rylander <mrylander@gmail.com>
2011-07-07 14:34:20 -0400

    Backport of staff_search_find_no_copies branch from Thomas Berezansky
    Hand-backport of:
     4d0dae6ee876830cb87ee393a41fcdc478ecc6b0
     9a356a2371930c76554fd3dc5a7d10dc21cf914a
     a45184da2ac9f490886ebde47fcc8af4227afcf0
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0561'); -- dbs via miker
+INSERT INTO config.upgrade_log (version) VALUES ('0573'); -- miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
