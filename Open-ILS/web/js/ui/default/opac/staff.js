936ab552ec7f46ed491fe1761532358d5a0d9512
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2011-07-11 18:17:13 -0400

    When coming from staff client patron interface,
    pre-populate the patron barcode text box by which staff places a hold
    for a patron.
    
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/web/js/ui/default/opac/staff.js
+++ Open-ILS/web/js/ui/default/opac/staff.js
@@ -14,13 +14,28 @@ function runEvt(scope, name) {
         for(var i in evt) {evt[i].apply(evt[i], args);}
     } 
 }
-function staff_hold_nicety(thing) {
+function staff_hold_usr_input_disabler(input) {
     document.getElementById("hold_usr_input").disabled =
-        Boolean(Number(thing.value));
+        Boolean(Number(input.value));
+}
+function staff_hold_usr_prepop() {
+    if (xulG && xulG.patron_barcode) {
+        document.getElementById("hold_usr_input").value = xulG.patron_barcode;
+        document.getElementById("hold_usr_input").disabled = false;
+        document.getElementById("hold_usr_is_requestor_not").checked = true;
+
+        var kill_this =
+            document.getElementById("hold_usr_is_requestor").parentNode;
+        kill_this.parentNode.removeChild(kill_this);
+    }
 }
 window.onload = function() {
     // record details page events
     var rec = location.href.match(/\/opac\/record\/(\d+)/);
     if(rec && rec[1]) { runEvt('rdetail', 'recordRetrieved', rec[1]); }
+
+    if (document.getElementById("hold_usr_input"))
+        staff_hold_usr_prepop();
+
     // fire other events the staff client is expecting...
 }
