581ec7cf25074b8c6911366026ca3ee68f109452
Galen Charlton <gmc@esilibrary.com>
2011-07-13 10:59:15 -0400

    fix backporting thinko
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0576.fix_maintain_901_quoting.sql
+++ Open-ILS/src/sql/Pg/upgrade/0576.fix_maintain_901_quoting.sql
@@ -6,7 +6,7 @@
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('0576', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0576');
 
 CREATE OR REPLACE FUNCTION evergreen.maintain_901 () RETURNS TRIGGER AS $func$
 DECLARE
