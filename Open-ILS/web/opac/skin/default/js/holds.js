8589cf090b400c2e4e35233825e45027a4fde15d
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2011-07-14 16:41:42 -0400

    Corrected the logic of format string building for metarecord holds and...
    made hold possiblity checking for metarecord holds respect the format
    string (so you can't place holds on formats for which there will never
    be anything targetable)
    
    Made possible by much concerted effort from Mike Rylander
    
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/web/opac/skin/default/js/holds.js
+++ Open-ILS/web/opac/skin/default/js/holds.js
@@ -700,14 +700,14 @@ function holdsSetSelectedFormats() {
 
 	var fstring = "";
 
-	if( contains(vals, 'at-d') || contains(vals, 'at-s')) {
+	if( contains(vals, 'at-d') || contains(vals, 'at-s') || contains(vals, 'at')) {
 		if( contains(vals, 'at') ) {
 			fstring = 'at';
 		} else if (contains(vals, 'at-s') && contains(vals, 'at-d')) {
 			fstring = 'at-sd';
 		} else if (!contains(vals, 'at-s')) {
 			fstring = 'at-d';
-	} else {
+		} else {
 			fstring = 'at-s';
 		}
 	}
@@ -738,6 +738,7 @@ function holdsCheckPossibility(pickuplib, hold, recurse) {
 		issuanceid : holdArgs.issuance,
 		copy_id : holdArgs.copy,
 		hold_type : holdArgs.type,
+		holdable_formats : holdArgs.holdable_formats,
 		patronid : holdArgs.recipient.id(),
 		depth : 0, 
 		pickup_lib : pickuplib 
@@ -876,6 +877,8 @@ function holdsBuildHoldFromWindow() {
 	if(fstring) { 
 		hold.hold_type('M'); 
 		hold.holdable_formats(fstring);
+		if (fstring)
+			holdArgs.holdable_formats = fstring;
 		hold.target(holdArgs.metarecord);
 	}
 	return hold;
