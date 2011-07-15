5166299cd88855b4a872733b1b5896ef29fd97bf
Thomas Berezansky <tsbere@mvlc.org>
2011-07-15 15:55:18 -0400

    Unwrapped upgrade script
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.part_holds_bib_report.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE VIEW reporter.hold_request_record AS
+SELECT	id,
+	target,
+	hold_type,
+	CASE
+		WHEN hold_type = 'T'
+			THEN target
+		WHEN hold_type = 'I'
+			THEN (SELECT ssub.record_entry FROM serial.subscription ssub JOIN serial.issuance si ON (si.subscription = ssub.id) WHERE si.id = ahr.target)
+		WHEN hold_type = 'V'
+			THEN (SELECT cn.record FROM asset.call_number cn WHERE cn.id = ahr.target)
+		WHEN hold_type IN ('C','R','F')
+			THEN (SELECT cn.record FROM asset.call_number cn JOIN asset.copy cp ON (cn.id = cp.call_number) WHERE cp.id = ahr.target)
+		WHEN hold_type = 'M'
+			THEN (SELECT mr.master_record FROM metabib.metarecord mr WHERE mr.id = ahr.target)
+        WHEN hold_type = 'P'
+            THEN (SELECT bmp.record FROM biblio.monograph_part bmp WHERE bmp.id = ahr.target)
+	END AS bib_record
+  FROM	action.hold_request ahr;
+
