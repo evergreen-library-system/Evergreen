14504ab4e9f534776ab373c9bd69938fa97a5392
Mike Rylander <mrylander@gmail.com>
2011-07-16 10:25:44 -0400

    Stamping upgrade script for "Opac Renewal at original circ library"
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0581.schema.opac_renewal_reuse_circ_lib.sql
+++ Open-ILS/src/sql/Pg/upgrade/0581.schema.opac_renewal_reuse_circ_lib.sql
@@ -1,3 +1,12 @@
+-- Evergreen DB patch 0581.schema.opac_renewal_reuse_circ_lib.sql
+--
+--
+BEGIN;
+
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0581', :eg_version);
+
 INSERT INTO config.global_flag (name, label, enabled)
     VALUES (
         'circ.opac_renewal.use_original_circ_lib',
@@ -9,3 +18,6 @@ INSERT INTO config.global_flag (name, label, enabled)
         ),
         FALSE
     );
+
+
+COMMIT;
