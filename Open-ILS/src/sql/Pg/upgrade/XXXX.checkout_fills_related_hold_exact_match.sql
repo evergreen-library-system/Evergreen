5616e3c5ec026f9cef1aa9950ffa1625cbca1a2a
Thomas Berezansky <tsbere@mvlc.org>
2011-07-25 15:28:47 -0400

    Unwrapped Upgrade Script
    for Checkout Fills Related Hold modifications
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.checkout_fills_related_hold_exact_match.sql
@@ -0,0 +1,6 @@
+INSERT into config.org_unit_setting_type
+( name, label, description, datatype ) VALUES
+( 'circ.checkout_fills_related_hold_exact_match_only',
+    'Checkout Fills Related Hold On Valid Copy Only',
+    'When filling related holds on checkout only match on items that are valid for opportunistic capture for the hold. Without this set a Title or Volume hold could match when the item is not holdable. With this set only holdable items will match.',
+    'bool');
