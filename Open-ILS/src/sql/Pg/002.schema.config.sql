f88ac0f74d89ed215e3d1d93aca9ccbd284ede49
Bill Erickson <berick@esilibrary.com>
2011-07-28 12:28:23 -0400

    Revert "Add lccn field to the identifier search class"
    This change will be absorbed when merging working/user/dbs/oclcnumbers,
    which I will do pronto...
    
    This reverts commit 94f69d21b6759dc43c151c7796dafee74083f62c.

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -86,7 +86,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0587', :eg_version); -- senator
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0586', :eg_version); -- senator/berick
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
