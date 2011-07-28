94f69d21b6759dc43c151c7796dafee74083f62c
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2011-07-28 12:09:01 -0400

    Add lccn field to the identifier search class
    Facilitates easily searching by LCCN (010 ‡a).
    
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/0587.data.config.metabib_field.lccn.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0587', :eg_version);
+
+INSERT INTO config.metabib_field (
+    id, field_class, name, label, xpath, weight, format,
+    search_field, facet_field
+) VALUES (
+    29, 'identifier', 'lccn', oils_i18n_gettext(29, 'LCCN', 'cmf', 'label'),
+    '//marc:datafield[@tag="010"]/marc:subfield[@code="a"]', 1,
+    'marcxml', TRUE, FALSE
+);
+
+COMMIT;
