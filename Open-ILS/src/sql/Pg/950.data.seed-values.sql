a0f7fa7d5e157777ff38a22bd6f180e1b5d98028
Michael Peters <mrpeters@library.in.gov>
2011-08-04 08:59:15 -0400

    LP#818311:  ACQ_INVOICE_REOPEN is missing
    This patch adds the missing ACQ_INVOICE_REOPEN to the Acquisitions
    Administrator user.
    
    Signed-off-by: Michael Peters <mrpeters@library.in.gov>
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -1438,8 +1438,9 @@ INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 510, 'UPDATE_PATRON_COLLECTIONS_EXEMPT', oils_i18n_gettext(510,
     'Allows a user to indicate that a patron is exempt from collections processing', 'ppl', 'description')),
  ( 511, 'PERSISTENT_LOGIN', oils_i18n_gettext( 511,
-    'Allows a user to authenticate and get a long-lived session (length configured in opensrf.xml)', 'ppl', 'description' ));
-
+    'Allows a user to authenticate and get a long-lived session (length configured in opensrf.xml)', 'ppl', 'description' )),
+ ( 512, 'ACQ_INVOICE_REOPEN', oils_i18n_gettext( 512,
+    'Allows a user to reopen an Acquisitions invoice', 'ppl', 'description' ));
 
 SELECT SETVAL('permission.perm_list_id_seq'::TEXT, 1000);
 
@@ -2151,6 +2152,7 @@ INSERT INTO permission.grp_perm_map (grp, perm, depth, grantable)
 		pgt.name = 'Acquisitions Administrator' AND
 		aout.name = 'Consortium' AND
 		perm.code IN (
+			'ACQ_INVOICE_REOPEN',
 			'ACQ_XFER_MANUAL_DFUND_AMOUNT',
 			'ADMIN_ACQ_CANCEL_CAUSE',
 			'ADMIN_ACQ_CLAIM',
