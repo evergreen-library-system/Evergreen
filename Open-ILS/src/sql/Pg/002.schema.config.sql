1a4e3100c763c6a71926be16d0288238a4a2ac4d
Mike Rylander <mrylander@gmail.com>
2011-08-08 14:05:11 -0400

    Bumping version numbers and ChangeLog
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -58,6 +58,7 @@ CREATE TABLE config.upgrade_log (
 );
 
 INSERT INTO config.upgrade_log (version) VALUES ('0592'); -- mrpeters-isl/miker
+INSERT INTO config.upgrade_log (version) VALUES ('2.0.8');
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
