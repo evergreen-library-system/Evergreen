dbbb7f3e6520e61886f97c33f9ed3728d7c43c46
Galen Charlton <gmc@esilibrary.com>
2011-08-09 15:30:56 -0400

    lp 823496: don't fail to index personal names that have relators
    The default definition for the author|other index inadvertantly
    excluded names from 600, 700, 720, and 800 fields that have
    relator values ($e) or codes ($4).
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.fix_author_other_index.sql
@@ -0,0 +1,28 @@
+-- Evergreen DB patch XXXX.fix_author_other_index.sql
+--
+-- Fix author|other index so that it doesn't exclude 700
+-- fields that contain relator values in the $e or $4.
+--
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+UPDATE config.metabib_field 
+SET xpath = $$//mods32:mods/mods32:name[@type='personal' and not(mods32:role/mods32:roleTerm[text()='creator'])]$$
+WHERE field_class = 'author'
+AND name = 'other'
+AND xpath = $$//mods32:mods/mods32:name[@type='personal' and not(mods32:role)]$$
+AND format = 'mods32';
+
+-- To reindex the affected bibs, you can run something like this:
+--
+-- SELECT metabib.reingest_metabib_field_entries(record)
+-- FROM (
+--   SELECT DISTINCT record
+--   FROM metabib.real_full_rec
+--   WHERE tag IN ('600', '700', '720', '800')
+--   AND   subfield IN ('4', 'e')
+-- ) a;
+
+COMMIT;
