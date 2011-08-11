7597eabf7881bcb2fae989a00fddff6a451a69d4
Thomas Berezansky <tsbere@mvlc.org>
2011-08-11 09:36:36 -0400

    Unwrapped upgrade script for oua_force_order
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.oua_force_order.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION actor.org_unit_ancestors( INT ) RETURNS SETOF actor.org_unit AS $$
+    WITH RECURSIVE org_unit_ancestors_distance(id, distance) AS (
+            SELECT $1, 0
+        UNION
+            SELECT ou.parent_ou, ouad.distance+1
+            FROM actor.org_unit ou JOIN org_unit_ancestors_distance ouad ON (ou.id = ouad.id)
+            WHERE ou.parent_ou IS NOT NULL
+    )
+    SELECT ou.* FROM actor.org_unit ou JOIN org_unit_ancestors_distance ouad USING (id) ORDER BY ouad.distance;
+$$ LANGUAGE SQL ROWS 1;
+
