60722675c66fe28ce540e640722969da2d5b76ce
Thomas Berezansky <tsbere@mvlc.org>
2011-08-15 09:04:13 -0400

    Unwrapped upgrade script for security lockdown
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.security_lockdown.sql
@@ -0,0 +1,3 @@
+INSERT INTO permission.perm_list ( id, code, description ) VALUES
+ ( 513, 'DEBUG_CLIENT', oils_i18n_gettext( 513,
+    'Allows a user to use debug functions in the staff client', 'ppl', 'description' ));
