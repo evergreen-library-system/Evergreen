d36543b93a351f805da664690658b53054636d8d
Thomas Berezansky <tsbere@mvlc.org>
2011-08-23 08:33:04 -0400

    Five initial OU settings for %INCLUDE()% use
    header_text
    footer_text
    notice_text
    alert_text
    event_text
    
    None are really restricted to a specific purpose. Names are more of a
    hint than a requirement.
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -2858,6 +2858,87 @@ INSERT into config.org_unit_setting_type
         ),
         'integer'
     )
+,(
+        'circ.staff_client.receipt.header_text',
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.header_text',
+            'Receipt Template: Content of header_text include',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.header_text',
+            'Text/HTML/Macros to be inserted into receipt templates in place of %INCLUDE(header_text)%',
+            'coust',
+            'description'
+        ),
+        'string'
+    )
+,(
+        'circ.staff_client.receipt.footer_text',
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.footer_text',
+            'Receipt Template: Content of footer_text include',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.footer_text',
+            'Text/HTML/Macros to be inserted into receipt templates in place of %INCLUDE(footer_text)%',
+            'coust',
+            'description'
+        ),
+        'string'
+    )
+,(
+        'circ.staff_client.receipt.notice_text',
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.notice_text',
+            'Receipt Template: Content of notice_text include',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.notice_text',
+            'Text/HTML/Macros to be inserted into receipt templates in place of %INCLUDE(notice_text)%',
+            'coust',
+            'description'
+        ),
+        'string'
+    )
+,(
+        'circ.staff_client.receipt.alert_text',
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.alert_text',
+            'Receipt Template: Content of alert_text include',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.alert_text',
+            'Text/HTML/Macros to be inserted into receipt templates in place of %INCLUDE(alert_text)%',
+            'coust',
+            'description'
+        ),
+        'string'
+    )
+,(
+        'circ.staff_client.receipt.event_text',
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.event_text',
+            'Receipt Template: Content of event_text include',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'circ.staff_client.receipt.event_text',
+            'Text/HTML/Macros to be inserted into receipt templates in place of %INCLUDE(event_text)%',
+            'coust',
+            'description'
+        ),
+        'string'
+    )
+
 ;
 
 UPDATE config.org_unit_setting_type
