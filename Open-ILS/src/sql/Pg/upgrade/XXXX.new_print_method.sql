4894c8f25d5a59dbd5d830c28fc9f50fe31cb92f
Thomas Berezansky <tsbere@mvlc.org>
2011-08-24 11:32:15 -0400

    Unwrapped upgrade script for print.custom_js_file
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.new_print_method.sql
@@ -0,0 +1,20 @@
+INSERT into config.org_unit_setting_type
+( name, grp, label, description, datatype ) VALUES
+(
+        'print.custom_js_file', 'circ',
+        oils_i18n_gettext(
+            'print.custom_js_file',
+            'Printing: Custom Javascript File',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'print.custom_js_file',
+            'Full URL path to a Javascript File to be loaded when printing. Should'
+            || ' implement a print_custom function for DOM manipulation. Can change'
+            || ' the value of the do_print variable to false to cancel printing.',
+            'coust',
+            'description'
+        ),
+        'string'
+    );
