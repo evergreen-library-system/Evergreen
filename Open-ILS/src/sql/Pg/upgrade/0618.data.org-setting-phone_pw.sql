47fdb4c815166fa3630ed22bddd6a1dfa1cc8b70
Jason Etheridge <jason@esilibrary.com>
2011-09-08 01:26:34 -0400

    lp#794153 stamping upgrade script for phone pw
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0618.data.org-setting-phone_pw.sql
+++ Open-ILS/src/sql/Pg/upgrade/0618.data.org-setting-phone_pw.sql
@@ -1,3 +1,9 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0618', :eg_version);
+
 UPDATE config.org_unit_setting_type SET description = E'The Regular Expression for validation on the day_phone field in patron registration. Note: The first capture group will be used for the "last 4 digits of phone number" feature, if enabled. Ex: "[2-9]\\d{2}-\\d{3}-(\\d{4})( x\\d+)?" will ignore the extension on a NANP number.' WHERE name = 'ui.patron.edit.au.day_phone.regex';
 
 UPDATE config.org_unit_setting_type SET description = 'The Regular Expression for validation on phone fields in patron registration. Applies to all phone fields without their own setting. NOTE: See description of the day_phone regex for important information about capture groups with it.' WHERE name = 'ui.patron.edit.phone.regex';
+
+COMMIT;
