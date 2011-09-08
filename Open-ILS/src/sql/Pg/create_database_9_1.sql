0704590d627d437f7796b1a2cf90257a7f5a52a5
Thomas Berezansky <tsbere@mvlc.org>
2011-09-08 18:09:49 -0400

    Take a stab at 9.1 support
    Second create database SQL to deal with
    CREATE EXTENSION
    instead of contrib files.
    
    Check create DB steps for errors. If any found, die.
    This should cover "could not load file" from pre-9.1 as well.
    
    Also copied the 9.0 fts-config as 9.1 to avoid that warning during
    database load.
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

---
+++ Open-ILS/src/sql/Pg/create_database_9_1.sql
@@ -0,0 +1,23 @@
+-- This file is intended to be called by eg_db_config.pl
+
+-- If manually calling:
+-- Connect to the postgres database initially
+-- Specify the database to create as -vdb_name=DATABASE
+-- Specify the postgres contrib directory as -vcontrib_dir=CONTRIBDIR
+-- You can get the contrib directory using pg_config --sharedir and adding a /contrib to it
+
+-- NOTE: This file does not do transactions
+-- This is intentional. Please do not wrap in BEGIN/COMMIT.
+DROP DATABASE IF EXISTS :db_name;
+
+CREATE DATABASE :db_name TEMPLATE template0 ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C';
+
+\connect :db_name
+
+CREATE LANGUAGE plperl;
+CREATE LANGUAGE plperlu;
+
+CREATE EXTENSION tablefunc;
+CREATE EXTENSION tsearch2;
+CREATE EXTENSION xml2;
+CREATE EXTENSION hstore;
