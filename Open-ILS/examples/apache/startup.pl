765e66aa68239bdb1b6496325d8b56d900bee447
Dan Scott <dscott@laurentian.ca>
2011-09-16 09:59:54 -0400

    LP 851915: Remove references to /openils/lib/perl5
    To prevent old versions of the OpenSRF and Evergreen Perl modules from
    conflicting with the new versions of these modules (which are now
    located in the default system Perl package directories), remove
    references to them.
    
    Also, O:W:AddedContent contains an old reference to a distro-specific
    Perl directory that we can clean up at the same time.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/examples/apache/startup.pl
+++ Open-ILS/examples/apache/startup.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-use lib qw( /openils/lib/perl5 ); 
 use OpenILS::WWW::Exporter qw( /openils/conf/opensrf_core.xml );
 use OpenILS::WWW::SuperCat qw( /openils/conf/opensrf_core.xml );
 use OpenILS::WWW::AddedContent qw( /openils/conf/opensrf_core.xml );
