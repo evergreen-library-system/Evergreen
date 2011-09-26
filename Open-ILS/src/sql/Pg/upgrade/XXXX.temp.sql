0bb87e5c2c8cc4d8ec2e76773a26d1c5f07dbd38
Thomas Berezansky <tsbere@mvlc.org>
2011-09-26 10:19:20 -0400

    Unwrapped, badly named upgrade script
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.temp.sql
@@ -0,0 +1,26 @@
+INSERT INTO config.org_unit_setting_type (name, grp, label, description, datatype) VALUES
+( 'opac.username_regex', 'glob',
+    oils_i18n_gettext('opac.username_regex',
+        'Patron username format',
+        'coust', 'label'),
+    oils_i18n_gettext('opac.username_regex',
+        'Regular expression defining the patron username format, used for patron registration and self-service username changing only',
+        'coust', 'description'),
+    'string')
+,( 'opac.lock_usernames', 'glob',
+    oils_i18n_gettext('opac.lock_usernames',
+        'Lock Usernames',
+        'coust', 'label'),
+    oils_i18n_gettext('opac.lock_usernames',
+        'If enabled username changing via the OPAC will be disabled',
+        'coust', 'description'),
+    'bool')
+,( 'opac.unlimit_usernames', 'glob',
+    oils_i18n_gettext('opac.unlimit_usernames',
+        'Allow multiple username changes',
+        'coust', 'label'),
+    oils_i18n_gettext('opac.unlimit_usernames',
+        'If enabled (and Lock Usernames is not set) patrons will be allowed to change their username when it does not look like a barcode. Otherwise username changing in the OPAC will only be allowed when the patron''s username looks like a barcode.',
+        'coust', 'description'),
+    'bool')
+;
