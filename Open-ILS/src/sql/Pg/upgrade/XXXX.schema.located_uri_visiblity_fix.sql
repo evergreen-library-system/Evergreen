9b8c9668d8e9e086c32e3df2d6234c4f01632970
Mike Rylander <mrylander@gmail.com>
2011-10-03 15:01:26 -0400

    Correct Located URI visiblity
    Located URIs should be visible when they exist at the search context org unit
    or an ancestor thereof.  However, before the change embodied in this commit,
    they effectively acted exactly like copies, making records visible anywhere
    within the context+depth range.
    
    An example, using the stock example OU hierarchy:
    
     * Before this change, a Located URI with an owner of BR1 would cause its
    record to show up in a search at (or scoped to via a depth parameter) BR1,
    SYS1 or CONS.
    
     * Before this change, a Located URI with an owner of SYS1 would NOT cause its
    record to show up in a search at (or scoped to via a depth parameter) BR1!
    
     * After this change, a Located URI with an owner of BR1 will cause its record
    to show up only in a search with a context OU of BR1, irrespective of depth
    scoping.
    
     * After this change, a Located URI with an owner of SYS1 WILL cause its
    record to show up in a search with a context OU of BR1 or SYS1, regardless of
    depth scoping.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.schema.located_uri_visiblity_fix.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.located_uri_visiblity_fix.sql
@@ -1,37 +1,6 @@
-/*
- * Copyright (C) 2007-2010  Equinox Software, Inc.
- * Mike Rylander <miker@esilibrary.com> 
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
-
-
-DROP SCHEMA IF EXISTS search CASCADE;
-
 BEGIN;
 
-CREATE SCHEMA search;
-
-CREATE TABLE search.relevance_adjustment (
-    id          SERIAL  PRIMARY KEY,
-    active      BOOL    NOT NULL DEFAULT TRUE,
-    field       INT     NOT NULL REFERENCES config.metabib_field (id) DEFERRABLE INITIALLY DEFERRED,
-    bump_type   TEXT    NOT NULL CHECK (bump_type IN ('word_order','first_word','full_match')),
-    multiplier  NUMERIC NOT NULL DEFAULT 1.0
-);
-CREATE UNIQUE INDEX bump_once_per_field_idx ON search.relevance_adjustment ( field, bump_type );
-
-CREATE TYPE search.search_result AS ( id BIGINT, rel NUMERIC, record INT, total INT, checked INT, visible INT, deleted INT, excluded INT );
-CREATE TYPE search.search_args AS ( id INT, field_class TEXT, field_name TEXT, table_alias TEXT, term TEXT, term_type TEXT );
+INSERT INTO config.upgrade_log (version) VALUES ('XXXX'); -- miker
 
 CREATE OR REPLACE FUNCTION search.query_parser_fts (
 
