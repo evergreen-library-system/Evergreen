cb56b66329964ecf0fdd27ae721b93d321b30c62
Jason Etheridge <jason@esilibrary.com>
2011-10-04 14:22:15 -0400

    pin version for upgrade script
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0634.security_lockdown.sql
+++ Open-ILS/src/sql/Pg/upgrade/0634.security_lockdown.sql
@@ -1,3 +1,10 @@
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0634', :eg_version);
+
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 513, 'DEBUG_CLIENT', oils_i18n_gettext( 513,
     'Allows a user to use debug functions in the staff client', 'ppl', 'description' ));
+
+COMMIT;
