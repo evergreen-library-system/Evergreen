30fa94a8d64250e336b4f2b56b2901414fcc6323
Thomas Berezansky <tsbere@mvlc.org>
2011-10-18 10:07:08 -0400

    Stamping upgrade script for LP#847973
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>

--- Open-ILS/src/sql/Pg/upgrade/0641.schema.org_unit_setting_json_check.sql
+++ Open-ILS/src/sql/Pg/upgrade/0641.schema.org_unit_setting_json_check.sql
@@ -1,10 +1,10 @@
--- Evergreen DB patch XXXX.schema.org_unit_setting_json_check.sql
+-- Evergreen DB patch 0641.schema.org_unit_setting_json_check.sql
 --
 --
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0641', :eg_version);
 
 ALTER TABLE actor.org_unit_setting ADD CONSTRAINT aous_must_be_json CHECK ( evergreen.is_json(value) );
 
