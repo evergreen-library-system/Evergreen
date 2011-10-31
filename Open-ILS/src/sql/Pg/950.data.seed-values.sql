aed4b69715f34188a9c906d72345632d41283415
Bill Erickson <berick@esilibrary.com>
2011-10-31 14:56:48 -0400

    Vandelay; API for creating buckets from queues
    * Adds a new biblio record entry bucket type of "vandelay_queue"
    * Adds a new API call that allows callers to create new buckets (or add
    to existing buckets), copying imported records from a vandelay queue
    into the bucket.
    
    open-ils.vandelay.bib_queue.to_bucket
    
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -10396,3 +10396,10 @@ INSERT INTO action_trigger.validator ( module, description ) VALUES (
     'ReservationIsAvailable',
     'Checked that a reserved resource is available for checkout'
 );
+
+INSERT INTO container.biblio_record_entry_bucket_type (code, label) VALUES (
+    'vandelay_queue',
+    oils_i18n_gettext('vandelay_queue', 'Vandelay Queue', 'cbrebt', 'label')
+);
+
+
