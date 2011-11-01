176890efb3162fa488fc4cfbfc9cc23fc350c976
Thomas Berezansky <tsbere@mvlc.org>
2011-11-01 10:11:24 -0400

    Seed values/upgrade script for card edit perms
    Not assigned to anyone by default.
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.patron_card_editor.sql
@@ -0,0 +1,6 @@
+INSERT INTO permission.perm_list ( id, code, description ) VALUES
+ ( 514, 'UPDATE_PATRON_ACTIVE_CARD', oils_i18n_gettext( 514,
+    'Allows a user to manually adjust a patron''s active cards', 'ppl', 'description')),
+ ( 515, 'UPDATE_PATRON_PRIMARY_CARD', oils_i18n_gettext( 515,
+    'Allows a user to manually adjust a patron''s primary card', 'ppl', 'description'));
+
