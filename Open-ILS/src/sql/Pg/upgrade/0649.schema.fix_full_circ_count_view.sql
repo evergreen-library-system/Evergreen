b10df430e2afc97012bb4749b2b05226876db2db
Mike Rylander <mrylander@gmail.com>
2011-11-15 16:22:08 -0500

    Stamped upgrade for full circ count view fix
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0649.schema.fix_full_circ_count_view.sql
+++ Open-ILS/src/sql/Pg/upgrade/0649.schema.fix_full_circ_count_view.sql
@@ -1,3 +1,14 @@
+-- Evergreen DB patch 0649.schema.fix_full_circ_count_view.sql
+--
+-- FIXME: insert description of change, if needed
+--
+BEGIN;
+
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0649', :eg_version);
+
+-- FIXME: add/check SQL statements to perform the upgrade
 CREATE OR REPLACE VIEW extend_reporter.full_circ_count AS
  SELECT cp.id, COALESCE(c.circ_count, 0::bigint) + COALESCE(count(DISTINCT circ.id), 0::bigint) + COALESCE(count(DISTINCT acirc.id), 0::bigint) AS circ_count
    FROM asset."copy" cp
@@ -5,3 +16,6 @@ CREATE OR REPLACE VIEW extend_reporter.full_circ_count AS
    LEFT JOIN "action".circulation circ ON circ.target_copy = cp.id
    LEFT JOIN "action".aged_circulation acirc ON acirc.target_copy = cp.id
   GROUP BY cp.id, c.circ_count;
+
+
+COMMIT;
