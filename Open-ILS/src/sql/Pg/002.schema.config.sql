f015e53f90ec0ab52c5d66fac24ad34d592922ee
Mike Rylander <mrylander@gmail.com>
2011-11-16 12:07:54 -0500

    Stamping upgrade script for copy visibility vs peer-bibs fix
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0649'); -- dbwells/tsbere/miker
+INSERT INTO config.upgrade_log (version) VALUES ('0650'); -- tsbere/miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
