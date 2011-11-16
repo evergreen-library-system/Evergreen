3b4dc8585d5a6f5f4b77f2868f02acfb035f321f
Mike Rylander <mrylander@gmail.com>
2011-11-16 12:00:48 -0500

    Stamping upgrade script for copy visibility vs peer-bibs fix
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0650.function.copy_visibility_vs_peer_bibs.sql
+++ Open-ILS/src/sql/Pg/upgrade/0650.function.copy_visibility_vs_peer_bibs.sql
@@ -1,3 +1,12 @@
+-- Evergreen DB patch 0650.function.copy_visibility_vs_peer_bibs.sql
+--
+--
+BEGIN;
+
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0650', :eg_version);
+
 CREATE OR REPLACE FUNCTION asset.cache_copy_visibility () RETURNS TRIGGER as $func$
 DECLARE
     add_front       TEXT;
@@ -205,3 +214,6 @@ BEGIN
     RETURN NEW;
 END;
 $func$ LANGUAGE PLPGSQL;
+
+
+COMMIT;
