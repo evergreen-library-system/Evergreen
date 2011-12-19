dcf2493f4ead094c8ded527b120e8ae057a7e5f2
Dan Scott <dscott@laurentian.ca>
2011-12-19 11:17:12 -0500

    Wrap upgrade script for CREATE_REPORT_TEMPLATE permission
    Permissions to create report templates will automatically be added to
    any group that currently has the ability to run reports as part of the
    upgrade process; if you want to make use of the ability to prevent some
    groups from creating ad-hoc reports, remove the CREATE_REPORT_TEMPLATE
    permission from permission.grp_perm_map for that group.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- Open-ILS/src/sql/Pg/upgrade/0659.add_create_report_perms.sql
+++ Open-ILS/src/sql/Pg/upgrade/0659.add_create_report_perms.sql
@@ -1,3 +1,13 @@
+-- Evergreen DB patch 0659.add_create_report_perms.sql
+--
+-- Add a permission to control the ability to create report templates
+--
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0659', :eg_version);
+
+-- FIXME: add/check SQL statements to perform the upgrade
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 516, 'CREATE_REPORT_TEMPLATE', oils_i18n_gettext( 516,
     'Allows a user to create report templates', 'ppl', 'description' ));
@@ -10,3 +20,6 @@ INSERT INTO permission.grp_perm_map (grp, perm, depth, grantable)
                 FROM permission.perm_list
                 WHERE code = 'RUN_REPORTS'
         );
+
+
+COMMIT;
