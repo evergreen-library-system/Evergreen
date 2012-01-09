1fc6e778db6ba859c63c20a799e7c4b2cb331837
Mike Rylander <mrylander@gmail.com>
2012-01-09 15:53:28 -0500

    Placeholder upgrade script for late-backport of a fix for 2.0
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,7 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO config.upgrade_log (version) VALUES ('0658'); -- jamesrf/dbs
+INSERT INTO config.upgrade_log (version) VALUES ('0665'); -- phasefx/miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
