df57275f7715752aeb1c6877a55072710711bb1a
Mike Rylander <mrylander@gmail.com>
2012-01-10 15:14:59 -0500

    Terminology adjustment to reduce conflicts with the term "recall"
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -1452,7 +1452,7 @@ INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 517, 'COPY_HOLDS_FORCE', oils_i18n_gettext( 517, 
     'Allow a user to place a force hold on a specific copy', 'ppl', 'description' )),
  ( 518, 'COPY_HOLDS_RECALL', oils_i18n_gettext( 518, 
-    'Allow a user to place a recall hold on a specific copy', 'ppl', 'description' ));
+    'Allow a user to place a cataloging recall on a specific copy', 'ppl', 'description' ));
 
 SELECT SETVAL('permission.perm_list_id_seq'::TEXT, 1000);
 
