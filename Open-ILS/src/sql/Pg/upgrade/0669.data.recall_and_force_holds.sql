0ba1080ee1277006e6c41ba299c542994e470358
Mike Rylander <mrylander@gmail.com>
2012-01-10 15:13:58 -0500

    Stamping force and recall hold permission addition script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0669.data.recall_and_force_holds.sql
+++ Open-ILS/src/sql/Pg/upgrade/0669.data.recall_and_force_holds.sql
@@ -1,5 +1,19 @@
+-- Evergreen DB patch 0669.data.recall_and_force_holds.sql
+--
+-- FIXME: insert description of change, if needed
+--
+BEGIN;
+
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0669', :eg_version);
+
+-- FIXME: add/check SQL statements to perform the upgrade
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 517, 'COPY_HOLDS_FORCE', oils_i18n_gettext( 517, 
     'Allow a user to place a force hold on a specific copy', 'ppl', 'description' )),
  ( 518, 'COPY_HOLDS_RECALL', oils_i18n_gettext( 518, 
     'Allow a user to place a recall hold on a specific copy', 'ppl', 'description' ));
+
+
+COMMIT;
