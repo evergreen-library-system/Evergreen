5b7b588c3e41b522f39c46deb2ba48cc92acf9ea
Mike Rylander <mrylander@gmail.com>
2012-02-28 14:58:38 -0500

    Stamping default-merge-profiles upgrade script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0678.data.vandelay-default-merge-profiles.sql
+++ Open-ILS/src/sql/Pg/upgrade/0678.data.vandelay-default-merge-profiles.sql
@@ -1,8 +1,8 @@
--- Evergreen DB patch XXXX.data.vandelay-default-merge-profiles.sql
+-- Evergreen DB patch 0678.data.vandelay-default-merge-profiles.sql
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0678', :eg_version);
 
 INSERT INTO vandelay.merge_profile (owner, name, replace_spec) 
     VALUES (1, 'Match-Only Merge', '901c');
