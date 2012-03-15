ebaf0fd679a1f466205cffa1a3edfd8dffac3e5f
Bill Erickson <berick@esilibrary.com>
2012-03-15 12:06:48 -0400

    stamped upgrade for hold-avail-email-notify validation
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0683.hold_available_email_notify.sql
+++ Open-ILS/src/sql/Pg/upgrade/0683.hold_available_email_notify.sql
@@ -1,3 +1,8 @@
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0683', :eg_version);
+
 INSERT INTO action_trigger.event_params (event_def, param, value)
     VALUES (5, 'check_email_notify', 1);
 INSERT INTO action_trigger.event_params (event_def, param, value)
@@ -15,6 +20,8 @@ INSERT INTO action_trigger.validator (module,description) VALUES
 UPDATE action_trigger.event_definition SET validator = 'HoldNotifyCheck' WHERE id = 9;
 
 -- NOT COVERED: Adding check_sms_notify to the proper trigger. It doesn't have a static id.
+
+COMMIT;
 
 --UNDO
 --UPDATE action_trigger.event_definition SET validator = 'NOOP_True' WHERE id = 9;
