85480a7198ec50b0c32fd434c0d59feafa0a013d
Mike Rylander <mrylander@gmail.com>
2012-03-19 15:58:51 -0400

    DB layout for recording custom toolbars
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>

--- Open-ILS/src/sql/Pg/005.schema.actors.sql
+++ Open-ILS/src/sql/Pg/005.schema.actors.sql
@@ -630,4 +630,22 @@ CREATE TABLE actor.usr_activity (
     event_time  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE actor.toolbar (
+    id          BIGSERIAL   PRIMARY KEY,
+    ws          INT         REFERENCES actor.workstation (id) ON DELETE CASCADE,
+    org         INT         REFERENCES actor.org_unit (id) ON DELETE CASCADE,
+    usr         INT         REFERENCES actor.usr (id) ON DELETE CASCADE,
+    label       TEXT        NOT NULL,
+    layout      TEXT        NOT NULL,
+	CONSTRAINT label_once_per_ws UNIQUE (ws, label) WHERE ws IS NOT NULL,
+	CONSTRAINT label_once_per_org UNIQUE (org, label) WHERE org IS NOT NULL,
+	CONSTRAINT label_once_per_usr UNIQUE (usr, label) WHERE usr IS NOT NULL,
+    CONSTRAINT only_one_type CHECK (
+        (ws IS NOT NULL AND COALESCE(org,usr) IS NULL) OR
+        (org IS NOT NULL AND COALESCE(ws,usr) IS NULL) OR
+        (usr IS NOT NULL AND COALESCE(org,ws) IS NULL)
+    ),
+    CONSTRAINT layout_must_be_json CHECK ( is_json(layout) )
+);
+
 COMMIT;
