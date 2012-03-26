5db8fb03ca1a7ebbe176bc05936018b411f93b4d
Bill Erickson <berick@esilibrary.com>
2012-03-26 14:02:00 -0400

    kpac: save to anon-list
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/WWW/EGKPacLoader.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/WWW/EGKPacLoader.pm
@@ -51,12 +51,17 @@ sub load {
 sub load_getit {
     my $self = shift;
     my $ctx = $self->ctx;
+    my $rec_id = $ctx->{page_args}->[0];
+    my $bbag_id = $self->cgi->param('bookbag');
+    my $action = $self->cgi->param('action') || '';
 
     # first load the record
     my $stat = $self->load_record(no_search => 1);
     return $stat unless $stat == Apache2::Const::OK;
 
     $self->ctx->{page} = 'getit'; # repair the page
+
+    return $self->save_item_to_bookbag($rec_id, $bbag_id) if $action eq 'save';
 
     # if the user is logged in, fetch his bookbags
     if ($ctx->{user}) {
@@ -68,7 +73,6 @@ sub load_getit {
                     order_by => {cbreb => 'name'},
                     limit => $self->cgi->param('bbag_limit') || 100,
             }],
-            {substream => 1}
         );
     }
 
@@ -76,6 +80,27 @@ sub load_getit {
     return Apache2::Const::OK;
 }
 
+sub save_item_to_bookbag {
+    my $self = shift;
+    my $rec_id = shift;
+    my $bookbag_id = shift;
+
+    if ($bookbag_id) { 
+        # save to existing bookbag
+
+    } else { 
+        # save to anonymous list
+       
+        # set some params assumed to exist for load_mylist_add
+        $self->cgi->param('record', $rec_id);
+        (my $new_uri = $self->apache->unparsed_uri) =~ s/getit/getit_results/g;
+        $self->cgi->param('redirect_to', $new_uri);
+
+        $self->load_mylist_add;
+    }
+
+    return Apache2::Const::OK;
+}
 
 sub load_kpac_config {
     my $self = shift;
