d523b0fd103949e6f8ab2296e3657753cf4fec80
Jason Etheridge <jason@esilibrary.com>
2012-03-26 12:27:38 -0400

    version fix for 0692/0693
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0692.data.fine-when-closed.sql
+++ Open-ILS/src/sql/Pg/upgrade/0692.data.fine-when-closed.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('0693', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0692', :eg_version);
 
 INSERT INTO config.org_unit_setting_type
     (name, label, description, grp, datatype)
