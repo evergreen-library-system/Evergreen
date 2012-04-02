7920d760358dd0201a7ca2234710f68a602775f4
Bill Erickson <berick@esilibrary.com>
2012-04-02 17:15:25 -0400

    Add ui.hide_copy_editor_fields to baseline seed data
    Without it, the "Hide Fields" option in the copy editor will not
    function.
    
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -11553,3 +11553,26 @@ INSERT INTO config.global_flag (name, enabled, label)
             'label'
         )
     );
+
+INSERT INTO config.org_unit_setting_type ( name, label, description, datatype, grp )
+    VALUES (
+        'ui.hide_copy_editor_fields',
+        oils_i18n_gettext(
+            'ui.hide_copy_editor_fields',
+            'GUI: Hide these fields within the Item Attribute Editor',
+            'coust',
+            'label'
+        ),
+        oils_i18n_gettext(
+            'ui.hide_copy_editor_fields',
+            'This setting may be best maintained with the dedicated configuration'
+            || ' interface within the Item Attribute Editor.  However, here it'
+            || ' shows up as comma separated list of field identifiers to hide.',
+            'coust',
+            'description'
+        ),
+        'array',
+        'gui'
+    );
+
+
