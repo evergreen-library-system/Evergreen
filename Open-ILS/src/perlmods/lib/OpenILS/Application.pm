1d0a166830343029ab3d182ee15f430f2a8929e1
Thomas Berezansky <tsbere@mvlc.org>
2012-04-02 13:55:13 -0400

    Tagging version
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>

--- Open-ILS/src/perlmods/lib/OpenILS/Application.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application.pm
@@ -6,7 +6,7 @@ use base qw/OpenSRF::Application/;
 sub ils_version {
     # version format is "x-y-z", for example "2-0-0" for Evergreen 2.0.0
     # For branches, format is "x-y"
-    return "HEAD";
+    return "2-2";
 }
 
 __PACKAGE__->register_method(
