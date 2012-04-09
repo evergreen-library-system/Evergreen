fc5f7714c978fd7a5b5c4ce77d6a993e10dd6ccf
Thomas Berezansky <tsbere@mvlc.org>
2012-04-08 21:32:25 -0400

    Stamp tpac value maps upgrade script
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>

--- Open-ILS/src/sql/Pg/upgrade/0703.tpac_value_maps.sql
+++ Open-ILS/src/sql/Pg/upgrade/0703.tpac_value_maps.sql
@@ -1,4 +1,12 @@
+-- Evergreen DB patch 0703.tpac_value_maps.sql
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0703', :eg_version);
+
 ALTER TABLE config.coded_value_map
     ADD COLUMN opac_visible BOOL NOT NULL DEFAULT TRUE,
     ADD COLUMN search_label TEXT,
     ADD COLUMN is_simple BOOL NOT NULL DEFAULT FALSE;
+
+COMMIT;
