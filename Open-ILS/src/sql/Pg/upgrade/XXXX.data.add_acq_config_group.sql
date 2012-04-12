dc59cbc5d2fee3403a08d5dd6023c035d98ea3a0
Dan Scott <dan@coffeecode.net>
2012-04-11 21:56:56 -0400

    Group acquisition OU settings in a new "Acquisitions" group
    As seen in LP867465, the current grouping of acquisition OU settings
    under the generic "lib" group leads to confusion about which
    context "default shelf location" and the like apply to. Creating a new
    "Acquisitions" group will help clarify that current ambiguity.
    
    Signed-off-by: Dan Scott <dan@coffeecode.net>
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.add_acq_config_group.sql
@@ -0,0 +1,6 @@
+INSERT INTO config.settings_group (name, label) VALUES
+('acq', oils_i18n_gettext('config.settings_group.system', 'Acquisitions', 'coust', 'label'));
+
+UPDATE config.org_unit_setting_type
+    SET grp = 'acq'
+    WHERE name LIKE 'acq%';
