115595dca577d9192474ac3d620ca079b89f39cd
Bill Erickson <berick@esilibrary.com>
2012-05-04 10:33:43 -0400

    Adding a small pile of missing permissions
    ADMIN_ADDRESS_ALERT
    VIEW_ADDRESS_ALERT
    ADMIN_COPY_LOCATION_GROUP
    ADMIN_USER_ACTIVITY_TYPE
    
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -1549,7 +1549,16 @@ INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 529, 'ADMIN_IMPORT_MATCH_SET', oils_i18n_gettext( 529,
     'Allows a user to create/retrieve/update/delete vandelay match sets', 'ppl', 'description' )),
  ( 530, 'VIEW_IMPORT_MATCH_SET', oils_i18n_gettext( 530,
-    'Allows a user to view vandelay match sets', 'ppl', 'description' ));
+    'Allows a user to view vandelay match sets', 'ppl', 'description' )),
+ ( 531, 'ADMIN_ADDRESS_ALERT', oils_i18n_gettext( 531,
+    'Allows a user to create/retrieve/update/delete address alerts', 'ppl', 'description' )), 
+ ( 532, 'VIEW_ADDRESS_ALERT', oils_i18n_gettext( 532,
+    'Allows a user to view address alerts', 'ppl', 'description' )), 
+ ( 533, 'ADMIN_COPY_LOCATION_GROUP', oils_i18n_gettext( 533,
+    'Allows a user to create/retrieve/update/delete copy location groups', 'ppl', 'description' )), 
+ ( 534, 'ADMIN_USER_ACTIVITY_TYPE', oils_i18n_gettext( 534,
+    'Allows a user to create/retrieve/update/delete user activity types', 'ppl', 'description' ));
+
 
 SELECT SETVAL('permission.perm_list_id_seq'::TEXT, 1000);
 
