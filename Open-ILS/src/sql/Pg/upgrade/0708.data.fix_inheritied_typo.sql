470c9039a2af7db57ba4162dbf854d604056bf85
Bill Erickson <berick@esilibrary.com>
2012-05-04 14:46:16 -0400

    Stamping upgrade for 'inheritied' typo repair
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0708.data.fix_inheritied_typo.sql
+++ Open-ILS/src/sql/Pg/upgrade/0708.data.fix_inheritied_typo.sql
@@ -2,6 +2,12 @@
 --
 -- Fixes a typo in the name of a global flag
 
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0708', :eg_version);
+
 UPDATE config.global_flag
 SET name = 'opac.org_unit.non_inherited_visibility'
 WHERE name = 'opac.org_unit.non_inheritied_visibility';
+
+COMMIT;
