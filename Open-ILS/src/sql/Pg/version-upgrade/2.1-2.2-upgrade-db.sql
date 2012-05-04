4bc4f9d59f0c58f177becaa0280656b55104a5f9
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2012-05-04 12:23:19 -0400

    Add missing upgrade scripts 0705, 0707 to point-to-point upgrade script
    These come from bugfixes I pushed to master and rel_2_2 on 28 April 2012
    without remembering to address the point-to-point version upgrade script.
    
    See c208754eafadb005439800f7fedd33241fca7b65 (LP #980199) and
    89438b46fc39270ac3d47345cd99caacf6b6dee1 (LP #919279).
    
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/src/sql/Pg/version-upgrade/2.1-2.2-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/2.1-2.2-upgrade-db.sql
@@ -15692,3 +15692,55 @@ SELECT metabib.reingest_metabib_field_entries(source)
 
 
 COMMIT;
+
+-- Evergreen DB patch 0705.data.custom-org-tree-perms.sql
+--
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0705', :eg_version);
+
+INSERT INTO permission.perm_list (id, code, description)
+    VALUES (
+        528,
+        'ADMIN_ORG_UNIT_CUSTOM_TREE',
+        oils_i18n_gettext(
+            528,
+            'User may update custom org unit trees',
+            'ppl',
+            'description'
+        )
+    );
+
+COMMIT;
+
+
+-- Evergreen DB patch 0707.schema.acq-vandelay-integration.sql
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0707', :eg_version);
+
+-- seed data --
+
+INSERT INTO permission.perm_list ( id, code, description )
+    VALUES (
+        529,
+        'ADMIN_IMPORT_MATCH_SET',
+        oils_i18n_gettext(
+            529,
+            'Allows a user to create/retrieve/update/delete vandelay match sets',
+            'ppl',
+            'description'
+        )
+    ), (
+        530,
+        'VIEW_IMPORT_MATCH_SET',
+        oils_i18n_gettext(
+            530,
+            'Allows a user to view vandelay match sets',
+            'ppl',
+            'description'
+        )
+    );
+
+COMMIT;
