a97c87a8d1003cd2aff8c2462d52e68b1021cf33
Jason Stephenson <jstephenson@mvlc.org>
2012-05-10 14:42:19 -0400

    ChangeLog and version bumps.
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -57,6 +57,7 @@ CREATE TABLE config.upgrade_log (
     install_date    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
+INSERT INTO config.upgrade_log (version) VALUES ('2.0.11');
 INSERT INTO config.upgrade_log (version) VALUES ('0691'); -- senator/miker
 
 CREATE TABLE config.bib_source (
