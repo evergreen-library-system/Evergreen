7f46dc3ada677bbfdd056e3da6dd01e7482abb51
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2012-05-10 16:19:26 -0400

    In the 2.1-2.2 upgrade script, move 0691 into a failures-are-ok zone
    Mainly because it's already covered in the 2.1.1-2.1.2 upgrade script.
    Thanks to George Duimovich for pointing this out in LP #997759.
    
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/src/sql/Pg/version-upgrade/2.1-2.2-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/2.1-2.2-upgrade-db.sql
@@ -11505,19 +11505,6 @@ EXPLAIN ANALYZE SELECT unapi.bre(36,'marcxml','record','{holdings_xml,mra,acp,ac
 */
 
 
-SELECT evergreen.upgrade_deps_block_check('0691', :eg_version);
-
-CREATE INDEX poi_po_idx ON acq.po_item (purchase_order);
-
-CREATE INDEX ie_inv_idx on acq.invoice_entry (invoice);
-CREATE INDEX ie_po_idx on acq.invoice_entry (purchase_order);
-CREATE INDEX ie_li_idx on acq.invoice_entry (lineitem);
-
-CREATE INDEX ii_inv_idx on acq.invoice_item (invoice);
-CREATE INDEX ii_po_idx on acq.invoice_item (purchase_order);
-CREATE INDEX ii_poi_idx on acq.invoice_item (po_item);
-
-
 SELECT evergreen.upgrade_deps_block_check('0692', :eg_version);
 
 INSERT INTO config.org_unit_setting_type
@@ -16140,4 +16127,23 @@ SELECT metabib.reingest_metabib_field_entries(source)
 
 
 COMMIT;
+
+-- outside of any transaction
+
+\qecho ************************************************************************
+\qecho                  Failures from here down are okay!
+\qecho ************************************************************************
+
+SELECT evergreen.upgrade_deps_block_check('0691', :eg_version);
+
+CREATE INDEX poi_po_idx ON acq.po_item (purchase_order);
+
+CREATE INDEX ie_inv_idx on acq.invoice_entry (invoice);
+CREATE INDEX ie_po_idx on acq.invoice_entry (purchase_order);
+CREATE INDEX ie_li_idx on acq.invoice_entry (lineitem);
+
+CREATE INDEX ii_inv_idx on acq.invoice_item (invoice);
+CREATE INDEX ii_po_idx on acq.invoice_item (purchase_order);
+CREATE INDEX ii_poi_idx on acq.invoice_item (po_item);
+
 
