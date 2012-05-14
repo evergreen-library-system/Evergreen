aa849941200ede41f6420f1a69f0b6c8ff3dafa5
Thomas Berezansky <tsbere@mvlc.org>
2012-05-14 15:30:18 -0400

    Bumping version numbers and adding Changelog
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -88,6 +88,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0711', :eg_version); -- senator
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('2.2-rc1', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
