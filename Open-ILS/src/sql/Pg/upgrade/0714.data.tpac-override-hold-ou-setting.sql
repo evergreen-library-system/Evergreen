a8c34b4547e92bd004fbfbad2734b125f5fc082c
Mike Rylander <mrylander@gmail.com>
2012-05-25 11:16:50 -0400

    Stamping upgrade script for TPAC hold override setting
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0714.data.tpac-override-hold-ou-setting.sql
+++ Open-ILS/src/sql/Pg/upgrade/0714.data.tpac-override-hold-ou-setting.sql
@@ -1,7 +1,7 @@
 
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0714', :eg_version);
 
 INSERT into config.org_unit_setting_type 
     (name, grp, label, description, datatype) 
