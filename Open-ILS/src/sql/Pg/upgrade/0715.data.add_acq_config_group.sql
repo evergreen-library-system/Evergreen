6ea45e80f34eadec2a2477c7f45df41c1669e32e
Bill Erickson <berick@esilibrary.com>
2012-05-25 14:54:23 -0400

    Stamping upgrade for ACQ org setting group
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0715.data.add_acq_config_group.sql
+++ Open-ILS/src/sql/Pg/upgrade/0715.data.add_acq_config_group.sql
@@ -1,6 +1,12 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0715', :eg_version);
+
 INSERT INTO config.settings_group (name, label) VALUES
 ('acq', oils_i18n_gettext('config.settings_group.system', 'Acquisitions', 'coust', 'label'));
 
 UPDATE config.org_unit_setting_type
     SET grp = 'acq'
     WHERE name LIKE 'acq%';
+
+COMMIT;
