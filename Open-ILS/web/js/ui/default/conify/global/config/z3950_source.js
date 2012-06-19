b825315fcffce55bccae770e5421d30d83377919
Bill Erickson <berick@esilibrary.com>
2012-06-19 10:23:22 -0400

    Z39.50 attributes management interface
    From the release notes:
    
    There is a new interface for managing Z39.50 attributes on a Z39.50
    source.  The interface is linked from each source name in the Z39.50
    Source administrative interface.
    
    Cloning
    ~~~~~~
    
    In addition to attribute creation, deletion, and editing, it's also
    possible to clone a set of attributes from one source into another.
    When cloning, any attributes present in the cloned source that are
    not present in the destination source are copied into the destination
    source.
    
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Dan Scott <dan@coffeecode.net>

--- Open-ILS/web/js/ui/default/conify/global/config/z3950_source.js
+++ Open-ILS/web/js/ui/default/conify/global/config/z3950_source.js
@@ -2,13 +2,76 @@ dojo.require('dojox.grid.DataGrid');
 dojo.require('dojo.data.ItemFileReadStore');
 dojo.require('dijit.form.NumberTextBox');
 dojo.require('dijit.form.CheckBox');
+dojo.require('dijit.Dialog');
 dojo.require('fieldmapper.OrgUtils');
 dojo.require('openils.widget.OrgUnitFilteringSelect');
 dojo.require('openils.widget.AutoGrid');
+dojo.require('openils.widget.FlattenerGrid');
+dojo.require('openils.widget.AutoFieldWidget');
+dojo.require('openils.User');
+dojo.require('openils.PermaCrud');
 var zsList;
 
 function buildZSGrid() {
-    zsGrid.loadAll({order_by:{czs : 'name'}});
+
+    if (sourceCode) {
+
+        zaGrid.overrideWidgetArgs.source = {
+            widgetValue : sourceCode, 
+            readOnly : true
+        };
+
+    } else {
+
+        zsGrid.loadAll({order_by:{czs : 'name'}});
+    }
+}
+
+function formatSourceName(val) {
+    return '<a href="' + location.href + '/' + escape(val) + '">' + val + '</a>';
+}
+
+var cloneSourceSelector;
+function showAttrCloneDialog() {
+    attrCloneDialog.show();
+    if (!cloneSourceSelector) {
+        cloneSourceSelector = new openils.widget.AutoFieldWidget({
+            fmClass : 'czs',
+            fmField : 'name',
+            selfReference : true,
+            parentNode : 'attr-clone-source'
+        });
+        cloneSourceSelector.build();
+    }
+}
+
+function cloneFromSource() {
+    var pcrud = new openils.PermaCrud({authtoken : openils.User.authtoken}); 
+    var remoteAttrs = pcrud.search('cza', {source : cloneSourceSelector.widget.attr('value')});
+    var myAttrs = pcrud.search('cza', {source : sourceCode});
+    var newAttrs = [];
+
+    dojo.forEach(remoteAttrs, 
+        function(rattr) {
+
+            // if this source already has an attribute with the same name, don't clobber it
+            if (myAttrs.filter(function(a) { return (a.name() == rattr.name()) })[0]) 
+                return;
+            
+            var newAttr = rattr.clone();
+            newAttr.id(null);
+            newAttr.isnew(true);
+            newAttr.source(sourceCode);
+            newAttrs.push(newAttr);
+        }
+    );
+
+    if (newAttrs.length) {
+        pcrud.create(newAttrs, 
+            {oncomplete : function() { zaGrid.refresh() }});
+    }
+
+    attrCloneDialog.hide();
 }
 
 openils.Util.addOnLoad(buildZSGrid);
