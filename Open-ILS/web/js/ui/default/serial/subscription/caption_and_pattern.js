abade21a678eb3ca1ec2e6c423b4c67aa8695a0f
Thomas Berezansky <tsbere@mvlc.org>
2012-07-26 16:52:55 -0400

    Another missing height for the pattern wizard
    Missed this one on the previous pass.
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- Open-ILS/web/js/ui/default/serial/subscription/caption_and_pattern.js
+++ Open-ILS/web/js/ui/default/serial/subscription/caption_and_pattern.js
@@ -43,7 +43,7 @@ function SCAPRow() {
                 window.openDialog(
                     xulG.url_prefix("XUL_SERIAL_PATTERN_WIZARD"),
                     "pattern_wizard",
-                    "width=800",
+                    "width=800,height=400",
                     function(value) {
                         self.controls.pattern_code.value = value;
                         self.controls.pattern_code.onchange();
