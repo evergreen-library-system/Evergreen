2cc9df22ef56b6814712132150f0cd9b47929bf5
edoceo <code@edoceo.com>
2012-08-01 19:33:20 -0700

    lp949322 Copy Patron barcode to clipboard when clicking their name CTRL+Click will open the patron record in a new tab
    Signed-off-by: edoceo <code@edoceo.com>
    
    This affects the patron label in Item Status -> Alt View -> Holds/Transits.  Looks good to me.
    
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- Open-ILS/xul/staff_client/server/circ/alternate_copy_summary.js
+++ Open-ILS/xul/staff_client/server/circ/alternate_copy_summary.js
@@ -626,6 +626,13 @@ function load_item() {
             JSAN.use('patron.util'); 
             var au_obj = patron.util.retrieve_fleshed_au_via_id( ses(), details.hold.usr() );
             $('hold_patron_name').setAttribute('value', $('circStrings').getFormattedString('staff.circ.copy_details.user_details', [au_obj.family_name(), au_obj.first_given_name(), au_obj.card().barcode()]) );
+            $('hold_patron_name').onclick = function(e) {
+                if (e.ctrlKey) {
+                   window.xulG.new_patron_tab( {}, { 'id' : au_obj.id() } );
+                   return;
+                }
+                copy_to_clipboard(au_obj.card().barcode());
+            };
 
             set("hold_status", details.hold.status()); 
             set("transit", details.hold.transit()); 
