1388b5f2611dcfbf5216c7c6cfee58443485d4a7
Thomas Berezansky <tsbere@mvlc.org>
2012-08-16 14:58:01 -0400

    Update permission name to match existing ones
    To be consistent with our previous naming scheme for these.
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.data.copy_editor_perms.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.copy_editor_perms.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO permission.perm_list (id, code, description)
-    VALUES (539, 'UPDATE_ui.hide_copy_editor_fields', 'Allows staff to edit displayed copy editor fields');
+    VALUES (539, 'UPDATE_ORG_UNIT_SETTING.ui.hide_copy_editor_fields', 'Allows staff to edit displayed copy editor fields');
 
 UPDATE config.org_unit_setting_type SET update_perm = 539 WHERE name = 'ui.hide_copy_editor_fields';
 
