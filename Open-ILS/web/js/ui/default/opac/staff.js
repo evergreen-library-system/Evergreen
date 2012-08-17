bf52858f4f5da72bf0ddaec6db8720a53ce99969
Thomas Berezansky <tsbere@mvlc.org>
2012-08-17 16:20:35 -0400

    TPac: Show email address when placing hold
    Both for staff and patron benefit.
    
    Also, we disable the checkbox if there is no email address on file.
    
    Signed-off-by: Thomas Berezansky <tsbere@mvlc.org>
    Signed-off-by: Ben Shum <bshum@biblio.org>
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

--- Open-ILS/web/js/ui/default/opac/staff.js
+++ Open-ILS/web/js/ui/default/opac/staff.js
@@ -62,6 +62,13 @@ function staff_hold_usr_barcode_changed(isload) {
         for(var i in update_elements) update_elements[i].value = load_info.settings['opac.default_sms_notify'];
         update_elements = document.getElementsByName('sms_carrier');
         for(var i in update_elements) update_elements[i].value = load_info.settings['opac.default_sms_carrier'];
+        update_elements = document.getElementsByName('email_notify');
+        for(var i in update_elements) {
+            update_elements[i].disabled = (load_info.user_email ? false : true);
+            if(update_elements[i].disabled) update_elements[i].checked = false;
+        }
+        update_elements = document.getElementsByName('email_address');
+        for(var i in update_elements) update_elements[i].textContent = load_info.user_email;
     }
 }
 window.onload = function() {
