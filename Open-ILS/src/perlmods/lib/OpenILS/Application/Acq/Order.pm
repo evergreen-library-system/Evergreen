5f62d8b337f0e6937967b10a8926377ad1a8c52a
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2012-08-28 16:09:00 -0400

    Acq: New lineitems added to existing POs should inherit provider
    Jeremy Miller of the Albany Public Library in Albany, OR, reports that
    through the interface at
        Actions For This Record -> View/Place Orders
    in the staff client, when you add a lineitem to an existing PO, the new
    lineitem has a null value in the "provider" field, instead of inheriting
    the expected value from the PO.
    
    This causes problems for some Acquisitions workflows.
    
    The feature containing the bug appeared in 2.2, and this fix for it can be
    backported that far.
    
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Acq/Order.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Acq/Order.pm
@@ -3416,6 +3416,7 @@ sub add_li_to_po {
         return {success => 0, li => $li, error => 'bad-li-state'};
     }
 
+    $li->provider($po->provider);
     $li->purchase_order($po_id);
     $li->state('pending-order');
     update_lineitem($mgr, $li) or return $e->die_event;
