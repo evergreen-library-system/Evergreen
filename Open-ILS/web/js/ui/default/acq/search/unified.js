evergreen-library-system/Evergreen

commit 55a82b570ee8562f3d9eaedad42f22d15b1b8205

Acq: fix widget choice for user-linked fields in Acquisitions Search
The Acquisitions Search interface (which also sits underneath the
Purchase Orders inteface, My Selection Lists, and one or two others)
stopped presenting the correct widget for search terms linked to
actor.usr.  This was brought on when PCRUD control was added to
actor.usr, so that means the bug affects only master, not rel_2_2 or
earlier.

Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
Signed-off-by: Bill Erickson <berick@esilibrary.com>

Open-ILS/web/js/ui/default/acq/search/unified.js
@@ -128,6 +128,8 @@ function TermSelectorFactory(terms) {
         ) {
             var term = this.getTerm();
             var widgetKey = this.uniq;
+            var target = termManager.getLinkTarget(term);
+
             if (matchHow.getValue() == "__in") {
                 new openils.widget.XULTermLoader({
                     "parentNode": parentNode
@@ -147,7 +149,9 @@ function TermSelectorFactory(terms) {
                     }
                 );
             } else if (term.hint == "acqlia" ||
-                (term.hint == "jub" && term.field == "eg_bib_id")) {
+                (term.hint == "jub" && term.field == "eg_bib_id") ||
+                term.datatype == "org_unit" ||
+                (term.datatype == "link" && target == "au")) {
                 /* The test for jub.eg_bib_id is a special case to prevent
                  * AutoFieldWidget from trying to render a ridiculous dropdown
                  * of every bib record ID in the system. */

commit c3176021f6b41a56ad6c529b90a615b682a76145

Acq: Fix General Search for Lineitem Attribute-based fields
Reported by Ben Shum and Kathy Lussier, the Acq General Search interface
was broken in that you couldn't use any of the search fields under
Lineitem Attribute.  This error was indeed introduced by recent commit
55a82b5 as Ben determined, but simply reverting that commit left other
things broken.

This commit actually targets the problem and seems to fix it in my
testing.

Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
Signed-off-by: Bill Erickson <berick@esilibrary.com>

Open-ILS/web/js/ui/default/acq/search/unified.js
@@ -129,7 +129,13 @@ function TermSelectorFactory(terms) {
         ) {
             var term = this.getTerm();
             var widgetKey = this.uniq;
-            var target = termManager.getLinkTarget(term);
+            var target;
+            try {
+                target = termManager.getLinkTarget(term);
+            } catch (E) {
+                void(0); /* ok for this to fail (it doesn't handle acqlia right,
+                            but we don't need it to in this case). */
+            };
 
             if (matchHow.getValue() == "__in") {
                 new openils.widget.XULTermLoader({