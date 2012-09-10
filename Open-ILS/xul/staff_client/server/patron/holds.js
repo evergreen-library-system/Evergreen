68408e9067f9f66574833bd4c4b61200130f3298
Ben Shum <bshum@biblio.org>
2012-09-10 15:03:59 -0400

    Hide Print Page button on Simplified Pull List
    On the Simplified Pull List interface, there is a button for "Print Page"
    that shows up in the upper right corner. The suggestion is to hide that
    button to avoid library staff printing just what's visible instead of using
    the proper "Print Pull List" button that's actually on the page.
    
    Signed-off-by: Ben Shum <bshum@biblio.org>
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/xul/staff_client/server/patron/holds.js
+++ Open-ILS/xul/staff_client/server/patron/holds.js
@@ -399,7 +399,7 @@ patron.holds.prototype = {
                                     "authtime": ses("authtime"),
                                     "no_xulG": false,
                                     "show_nav_buttons": true,
-                                    "show_print_button": true
+                                    "show_print_button": false
                                 };
                                 ["url_prefix", "new_tab", "set_tab",
                                     "close_tab", "new_patron_tab",
