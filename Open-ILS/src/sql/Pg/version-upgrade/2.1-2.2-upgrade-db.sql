f81737c0084a6189d278486bfa15bdb2c516d6f8
Dan Scott <dscott@laurentian.ca>
2012-09-26 23:25:53 -0400

    Apply the 0715 update for acq settings_group
    We add the 'acq' config.settings_group in the seed data, and add the
    other settings groups in the 2.1-2.2 upgrade script, but need to add the
    acq settings_group to the 2.1-2.2 upgrade script.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/src/sql/Pg/version-upgrade/2.1-2.2-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/2.1-2.2-upgrade-db.sql
@@ -12418,6 +12418,15 @@ INSERT INTO permission.perm_list ( id, code, description )
         )
     );
 
+-- 0715.data.add_acq_config_group
+SELECT evergreen.upgrade_deps_block_check('0715', :eg_version);
+
+INSERT INTO config.settings_group (name, label) VALUES
+('acq', oils_i18n_gettext('config.settings_group.system', 'Acquisitions', 'coust', 'label'));
+
+UPDATE config.org_unit_setting_type
+    SET grp = 'acq'
+    WHERE name LIKE 'acq%';
 
 -- Evergreen DB patch 0716.coded_value_map_id_seq_fix.sql
 
