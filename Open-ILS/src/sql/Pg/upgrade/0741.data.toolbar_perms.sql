95d400fa439135d3d42b26b9fe3c14d2ec767788
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2012-10-16 20:55:38 -0400

    Upgrade script numbering for new toolbar perms
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0741.data.toolbar_perms.sql
+++ Open-ILS/src/sql/Pg/upgrade/0741.data.toolbar_perms.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0741', :eg_version);
 
 INSERT INTO permission.perm_list ( id, code, description ) VALUES (
     540,
