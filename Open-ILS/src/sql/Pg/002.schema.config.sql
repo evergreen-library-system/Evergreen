5134977a87331b39dd4db0c1dae4386bdbfc9664
Ben Shum <bshum@biblio.org>
2012-12-18 00:12:38 -0500

    Stamping upgrade script for hold request email notify default false
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -87,7 +87,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0745', :eg_version); -- mrpeters-isl/bshum/dbs
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0746', :eg_version); -- mrpeters-isl/bshum
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/0746.action.hold_request.email_notify_default_false.sql
+++ Open-ILS/src/sql/Pg/upgrade/0746.action.hold_request.email_notify_default_false.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0746', :eg_version);
 
 ALTER TABLE action.hold_request ALTER COLUMN email_notify SET DEFAULT 'false';
 
