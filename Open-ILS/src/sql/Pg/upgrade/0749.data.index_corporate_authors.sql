70f19d8ecddd21953d978f255f8fd0bb172dc680
Ben Shum <bshum@biblio.org>
2012-12-23 15:38:43 -0500

    Stamping upgrade script for index corporate authors
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0749.data.index_corporate_authors.sql
+++ Open-ILS/src/sql/Pg/upgrade/0749.data.index_corporate_authors.sql
@@ -1,6 +1,11 @@
 -- Use MARC relator codes (710 subfield 4) to index corporate authors, along
 -- with the existing relator text (710 subfield e)
 
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('0749', :eg_version);
+
 UPDATE config.metabib_field
   SET xpath = $$//mods32:mods/mods32:name[@type='corporate'
     and (mods32:role/mods32:roleTerm[text()='creator']
@@ -16,3 +21,5 @@ SELECT metabib.reingest_metabib_field_entries(record, TRUE, TRUE, FALSE)
     AND subfield = '4'
     AND value IN ('cre', 'aut')
 ;
+
+COMMIT;
