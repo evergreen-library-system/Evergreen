b33e783ed76fbb15e96385e9ec4ab606b443b410
Jason Etheridge <jason@esilibrary.com>
2013-01-24 17:30:26 -0500

    move EventListenerList out of global_util.js
    So that we can more easily pull it into main.xul (which we also do here)
    
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/xul/staff_client/chrome/content/OpenILS/global_util.js
+++ Open-ILS/xul/staff_client/chrome/content/OpenILS/global_util.js
@@ -2,43 +2,6 @@
         xulG = window.arguments[0];
     }
 
-    function EventListenerList() {
-        this._listeners = [];
-        return this;
-    }
-
-    EventListenerList.prototype = {
-        'add' : function(node, type, listener, useCapture) {
-            try {
-                node.addEventListener(type,listener,useCapture);
-                this._listeners.push({
-                    'node' : node,
-                    'type' : type,
-                    'listener' : listener,
-                    'useCapture' : useCapture
-                });
-            } catch(E) {
-                alert(location.href + ' Error adding event listener ' + type + ': ' + E);
-            }
-        },
-
-        'removeAll' : function() {
-            try {
-                if (typeof this._listeners != 'undefined') {
-                    for (var i = 0; i < this._listeners.length; i++) {
-                        this._listeners[i].node.removeEventListener(
-                            this._listeners[i].type,
-                            this._listeners[i].listener,
-                            this._listeners[i].useCapture
-                        );
-                    }
-                }
-            } catch(E) {
-                alert(location.href + ' Error in unloadEventListeners(): ' + E);
-            }
-        }
-    }
-
     function $(id) { return document.getElementById(id); }
 
     function oils_unsaved_data_V() {
