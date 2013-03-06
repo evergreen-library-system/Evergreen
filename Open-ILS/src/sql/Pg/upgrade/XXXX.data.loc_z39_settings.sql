4b78507904e4802964a14a685d12bb4697db7657
Bill Erickson <berick@esilibrary.com>
2013-03-06 13:54:35 -0500

    Upgrade script for LP#1105225: LOC z39.50 seed data
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Ben Shum <bshum@biblio.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.loc_z39_settings.sql
@@ -0,0 +1,18 @@
+
+BEGIN;
+
+UPDATE config.z3950_source
+    SET host = 'lx2.loc.gov', port = 210, db = 'LCDB'
+    WHERE name = 'loc'
+        AND host = 'z3950.loc.gov'
+        AND port = 7090
+        AND db = 'Voyager';
+
+UPDATE config.z3950_attr
+    SET format = 6
+    WHERE source = 'loc'
+        AND name = 'lccn'
+        AND format = 1;
+
+COMMIT;
+
