c7b3c1cc6e78b37c4de1fadb92e6cb4320fa65d9
Ben Shum <bshum@biblio.org>
2013-03-07 01:48:46 -0500

    Stamping upgrade for LOC z39.50 target seed data
    Signed-off-by: Ben Shum <bshum@biblio.org>
    
    Conflicts:
    	Open-ILS/src/sql/Pg/002.schema.config.sql

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -87,7 +87,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0751', :eg_version); -- berick/senator
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0764', :eg_version); -- gmcharlt/berick/bshum
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
