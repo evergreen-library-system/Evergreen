43bc9c66ada9f8c14d071f2991f7e7949b226962
Dan Wells <dbw2@calvin.edu>
2013-03-13 16:35:20 -0400

    Translation file entries for serial note editors
    Signed-off-by: Dan Wells <dbw2@calvin.edu>
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>

--- Open-ILS/web/js/dojo/openils/serial/nls/serial.js
+++ Open-ILS/web/js/dojo/openils/serial/nls/serial.js
@@ -1,5 +1,8 @@
 {
     "CREATE_ISSUANCE": "Create Issuance",
     "MODIFY_ISSUANCE": "Modify Issuance",
-    "SAVE_SUCCESSFUL": "Save Successful"
+    "SAVE_SUCCESSFUL": "Save Successful",
+    "NOTES_SSUB": "Subscription Notes (ID: ${0})",
+    "NOTES_SDIST": "Distribution Notes (ID: ${0})",
+    "REQUIRE_ONE_ROW": "Use the checkboxes to select exactly one row for this operation."
 }
