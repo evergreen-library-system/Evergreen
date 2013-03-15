314b1a5dcb13a67cf146ea8b4c9c9722c57ce78b
Bill Erickson <berick@esilibrary.com>
2013-03-15 09:24:45 -0400

    LP 1155611 - prevent unnecessary load of fm_IDL.xml
    Ensure fm_IDL.xml is loaded only when IDL2js has not been loaded.
    
    Signed-off-by: Bill Erickson <berick@esilibrary.com>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/web/js/dojo/fieldmapper/IDL.js
+++ Open-ILS/web/js/dojo/fieldmapper/IDL.js
@@ -31,7 +31,10 @@ if(!dojo._hasResource["fieldmapper.IDL"]) {
                 window._preload_fieldmapper_IDL = null;
             }
 
-            if(!fieldmapper.IDL.fmclasses || !fieldmapper.IDL.fmclasses.length || (classlist && classlist.length)) {
+            if(!fieldmapper.IDL.fmclasses || 
+                    !Object.keys(fieldmapper.IDL.fmclasses).length || 
+                    (classlist && classlist.length)) {
+
                 var idl_url = this._URL_PATH;
 
                 if (classlist.length && (classlist.length > 1 || classlist[0] != '*')) {
