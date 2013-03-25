d589e0c1af415ce3d107d40b47b1b9ba4491ef6b
Mike Rylander <mrylander@gmail.com>
2013-03-25 14:51:21 -0400

    Revert "Remove search.query_parser_fts from schema"
    This reverts commit ab9fb958e387a20cfe9fafb6035fb72bc5f1fb3f.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/040.schema.asset.sql
+++ Open-ILS/src/sql/Pg/040.schema.asset.sql
@@ -127,7 +127,7 @@ CREATE TABLE asset.opac_visible_copies (
 );
 COMMENT ON TABLE asset.opac_visible_copies IS $$
 Materialized view of copies that are visible in the OPAC, used by
-staged search to speed up OPAC visibility checks on large
+search.query_parser_fts() to speed up OPAC visibility checks on large
 databases.  Contents are maintained by a set of triggers.
 $$;
 CREATE INDEX opac_visible_copies_idx1 on asset.opac_visible_copies (record, circ_lib);
