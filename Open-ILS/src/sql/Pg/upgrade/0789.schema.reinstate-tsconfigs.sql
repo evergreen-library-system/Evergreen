ab489beb66184572005a739aa975553bb3736073
Jason Stephenson <jstephenson@mvlc.org>
2013-04-17 14:33:48 -0400

    Stamping upgrade script for reinstate tsconfigs.
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/upgrade/0789.schema.reinstate-tsconfigs.sql
+++ Open-ILS/src/sql/Pg/upgrade/0789.schema.reinstate-tsconfigs.sql
@@ -1,6 +1,6 @@
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0789', :eg_version);
 
--- These can be run outside of a transaction
+-- These can and should be run outside of a transaction
 CREATE TEXT SEARCH CONFIGURATION title ( COPY = english_nostop );
 CREATE TEXT SEARCH CONFIGURATION author ( COPY = english_nostop );
 CREATE TEXT SEARCH CONFIGURATION subject ( COPY = english_nostop );
