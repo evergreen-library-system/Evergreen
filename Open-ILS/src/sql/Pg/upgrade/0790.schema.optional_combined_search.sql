b83e736ab52d4c35cf770e9143661898831d509c
Ben Shum <bshum@biblio.org>
2013-04-18 13:45:54 -0400

    Stamping upgrade script for optional combined search by class
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0790.schema.optional_combined_search.sql
+++ Open-ILS/src/sql/Pg/upgrade/0790.schema.optional_combined_search.sql
@@ -1,4 +1,8 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0790', :eg_version);
 
 ALTER TABLE config.metabib_class ADD COLUMN combined BOOL NOT NULL DEFAULT FALSE;
 UPDATE config.metabib_class SET combined = TRUE WHERE name = 'subject';
 
+COMMIT;
