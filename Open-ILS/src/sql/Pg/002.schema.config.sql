e04288482426a8fff34f1f49f7feaf4cc7731cc9
Bill Erickson <berick@esilibrary.com>
2013-05-16 11:24:06 -0400

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: Bill Erickson <berick@esilibrary.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -88,6 +88,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0788', :eg_version); -- bshum/dbwells
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('2.3.7', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
