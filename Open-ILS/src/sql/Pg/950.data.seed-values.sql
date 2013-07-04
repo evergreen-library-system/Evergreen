2caf74f567f89f44bb596e83cc4f12cece2f6f6a
Pasi Kallinen <pasi.kallinen@pttk.fi>
2013-07-04 12:00:48 +0300

    Make Vandelay merge profile names translatable.
    Signed-off-by: Pasi Kallinen <pasi.kallinen@pttk.fi>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -12630,11 +12630,11 @@ INSERT INTO action_trigger.environment (
     'owning_lib.billing_address'
 );
 
-INSERT INTO vandelay.merge_profile (owner, name, replace_spec) 
-    VALUES (1, 'Match-Only Merge', '901c');
+INSERT INTO vandelay.merge_profile (id, owner, name, replace_spec) 
+    VALUES (1, 1, oils_i18n_gettext(1, 'Match-Only Merge', 'vmp', 'name'), '901c');
 
-INSERT INTO vandelay.merge_profile (owner, name, preserve_spec) 
-    VALUES (1, 'Full Overlay', '901c');
+INSERT INTO vandelay.merge_profile (id, owner, name, preserve_spec) 
+    VALUES (2, 1, oils_i18n_gettext(2, 'Full Overlay', 'vmp', 'name'), '901c');
 
 -- user activity seed data --
 
