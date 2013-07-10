6bf2e8f997c28fb4c26a0565ff8ce1cb7cc36341
Mike Rylander <mrylander@gmail.com>
2013-07-10 12:44:33 -0400

    Stamping upgrade script for Desk Renewal Circ Lib
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0805.schema.desk_renewal_reuse_circ_lib.sql
+++ Open-ILS/src/sql/Pg/upgrade/0805.schema.desk_renewal_reuse_circ_lib.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0805', :eg_version);
 
 INSERT INTO config.global_flag (name, label, enabled)
     VALUES (
