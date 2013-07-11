357b6ca85f240b249f393cc24c44dbbccba75acd
Mike Rylander <mrylander@gmail.com>
2013-07-11 13:09:11 -0400

    Stamping hold-cancel-via-sip upgrade script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0806.data.hold_cancel_via_sip.sql
+++ Open-ILS/src/sql/Pg/upgrade/0806.data.hold_cancel_via_sip.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- check whether patch can be applied
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0806', :eg_version);
 
 INSERT INTO action.hold_request_cancel_cause (id,label) 
     VALUES (7,'Patron via SIP');
