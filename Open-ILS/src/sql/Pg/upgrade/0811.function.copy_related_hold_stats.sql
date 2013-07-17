71b298857646d89527d6f50d217ba696eebde3b9
Mike Rylander <mrylander@gmail.com>
2013-07-17 13:29:14 -0400

    Stamping upgrade script for copy stats SP fix
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0811.function.copy_related_hold_stats.sql
+++ Open-ILS/src/sql/Pg/upgrade/0811.function.copy_related_hold_stats.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0811', :eg_version);
 
 DROP FUNCTION action.copy_related_hold_stats(integer);
 
