fe8f940ad3c8fb96ebfb1e747970d4c2124dadb9
Garry Collum <gcollum@gmail.com>
2013-08-04 16:10:26 -0400

    lp1193487 Circulation Policy Column Picker not saving configuration.
    The circulation policy column picker is not saving its configuration
    because ui.grid_columns.conify.config.circ_matrix_matchpoint is not
    defined in the config.usr_setting_type table.  This updates the sql
    to populate the table.
    
    Signed-off-by: Garry Collum <gcollum@gmail.com>
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -10441,6 +10441,23 @@ INSERT INTO config.usr_setting_type (name,grp,opac_visible,label,description,dat
         'description'
     ),
     'string'
+), (
+    'ui.grid_columns.conify.config.circ_matrix_matchpoint',
+    'gui',
+    FALSE,
+    oils_i18n_gettext(
+        'ui.grid_columns.conify.config.circ_matrix_matchpoint',
+        'Circulation Policy Configuration',
+        'cust',
+        'label'
+    ),
+    oils_i18n_gettext(
+        'ui.grid_columns.conify.config.circ_matrix_matchpoint',
+        'Circulation Policy Configuration Column Settings',
+        'cust',
+        'description'
+    ),
+    'string'
 ) ;
 
 SELECT setval( 'config.sms_carrier_id_seq', 1000 );
