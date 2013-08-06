5fe3ec70460bdd75af7bccd2967ccb62dd475c81
Ben Shum <bshum@biblio.org>
2013-08-06 12:13:15 -0400

    Stamping upgrade script for "delate" typo
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0814.data.fix-delate-typo.sql
+++ Open-ILS/src/sql/Pg/upgrade/0814.data.fix-delate-typo.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0814', :eg_version);
 
 UPDATE permission.perm_list
 SET description = 'Allow a user to delete a provider'
