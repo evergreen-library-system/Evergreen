46a7817d1339487bacf6f056be1a8507a7bca9bf
Ben Shum <bshum@biblio.org>
2013-09-06 23:20:16 -0400

    Stamping upgrade script for biblio A/T title variable fix
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0825.data.bre_format.sql
+++ Open-ILS/src/sql/Pg/upgrade/0825.data.bre_format.sql
@@ -1,11 +1,11 @@
--- Evergreen DB patch XXXX.data.bre_format.sql
+-- Evergreen DB patch 0825.data.bre_format.sql
 --
 -- Fix some templates that loop over bibs to not have duplicated/run-on titles
 --
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0825', :eg_version);
 
 -- I think we shy away from modifying templates on existing systems, but this seems pretty safe...
 UPDATE
