5d546cc4f21c9ea9431f4530af28fa5e6ad631c6
Mike Rylander <mrylander@gmail.com>
2013-09-12 13:48:47 -0400

    Stamping P.V. SUPA upgrade scripts
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0826.data.server_addon_perms.sql
+++ Open-ILS/src/sql/Pg/upgrade/0826.data.server_addon_perms.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0826', :eg_version);
 
 INSERT INTO permission.perm_list ( id, code, description ) VALUES (
     551,
