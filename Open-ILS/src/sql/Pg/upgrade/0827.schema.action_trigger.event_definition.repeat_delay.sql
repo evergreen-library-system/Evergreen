75817fabba07f84ca7f12f282b328f02a1f7be10
Mike Rylander <mrylander@gmail.com>
2013-09-12 13:58:14 -0400

    Stamping upgrade script for repeatable events
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0827.schema.action_trigger.event_definition.repeat_delay.sql
+++ Open-ILS/src/sql/Pg/upgrade/0827.schema.action_trigger.event_definition.repeat_delay.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0827', :eg_version);
 
 ALTER TABLE action_trigger.event_definition ADD COLUMN repeat_delay INTERVAL;
 
