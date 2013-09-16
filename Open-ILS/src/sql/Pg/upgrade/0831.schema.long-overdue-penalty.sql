823c90a959026b2e5f1ba4b4593305547ab8321c
Mike Rylander <mrylander@gmail.com>
2013-09-16 12:47:46 -0400

    Stamping long overdue penalty upgrade script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0831.schema.long-overdue-penalty.sql
+++ Open-ILS/src/sql/Pg/upgrade/0831.schema.long-overdue-penalty.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0831', :eg_version);
 
 -- TODO: check for penalty ID collision before master merge; affects 
 -- config.standing_penalty and actor.calculate_system_penalties
