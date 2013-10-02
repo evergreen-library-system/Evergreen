2cb2011016ff24e84ad95ad38478e63df3b05861
Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
2013-10-02 15:28:35 -0400

    The identifier|bibcn field is best served by having no normalizers applied
    Signed-off-by: Lebbeous Fogle-Weekley <lebbeous@esilibrary.com>
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.bibcn_index_normalizers.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+-- check whether patch can be applied
+-- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+DELETE FROM config.metabib_field_index_norm_map
+    WHERE field = 25 AND norm IN (
+        SELECT id
+        FROM config.index_normalizer
+        WHERE func IN ('search_normalize','split_date_range')
+    );
+
+\qecho If your site's bibcn searches are affected by this issue, you may wish
+\qecho to reingest your bib records now.  It's probably not worth it for many
+\qecho sites.
+
+COMMIT;
