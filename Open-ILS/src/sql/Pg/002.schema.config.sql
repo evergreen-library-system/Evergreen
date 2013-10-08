bb6b0992b760cf98ba7481ee704b3357cb89dba7
Jason Stephenson <jstephenson@mvlc.org>
2013-10-08 15:05:14 -0400

    Stamping upgrade script for 0840 circulation column picker data.
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0838', :eg_version); -- senator/dbwells
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0840', :eg_version); -- gcollum/dyrcona
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
