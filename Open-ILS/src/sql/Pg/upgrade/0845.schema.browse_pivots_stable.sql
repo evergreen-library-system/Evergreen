c9a4dee7878648e671fe7c33c431a366746eab34
Jason Stephenson <jstephenson@mvlc.org>
2013-10-29 15:45:18 -0400

    Stamping upgrade script for 0845: browse pivots should be stable.
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/upgrade/0845.schema.browse_pivots_stable.sql
+++ Open-ILS/src/sql/Pg/upgrade/0845.schema.browse_pivots_stable.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- check whether patch can be applied
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0845', :eg_version);
 
 ALTER FUNCTION metabib.browse_pivot (integer[], text) STABLE;
 ALTER FUNCTION metabib.browse_bib_pivot (integer[], text) STABLE;
