26ec2c5d54ad3a106337542fd73130406d560441
Ben Shum <bshum@biblio.org>
2013-12-17 14:59:44 -0500

    Stamping 0849 - desk renewal description
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0849.schema.desk_renewal_desc_update.sql
+++ Open-ILS/src/sql/Pg/upgrade/0849.schema.desk_renewal_desc_update.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO config.upgrade_log (version) VALUES ('xxxx');
+SELECT evergreen.upgrade_deps_block_check('0849', :eg_version);
 
 UPDATE config.global_flag
     SET label = 'Circ: Use original circulation library on desk renewal instead of the workstation library'
