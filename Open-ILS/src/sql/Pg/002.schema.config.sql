9c7b271e94b900d2e11aab35ad69a7ed88825e1e
Mike Rylander <mrylander@gmail.com>
2014-01-16 15:44:48 -0500

    Renumbering the upgrade script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0851', :eg_version); -- dbs/dbwells
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0852', :eg_version); -- miker/senator
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
