3699196475f8bf160232e1e9f2718afb3681e4d5
Dan Wells <dbw2@calvin.edu>
2014-01-21 15:07:29 -0500

    Stamping 0853: 'Prefer external URL' OUS
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/0853.data.prefer_external_url_OUS.sql
+++ Open-ILS/src/sql/Pg/upgrade/0853.data.prefer_external_url_OUS.sql
@@ -5,7 +5,7 @@
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0853', :eg_version);
 
 INSERT into config.org_unit_setting_type
 ( name, grp, label, description, datatype, fm_class ) VALUES
