c0a0fc6e1298bf8921e0cb15039763127b1b70fe
Ben Shum <bshum@biblio.org>
2014-02-19 14:22:37 -0500

    LP1223903 - Stamping upgrade script for new authority_record_entry indexes
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0861.index.authority_record_entry.sql
+++ Open-ILS/src/sql/Pg/upgrade/0861.index.authority_record_entry.sql
@@ -1,2 +1,8 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0861', :eg_version);
+
 CREATE INDEX authority_record_entry_create_date_idx ON authority.record_entry ( create_date );
 CREATE INDEX authority_record_entry_edit_date_idx ON authority.record_entry ( edit_date );
+
+COMMIT;
