1351a3d4fee6616c1b2aca3f019452810889c05a
Dan Wells <dbw2@calvin.edu>
2014-03-27 14:19:34 -0400

    LP#1259665 Split series browse config from search
    When we added browse support in 2.5, we took extra care to ensure that
    our series browse entries would sort and display reliably.
    
    800t does not specify non-filing characters, so it was excluded from
    browse.  Unfortunately, since browse and search had a single root
    config, this removed 800t from search as well.
    
    While we may want to reconsider expanding later on, this commit takes
    a conservative approach, and uses all series values for search and
    facets, but only entries with a usable filing indicator will be
    extracted for browse.  It accomplishes this by splitting the series
    config in two, one config for search/faceting, and a second config
    dedicated to browse (similar to what was done for title).
    
    Signed-off-by: Dan Wells <dbw2@calvin.edu>
    Signed-off-by: Ben Shum <bshum@biblio.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.re-add_800_t_search_and_facets.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+-- Don't use Series search field as the browse field
+UPDATE config.metabib_field SET
+	browse_field = FALSE,
+	browse_xpath = NULL,
+	browse_sort_xpath = NULL,
+	xpath = $$//mods32:mods/mods32:relatedItem[@type="series"]/mods32:titleInfo[not(@type="nfi")]$$
+WHERE id = 1;
+
+-- Create a new series browse config
+INSERT INTO config.metabib_field ( id, field_class, name, label, format, xpath, search_field, authority_xpath, browse_field, browse_sort_xpath ) VALUES
+    (32, 'series', 'browse', oils_i18n_gettext(32, 'Series Title (Browse)', 'cmf', 'label'), 'mods32', $$//mods32:mods/mods32:relatedItem[@type="series"]/mods32:titleInfo[@type="nfi"]$$, FALSE, '//@xlink:href', TRUE, $$*[local-name() != "nonSort"]$$ );
+
+COMMIT;
+
+\qecho This is a full field-entry reingest of your bib records.
+\qecho It will take a while.
+\qecho You may cancel now without losing the effect of the rest of the
+\qecho upgrade script, and arrange the reingest later.
+\qecho .
+SELECT COUNT(metabib.reingest_metabib_field_entries(id))
+    FROM biblio.record_entry WHERE deleted IS FALSE;
