15845afca54695bf052d70f3b05bc1aaf242731f
Jason Etheridge <jason@esilibrary.com>
2014-05-01 04:15:55 -0400

    LP#1314920 change the desc for the org hiding setting
    so that it references physical_loc instead of ol
    
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -3982,7 +3982,7 @@ INSERT into config.org_unit_setting_type
         'Org Unit Hiding Depth',
         'coust', 'label'),
     oils_i18n_gettext('opac.org_unit_hiding.depth',
-        'This will hide certain org units in the public OPAC if the Original Location (url param "ol") for the OPAC inherits this setting.  This setting specifies an org unit depth, that together with the OPAC Original Location determines which section of the Org Hierarchy should be visible in the OPAC.  For example, a stock Evergreen installation will have a 3-tier hierarchy (Consortium/System/Branch), where System has a depth of 1 and Branch has a depth of 2.  If this setting contains a depth of 1 in such an installation, then every library in the System in which the Original Location belongs will be visible, and everything else will be hidden.  A depth of 0 will effectively make every org visible.  The embedded OPAC in the staff client ignores this setting.',
+        'This will hide certain org units in the public OPAC if the Physical Location (url param "physical_loc") for the OPAC inherits this setting.  This setting specifies an org unit depth, that together with the OPAC Physical Location determines which section of the Org Hierarchy should be visible in the OPAC.  For example, a stock Evergreen installation will have a 3-tier hierarchy (Consortium/System/Branch), where System has a depth of 1 and Branch has a depth of 2.  If this setting contains a depth of 1 in such an installation, then every library in the System in which the Physical Location belongs will be visible, and everything else will be hidden.  A depth of 0 will effectively make every org visible.  The embedded OPAC in the staff client ignores this setting.',
         'coust', 'description'),
     'integer', null)
 
---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.YAOUS-desc-update.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+UPDATE config.org_unit_setting_type
+    SET description = replace(replace(description,'Original','Physical'),'"ol"','"physical_loc"')
+    WHERE name = 'opac.org_unit_hiding.depth';
+
+COMMIT;
