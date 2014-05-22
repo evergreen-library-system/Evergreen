6145fdb54b4d7f7d4acd8487c66dfaa2406731e1
Dan Wells <dbw2@calvin.edu>
2014-05-22 11:24:41 -0400

    LP#1314920 Stamping upgrade script
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0880', :eg_version); -- miker/sreyseng/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0881', :eg_version); -- phasefx/dbwells
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
