611673967f308a806842b8a30e09cdee10e742da
Ben Shum <bshum@biblio.org>
2014-08-27 00:49:48 -0400

    LP#1189556: stamping upgrade script for URL_VERIFY description typo fix
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0891.data.url-verify-permission-typo.sql
+++ Open-ILS/src/sql/Pg/upgrade/0891.data.url-verify-permission-typo.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0891', :eg_version);
 
 UPDATE permission.perm_list
 SET description = 'Allows a user to process and verify URLs'
