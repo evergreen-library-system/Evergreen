8834a32c3bb6e5e496e2be7841f7f1590ecbd50e
Ben Shum <bshum@biblio.org>
2014-10-10 11:21:34 -0400

    LP#1368314: Stamping upgrade script for RDA support to reporter.simple_record
    Signed-off-by: Ben Shum <bshum@biblio.org>
    
    Conflicts:
    	Open-ILS/src/sql/Pg/002.schema.config.sql

--- Open-ILS/src/sql/Pg/upgrade/0893.schema.reporter.simple_record.sql
+++ Open-ILS/src/sql/Pg/upgrade/0893.schema.reporter.simple_record.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version); 
+SELECT evergreen.upgrade_deps_block_check('0893', :eg_version); 
 
 CREATE OR REPLACE VIEW reporter.simple_record AS
 SELECT  r.id,
