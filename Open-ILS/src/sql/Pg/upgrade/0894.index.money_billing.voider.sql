896dea54555012783ca795fb4bbc40a7d22695fc
Ben Shum <bshum@biblio.org>
2014-10-10 11:35:25 -0400

    LP#1374551: Stamping upgrade script for new index on money.billing.voider
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0894.index.money_billing.voider.sql
+++ Open-ILS/src/sql/Pg/upgrade/0894.index.money_billing.voider.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0894', :eg_version);
 
 CREATE INDEX m_b_voider_idx ON money.billing (voider);
 
