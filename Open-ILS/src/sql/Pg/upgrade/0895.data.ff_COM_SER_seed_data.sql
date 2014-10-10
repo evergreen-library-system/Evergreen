26cf720e8c90223ca4b7b45cf0255b5cd42e7a3c
Ben Shum <bshum@biblio.org>
2014-10-10 13:18:03 -0400

    LP#1309664: Stamping upgrade script for new seed data for COM and SER
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0895.data.ff_COM_SER_seed_data.sql
+++ Open-ILS/src/sql/Pg/upgrade/0895.data.ff_COM_SER_seed_data.sql
@@ -1,3 +1,6 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0895', :eg_version);
 
 INSERT INTO config.marc21_ff_pos_map (fixed_field, tag, rec_type,start_pos, length, default_val) VALUES ('File', '008', 'COM', 26, 1, 'u');
 INSERT INTO config.marc21_ff_pos_map (fixed_field, tag, rec_type,start_pos, length, default_val) VALUES ('File', '006', 'COM', 9, 1, 'u');
@@ -9,3 +12,5 @@ INSERT INTO config.marc21_ff_pos_map (fixed_field, tag, rec_type,start_pos, leng
 INSERT INTO config.record_attr_definition (name,label,fixed_field) values ('file','File','File');
 INSERT INTO config.record_attr_definition (name,label,fixed_field) values ('freq','Freq','Freq');
 INSERT INTO config.record_attr_definition (name,label,fixed_field) values ('regl','Regl','Regl');
+
+COMMIT;
