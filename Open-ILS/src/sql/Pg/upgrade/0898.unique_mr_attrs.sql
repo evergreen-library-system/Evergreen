e8b18ef6ab6654ef2f29fbf2e0fdca9b0c9188b0
Jason Stephenson <jstephenson@mvlc.org>
2014-12-10 16:03:11 -0500

    LP#1401177: Stamping upgrade script of unique_mr_attrs.sql.
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/upgrade/0898.unique_mr_attrs.sql
+++ Open-ILS/src/sql/Pg/upgrade/0898.unique_mr_attrs.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0898', :eg_version);
 
 CREATE OR REPLACE FUNCTION unapi.mmr_mra (
     obj_id BIGINT,
