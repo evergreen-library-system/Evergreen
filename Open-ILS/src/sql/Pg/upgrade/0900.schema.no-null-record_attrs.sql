fce2916d590f766b5bc6df60f0d12375712d0a61
Ben Shum <bshum@biblio.org>
2014-12-16 15:01:53 -0500

    LP#1400376: Stamping upgrade script for fix to metabib.record_attr
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0900.schema.no-null-record_attrs.sql
+++ Open-ILS/src/sql/Pg/upgrade/0900.schema.no-null-record_attrs.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+SELECT evergreen.upgrade_deps_block_check('0900', :eg_version);
+
 CREATE OR REPLACE VIEW metabib.record_attr AS
     SELECT  id, HSTORE( ARRAY_AGG( attr ), ARRAY_AGG( value ) ) AS attrs
       FROM  metabib.record_attr_flat
