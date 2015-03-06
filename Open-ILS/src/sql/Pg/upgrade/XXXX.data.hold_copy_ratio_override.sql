f02fd0f307d5afdc0051b27bfaf685da224a9b2c
Bill Erickson <berickxx@gmail.com>
2015-03-06 11:43:43 -0500

    LP#1234220 hold ratio renewal override perms
    New permissions for overriding renewal failure events:
    
    TOTAL_HOLD_COPY_RATIO_EXCEEDED.override
    AVAIL_HOLD_COPY_RATIO_EXCEEDED.override
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.hold_copy_ratio_override.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+INSERT INTO permission.perm_list (id, code, description) 
+VALUES (  
+    560, 
+    'TOTAL_HOLD_COPY_RATIO_EXCEEDED.override',
+    oils_i18n_gettext(
+        560,
+        'Override the TOTAL_HOLD_COPY_RATIO_EXCEEDED event',
+        'ppl', 
+        'description'
+    )
+);
+
+INSERT INTO permission.perm_list (id, code, description) 
+VALUES (  
+    561, 
+    'AVAIL_HOLD_COPY_RATIO_EXCEEDED.override',
+    oils_i18n_gettext(
+        561,
+        'Override the AVAIL_HOLD_COPY_RATIO_EXCEEDED event',
+        'ppl', 
+        'description'
+    )
+);
+
+COMMIT;
