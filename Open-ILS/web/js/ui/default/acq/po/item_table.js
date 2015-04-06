6f869ff1052c41c600079d56679578895ac33acc
Bill Erickson <berickxx@gmail.com>
2015-04-06 11:24:33 -0400

    LP#1436906 clean up PO direct charges on delete/cancel
    1. Deleting a "direct charge" from a PO removes the linked fund_debit if
    the fund_debit has not been paid (i.e. it's still encumbered).
    
    If the debit has been paid, the charge cannot be removed, unless/until
    the debit is re-encumbered by un-invoicing the charge.
    
    2. When a PO is canceled, fund_debits linked to PO items are removed.
    As above, if the debit has been paid (invoiced), the PO cannot be
    canceled, unless/until direct charge debits are un-invoiced.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/web/js/ui/default/acq/po/item_table.js
+++ Open-ILS/web/js/ui/default/acq/po/item_table.js
@@ -110,13 +110,16 @@ function PoItemTable() {
     this.deleteRow = function(id) {
         if (id > 0) {
             progressDialog.show(true);
-            pcrud.eliminate(
-                this.realItems[id], {
-                    "oncomplete": function(r) {
+            fieldmapper.standardRequest(
+                ['open-ils.acq', 'open-ils.acq.po_item.delete'],
+                {   async : true,
+                    params: [openils.User.authtoken, id],
+                    oncomplete : function(r) {
                         progressDialog.hide();
                         r = openils.Util.readResponse(r); /* may not use */
-
-                        self._deleteRow(id);
+                        if (r == '1') {
+                            self._deleteRow(id);
+                        } 
                     }
                 }
             );
