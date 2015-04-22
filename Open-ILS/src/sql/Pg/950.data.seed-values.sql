6d9d244457539594dc1a27ce3c4381d5e87beee5
Josh Stompro <stomproj@larl.org>
2015-04-22 14:36:18 -0500

    LP#1124498 - Added 30 Day pre expire notice to 950.data.seed-values.sql
    This will install this notice for new installs.
    
    Signed-off-by: Josh Stompro <stomproj@larl.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -14313,6 +14313,52 @@ INSERT INTO action_trigger.environment (event_def, path) VALUES
     (53, 'pickup_lib'),
     (53, 'bib_rec.bib_record.simple_record');
 
+-- 30 Day Pre Expire A/T Notice - Notify customers before their account expires
+
+INSERT INTO action_trigger.hook (key, core_type, description, passive)
+    VALUES ('expire', 'au', 'Account is expired', 't');
+	
+INSERT INTO action_trigger.event_definition (
+    active, owner, name, hook,
+    validator, reactor, delay, delay_field,
+    max_delay, repeat_delay, template
+) VALUES (
+    'f', '1', '30 Day Account Expiration Courtesy Notice','expire',
+    'NOOP_True', 'SendEmail', '-30 days', 'expire_date', '-29 days', '30 days',
+$$
+[%- USE date -%]
+[%- user = target -%]
+[%- lib = target.home_ou -%]
+To: [%- params.recipient_email || user.email %]
+From: [%- helpers.get_org_setting(target.home_ou.id, 'org.bounced_emails') || lib.email || params.sender_email || default_sender %]
+Reply-To: [%- helpers.get_org_setting(target.home_ou.id, 'org.bounced_emails') || lib.email || params.sender_email || default_sender %]
+Subject: Courtesy Notice - Library Account Expiration in 30 days
+Auto-Submitted: auto-generated
+
+Dear [% user.first_given_name %] [% user.family_name %],
+
+Our records indicate your library account is due to expire in 30 days.  Please visit your local library at your convenience to renew your account in order to avoid a disruption in access to library service.
+
+Sincerely,
+[% lib.name %]
+
+Contact your library for more information:
+
+[% lib.name %]
+[%- SET addr = lib.mailing_address -%]
+[%- IF !addr -%] [%- SET addr = lib.billing_address -%] [%- END %]
+[% addr.street1 %] [% addr.street2 %]
+[% addr.city %], [% addr.state %]
+[% addr.post_code %]
+[% lib.phone %]
+
+$$);
+
+INSERT INTO action_trigger.environment (event_def, path) VALUES
+    (currval('action_trigger.event_definition_id_seq'), 'home_ou.mailing_address'),
+    (currval('action_trigger.event_definition_id_seq'), 'home_ou.billing_address');
+
+    
 -- OUS's for patron self-reg
 INSERT INTO config.org_unit_setting_type
     (name, grp, datatype, label, description)
