a95199bb5f49ed11dfc27bbeefbd8a23c4f9e46d
Galen Charlton <gmc@esilibrary.com>
2015-04-22 13:43:19 +0000

    LP#1447168: add an index on authority.simple_heading.record
    This index is needed so that full table scans on ash aren't
    needed during authority record reingest.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.index_ash_record.sql
@@ -0,0 +1,9 @@
+-- index authority.simple_heading.record so that reingesting
+-- authority records does not require a sequential scan of ash
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+CREATE INDEX authority_simple_heading_record_idx ON authority.simple_heading (record);
+
+COMMIT;
