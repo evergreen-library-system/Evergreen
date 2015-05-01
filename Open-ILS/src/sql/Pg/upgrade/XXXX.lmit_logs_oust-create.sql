34337bda97bb667dd06b3c90c539e60b58530ebe
Jason Stephenson <jstephenson@mvlc.org>
2015-05-01 15:33:05 -0400

    LP 1450561: Restore org. unit settings history limit function and trigger
    The evergreen.limit_oustl function and the limit_log_oustl trigger on
    config.org_unit_setting_type_log were missed in the upgrade scripts
    when these were added to the base schema.  This commit adds them and a
    pgtap test to check that they exist after.
    
    This commit also alters the body of the limit_oustl function to take into
    account of the org unit when deleting entries from the log table.  This
    change allows multiple consortium members to have their own five history
    entries.  The original code only kept the last five entries of any given
    setting across the whole installation.
    
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>
    Signed-off-by: Ben Shum <bshum@biblio.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.lmit_logs_oust-create.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+CREATE OR REPLACE FUNCTION evergreen.limit_oustl() RETURNS TRIGGER AS $oustl_limit$
+    BEGIN
+        -- Only keeps the most recent five settings changes.
+        DELETE FROM config.org_unit_setting_type_log WHERE field_name = NEW.field_name AND org = NEW.org AND date_applied NOT IN 
+        (SELECT date_applied FROM config.org_unit_setting_type_log WHERE field_name = NEW.field_name AND org = NEW.org ORDER BY date_applied DESC LIMIT 4);
+        
+        IF (TG_OP = 'UPDATE') THEN
+            RETURN NEW;
+        ELSIF (TG_OP = 'INSERT') THEN
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$oustl_limit$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS limit_logs_oust ON config.org_unit_setting_type_log;
+
+CREATE TRIGGER limit_logs_oust
+    BEFORE INSERT OR UPDATE ON config.org_unit_setting_type_log
+    FOR EACH ROW EXECUTE PROCEDURE limit_oustl();
+
+COMMIT;
