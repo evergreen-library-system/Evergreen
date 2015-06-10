f2f651de27a5433a8d1e5e6d127dabc2de4f40e9
Dan Wells <dbw2@calvin.edu>
2015-06-10 14:29:27 -0400

    LP#1447168 Stamping upgrade script for index on ash record
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/0918.schema.index_ash_record.sql
+++ Open-ILS/src/sql/Pg/upgrade/0918.schema.index_ash_record.sql
@@ -2,7 +2,7 @@
 -- authority records does not require a sequential scan of ash
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0918', :eg_version);
 
 CREATE INDEX authority_simple_heading_record_idx ON authority.simple_heading (record);
 
