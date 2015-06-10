08bd7ca10c0650182093041d5102767d59a33953
Dan Wells <dbw2@calvin.edu>
2015-06-10 15:05:13 -0400

    LP#1289486 Stamping upgrade for dropping acq audit fkey
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.schema.drop-acq-audit-fkey.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
-
-ALTER TABLE acq.acq_lineitem_history DROP CONSTRAINT IF EXISTS acq_lineitem_history_queued_record_fkey;
-
-COMMIT;
