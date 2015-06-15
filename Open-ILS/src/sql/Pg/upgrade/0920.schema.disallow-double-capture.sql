880c4f4a73caed2a20772ffcb83cb1e2ba9545fa
Jason Stephenson <jstephenson@mvlc.org>
2015-06-15 16:29:02 -0400

    LP 902255: Fix upgrade script.
    Run as-is, the upgrade script produces an error:
    
    ERROR:  CREATE INDEX CONCURRENTLY cannot run inside a transaction block
    
    Following this conversation in IRC, it was decided to remove the keyword
    CONCURRENTLY from the create index statement.
    
    http://irc.evergreen-ils.org/evergreen/2015-06-15#i_182857
    
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- Open-ILS/src/sql/Pg/upgrade/0920.schema.disallow-double-capture.sql
+++ Open-ILS/src/sql/Pg/upgrade/0920.schema.disallow-double-capture.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 SELECT evergreen.upgrade_deps_block_check('0920', :eg_version);
 
-CREATE UNIQUE INDEX CONCURRENTLY
+CREATE UNIQUE INDEX
     hold_request_capture_protect_idx ON action.hold_request (current_copy)
     WHERE   current_copy IS NOT NULL -- sometimes null in old/bad data
             AND capture_time IS NOT NULL
