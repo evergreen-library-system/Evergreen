ead8f829d65a461ec1c74365fe9748b8e5a0dca1
Bill Erickson <berickxx@gmail.com>
2015-07-14 22:04:39 -0400

    webstaff: Browser client: integrate address alert HTML UI
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

---
+++ Open-ILS/web/js/ui/default/staff/admin/actor/app.js
@@ -0,0 +1,50 @@
+/**
+ * App to drive the base page. 
+ * Login Form
+ * Splash Page
+ */
+
+angular.module('egAdminActor',
+    ['ngRoute', 'ui.bootstrap', 'egCoreMod','egUiMod'])
+
+.config(['$routeProvider','$locationProvider','$compileProvider', 
+ function($routeProvider , $locationProvider , $compileProvider) {
+
+    $locationProvider.html5Mode(true);
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|blob):/); 
+    var resolver = {delay : function(egStartup) {return egStartup.go()}};
+
+    $routeProvider.when('/admin/actor/address_alert', {
+        templateUrl: './admin/actor/t_address_alert',
+        controller: 'AddressAlertCtl',
+        resolve : resolver
+    });
+
+    // default page 
+    /*
+    $routeProvider.otherwise({
+        templateUrl : 'user-perms-template',
+        controller: 'UserPermsCtrl',
+        resolve : resolver
+    });
+    */
+}])
+
+.controller('AddressAlertCtl',
+       ['$scope','$routeParams','$window','$location','egCore',
+function($scope , $routeParams , $window , $location , egCore) {
+
+    console.log('addr alert');
+    
+    $scope.funcs = {
+        ses : egCore.auth.token(),
+    }
+
+    // have to use the full URL, not just the path, to ensure
+    // the embeded page is not a nested version of this page (ad infinitum)
+    $scope.address_alert_url = $location.absUrl().replace(
+        /\/eg\/staff.*/, '/eg/conify/global/actor/address_alert');
+
+    console.log($scope.address_alert_url);
+
+}])
