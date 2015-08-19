1ee6d382348c5849b3ba93eb0f18d083b11e4b6d
Ben Shum <bshum@biblio.org>
2015-08-18 23:06:39 -0400

    LP#1419172: Stamping upgrade script for optimizing full circ count
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0926', :eg_version); -- dpearl/kmlussier/bshum
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0927', :eg_version); -- dbwells/csharp/bshum
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/0927.schema.optimize_full_circ_count.sql
+++ Open-ILS/src/sql/Pg/upgrade/0927.schema.optimize_full_circ_count.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0927', :eg_version);
 
 CREATE OR REPLACE VIEW extend_reporter.full_circ_count AS
    SELECT cp.id,
