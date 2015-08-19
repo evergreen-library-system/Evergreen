813541a513bd8d8ecc2594350d28e1e1acaa06dc
Jason Stephenson <jstephenson@mvlc.org>
2015-08-19 15:01:10 -0400

    LP 1438136: Stamping upgrade script.
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>
    Signed-off-by: Ben Shum <bshum@biblio.org>
    
    Conflicts:
    	Open-ILS/src/sql/Pg/002.schema.config.sql

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0936', :eg_version); -- gmcharlt/dyrcona/bshum
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0940', :eg_version); -- miker/jboyer/csharp/dyrcona
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
