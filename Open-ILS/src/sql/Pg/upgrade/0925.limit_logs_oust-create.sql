bf1d94684282a35e9bff93ff583691b0d443eb94
Ben Shum <bshum@biblio.org>
2015-08-18 22:22:42 -0400

    LP#1450561: Stamping upgrade script for library settings editor history limits
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0925.limit_logs_oust-create.sql
+++ Open-ILS/src/sql/Pg/upgrade/0925.limit_logs_oust-create.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0925', :eg_version);
 
 CREATE OR REPLACE FUNCTION evergreen.limit_oustl() RETURNS TRIGGER AS $oustl_limit$
     BEGIN
