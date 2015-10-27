a261d6827ca7e09fd9aee9ca86cf29321d25e30a
Bill Erickson <berickxx@gmail.com>
2015-10-27 14:08:53 -0400

    LP#1510641 Always show actions selector in PO view
    Ensure the actions selector is always visible for PO's, even when no
    lineitems are present.  This allows printing and history view (and
    potentially future actions) for PO's that only contain direct charges.
    
    The custom Add Brief Record button is no longer present, since the same
    action is accessible from the actions selector.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/web/js/ui/default/acq/po/view_po.js
+++ Open-ILS/web/js/ui/default/acq/po/view_po.js
@@ -477,7 +477,6 @@ function init() {
 
 function init2() {
 
-    var zeroLi = true;
     fieldmapper.standardRequest(
         ['open-ils.acq', 'open-ils.acq.lineitem.search'],
         {   async: true,
@@ -487,7 +486,6 @@ function init2() {
                 {flesh_attrs:true, flesh_notes:true, flesh_cancel_reason:true, clear_marc:true}
             ],
             onresponse: function(r) {
-                zeroLi = false;
                 liTable.show('list');
                 var li = openils.Util.readResponse(r);
                 liTable.addLineitem(li);
@@ -495,7 +493,6 @@ function init2() {
 
             oncomplete : function() {
                 if (liFocus) liTable.drawCopies(liFocus);
-                if(zeroLi) openils.Util.show('acq-po-no-lineitems');
             }
         }
     );
