e1367b7c64f20a741cd7e015c3d2dea89353c448
Ben Shum <bshum@biblio.org>
2015-11-05 18:12:35 -0500

    LP#937789: Stamping upgrade script for part deletion
    Signed-off-by: Ben Shum <bshum@biblio.org>

--- Open-ILS/src/sql/Pg/upgrade/0948.schema.fake-delete-parts.sql
+++ Open-ILS/src/sql/Pg/upgrade/0948.schema.fake-delete-parts.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+SELECT evergreen.upgrade_deps_block_check('0948', :eg_version);
+
 ALTER TABLE biblio.monograph_part ADD COLUMN deleted BOOL NOT NULL DEFAULT FALSE;
 CREATE RULE protect_mono_part_delete AS ON DELETE TO biblio.monograph_part DO INSTEAD (
     UPDATE biblio.monograph_part SET deleted = TRUE WHERE OLD.id = biblio.monograph_part.id;
