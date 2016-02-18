51c58f55324e77579473a26f323bf22b72a499b1
Kathy Lussier <klussier@masslnc.org>
2016-02-18 18:51:33 -0500

    LP#1486151 Stamping upgrade script for password reset regex description
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/upgrade/0955.schema.password-reset-regex-description.sql
+++ Open-ILS/src/sql/Pg/upgrade/0955.schema.password-reset-regex-description.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0955', :eg_version);
 
 UPDATE config.org_unit_setting_type
 SET description = 'Regular expression defining the password format.  Note: Be sure to update the update_password_msg.tt2 TPAC template with a user-friendly description of your password strength requirements.'
