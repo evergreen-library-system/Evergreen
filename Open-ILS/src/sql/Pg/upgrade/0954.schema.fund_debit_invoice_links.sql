deb37b86228082e8af2d009c943137f039cdcb90
Kathy Lussier <klussier@masslnc.org>
2016-02-18 12:58:03 -0500

    LP#1380709 Stamping upgrade script for fund debit invoice links
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/upgrade/0954.schema.fund_debit_invoice_links.sql
+++ Open-ILS/src/sql/Pg/upgrade/0954.schema.fund_debit_invoice_links.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0954', :eg_version);
 
 ALTER TABLE acq.fund_debit 
     ADD COLUMN invoice_entry INTEGER 
