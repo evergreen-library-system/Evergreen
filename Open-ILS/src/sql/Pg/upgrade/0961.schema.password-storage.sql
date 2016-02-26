5f9da3a7a4dfbe5ccc67e9d009050b76a34390cc
Bill Erickson <berickxx@gmail.com>
2016-02-26 10:11:56 -0500

    LP#1468422 Stamping DB upgrade for actor.passwd
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0961.schema.password-storage.sql
+++ Open-ILS/src/sql/Pg/upgrade/0961.schema.password-storage.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0961', :eg_version);
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
