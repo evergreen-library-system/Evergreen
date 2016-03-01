c658bd0d5dd7ae5855d1c140b1c770551217b707
Terran McCanna <tmccanna@georgialibraries.org>
2016-03-01 17:35:28 -0500

    LP#1551447 Selfcheck: Printing Fines Prints All Open Transactions
    This corrects the behavior of the selfcheck fines receipt print template.
    Prior to this, it printed all open billable transactions (eg, every item
    that the patron had checked out) regardless of whether or not it had bills
    associated with it. This adds a small bit of logic to the template to
    check for a balance due. It also updates the javascript to check for a
    balance on the account before launching the print function to prevent the
    system from taking unnecessary action.
    
    This includes a SQL upgrade script to update the Self-Checked Fines
    Receipt template for systems that are already in production, as well as
    including an update to the seed data.
    
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Jennifer Pringle <jennifer.pringle@bc.libraries.coop>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.selfcheck_fines_printing.sql
@@ -0,0 +1,36 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+UPDATE action_trigger.event_definition SET template = 
+$$
+[%- USE date -%]
+[%- SET user = target -%]
+<div>
+    <style> li { padding: 8px; margin 5px; }</style>
+    <div>[% date.format %]</div>
+    <br/>   
+	Fines for:<br/>
+    [% user.family_name %], [% user.first_given_name %]
+    <ol>
+    [% FOR xact IN user.open_billable_transactions_summary %]
+        [% IF xact.balance_owed > 0 %]
+            <li>
+                <div>Details: 
+                    [% IF xact.xact_type == 'circulation' %]
+                        [%- helpers.get_copy_bib_basics(xact.circulation.target_copy).title -%]
+                    [% ELSE %]
+                        [%- xact.last_billing_type -%]
+                    [% END %]
+                </div>
+                <div>Total Billed: [% xact.total_owed %]</div>
+                <div>Total Paid: [% xact.total_paid %]</div>
+                <div>Balance Owed : [% xact.balance_owed %]</div>
+            </li>
+        [% END %]
+    [% END %]
+    </ol>
+</div>
+$$ WHERE id=13;
+
+COMMIT;
