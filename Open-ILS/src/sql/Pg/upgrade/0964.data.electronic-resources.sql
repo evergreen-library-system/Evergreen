8a5406b3d5651d6e837a0c27ed41457b8fe0492e
Ben Shum <ben@evergreener.net>
2016-03-01 21:25:40 -0500

    LP#1519055: Stamping upgrade script for electronic resources filter
    Signed-off-by: Ben Shum <ben@evergreener.net>

--- Open-ILS/src/sql/Pg/upgrade/0964.data.electronic-resources.sql
+++ Open-ILS/src/sql/Pg/upgrade/0964.data.electronic-resources.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0964', :eg_version);
 
 INSERT INTO config.coded_value_map
     (id, ctype, code, opac_visible, value, search_label) VALUES
