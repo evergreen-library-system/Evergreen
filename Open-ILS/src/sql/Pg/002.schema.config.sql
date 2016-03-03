634d20e550ee94022ced7c2dde17dffa4e458997
Galen Charlton <gmc@esilibrary.com>
2016-03-02 21:13:02 -0500

    LP#1533329: stamp DB update
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0968', :eg_version); -- jstompro/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('0969', :eg_version); -- jeffdavis/stompro
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
