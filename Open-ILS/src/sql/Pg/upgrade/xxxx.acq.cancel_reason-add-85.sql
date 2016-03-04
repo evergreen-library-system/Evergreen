73c470bd2189d51bf7d8fac64c7be5308cc68bc8
Josh Stompro <stomproj@larl.org>
2016-03-04 13:51:34 -0600

    LP#1550495 - Add EDI Cancel Code 85 - used by Baker & Taylor
    Baker & Taylor send back a quantity code 85 response when they cancel
    a line item.  This code wasn't included as one of the built in EDI
    codes so those line items were not being canceled automatically in
    evergreen.
    
    The system looks for the response code + 1200 to find the cancel reason,
    so this code is entered as ID 1285 in the acq.cancel_reason table.
    
    I received confirmation from another system that uses B&T as an EDI vendor
    that they also receive code 85 responses when something gets canceled.
    
    Signed-off-by: Josh Stompro <stomproj@larl.org>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/xxxx.acq.cancel_reason-add-85.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+-- INSERT INTO config.upgrade_log (version) VALUES ('xxxx');
+
+-- LP#1550495 - Add Baker&Taylor EDI Quantity Cancel Code
+-- Insert EDI Cancel Reason 85 (1200 + 85 = 1285) if it doesn't already exist
+INSERT INTO acq.cancel_reason 
+   (org_unit, keep_debits, id, label, description)
+   SELECT 
+     1, 'f',( 85+1200),
+     oils_i18n_gettext(1285, 'Canceled: By Vendor', 'acqcr', 'label'),
+     oils_i18n_gettext(1285, 'Line item canceled by vendor', 'acqcr', 'description')
+   WHERE NOT EXISTS (
+    SELECT 1 FROM acq.cancel_reason where id=(85+1200)
+   );
+
+
+COMMIT;
