9067b32763637299597bbe9ad4dc9bc084b1c102
Jason Stephenson <jason@sigio.com>
2016-03-10 21:13:57 -0500

    LP 1507807: Add org unit settings for in-house-use copy alerts.
    Add circ.in_house_use.copy_alert and circ.in_house_use.checkin_alert
    org. unit setting types to seed data and upgrade script.
    
    No default values are provided for the new settings.  They are not
    defined (i.e. off) by default.
    
    Add a PgTap test to verify that the org. unit setting types exist in
    the database.
    
    See the release notes for what these settings do.
    
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -16476,3 +16476,25 @@ INSERT INTO action_trigger.environment (
     currval('action_trigger.event_definition_id_seq'),
     'circ_lib'
 );
+
+INSERT INTO config.org_unit_setting_type
+(name, grp, label, description, datatype)
+VALUES
+('circ.in_house_use.copy_alert',
+ 'circ',
+ oils_i18n_gettext('circ.in_house_use.copy_alert',
+     'Display copy alert for in-house-use',
+     'coust', 'label'),
+ oils_i18n_gettext('circ.in_house_use.copy_alert',
+     'Display copy alert for in-house-use',
+     'coust', 'description'),
+ 'bool'),
+('circ.in_house_use.checkin_alert',
+ 'circ',
+ oils_i18n_gettext('circ.in_house_use.checkin_alert',
+     'Display copy location checkin alert for in-house-use',
+     'coust', 'label'),
+ oils_i18n_gettext('circ.in_house_use.checkin_alert',
+     'Display copy location checkin alert for in-house-use',
+     'coust', 'description'),
+ 'bool');
