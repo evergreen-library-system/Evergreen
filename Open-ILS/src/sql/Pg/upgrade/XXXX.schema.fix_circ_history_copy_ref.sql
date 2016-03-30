95def1984b4ccded97a0657e8196d4d4e95e96d7
Bill Erickson <berickxx@gmail.com>
2016-03-30 17:49:26 -0400

    LP#1564079 Checkout history handles serials
    Remove the target_copy foreign key constraint on action.usr_circ_history
    so that it does not prevent creation of rows from circulation's that
    link to serial.unit objects.  Similar to action.circulation, the foreign
    key constraint is enforced via a evergreen.fake_fkey_tgr trigger
    instead.
    
    This modifies the 2.9.3-2.10.0-upgrade-db.sql and 0960 upgrade scripts,
    since without this change, the scripts may fail.
    
    A separate upgrade script is added to drop the constraint for those who
    have already successfully run the the 2.9.3-2.10.0-upgrade-db.sql script
    (i.e. those who don't use serials).  For simplicity, the
    evergreen.fake_fkey_tgr trigger is only applied in this separate upgrade
    script (in addition to the base schema, of course).
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.fix_circ_history_copy_ref.sql
@@ -0,0 +1,14 @@
+
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+ALTER TABLE action.usr_circ_history 
+    DROP CONSTRAINT IF EXISTS usr_circ_history_target_copy_fkey;
+
+CREATE TRIGGER action_usr_circ_history_target_copy_trig 
+    AFTER INSERT OR UPDATE ON action.usr_circ_history 
+    FOR EACH ROW EXECUTE PROCEDURE evergreen.fake_fkey_tgr('target_copy');
+
+COMMIT;
+
