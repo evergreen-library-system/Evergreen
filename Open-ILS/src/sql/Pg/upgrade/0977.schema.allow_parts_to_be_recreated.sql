c86b93b9c3a2e49934072b2bab8388c81dfe8cf7
Galen Charlton <gmc@esilibrary.com>
2016-04-28 11:31:20 -0400

    LP#1569884: stamp schema update
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/0977.schema.allow_parts_to_be_recreated.sql
+++ Open-ILS/src/sql/Pg/upgrade/0977.schema.allow_parts_to_be_recreated.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0977', :eg_version); -- Callender/Dyrcona/gmcharlt
 
 ALTER TABLE biblio.monograph_part DROP CONSTRAINT "record_label_unique";
 CREATE UNIQUE INDEX record_label_unique_idx ON biblio.monograph_part (record, label) WHERE deleted = FALSE;
