6035a5e1eaeebc75a134b6e31e4717f283ddd4d9
Galen Charlton <gmc@esilibrary.com>
2016-04-28 11:27:11 -0400

    LP#1569884: fix schema update script
    CREATE INDEX cannot run concurrently in a transaction
    block period, at least as of 9.4.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/XXXX.schema.allow_parts_to_be_recreated.sql
+++ Open-ILS/src/sql/XXXX.schema.allow_parts_to_be_recreated.sql
@@ -3,6 +3,6 @@ BEGIN;
 SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 
 ALTER TABLE biblio.monograph_part DROP CONSTRAINT "record_label_unique";
-CREATE UNIQUE INDEX CONCURRENTLY record_label_unique_idx ON biblio.monograph_part (record, label) WHERE deleted = FALSE;
+CREATE UNIQUE INDEX record_label_unique_idx ON biblio.monograph_part (record, label) WHERE deleted = FALSE;
 
 COMMIT;
