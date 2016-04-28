491c2cd6e0190c7e7af621d1becc47c713033f71
Mike Rylander <mrylander@gmail.com>
2016-04-28 11:54:28 -0400

    LP#1568195: Stamping upgrade script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0978.schema.make_ous_batch_retrieve_func_nonvariadic.sql
+++ Open-ILS/src/sql/Pg/upgrade/0978.schema.make_ous_batch_retrieve_func_nonvariadic.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0978', :eg_version);
 
 -- note: it is not necessary to explicitly drop the previous VARIADIC
 -- version of this stored procedure; create or replace function...
