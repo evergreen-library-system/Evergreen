7292174f182bba027362946c0a4a47d3ba2534da
Galen Charlton <gmc@esilibrary.com>
2016-05-11 13:17:55 -0400

    webstaff: add server administration page
    This patch adds a landing page for Server Administration and
    code to embed all of the admin pages available under Serer
    Administration in the XUL staff client.
    
    TODO:
    
    * fix the permission groups editor so that it works
      when embedded in the web staff client
    * refactor admin/local/app.js and admin/server/app.js to
      unify some of the copy and pasting
    * replace the embedded legacy OU editor with a new
      angular one
    * fixes some CSS issues that make text hard to read in
      a few places
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

---
+++ Open-ILS/web/js/ui/default/staff/admin/server/app.js
@@ -0,0 +1,80 @@
+angular.module('egServerAdmin',
+    ['ngRoute', 'ui.bootstrap', 'egCoreMod','egUiMod'])
+
+.config(['$routeProvider','$locationProvider','$compileProvider', 
+ function($routeProvider , $locationProvider , $compileProvider) {
+
+    $locationProvider.html5Mode(true);
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|blob):/); 
+    var resolver = {delay : function(egStartup) {return egStartup.go()}};
+
+    var eframe_template = 
+        '<eg-embed-frame url="server_admin_url" handlers="funcs"></eg-embed-frame>';
+
+    // old-style Confiy
+    $routeProvider.when('/admin/server/legacy/:schema/:page', {
+        template: eframe_template,
+        controller: 'EmbedOldConifyCtl',
+        resolve : resolver
+    });
+   
+    // Conify page handler (some authority admin interfaces live
+    // under global/cat/authority/)
+    $routeProvider.when('/admin/server/:module/:schema/:page', {
+        template: eframe_template,
+        controller: 'EmbedConifyCtl',
+        resolve : resolver
+    });
+
+    // Conify page handler
+    $routeProvider.when('/admin/server/:schema/:page', {
+        template: eframe_template,
+        controller: 'EmbedConifyCtl',
+        resolve : resolver
+    });
+
+    // default page 
+    $routeProvider.otherwise({
+        templateUrl : './admin/server/t_splash',
+        resolve : resolver
+    });
+}])
+
+.controller('EmbedConifyCtl', 
+       ['$scope','$routeParams','$location','egCore',
+function($scope , $routeParams , $location , egCore) {
+
+    $scope.funcs = {
+        ses : egCore.auth.token(),
+    }
+    var conify_path = '/eg/conify/global/' +
+        (angular.isDefined($routeParams.module) ? ($routeParams.module + '/') : '') +
+        $routeParams.schema + '/' + $routeParams.page;
+
+    // embed URL must include protocol/domain or it will be loaded via
+    // push-state, resulting in an infinitely nested pages.
+    $scope.server_admin_url = 
+        $location.absUrl().replace(/\/eg\/staff.*/, conify_path);
+
+    console.log('Loading server admin URL: ' + $scope.server_admin_url);
+
+}])
+
+.controller('EmbedOldConifyCtl', 
+       ['$scope','$routeParams','$location','egCore',
+function($scope , $routeParams , $location , egCore) {
+
+    $scope.funcs = {
+        ses : egCore.auth.token(),
+    }
+    var conify_path = '/conify/global/' +
+        $routeParams.schema + '/' + $routeParams.page + '.html';
+
+    // embed URL must include protocol/domain or it will be loaded via
+    // push-state, resulting in an infinitely nested pages.
+    $scope.server_admin_url = 
+        $location.absUrl().replace(/\/eg\/staff.*/, conify_path);
+
+    console.log('Loading server admin URL: ' + $scope.server_admin_url);
+
+}])
