0325d0a5fcee2c6747064bdedee5d4c3462ea3f0
blake <blake@mobiusconsortium.org>
2016-05-17 16:27:00 -0500

    LP1582354 report able to show bibs where the last copy was deleted cancels
    Created a view for ease of reporting. This source table allows you to construct
    a clever aggregate report template which will report bibliographic ID's where a
    library or a group of libraries no longer have a copy attached but had* a copy
    attached. This is especially useful when a holdings sync is required with an
    external vendor.
    
    Signed-off-by: blake <blake@mobiusconsortium.org>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Jason Etheridge <jason@EquinoxInitiative.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.LP1582354_report_able_to_show_bibs_where_the_last_copy_was_deleted_cancels.sql
@@ -0,0 +1,30 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+DROP VIEW IF EXISTS extend_reporter.copy_count_per_org;
+
+
+CREATE OR REPLACE VIEW extend_reporter.copy_count_per_org AS
+ SELECT acn.record AS bibid,
+    ac.circ_lib,
+    max(ac.edit_date) AS last_edit_time,
+    min(ac.deleted::integer) AS has_only_deleted_copies,
+    count(
+        CASE
+            WHEN ac.deleted THEN ac.id
+            ELSE NULL::bigint
+        END) AS deleted_count,
+    count(
+        CASE
+            WHEN NOT ac.deleted THEN ac.id
+            ELSE NULL::bigint
+        END) AS visible_count,
+    count(*) AS total_count
+   FROM asset.call_number acn,
+    asset.copy ac
+  WHERE ac.call_number = acn.id
+  GROUP BY acn.record, ac.circ_lib;
+
+
+COMMIT;
