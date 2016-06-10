65778dcf8987cdd09ebd9df3db507ee9cc2cad45
Kathy Lussier <klussier@masslnc.org>
2016-06-10 11:51:41 -0400

    LP#1526858: Stamping upgrade script for faster alhr query
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>
    
    Conflicts:
    	Open-ILS/src/sql/Pg/002.schema.config.sql

--- Open-ILS/src/sql/Pg/upgrade/0981.schema.indexes_for_alhr_queries.sql
+++ Open-ILS/src/sql/Pg/upgrade/0981.schema.indexes_for_alhr_queries.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version); 
+SELECT evergreen.upgrade_deps_block_check('0981', :eg_version); 
 
 CREATE INDEX hold_request_copy_capture_time_idx ON action.hold_request (current_copy,capture_time);
 CREATE INDEX hold_request_open_captured_shelf_lib_idx ON action.hold_request (current_shelf_lib) WHERE capture_time IS NOT NULL AND fulfillment_time IS NULL AND (pickup_lib <> current_shelf_lib);
