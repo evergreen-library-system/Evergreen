46afbbc822384758645244d359849d72800bac29
Jason Stephenson <jstephenson@mvlcstaff.org>
2016-06-15 15:08:53 -0400

    Add 2.9.5 to 2.9.6 upgrade script.
    Signed-off-by: Jason Stephenson <jstephenson@mvlcstaff.org>

---
+++ Open-ILS/src/sql/Pg/version-upgrade/2.9.5-2.9.6-upgrade-db.sql
@@ -0,0 +1,11 @@
+--Upgrade Script for 2.9.5 to 2.9.6
+\set eg_version '''2.9.6'''
+BEGIN;
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('2.9.6', :eg_version);
+
+SELECT evergreen.upgrade_deps_block_check('0981', :eg_version); 
+
+CREATE INDEX hold_request_copy_capture_time_idx ON action.hold_request (current_copy,capture_time);
+CREATE INDEX hold_request_open_captured_shelf_lib_idx ON action.hold_request (current_shelf_lib) WHERE capture_time IS NOT NULL AND fulfillment_time IS NULL AND (pickup_lib <> current_shelf_lib);
+
+COMMIT;
