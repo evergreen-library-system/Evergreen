5317ba59cace4fd62365fc084addac6a3315fcd0
Mike Rylander <mrylander@gmail.com>
2016-08-09 14:55:30 -0400

    Stamping upgrade for bib_source-in-901
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0988.schema.bib_source_in_901.sql
+++ Open-ILS/src/sql/Pg/upgrade/0988.schema.bib_source_in_901.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0988', :eg_version);
 
 CREATE OR REPLACE FUNCTION evergreen.maintain_901 () RETURNS TRIGGER AS $func$
 use strict;
