ad10cde090791e02cb106e36a310581bfaafface
Mike Rylander <mrylander@gmail.com>
2016-08-09 12:41:54 -0400

    Stamping upgrade for accent-ignoring patron search
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0986.schema.patron_unaccent.sql
+++ Open-ILS/src/sql/Pg/upgrade/0986.schema.patron_unaccent.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0986', :eg_version);
 
 CREATE EXTENSION IF NOT EXISTS unaccent SCHEMA public;
 
