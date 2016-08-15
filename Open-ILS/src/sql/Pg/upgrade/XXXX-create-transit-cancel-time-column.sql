3992d368ac9d08aef664c55876ad155cc396a2a5
Chris Sharp <csharp@georgialibraries.org>
2016-08-15 10:41:52 -0400

    LP#1612752 - s/abort/cancel/ in user interfaces.
    Replacing the technical term "abort" with the more
    end-user-understandable "cancel". In client-visible
    language.  The underlying code has retained "abort".
    
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/XXXX-create-transit-cancel-time-column.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX-create-transit-cancel-time-column.sql
@@ -5,4 +5,10 @@ SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 ALTER TABLE action.transit_copy
 	ADD COLUMN cancel_time TIMESTAMPTZ;
 
+-- change "abort" to "cancel" in perm descriptions
+UPDATE permission.perm_list SET description = 'Allow a user to cancel a copy transit if the user is at the transit destination or source' WHERE code = 'ABORT_TRANSIT';
+UPDATE permission.perm_list SET description = 'Allow a user to cancel a copy transit if the user is not at the transit source or dest' WHERE code = 'ABORT_REMOTE_TRANSIT'; 
+UPDATE permission.perm_list SET description = 'Allows a user to cancel a transit on a copy with status of LOST' WHERE code = 'ABORT_TRANSIT_ON_LOST';
+UPDATE permission.perm_list SET description = 'Allows a user to cancel a transit on a copy with status of MISSING' WHERE code = 'ABORT_TRANSIT_ON_MISSING';
+
 COMMIT;
