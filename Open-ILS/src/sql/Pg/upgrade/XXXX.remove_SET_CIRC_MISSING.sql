5f8238c26c7dadf848c946103f80126f3edec49e
Chris Sharp <csharp@georgialibraries.org>
2016-08-20 14:25:07 -0400

    LP#1498155 - Also remove user-assigned SET_CIRC_MISSING perm.
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.remove_SET_CIRC_MISSING.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.remove_SET_CIRC_MISSING.sql
@@ -4,6 +4,10 @@ BEGIN;
 DELETE FROM permission.grp_perm_map where perm in 
 (select id from permission.perm_list where code='SET_CIRC_MISSING');
 
+--delete all instances from permission.usr_perm_map too
+DELETE FROM permission.usr_perm_map where perm in
+(select id from permission.perm_list where code='SET_CIRC_MISSING');
+
 --delete from permission.perm_list
 DELETE FROM permission.perm_list where code='SET_CIRC_MISSING';
 
