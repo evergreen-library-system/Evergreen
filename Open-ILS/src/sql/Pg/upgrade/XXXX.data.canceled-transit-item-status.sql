fcc46ccaa72260f8dc970df156985c983f1e19f4
Chris Sharp <csharp@georgialibraries.org>
2016-08-22 08:58:24 -0400

    LP#1613374 - Make 'Canceled Transit' status holdable and OPAC-visible.
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.data.canceled-transit-item-status.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.canceled-transit-item-status.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 
-INSERT INTO config.copy_status (id, name) VALUES (18,oils_i18n_gettext(18, 'Canceled Transit', 'ccs', 'name'));
+INSERT INTO config.copy_status (id, name, holdable, opac_visible) VALUES (18,oils_i18n_gettext(18, 'Canceled Transit', 'ccs', 'name'), 't', 't');
 
 COMMIT;
