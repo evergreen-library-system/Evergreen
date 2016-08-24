8decad6183c9c3786377bd59eae8c55472ab35c4
Mike Rylander <mrylander@gmail.com>
2016-08-24 18:07:13 -0400

    Stamping upgrade script for Canceled Transit status branch
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0997.data.canceled-transit-item-status.sql
+++ Open-ILS/src/sql/Pg/upgrade/0997.data.canceled-transit-item-status.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0997', :eg_version);
 
 INSERT INTO config.copy_status (id, name, holdable, opac_visible) VALUES (18,oils_i18n_gettext(18, 'Canceled Transit', 'ccs', 'name'), 't', 't');
 
