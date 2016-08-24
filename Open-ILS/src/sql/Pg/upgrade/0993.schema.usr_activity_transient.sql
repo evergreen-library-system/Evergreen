95ea3d5223963777f8d76d11ab0dd7e4eeeeceff
Mike Rylander <mrylander@gmail.com>
2016-08-24 12:38:58 -0400

    Stamping upgrade script for transient usr_activity
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0993.schema.usr_activity_transient.sql
+++ Open-ILS/src/sql/Pg/upgrade/0993.schema.usr_activity_transient.sql
@@ -1,7 +1,7 @@
 
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0993', :eg_version);
 
 ALTER TABLE config.usr_activity_type 
     ALTER COLUMN transient SET DEFAULT TRUE;
