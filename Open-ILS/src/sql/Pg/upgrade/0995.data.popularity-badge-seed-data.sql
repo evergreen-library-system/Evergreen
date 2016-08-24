7bdfe9f5a2e8771c481bf8e15ad7da350b654533
Mike Rylander <mrylander@gmail.com>
2016-08-24 14:55:29 -0400

    Stamping upgrade script for badge seed data
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0995.data.popularity-badge-seed-data.sql
+++ Open-ILS/src/sql/Pg/upgrade/0995.data.popularity-badge-seed-data.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0995', :eg_version);
 
 INSERT INTO rating.badge (name, description, scope, weight, horizon_age, importance_age, importance_interval, importance_scale, recalc_interval, popularity_parameter, percentile)
    VALUES('Top Holds Over Last 5 Years', 'The top 97th percentile for holds requested over the past five years on all materials. More weight is given to holds requested over the last year, with importance decreasing for every year after that.', 1, 3, '5 years', '5 years', '1 year', 2, '1 day', 2, 97);
