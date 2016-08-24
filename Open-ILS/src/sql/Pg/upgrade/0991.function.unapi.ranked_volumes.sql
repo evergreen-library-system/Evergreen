51759b283ca19b15a252faa830c2e4b4a3199695
Mike Rylander <mrylander@gmail.com>
2016-08-24 11:58:56 -0400

    Stamping upgrade script for ranked-volumes update
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/0991.function.unapi.ranked_volumes.sql
+++ Open-ILS/src/sql/Pg/upgrade/0991.function.unapi.ranked_volumes.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('0991', :eg_version);
 
 CREATE OR REPLACE FUNCTION evergreen.ranked_volumes(
     bibid BIGINT[], 
