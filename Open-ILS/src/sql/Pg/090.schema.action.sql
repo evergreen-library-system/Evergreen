00692d6b4a022ed1c725b26b6daf373cf678b754
Josh Stompro <stomproj@larl.org>
2016-08-29 14:07:48 -0500

    LP#1615736 - Add an index on the usr field of action.usr_circ_history.
    Improve the performance of looking up a users circ history by adding
    an index on action.usr_circ_history( usr ).
    
    To test for performance improvements time doing a search for a particular
    users history before and after the index is created.
    
    Signed-off-by: Josh Stompro <stomproj@larl.org>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/090.schema.action.sql
+++ Open-ILS/src/sql/Pg/090.schema.action.sql
@@ -1412,6 +1412,8 @@ CREATE TABLE action.usr_circ_history (
                  ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED
 );
 
+CREATE INDEX action_usr_circ_history_usr_idx ON action.usr_circ_history ( usr );
+
 CREATE TRIGGER action_usr_circ_history_target_copy_trig 
     AFTER INSERT OR UPDATE ON action.usr_circ_history 
     FOR EACH ROW EXECUTE PROCEDURE evergreen.fake_fkey_tgr('target_copy');
