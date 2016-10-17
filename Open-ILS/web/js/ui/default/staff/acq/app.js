14f3c772bf15979087bada5085c61f98b0186e46
Jason Etheridge <jason@esilibrary.com>
2016-10-17 13:53:36 -0400

    webstaff: first Acq menu-entry and embedded UI
    * General Search
    
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

---
+++ Open-ILS/web/js/ui/default/staff/acq/app.js
@@ -0,0 +1,46 @@
+angular.module('egAcquisitions',
+    ['ngRoute', 'ui.bootstrap', 'egCoreMod','egUiMod'])
+
+.config(['$routeProvider','$locationProvider','$compileProvider', 
+ function($routeProvider , $locationProvider , $compileProvider) {
+
+    $locationProvider.html5Mode(true);
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|blob):/); 
+    var resolver = {delay : function(egStartup) {return egStartup.go()}};
+
+    var eframe_template = 
+        '<eg-embed-frame url="acq_url" handlers="funcs"></eg-embed-frame>';
+
+    $routeProvider.when('/acq/legacy/:noun/:verb', {
+        template: eframe_template,
+        controller: 'EmbedAcqCtl',
+        resolve : resolver
+    });
+
+    // default page 
+    $routeProvider.otherwise({
+        templateUrl : './t_splash',
+        resolve : resolver
+    });
+}])
+
+.controller('EmbedAcqCtl', 
+       ['$scope','$routeParams','$location','egCore',
+function($scope , $routeParams , $location , egCore) {
+
+    $scope.funcs = {
+        ses : egCore.auth.token(),
+    }
+
+    var acq_path = '/eg/acq/' + 
+        $routeParams.noun + '/' + $routeParams.verb;
+
+    // embed URL must include protocol/domain or it will be loaded via
+    // push-state, resulting in an infinitely nested pages.
+    $scope.acq_url = 
+        $location.absUrl().replace(/\/eg\/staff.*/, acq_path);
+
+    console.log('Loading Acq URL: ' + $scope.acq_url);
+
+}])
+
