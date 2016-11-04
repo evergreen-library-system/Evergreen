6745a4a3c573499af2b95870669ac199101fca95
Mike Rylander <mrylander@gmail.com>
2016-11-04 13:25:31 -0400

    Put the toaster at the bottom right everywhere
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/web/js/ui/default/staff/services/core.js
+++ Open-ILS/web/js/ui/default/staff/services/core.js
@@ -3,4 +3,11 @@
  * egCoreMod houses all of the services, etc. required by all pages
  * for basic functionality.
  */
-angular.module('egCoreMod', ['cfp.hotkeys', 'ngFileSaver', 'ngCookies', 'ngToast']);
+angular.module('egCoreMod', ['cfp.hotkeys', 'ngFileSaver', 'ngCookies', 'ngToast'])
+
+.config(['ngToastProvider', function(ngToastProvider) {
+  ngToastProvider.configure({
+    verticalPosition: 'bottom',
+    animation: 'fade'
+  });
+}]);
