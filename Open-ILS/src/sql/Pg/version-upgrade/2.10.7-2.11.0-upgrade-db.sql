fd5cd290c40b238f99645e498e143f29eb750175
Galen Charlton <gmc@esilibrary.com>
2016-11-16 12:18:29 -0500

    LP#1618183: more work backporting update to rel_2_10
    This patch tries to ensure that if somebody running 2.10.8 (or a
    later 2.10.x) attempts to upgrade to 2.11.1 or later  using the
    monolithic version upgrade scripts, the presence of the es-ES locale won't
    abort the 2.10.7-2.11.0 update.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/version-upgrade/2.10.7-2.11.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/2.10.7-2.11.0-upgrade-db.sql
@@ -3333,8 +3333,9 @@ CREATE TABLE staging.setting_stage (
 SELECT evergreen.upgrade_deps_block_check('1000', :eg_version);
 
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
-    VALUES ('es-ES', 'spa', oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'name'),
-        oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'description'));
+    SELECT 'es-ES', 'spa', oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'name'),
+        oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'description')
+    WHERE NOT EXISTS (SELECT 1 FROM config.i18n_locale WHERE code = 'es-ES');
 
 COMMIT;
 
