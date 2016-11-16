0a21c3aa5d349fa341896e0449f0e67d504dd5a8
Galen Charlton <gmc@esilibrary.com>
2016-11-16 12:16:14 -0500

    LP#1618183: restamp backport of schema update script
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/1002.data.add-spanish-to-i18n-locale-rel_2_10.sql
+++ Open-ILS/src/sql/Pg/upgrade/1002.data.add-spanish-to-i18n-locale-rel_2_10.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('1000', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1002', :eg_version);
 
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
     SELECT 'es-ES', 'spa', oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'name'),
