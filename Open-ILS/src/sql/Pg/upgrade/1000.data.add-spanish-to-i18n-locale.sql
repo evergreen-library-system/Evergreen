343f21c7aa2df3bb7f5d40ddff27e3a7a9921aaa
Galen Charlton <gmc@esilibrary.com>
2016-11-16 11:56:16 -0500

    LP#1618183: make upgrade not die if es-ES locale already exists
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/1000.data.add-spanish-to-i18n-locale.sql
+++ Open-ILS/src/sql/Pg/upgrade/1000.data.add-spanish-to-i18n-locale.sql
@@ -5,7 +5,8 @@ BEGIN;
 SELECT evergreen.upgrade_deps_block_check('1000', :eg_version);
 
 INSERT INTO config.i18n_locale (code,marc_code,name,description)
-    VALUES ('es-ES', 'spa', oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'name'),
-        oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'description'));
+    SELECT 'es-ES', 'spa', oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'name'),
+        oils_i18n_gettext('es-ES', 'Spanish', 'i18n_l', 'description')
+    WHERE NOT EXISTS (SELECT 1 FROM config.i18n_locale WHERE code = 'es-ES');
 
 COMMIT;
