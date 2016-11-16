bd8e4f06e5451abab751cb6b0ff3b33588b28c20
Galen Charlton <gmc@esilibrary.com>
2016-11-16 11:46:40 -0500

    LP#1615736: stamping schema update
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Open-ILS/src/sql/Pg/upgrade/1001.schema.action_usr_circ_history_index.sql
+++ Open-ILS/src/sql/Pg/upgrade/1001.schema.action_usr_circ_history_index.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1001', :eg_version); -- stompro/gmcharlt
 
 CREATE INDEX action_usr_circ_history_usr_idx ON action.usr_circ_history ( usr );
 
