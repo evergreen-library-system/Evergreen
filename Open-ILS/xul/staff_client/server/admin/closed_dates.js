d708e9d8247e0cbeb7f42c754a84070d4cc5ad9f
Dan Scott <dscott@laurentian.ca>
2016-12-12 15:39:08 -0500

    LP#1432753 Restore "All day" verbiage to Closed Dates editor
    Commit ede7e78925 replaced the JSAN calls to util.date.formatted_date() with
    inline date/time handling, in the process returning times with granularity to
    the minute instead of to the second. This resulted in the test for "all day"
    closings always failing.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/xul/staff_client/server/admin/closed_dates.js
+++ Open-ILS/xul/staff_client/server/admin/closed_dates.js
@@ -191,7 +191,7 @@ function cdBuildRow( date ) {
     var row;
     var flesh = false;
 
-    if( sh == '00:00:00' && eh == '23:59:59' ) {
+    if( sh == '00:00' && eh == '23:59' ) {
 
         if( sd == ed ) {
             row = cdAllDayTemplate.cloneNode(true);
