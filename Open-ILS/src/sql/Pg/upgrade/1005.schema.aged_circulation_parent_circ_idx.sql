6badeb7a7ce8ab4172808343b1e88e068d1fc524
Bill Erickson <berickxx@gmail.com>
2017-01-17 15:24:53 -0500

    LP#1657241 Stamping aged circ parent_circ index
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1005.schema.aged_circulation_parent_circ_idx.sql
+++ Open-ILS/src/sql/Pg/upgrade/1005.schema.aged_circulation_parent_circ_idx.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1005', :eg_version);
 
 CREATE INDEX action_aged_circulation_parent_circ_idx ON action.aged_circulation (parent_circ);
 
