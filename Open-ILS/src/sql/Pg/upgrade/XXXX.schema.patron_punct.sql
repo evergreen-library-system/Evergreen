adee7416a42a29072c76e7062e9e7bb0aee3376c
Dan Pearl <dpearl@cwmars.org>
2017-02-08 17:57:20 -0500

    LP#1613341 - Patron name search should be punctuation-insensitive
    O'Brien will be matched by Obrien, O'Brien, O Brien etc.
    Grubbly-Plank will be matched by Grubbly Plank, Grubbly-Plank, Grubblyplank, etc.
    This feature is controlled by the same org setting that controls ignoring
    of diacritical marks.
    
    Signed-off-by: Dan Pearl <dpearl@cwmars.org>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.patron_punct.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('0986', :eg_version);
+
+CREATE OR REPLACE FUNCTION evergreen.unaccent_and_squash ( IN arg text) RETURNS text
+    IMMUTABLE STRICT AS $$
+	BEGIN
+	RETURN evergreen.lowercase(unaccent(regexp_replace(arg, '[\s[:punct:]]','','g')));
+	END;
+$$ LANGUAGE PLPGSQL;
+
+-- Upon upgrade, we need to
+-- reindex because the definition of the unaccent_and_squash function
+-- has changed.
+REINDEX INDEX actor.actor_usr_first_given_name_unaccent_idx;
+REINDEX INDEX actor.actor_usr_second_given_name_unaccent_idx;
+REINDEX INDEX actor.actor_usr_family_name_unaccent_idx;
+
+COMMIT;
+
+
