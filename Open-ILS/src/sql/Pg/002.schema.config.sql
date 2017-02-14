422ef9560227739be2c90d3af7434965e9ab1707
Kathy Lussier <klussier@masslnc.org>
2017-02-14 00:44:06 -0500

    LP#1663663: Stamping upgrade script for overlay merge profile fix
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -91,7 +91,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1005', :eg_version); -- csharp/berick
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1012', :eg_version); -- gmcharlt/kmlussier
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
