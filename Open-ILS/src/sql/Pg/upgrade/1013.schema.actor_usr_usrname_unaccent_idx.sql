9a9f262eacb0149ded95bab599edae4828ef045d
Galen Charlton <gmc@equinoxinitiative.org>
2017-02-14 09:17:22 -0500

    LP#1643932: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1013.schema.actor_usr_usrname_unaccent_idx.sql
+++ Open-ILS/src/sql/Pg/upgrade/1013.schema.actor_usr_usrname_unaccent_idx.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version); -- csharp/miker
+SELECT evergreen.upgrade_deps_block_check('1013', :eg_version); -- csharp/miker/gmcharlt
 
 CREATE INDEX actor_usr_usrname_unaccent_idx ON actor.usr (evergreen.unaccent_and_squash(usrname));
 
