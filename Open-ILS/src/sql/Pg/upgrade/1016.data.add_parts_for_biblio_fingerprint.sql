4ff655b82870af27f2b30052442d75ffce40db7c
Mike Rylander <mrylander@gmail.com>
2017-02-16 10:01:33 -0500

    Stamping upgrade script for including parts in bib fingerprints
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1016.data.add_parts_for_biblio_fingerprint.sql
+++ Open-ILS/src/sql/Pg/upgrade/1016.data.add_parts_for_biblio_fingerprint.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1016', :eg_version);
 
 INSERT INTO config.biblio_fingerprint (name, xpath, format)
     VALUES (
