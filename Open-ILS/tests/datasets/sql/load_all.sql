1a9b8120518584740dd6236ed04d6f0758b5fd36
Ben Shum <ben@evergreener.net>
2017-02-20 22:34:54 -0500

    LP#1541559: Change order of test bib loading
    Move the new ebook bibs to the end of the script and away from the
    rest of bib loading and item creation to avoid breaking tests.
    
    Signed-off-by: Ben Shum <ben@evergreener.net>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/tests/datasets/sql/load_all.sql
+++ Open-ILS/tests/datasets/sql/load_all.sql
@@ -30,9 +30,6 @@ BEGIN;
 -- load RDA bibs
 \i bibs_rda.sql
 
--- load EbookAPI bibs
-\i bibs_ebook_api.sql
-
 -- insert all loaded bibs into the biblio.record_entry in insert order
 INSERT INTO biblio.record_entry (marc, last_xact_id) 
     SELECT marc, tag FROM marcxml_import ORDER BY id;
@@ -67,6 +64,16 @@ INSERT INTO biblio.record_entry (marc, last_xact_id)
 
 -- funds, orders, etc.
 \i acq.sql
+
+-- delete previously imported bibs
+DELETE FROM marcxml_import;
+
+-- load EbookAPI bibs
+\i bibs_ebook_api.sql
+
+-- insert all loaded bibs into the biblio.record_entry in insert order
+INSERT INTO biblio.record_entry (marc, last_xact_id)
+    SELECT marc, tag FROM marcxml_import ORDER BY id;
 
 -- clean up the env
 \i env_destroy.sql
