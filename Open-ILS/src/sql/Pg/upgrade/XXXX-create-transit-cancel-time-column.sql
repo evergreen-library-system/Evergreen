0584c40feb01c666e82e0ff3ac178a3cbeea5991
Chris Sharp <csharp@georgialibraries.org>
2017-02-21 07:23:42 -0500

    LP#1612752 - Do not clobber local perm description changes.
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/XXXX-create-transit-cancel-time-column.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX-create-transit-cancel-time-column.sql
@@ -5,10 +5,21 @@ SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 ALTER TABLE action.transit_copy
 	ADD COLUMN cancel_time TIMESTAMPTZ;
 
--- change "abort" to "cancel" in perm descriptions
-UPDATE permission.perm_list SET description = 'Allow a user to cancel a copy transit if the user is at the transit destination or source' WHERE code = 'ABORT_TRANSIT';
-UPDATE permission.perm_list SET description = 'Allow a user to cancel a copy transit if the user is not at the transit source or dest' WHERE code = 'ABORT_REMOTE_TRANSIT'; 
-UPDATE permission.perm_list SET description = 'Allows a user to cancel a transit on a copy with status of LOST' WHERE code = 'ABORT_TRANSIT_ON_LOST';
-UPDATE permission.perm_list SET description = 'Allows a user to cancel a transit on a copy with status of MISSING' WHERE code = 'ABORT_TRANSIT_ON_MISSING';
-
+-- change "abort" to "cancel" in stock perm descriptions
+UPDATE permission.perm_list 
+	SET description = 'Allow a user to cancel a copy transit if the user is at the transit destination or source' 
+	WHERE code = 'ABORT_TRANSIT'
+	AND description = 'Allow a user to abort a copy transit if the user is at the transit destination or source';
+UPDATE permission.perm_list 
+	SET description = 'Allow a user to cancel a copy transit if the user is not at the transit source or dest' 
+	WHERE code = 'ABORT_REMOTE_TRANSIT'
+	AND description = 'Allow a user to abort a copy transit if the user is not at the transit source or dest';
+UPDATE permission.perm_list 
+	SET description = 'Allows a user to cancel a transit on a copy with status of LOST' 
+	WHERE code = 'ABORT_TRANSIT_ON_LOST'
+	AND description = 'Allows a user to abort a transit on a copy with status of LOST';
+UPDATE permission.perm_list 
+	SET description = 'Allows a user to cancel a transit on a copy with status of MISSING' 
+	WHERE code = 'ABORT_TRANSIT_ON_MISSING'
+	AND description = 'Allows a user to abort a transit on a copy with status of MISSING';
 COMMIT;
