c7b42d325a55c588f21da73b841780b72a3deb5a
Kathy Lussier <klussier@masslnc.org>
2017-03-14 15:53:20 -0400

    LP# 1668414: Stamping upgrade script for oneclickdigital base URI
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/upgrade/1031.data.org-setting.oneclickdigital-base-uri.sql
+++ Open-ILS/src/sql/Pg/upgrade/1031.data.org-setting.oneclickdigital-base-uri.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1031', :eg_version);
 
 INSERT INTO config.org_unit_setting_type
     (name, label, description, grp, datatype) 
