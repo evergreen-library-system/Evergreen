6d94067f076785499a1ffaf60dce193aa57f0a41
Galen Charlton <gmc@equinoxinitiative.org>
2017-05-25 09:24:24 -0400

    LP#1498155: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1041.data.remove_SET_CIRC_MISSING.sql
+++ Open-ILS/src/sql/Pg/upgrade/1041.data.remove_SET_CIRC_MISSING.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1041', :eg_version); -- stompro/csharp/gmcharlt
+
 --delete all instances from permission.grp_perm_map first
 DELETE FROM permission.grp_perm_map where perm in 
 (select id from permission.perm_list where code='SET_CIRC_MISSING');
