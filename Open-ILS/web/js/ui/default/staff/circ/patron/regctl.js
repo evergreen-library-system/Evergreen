evergreen-library-system/Evergreen

commit 997e277207b4051b028d93bcab26c70645663081

LP#1696495-WebStaff fix user_settings data caching between patron views
Appends a few promises to make sure that the user_settings are refreshed
each time a user is loaded from within patron app.
Note that testing this requires that LP1642035's fix be in place...
Otherwise you can't properly add user_settings inputs in Webstaff.

Signed-off by: Cesar Velez <cesar.velez@equinoxinitiative.org>

Signed-off-by: Kathy Lussier <klussier@masslnc.org>

Open-ILS/web/js/ui/default/staff/circ/patron/regctl.js
@@ -584,7 +584,7 @@ angular.module('egCoreMod')
             return service.init_new_patron();
 
         service.patron = current;
-        return service.init_existing_patron(current)
+        return $q.when(service.init_existing_patron(current));
     }
 
     service.ingest_address = function(patron, addr) {
@@ -655,7 +655,7 @@ angular.module('egCoreMod')
             service.stat_cat_entry_maps[map.stat_cat.id] = map.stat_cat_entry;
         });
 
-        return patron;
+        service.patron = patron;
     }
 
     service.init_new_patron = function() {
@@ -1176,14 +1176,15 @@ function($scope , $routeParams , $q , $uibModal , $window , egCore ,
         $scope.initTab ? // initTab comes from patron app
             $scope.initTab('edit', $routeParams.id) : $q.when(),
 
-        patronRegSvc.init()
+        patronRegSvc.init(),
 
-    ]).then(function() {
+    ]).then(function(){ return patronRegSvc.init_patron(patronSvc ? patronSvc.current : null) })
+      .then(patronRegSvc.get_user_settings)
+      .then(function() {
         // called after initTab and patronRegSvc.init have completed
-
-        var prs = patronRegSvc; // brevity
         // in standalone mode, we have no patronSvc
-        $scope.patron = prs.init_patron(patronSvc ? patronSvc.current : null);
+        var prs = patronRegSvc;
+        $scope.patron = prs.patron;
         $scope.field_doc = prs.field_doc;
         $scope.edit_profiles = prs.edit_profiles;
         $scope.ident_types = prs.ident_types;
@@ -1200,12 +1201,7 @@ function($scope , $routeParams , $q , $uibModal , $window , egCore ,
         $scope.stage_user_requestor = prs.stage_user_requestor;
 
         $scope.user_settings = prs.user_settings;
-        // clone the user settings back into the patronRegSvc so
-        // we have a copy of the original state of the settings.
         prs.user_settings = {};
-        angular.forEach($scope.user_settings, function(val, key) {
-            prs.user_settings[key] = val;
-        });
 
         extract_hold_notify();
         $scope.handle_home_org_changed();
@@ -1581,7 +1577,7 @@ function($scope , $routeParams , $q , $uibModal , $window , egCore ,
     }
 
     function extract_hold_notify() {
-        notify = $scope.user_settings['opac.hold_notify'];
+        var notify = $scope.user_settings['opac.hold_notify'];
         if (!notify) return;
         $scope.hold_notify_phone = Boolean(notify.match(/phone/));
         $scope.hold_notify_email = Boolean(notify.match(/email/));

commit 72476c874d540738aff44b1a714d4c068b4c756c

LP#1702756 - WebStaff fix for bug that was introduced by 997e2772
While addressing LP#1696495 fixed the bug of user settings caching
improperly between patron edits, it caused an issue when the regctl
was loaded for registering a new patron. This corrects that.

Signed-off by: Cesar Velez <cesar.velez@equinoxinitiative.org>
Signed-off-by: Josh Stompro <stomproj@larl.org>
Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

Open-ILS/web/js/ui/default/staff/circ/patron/regctl.js
@@ -581,7 +581,7 @@ angular.module('egCoreMod')
     service.init_patron = function(current) {
 
         if (!current)
-            return service.init_new_patron();
+            return $q.when(service.init_new_patron());
 
         service.patron = current;
         return $q.when(service.init_existing_patron(current));
@@ -656,6 +656,7 @@ angular.module('egCoreMod')
         });
 
         service.patron = patron;
+        return patron;
     }
 
     service.init_new_patron = function() {
@@ -1180,13 +1181,12 @@ function($scope , $routeParams , $q , $uibModal , $window , egCore ,
 
         patronRegSvc.init(),
 
-    ]).then(function(){ return patronRegSvc.init_patron(patronSvc ? patronSvc.current : null) })
-      .then(patronRegSvc.get_user_settings)
-      .then(function() {
+    ]).then(function(){ return patronRegSvc.init_patron(patronSvc ? patronSvc.current : patronRegSvc.patron ) })
+      .then(function(patron) {
         // called after initTab and patronRegSvc.init have completed
         // in standalone mode, we have no patronSvc
         var prs = patronRegSvc;
-        $scope.patron = prs.patron;
+        $scope.patron = patron;
         $scope.field_doc = prs.field_doc;
         $scope.edit_profiles = prs.edit_profiles;
         $scope.ident_types = prs.ident_types;
@@ -1206,13 +1206,13 @@ function($scope , $routeParams , $q , $uibModal , $window , egCore ,
         prs.user_settings = {};
 
         extract_hold_notify();
+        if ($scope.patron.isnew)
+            set_new_patron_defaults(prs);
+        
         $scope.handle_home_org_changed();
 
         if ($scope.org_settings['ui.patron.edit.default_suggested'])
             $scope.edit_passthru.vis_level = 1;
-
-        if ($scope.patron.isnew) 
-            set_new_patron_defaults(prs);
 
         // Stat cats are fetched from open-ils.storage, where 't'==1
         $scope.hasRequiredStatCat = prs.stat_cats.filter(