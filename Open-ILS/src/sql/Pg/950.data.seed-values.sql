665c8af4df73817d4f4139fb6f71fcf86533177b
Galen Charlton <gmc@equinoxinitiative.org>
2017-07-27 22:31:47 +0000

    LP#1638299: improve display of related headings in browse
    This patch adds a new global flag to control whether see
    alsos are displayed at all; it also allows better labels based
    on the heading purpose.
    
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -16881,6 +16881,17 @@ INSERT INTO config.global_flag (name, label, value, enabled) VALUES (
     TRUE
 );
 
+INSERT INTO config.global_flag (name, label, enabled) VALUES (
+    'opac.show_related_headings_in_browse',
+    oils_i18n_gettext(
+        'opac.show_related_headings_in_browse',
+        'Display related headings (see-also) in browse',
+        'cgf',
+        'label'
+    ),
+    TRUE
+);
+
 INSERT INTO config.settings_group (name, label)
     VALUES ('ebook_api', oils_i18n_gettext('ebook_api', 'Ebook API Integration', 'csg', 'label'));
 
---
+++ Open-ILS/src/sql/Pg/upgrade/WWWW.cgf.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+INSERT INTO config.global_flag (name, label, enabled) VALUES (
+    'opac.show_related_headings_in_browse',
+    oils_i18n_gettext(
+        'opac.show_related_headings_in_browse',
+        'Display related headings (see-also) in browse',
+        'cgf',
+        'label'
+    ),
+    TRUE
+);
+
+COMMIT;
