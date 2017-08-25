c095a3adb3649c575bcfcb846040c16b79757661
Bill Erickson <berickxx@gmail.com>
2017-08-25 12:40:20 -0400

    LP#1251394 Author display field xpath
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/YYYY.data.metabib-display-field.sql
+++ Open-ILS/src/sql/Pg/upgrade/YYYY.data.metabib-display-field.sql
@@ -14,6 +14,11 @@ INSERT INTO config.metabib_field ( id, field_class, name, label,
      'mods32', $$//mods32:mods/mods32:name[mods32:role/mods32:roleTerm[text()='creator']]$$, 
      TRUE, $$//*[local-name()='namePart']$$ ); -- /* to fool vim */;
 
+-- 'author' field
+UPDATE config.metabib_field SET display_xpath = 
+    $$//*[local-name()='namePart']$$ -- /* to fool vim */
+    WHERE id = 8;
+
 INSERT INTO config.display_field_map (name, field, multi) VALUES
     ('title', 6, FALSE),
     ('author', 8, FALSE),
