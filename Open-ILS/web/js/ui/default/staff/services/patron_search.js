3195aace7a6e7e094619fcc11684f508e2f65661
Kathy Lussier <klussier@masslnc.org>
2017-09-13 13:07:29 -0400

    LP#1706124: Make include inactive patrons checkbox sticky
    The state of the 'include inactive' checkbox on the patron search page will now
    be remembered when users return to the page. Many thanks to Cesar Velez,
    whose code for bug 1697754 inspired this patch!
    
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/web/js/ui/default/staff/services/patron_search.js
+++ Open-ILS/web/js/ui/default/staff/services/patron_search.js
@@ -745,7 +745,17 @@ function($scope,  $q,  $routeParams,  $timeout,  $window,  $location,  egCore,
     egCore.hatch.getItem('eg.circ.patron.search.show_extras')
     .then(function(val) {$scope.showExtras = val});
 
-    // map form arguments into search params
+// check searchArgs.inactive setting
+    egCore.hatch.getItem('searchArgs.inactive')
+                .then(function(searchInactive){
+                    if (searchInactive) $scope.searchArgs.inactive = searchInactive; 
+                });    
+
+     $scope.onSearchInactiveChanged = function(){
+        egCore.hatch.setItem('searchArgs.inactive', $scope.searchArgs.inactive);
+    }
+
+// map form arguments into search params
     function compileSearch(args) {
         var search = {};
         angular.forEach(args, function(val, key) {
