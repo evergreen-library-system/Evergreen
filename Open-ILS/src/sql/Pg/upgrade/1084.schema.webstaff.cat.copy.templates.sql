97bd8265fec556182c15eecc7e5da4b7e013ef15
Mike Rylander <mrylander@gmail.com>
2017-11-27 13:53:40 -0500

    Stamping upgrade script for xul-to-web copy template translation
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1084.schema.webstaff.cat.copy.templates.sql
+++ Open-ILS/src/sql/Pg/upgrade/1084.schema.webstaff.cat.copy.templates.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1084', :eg_version);
 
 INSERT INTO config.usr_setting_type (name, label, description, datatype)
   VALUES ('webstaff.cat.copy.templates', 'Web Client Copy Editor Templates', 'Web Client Copy Editor Templates', 'object');
