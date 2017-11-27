810322a2acf01cb73e930e6ca44d77fd3363ae28
Galen Charlton <gmc@equinoxinitiative.org>
2017-11-27 11:08:23 -0500

    LP#1691269: (follow-up) include new cust in seed data
    Also s/Copier/Copy/.
    
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.schema.webstaff.cat.copy.templates.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.webstaff.cat.copy.templates.sql
@@ -3,6 +3,6 @@ BEGIN;
 SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 
 INSERT INTO config.usr_setting_type (name, label, description, datatype)
-  VALUES ('webstaff.cat.copy.templates', 'Web Client Copier Editor Templates', 'Web Client Copy Editor Templates', 'object');
+  VALUES ('webstaff.cat.copy.templates', 'Web Client Copy Editor Templates', 'Web Client Copy Editor Templates', 'object');
 
 COMMIT;
