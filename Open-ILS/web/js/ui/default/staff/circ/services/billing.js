3b596a0ac12e5edbe9fa1d508d8ba4116a230f63
Jason Boyer <jboyer@library.in.gov>
2018-01-04 13:38:00 -0500

    LP1712646: Require a Billing Type to Bill User
    Previously billing a user without choosing a billing type
    would either fail silently or default to misc. This patch
    requires staff to choose a billing type before the new
    bill window can be dismissed.
    
    Signed-off-by: Jason Boyer <jboyer@library.in.gov>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/web/js/ui/default/staff/circ/services/billing.js
+++ Open-ILS/web/js/ui/default/staff/circ/services/billing.js
@@ -64,8 +64,9 @@ function($uibModal , $q , egCore) {
     // fetch the org-focused billing types
     // Cache on egEnv
     service.fetchBillingTypes = function() {
-        if (egCore.env.cbt) 
+        if (egCore.env.cbt) {
             return $q.when(egCore.env.cbt.list);
+        }
 
         return egCore.pcrud.search('cbt', 
             {   // first 100 are reserved for system-generated bills
@@ -170,7 +171,4 @@ function($uibModal , $q , egCore) {
 
     return service;
 }]);
-
-
-
 
