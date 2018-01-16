02cfe27d07cf93b612825e45a6975bcb2d5e80b8
Mike Rylander <mrylander@gmail.com>
2018-01-16 14:39:08 -0500

    LP#1743639: opac_visible on acplg is not what it seems
    The asset.copy_location_group table has a column called opac_visible.
    However, unlike other tables with a column of that name, it controls the
    visibility of the location group itself, not the visibility of the copies
    within the group.  This is regretable because the 3.0+ search code thinks
    that it should hide copies.
    
    This commit adjusts the default location group filter applied to patron
    searches so that they act as expected.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.function.copy_location_group_visible.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE OR REPLACE FUNCTION asset.location_group_default () RETURNS TEXT AS $f$
+    SELECT '!()'::TEXT; -- For now, as there's no way to cause a location group to hide all copies.
+/*
+    SELECT  '!(' || ARRAY_TO_STRING(ARRAY_AGG(search.calculate_visibility_attribute(id, 'location_group')),'|') || ')'
+      FROM  asset.copy_location_group
+      WHERE NOT opac_visible;
+*/
+$f$ LANGUAGE SQL STABLE;
+
+COMMIT;
+
