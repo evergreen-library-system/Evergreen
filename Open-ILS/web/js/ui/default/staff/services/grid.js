d37314d9b7ca7a638a6b899010f609c7d6618f0d
Mike Rylander <mrylander@gmail.com>
2018-01-29 16:37:15 -0500

    LP#1707063: Naive ng-class test for last-column-modified
    Instead of comparing a (potentially undefined) property of a column, we'll
    just compare the column object directly when checking to see if a column
    header should be highlighted for "last touched in editing mode."
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Cesar Velez <cesar.velez@equinoxinitiative.org>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/web/js/ui/default/staff/services/grid.js
+++ Open-ILS/web/js/ui/default/staff/services/grid.js
@@ -765,8 +765,14 @@ angular.module('egGridMod',
                     column.flex = 1;
             }
             $scope.modifyColumnFlex = function(col, val) {
-                $scope.lastModColumn = col.name;
+                $scope.lastModColumn = col;
                 grid.modifyColumnFlex(col, val);
+            }
+
+            $scope.isLastModifiedColumn = function(col) {
+                if ($scope.lastModColumn)
+                    return $scope.lastModColumn === col;
+                return false;
             }
 
             grid.modifyColumnPos = function(col, diff) {
@@ -801,7 +807,7 @@ angular.module('egGridMod',
             }
 
             $scope.modifyColumnPos = function(col, diff) {
-                $scope.lastModColumn = col.name;
+                $scope.lastModColumn = col;
                 return grid.modifyColumnPos(col, diff);
             }
 
