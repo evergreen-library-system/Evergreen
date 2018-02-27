3c1b6d0a129a45e6073e78cdeaaf2c59515b83cd
Dan Wells <dbw2@calvin.edu>
2018-02-27 16:08:46 -0500

    LP#1737016 Stamping upgrade script for easing constraints on afs.code
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/1091.schema.lp1737016_afs_code.sql
+++ Open-ILS/src/sql/Pg/upgrade/1091.schema.lp1737016_afs_code.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1091', :eg_version);
 
 ALTER TABLE acq.funding_source DROP CONSTRAINT funding_source_code_key;
 ALTER TABLE acq.funding_source ALTER COLUMN code SET NOT NULL;
