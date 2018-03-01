13be0c3e4bd80a5f899b8d7723b9e6b64efe738e
Kathy Lussier <klussier@masslnc.org>
2018-02-28 20:16:44 -0500

    LP#1732275: Stamping upgrade script for items out notice count setting
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1103', :eg_version);
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1105', :eg_version); --cesardv/kmlussier
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1105.schema.webstaff.circ.itemsout_notice_count_excludes_courtesies.sql
+++ Open-ILS/src/sql/Pg/upgrade/1105.schema.webstaff.circ.itemsout_notice_count_excludes_courtesies.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1105', :eg_version);
 
 INSERT into config.org_unit_setting_type (name, label, grp, description, datatype) 
 values ('webstaff.circ.itemsout_notice_count_excludes_courtesies','Exclude Courtesy Notices from Patrons Itemsout Notices Count',
