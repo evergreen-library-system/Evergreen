425bc5e41e99e2eaf1c87f8fc2e94103574316e4
Jeff Godin <jgodin@tadl.org>
2018-03-01 14:06:06 -0500

    LP#1748924 Stamping upgrade script
    Stamping upgrade script for expanding billing timestamps
    
    Signed-off-by: Jeff Godin <jgodin@tadl.org>

--- Open-ILS/src/sql/Pg/upgrade/1106.data.expand_billing_timestamps.sql
+++ Open-ILS/src/sql/Pg/upgrade/1106.data.expand_billing_timestamps.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1106', :eg_version);
 
 ALTER TABLE money.billing
 	ADD COLUMN create_date TIMESTAMP WITH TIME ZONE,
