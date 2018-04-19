90d3ef2e7c8493f982e1630941b2a8d7aae28298
Chris Sharp <csharp@georgialibraries.org>
2018-04-18 22:45:31 -0400

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1107', :eg_version); --rhamby/kmlussier
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.0.7', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
