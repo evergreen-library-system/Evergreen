915d18aafc9176b784b30d5fd077ae6d612e167c
Remington Steed <rjs7@calvin.edu>
2018-05-15 09:46:41 -0400

    LP#1325704: Update Items-Out YAOUS descrip for web client
    Three settings refer to the "top" and "bottom" lists on the XUL client
    Items Out screen. With the web client, these have become three named
    tabs. This commit updates the setting descriptions in the seed data so
    they reflect the new design. I also attempted to make the meaning of the
    values more clear by labeling the two subsettings as "A" and "B".
    
    Signed-off-by: Remington Steed <rjs7@calvin.edu>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -16954,12 +16954,13 @@ VALUES (
     ),
     oils_i18n_gettext(
         'ui.circ.items_out.longoverdue',
-'Value is a numeric code, describing which list the circulation '||
-'should appear while checked out and whether the circulation should '||
-'continue to appear in the bottom list, when checked in with '||
+'Value is a numeric code, describing: A. In which tab ("Items Checked Out", '||
+'or "Other/Special Circulations") the circulation '||
+'should appear while checked out, and B. Whether the circulation should '||
+'continue to appear in the "Other" tab when checked in with '||
 'oustanding fines.  '||
-'1 = top list, bottom list.  2 = bottom list, bottom list.  ' ||
-'5 = top list, do not display.  6 = bottom list, do not display.',
+'1 = (A) "Items", (B) "Other".  2 = (A) "Other", (B) "Other".  ' ||
+'5 = (A) "Items", (B) do not display.  6 = (A) "Other", (B) do not display.',
         'coust',
         'description'
     )
@@ -16973,12 +16974,13 @@ VALUES (
     ),
     oils_i18n_gettext(
         'ui.circ.items_out.lost',
-'Value is a numeric code, describing which list the circulation '||
-'should appear while checked out and whether the circulation should '||
-'continue to appear in the bottom list, when checked in with '||
+'Value is a numeric code, describing: A. In which tab ("Items Checked Out", '||
+'or "Other/Special Circulations") the circulation '||
+'should appear while checked out, and B. Whether the circulation should '||
+'continue to appear in the "Other" tab when checked in with '||
 'oustanding fines.  '||
-'1 = top list, bottom list.  2 = bottom list, bottom list.  ' ||
-'5 = top list, do not display.  6 = bottom list, do not display.',
+'1 = (A) "Items", (B) "Other".  2 = (A) "Other", (B) "Other".  ' ||
+'5 = (A) "Items", (B) do not display.  6 = (A) "Other", (B) do not display.',
         'coust',
         'description'
     )
@@ -16992,12 +16994,13 @@ VALUES (
     ),
     oils_i18n_gettext(
         'ui.circ.items_out.claimsreturned',
-'Value is a numeric code, describing which list the circulation '||
-'should appear while checked out and whether the circulation should '||
-'continue to appear in the bottom list, when checked in with '||
+'Value is a numeric code, describing: A. In which tab ("Items Checked Out", '||
+'or "Other/Special Circulations") the circulation '||
+'should appear while checked out, and B. Whether the circulation should '||
+'continue to appear in the "Other" tab when checked in with '||
 'oustanding fines.  '||
-'1 = top list, bottom list.  2 = bottom list, bottom list.  ' ||
-'5 = top list, do not display.  6 = bottom list, do not display.',
+'1 = (A) "Items", (B) "Other".  2 = (A) "Other", (B) "Other".  ' ||
+'5 = (A) "Items", (B) do not display.  6 = (A) "Other", (B) do not display.',
         'coust',
         'description'
     )
