3ea9d98244ff052b9a36ceb1db5e2c0d790afa08
Jason Stephenson <jason@sigio.com>
2018-06-27 16:08:17 -0400

    LP 1758160: Bump version in upgrade_log.
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1110', :eg_version); --gmcharlt/cesardv/kmlussier
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1112', :eg_version); -- Bmagic/JBoyer/Dyrcona
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
