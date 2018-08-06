d9d9d766b1a772f749b1bcf4e9ebbadd929bc4d9
Bill Erickson <berickxx@gmail.com>
2018-08-06 15:18:35 -0400

    LP#1785681 Patron address copy to clipboard
    Support copying formatted addresses to the clipboard in the browser
    staff client, similar to the XUL client.  Action is performed via a new
    link in the addresses section (next to "print") in the patron summary
    sidebar.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Garry Collum <gcollum@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/web/js/ui/default/staff/circ/patron/app.js
+++ Open-ILS/web/js/ui/default/staff/circ/patron/app.js
@@ -339,6 +339,24 @@ function($scope,  $q , $location , $filter , egCore , egNet , egUser , egAlertDi
         });
     }
 
+    $scope.copy_address = function(addr) {
+        // Alas, navigator.clipboard is not yet supported in FF and others.
+        var lNode = document.querySelector('#patron-address-copy-' + addr.id());
+
+        // Un-hide the textarea just long enough to copy its data.
+        // Using node.style instead of ng-show/ng-hide in hopes it 
+        // will be quicker, so the user never sees the textarea.
+        lNode.style.visibility = 'visible';
+        lNode.focus();
+        lNode.select();
+
+        if (!document.execCommand('copy')) {
+            console.error('Copy command failed');
+        }
+
+        lNode.style.visibility = 'hidden';
+    }
+
     $scope.toggle_expand_summary = function() {
         if ($scope.collapsePatronSummary) {
             $scope.collapsePatronSummary = false;
