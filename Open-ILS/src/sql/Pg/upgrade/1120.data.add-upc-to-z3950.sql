f1c3c905d3c5efded57616f67d630c801e713073
Bill Erickson <berickxx@gmail.com>
2018-08-24 16:05:09 -0400

    LP#1768902 Stamping DB upgrade for Z39 UPC
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1120.data.add-upc-to-z3950.sql
+++ Open-ILS/src/sql/Pg/upgrade/1120.data.add-upc-to-z3950.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1120', :eg_version);
 
 --Only insert if the attributes are not already present
 
