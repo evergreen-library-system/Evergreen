0c4238dfce4cf2375ff02099b6dab55b692ec8a4
Mike Rylander <mrylander@gmail.com>
2018-08-30 16:37:21 -0400

    LP#1712854: Include shelf-expired holds in the "clearable" set
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Storage/Publisher/action.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Storage/Publisher/action.pm
@@ -2173,7 +2173,7 @@ SELECT  h.id, h.request_time, h.capture_time, h.fulfillment_time, h.checkin_time
              ELSE 5
         END AS hold_status,
 
-        (h.cancel_time IS NOT NULL OR (h.current_shelf_lib IS NOT NULL AND h.current_shelf_lib <> h.pickup_lib)) AS clear_me,
+        (h.shelf_expire_time < NOW() OR h.cancel_time IS NOT NULL OR (h.current_shelf_lib IS NOT NULL AND h.current_shelf_lib <> h.pickup_lib)) AS clear_me,
 
         (h.usr <> h.requestor) AS is_staff_hold,
 
