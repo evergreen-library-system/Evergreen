03317dc6af32e23339f693592ead3e5993691277
Kathy Lussier <klussier@masslnc.org>
2018-08-30 12:37:45 -0400

    LP#1744756: Stamping upgrade script for custom perm group display
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/upgrade/1121.schema.perm-group-display.sql
+++ Open-ILS/src/sql/Pg/upgrade/1121.schema.perm-group-display.sql
@@ -1,5 +1,5 @@
 BEGIN;
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1121', :eg_version);
 
 CREATE TABLE permission.grp_tree_display_entry (
     id      SERIAL PRIMARY KEY,
