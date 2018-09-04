1e6cbaca69712d708bf4e720e14ac78e1dc24ab6
Dan Wells <dbw2@calvin.edu>
2018-09-04 16:36:09 -0400

    LP#1777675 Stamping upgrade script for latest inventory date support
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/1125.schema.lp1777675_latest_inventory_date_support.sql
+++ Open-ILS/src/sql/Pg/upgrade/1125.schema.lp1777675_latest_inventory_date_support.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1125', :eg_version);
 
 CREATE TABLE asset.latest_inventory (
     id                          SERIAL                      PRIMARY KEY,
