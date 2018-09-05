11d9725b548c609b4610de55195b40f41e1c42ee
Kathy Lussier <klussier@masslnc.org>
2018-09-04 23:55:08 -0400

    LP#1635354: Stamping upgrade script for acqusitions invoice timestamp
    Signed-off-by: Kathy Lussier <klussier@masslnc.org>

--- Open-ILS/src/sql/Pg/upgrade/1128.schema.invoice-close-date.sql
+++ Open-ILS/src/sql/Pg/upgrade/1128.schema.invoice-close-date.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1128', :eg_version);
 
 DROP VIEW auditor.acq_invoice_lifecycle;
 
