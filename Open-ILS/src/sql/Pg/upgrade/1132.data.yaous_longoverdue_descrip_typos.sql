f594e2854a172ad61a5a27eaf3804d42a4a07020
Chris Sharp <csharp@georgialibraries.org>
2018-09-13 11:47:55 -0400

    LP#1774724 - stamp upgrade script

--- Open-ILS/src/sql/Pg/upgrade/1132.data.yaous_longoverdue_descrip_typos.sql
+++ Open-ILS/src/sql/Pg/upgrade/1132.data.yaous_longoverdue_descrip_typos.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- check whether patch can be applied
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1132', :eg_version); -- remingtron/csharp
 
 -- fix two typo/pasto's in setting descriptions
 UPDATE config.org_unit_setting_type
