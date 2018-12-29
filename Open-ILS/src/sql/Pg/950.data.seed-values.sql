6d19d729d6348926ecc31a59c934202bd85fef40
Jane Sandberg <sandbej@linnbenton.edu>
2018-12-29 15:17:02 -0800

    LP1809288: Add workstation grid settings for booking admin
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -19455,5 +19455,43 @@ VALUES (
 );
 
 
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.booking.booking.resource', 'gui', 'object',
+    oils_i18n_gettext (
+        'eg.grid.admin.booking.booking.resource',
+        'Grid Config: admin.booking.booking.resource',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.booking.booking.resource_attr', 'gui', 'object',
+    oils_i18n_gettext (
+    'eg.grid.admin.booking.booking.resource_attr',
+        'Grid Config: admin.booking.booking.resource_attr',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.booking.booking.resource_attr_map', 'gui', 'object',
+    oils_i18n_gettext (
+    'eg.grid.admin.booking.booking.resource_attr_map',
+        'Grid Config: admin.booking.booking.resource_attr_map',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.booking.booking.resource_attr_value', 'gui', 'object',
+    oils_i18n_gettext (
+    'eg.grid.admin.booking.booking.resource_attr_value',
+        'Grid Config: admin.booking.booking.resource_attr_value',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.booking.booking.resource_type', 'gui', 'object',
+    oils_i18n_gettext (
+    'eg.grid.admin.booking.booking.resource_type',
+        'Grid Config: admin.booking.booking.resource_type',
+        'cwst', 'label'
+    )
+);
+
 
 
