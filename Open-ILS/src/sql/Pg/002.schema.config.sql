8acac4652ca08321a117ad2292493d7755b48460
Galen Charlton <gmc@equinoxinitiative.org>
2019-02-18 13:37:00 -0500

    LP#1806709: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1148', :eg_version); -- csharp/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1149', :eg_version); -- Dyrcona/mmorgan/gmcharlt
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
