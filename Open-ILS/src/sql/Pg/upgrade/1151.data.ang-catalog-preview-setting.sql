0b4f44e817c2a91a93d1a13cba2bf8318fb12c8c
Dan Wells <dbw2@calvin.edu>
2019-02-20 17:05:25 -0500

    LP#1806087 Stamp upgrade script for staff catalog preview
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/1151.data.ang-catalog-preview-setting.sql
+++ Open-ILS/src/sql/Pg/upgrade/1151.data.ang-catalog-preview-setting.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1152', :eg_version);
 
 INSERT into config.org_unit_setting_type 
     (name, datatype, grp, label, description)
