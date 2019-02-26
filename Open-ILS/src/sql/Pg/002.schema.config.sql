5134ead472beaed1c2ff2f5d8a0f8d9b4d1b9a70
blake <blake@mobiusconsortium.org>
2019-02-26 09:08:41 -0600

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1142', :eg_version); -- csharp/bshum
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.1.10', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
