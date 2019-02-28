0fa743800e80f0554e90043905e55ef302094f95
Bill Erickson <berickxx@gmail.com>
2019-02-28 17:20:31 -0500

    LP1818153 Log user activity for websocket login/verify
    Adds support for logging additional user activity types:
    
     * websocket v1 login (apache2-websockets)
     * websocket v2 login (websocketd)
     * websocket v1 verify
     * websocket v2 verify
     * generic login (any ingress)
     * generic verify (any ingress)
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -18325,3 +18325,26 @@ WHERE tag = '555'
 AND control_set = 1
 AND ahf.heading_purpose = 'related'
 AND ahf.heading_type = 'genre_form_term';
+
+INSERT INTO config.usr_activity_type 
+    (id, ewhat, ehow, egroup, enabled, transient, label)
+VALUES (
+    25, 'login', 'ws-translator-v1', 'authen', TRUE, TRUE,
+    oils_i18n_gettext(25, 'Login via Websocket V1', 'cuat', 'label')
+), (
+    26, 'login', 'ws-translator-v2', 'authen', TRUE, TRUE,
+    oils_i18n_gettext(26, 'Login via Websocket V2', 'cuat', 'label')
+), (
+    27, 'verify', 'ws-translator-v1', 'authz', TRUE, TRUE,
+    oils_i18n_gettext(27, 'Verification via Websocket v1', 'cuat', 'label')
+), (
+    28, 'verify', 'ws-translator-v2', 'authz', TRUE, TRUE,
+    oils_i18n_gettext(28, 'Verifiation via Websocket V2', 'cuat', 'label')
+), (
+    29, 'login', NULL, 'authen', TRUE, TRUE,
+    oils_i18n_gettext(29, 'Generic Login', 'cuat', 'label')
+), (
+    30, 'verify', NULL, 'authz', TRUE, TRUE,
+    oils_i18n_gettext(30, 'Generic Verify', 'cuat', 'label')
+);
+
