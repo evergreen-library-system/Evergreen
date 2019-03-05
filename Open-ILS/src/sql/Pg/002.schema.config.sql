8c3537565269c854390cdb92c4a4bd6bd550a317
Bill Erickson <berickxx@gmail.com>
2019-03-05 12:21:00 -0500

    LP1818153 Stamping DB upgrade - WS user activity
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1153', :eg_version); -- abowling/kmlussier/csharp
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1154', :eg_version); -- berick/mmorgan
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
