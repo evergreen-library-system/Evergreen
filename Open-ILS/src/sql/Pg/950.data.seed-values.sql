55c0fd83e5555bbe6fca21f0f4626037cffa6a98
Michele Morgan <mmorgan@noblenet.org>
2019-04-05 16:55:19 -0400

    LP#1729435: Add Seed Data for WS type for Bill Full Details grids
    Adds the config.workstation_setting_type values to the seed data and
    an upgrade script to allow storing in the database.
    
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Jennifer Pringle <jennifer.pringle@bc.libraries.coop>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -19974,3 +19974,18 @@ VALUES (
         'cwst', 'label'
     )
 );
+
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.circ.patron.xact_details_details_bills', 'gui', 'object',
+    oils_i18n_gettext(
+    'eg.grid.circ.patron.xact_details_details_bills',
+    'Grid Config: circ.patron.xact_details_details_bills',
+    'cwst', 'label')
+), (
+    'eg.grid.circ.patron.xact_details_details_payments', 'gui', 'object',
+    oils_i18n_gettext(
+    'eg.grid.circ.patron.xact_details_details_payments',
+    'Grid Config: circ.patron.xact_details_details_payments',
+    'cwst', 'label')
+);
