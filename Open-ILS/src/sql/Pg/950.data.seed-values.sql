c26da318aa8480298718786bb5edebe1f87a1eef
Michele Morgan <mmorgan@noblenet.org>
2019-04-12 16:10:00 -0400

    LP#1774707: Add Seed Data for worstation setting for group member details
    Adds the config.workstation_setting_type for patron group member details
    to seed data and includes an upgrade script.
    
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -19945,3 +19945,13 @@ VALUES (
     )
 );
 
+INSERT INTO config.workstation_setting_type
+    (name, grp, datatype, label)
+VALUES (
+    'eg.grid.circ.patron.group_members', 'gui', 'object',
+    oils_i18n_gettext(
+    'eg.grid.circ.patron.group_members'
+    'Grid Config: circ.patron.group_members',
+    'cwst', 'label')
+);
+
