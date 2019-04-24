5b3c8479d9db5ace05060ce82e7b71a851ef10fd
Bill Erickson <berickxx@gmail.com>
2019-04-24 15:55:33 -0400

    LP1793005 Stamping upgrade / print settings as workstation settings
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1161', :eg_version); -- jboyer/stompro/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1162', :eg_version); -- berick/Bmagic
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
