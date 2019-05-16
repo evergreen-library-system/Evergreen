9a00355d7080b075e1695413eb87f254ff99870a
Kyle Huckins <khuckins@catalyte.io>
2019-05-16 22:05:55 +0000

    lp1821950 Require Call Number Label YAOUS
    - Add YAOUS to Require Call Number Labels in Copy Editor
    - Set new YAOUS to true
    - Don't display empty call number field warning if call numbers
    aren't required on the copy editor.
    - Properly enable/disable saving an item in the volcopy editor when
    CN label is empty, based on Require Call Number Label setting.
    
    Signed-off-by: Kyle Huckins <khuckins@catalyte.io>
    
     Changes to be committed:
    	modified:   Open-ILS/src/sql/Pg/950.data.seed-values.sql
    	new file:   Open-ILS/src/sql/Pg/upgrade/XXXX.lp1821950-call-number-label-required-yaous.sql
    	modified:   Open-ILS/web/js/ui/default/staff/cat/volcopy/app.js
    
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.lp1821950-call-number-label-required-yaous.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+INSERT into config.org_unit_setting_type
+( name, grp, label, description, datatype, fm_class ) VALUES
+( 'cat.require_call_number_labels', 'cat',
+  oils_i18n_gettext('cat.require_call_number_labels',
+    'Require call number labels in Copy Editor',
+    'coust', 'label'),
+  oils_i18n_gettext('cat.require_call_number_labels',
+    'Define whether Copy Editor requires Call Number labels',
+    'coust', 'description'),
+  'bool', null);
+
+INSERT INTO actor.org_unit_setting (org_unit, name, value) VALUES
+  (1, 'cat.require_call_number_labels', 'true');
+
+COMMIT;
