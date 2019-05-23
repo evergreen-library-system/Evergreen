9dc1d022b48ec9c67dfb6cd1766f9637ede0e205
Kyle Huckins <khuckins@catalyte.io>
2019-05-23 20:53:11 +0000

    lp1034058 Disable Renewal Due Dates in the Past
    Apply minimum date and an out of range flag to prevent
    selecting or inputting a past date when renewing items from
    Circulation->Renew Items or Patron->Items Out->Actions->
    Renew With Specific Due Date.
    
    To test
    -------
    [1] Create a loan.
    [2] From the patron's Item Out page, use the Renew With
        Specific Due Date action. Note that the date widget
        will allow you to specify a due date in the past.
    [3] From the Circulation -> Renew interface, note that
        the date widget will also let you specify a due
        date in the past.
    [4] Apply the patch and repeat steps #2 and #3. This time,
        a due date in the past cannot be entered, both via
        the calendar button and by direct entry in the text
        box.
    
    Signed-off-by: Kyle Huckins <khuckins@catalyte.io>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/web/js/ui/default/staff/circ/patron/items_out.js
+++ Open-ILS/web/js/ui/default/staff/circ/patron/items_out.js
@@ -472,6 +472,8 @@ function($scope , $q , $routeParams , $timeout , egCore , egUser , patronSvc ,
             controller : [
                         '$scope','$uibModalInstance',
                 function($scope , $uibModalInstance) {
+                    $scope.outOfRange = false;
+                    $scope.minDate = new Date();
                     $scope.args = {
                         barcodes : barcodes,
                         date : new Date()
