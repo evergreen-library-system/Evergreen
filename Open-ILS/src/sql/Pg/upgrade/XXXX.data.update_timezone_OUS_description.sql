d9750afce08146efc7faa9d75b346c0e7a4d8943
Galen Charlton <gmc@equinoxinitiative.org>
2019-05-23 11:17:44 -0400

    LP#1817332: (follow-up) tweak description
    Per a suggestion from Jane Sandberg, the description now includes
    an example of a time zone name with an underscore. Also, the database
    update will no longer overwrite any custom description that the
    Evergreen admin may have put in.
    
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.data.update_timezone_OUS_description.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.update_timezone_OUS_description.sql
@@ -5,10 +5,11 @@ BEGIN;
 UPDATE config.org_unit_setting_type
     SET description =
 'Define the time zone in which a library physically resides. Examples: America/Toronto, ' ||
-'America/Chicago, America/Denver, America/Vancouver, Europe/Prague. See Wikipedia for a ' ||
+'America/Chicago, America/Los_Angeles, America/Vancouver, Europe/Prague. See Wikipedia for a ' ||
 '<a href="https://en.wikipedia.org/wiki/List_of_tz_database_time_zones" target="_blank">complete list</a> ' ||
 '(Note: Only use "canonical" timezones).'
-    WHERE name = 'lib.timezone';
+WHERE name = 'lib.timezone'
+AND description = 'Define the time zone in which a library physically resides';
 
 COMMIT;
 
