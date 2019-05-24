c823a541d588d758ec1a3d0dd261d8945415749f
Bill Erickson <berickxx@gmail.com>
2019-05-24 15:06:24 -0400

    LP1817332 Stamping ugprade: timezone examples
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1166.data.update_timezone_OUS_description.sql
+++ Open-ILS/src/sql/Pg/upgrade/1166.data.update_timezone_OUS_description.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1166', :eg_version);
 
 UPDATE config.org_unit_setting_type
     SET description =
