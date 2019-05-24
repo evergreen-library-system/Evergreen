13296e612ca1b849c6b5b5ea43a444c3dc7d8e68
Bill Erickson <berickxx@gmail.com>
2019-05-24 15:06:24 -0400

    LP1817332 Stamping ugprade: timezone examples
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1165', :eg_version); -- jboyer/berick
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1166', :eg_version); -- remingtron/gmcharlt/berick
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
