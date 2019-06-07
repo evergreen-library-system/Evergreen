5d6e9eddbd6f1ec4e9c9c0c4c631d059202a46b7
Jane Sandberg <sandbej@linnbenton.edu>
2019-06-06 18:15:56 -0700

    LP1474874: Consistent page headers in the Angular client
    This commit ensures that each screen in the Angular staff client uses
    the <eg-staff-banner> component to better orient its users.
    
    This commit also gives the eg-staff-banner some <h1> semantics, per
    WebAIM's guidance: "Pages should be structured in a hierarchical manner,
    generally with one 1st degree headings (<h1>) being the most important
    (usually page titles or main content heading)" -- https://webaim.org/techniques/semanticstructure/
    
    To test:
    
    1) Go to [your-server]/eg2/en-US/staff/catalog/search
    2) Confirm that there is a header that gives the page title at the top
    of the page.
    3) Go to an Angular Vandelay page.
    4) Confirm that you see a similar header as in step 2.
    5) Go to an Angular Administration page.
    6) repeat step 4.
    
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>
    Signed-off-by: Jason Boyer <JBoyer@equinoxinitiative.org>

--- Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
+++ Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit, Input} from '@angular/core';
 @Component({
   selector: 'eg-staff-banner',
   template: `
-    <div class="lead alert alert-primary text-center pt-1 pb-1" role="alert">
+    <div class="lead alert alert-primary text-center pt-1 pb-1">
       <eg-title i18n-prefix [prefix]="bannerText"></eg-title>
        <span>{{bannerText}}</span>
     </div>
