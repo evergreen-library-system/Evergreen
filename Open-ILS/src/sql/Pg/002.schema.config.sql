3afd5233a787d399685b91635edf99c4a30a4afe
Jane Sandberg <sandbej@linnbenton.edu>
2019-06-13 13:57:14 -0700

    LP1759343: Stamping upgrade: annotate payment setting
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1166', :eg_version); -- remingtron/gmcharlt/berick
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1167', :eg_version); -- JBoyer/terran/sandbergja
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
