a1a03fa71d95bed03a5498d12c3be8c5f0ec10cf
Galen Charlton <gmc@equinoxinitiative.org>
2019-06-17 12:32:05 -0400

    LP#1831781: tweaks to eg-help-popover
    - Wrap the image in a button; this removes the need for setting
      tabindex and makes the cursor display as a pointer when it
      is over the popover.
    - Add aria-label attributes
    - add some usage comments
    
    Thanks to Jane Sandberg for the feedback that inspired this patch.
    
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>

--- Open-ILS/src/eg2/src/app/share/eg-help-popover/eg-help-popover.component.ts
+++ Open-ILS/src/eg2/src/app/share/eg-help-popover/eg-help-popover.component.ts
@@ -2,24 +2,31 @@ import { Component, OnInit, Input } from '@angular/core';
 import {NgbPopover} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'eg-help-popover',
-  templateUrl: './eg-help-popover.component.html',
-  styleUrls: ['./eg-help-popover.component.css']
+    selector: 'eg-help-popover',
+    templateUrl: './eg-help-popover.component.html',
+    styleUrls: ['./eg-help-popover.component.css']
 })
 export class EgHelpPopoverComponent implements OnInit {
 
-  @Input()
-  helpText = '';
+    // The text to display in the popover
+    @Input()
+    helpText = '';
 
-  @Input()
-  helpLink = '';
+    // An optional link to include in the popover. If supplied,
+    // the entire helpText is wrapped in it
+    @Input()
+    helpLink = '';
 
-  @Input()
-  placement = '';
+    // placement value passed to ngbPopover that controls
+    // where the popover is displayed. Values include
+    // 'auto', 'right', 'left', 'top-left', 'bottom-right',
+    // 'top', and so forth.
+    @Input()
+    placement = '';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
