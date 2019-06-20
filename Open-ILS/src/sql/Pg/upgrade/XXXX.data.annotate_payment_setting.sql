93a4bed2fa510655ddb835061f76917c26643661
Jason Stephenson <jason@sigio.com>
2019-06-20 11:35:04 -0400

    Revert "LP1759343: Stamping upgrade: annotate payment setting"
    This reverts commit d36620734f4f1ab10b70f48d4beef34fad18d36b.
    
    This commit was backported to rel_3_1 where it does not work.
    
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.data.annotate_payment_setting.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.annotate_payment_setting.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('1167', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 
 INSERT INTO config.workstation_setting_type (name,label,grp,datatype) VALUES ('eg.circ.bills.annotatepayment','Bills: Annotate Payment', 'circ', 'bool');
 
