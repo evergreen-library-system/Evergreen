c3d881bb1ae8299b94178f2483c5da663fe6c354
Dan Wells <dbw2@calvin.edu>
2019-06-28 16:16:57 -0400

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1167', :eg_version); -- JBoyer/terran/sandbergja
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.3.2', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
