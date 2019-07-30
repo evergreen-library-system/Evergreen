8be9f005555932f74b2e047145446df5d643b923
Galen Charlton <gmc@equinoxinitiative.org>
2019-07-30 10:21:17 -0400

    LP#1795972: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1168.data.apply_ws_setting_perm_description.sql
+++ Open-ILS/src/sql/Pg/upgrade/1168.data.apply_ws_setting_perm_description.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1168', :eg_version); -- csharp/khuckins/gmcharlt
 
 UPDATE permission.perm_list 
     SET description = oils_i18n_gettext(
