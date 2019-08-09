71a8adacf6a4711b0682e010ca275a9d1f2e85f8
Galen Charlton <gmc@equinoxinitiative.org>
2019-08-09 10:10:23 -0400

    fix bad conflict resolution made merging LP#1825851
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -20044,16 +20044,6 @@ VALUES (
     )
 );
 
-INSERT INTO config.workstation_setting_type
-    (name, grp, datatype, label)
-VALUES (
-    'eg.grid.circ.patron.group_members', 'gui', 'object',
-    oils_i18n_gettext(
-    'eg.grid.circ.patron.group_members',
-    'Grid Config: circ.patron.group_members',
-    'cwst', 'label')
-);
-
 INSERT INTO config.print_template 
     (id, name, locale, active, owner, label, template) 
 VALUES (
