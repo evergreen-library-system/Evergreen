529d556773255b1b2d86241a9d1d4d528d059d37
Bill Erickson <berickxx@gmail.com>
2019-08-16 16:50:34 -0400

    LP1840050 Anguar tree component realtime updates
    Teach the Angular tree component to re-render itself when its underyling
    tree has been replaced.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/eg2/src/app/share/tree/tree.component.ts
+++ Open-ILS/src/eg2/src/app/share/tree/tree.component.ts
@@ -37,7 +37,18 @@ nodeClicked(node: TreeNode) {
 })
 export class TreeComponent implements OnInit {
 
-    @Input() tree: Tree;
+    _tree: Tree;
+    @Input() set tree(t: Tree) {
+        if (t) {
+            this._tree = t;
+            this._tree.nodeList(); // reindex nodes
+        }
+    }
+
+    get tree(): Tree {
+        return this._tree;
+    }
+
     @Output() nodeClicked: EventEmitter<TreeNode>;
 
     constructor() {
