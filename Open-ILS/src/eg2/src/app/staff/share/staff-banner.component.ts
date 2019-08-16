d20011d9220b06077194dc76058f6a147f4c4a7d
Bill Erickson <berickxx@gmail.com>
2019-08-16 17:00:53 -0400

    LP1840050 Angular staff banner sets title
    Any UI that declares a staff banner gets the same text set as the
    page/title title as well.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
+++ Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
@@ -2,10 +2,12 @@ import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'eg-staff-banner',
-  template:
-    '<div class="lead alert alert-primary text-center pt-1 pb-1" role="alert">' +
-      '<span>{{bannerText}}</span>' +
-    '</div>'
+  template: `
+    <div class="lead alert alert-primary text-center pt-1 pb-1" role="alert">
+      <eg-title i18n-prefix [prefix]="bannerText"></eg-title>
+       <span>{{bannerText}}</span>
+    </div>
+    `
 })
 
 export class StaffBannerComponent {
