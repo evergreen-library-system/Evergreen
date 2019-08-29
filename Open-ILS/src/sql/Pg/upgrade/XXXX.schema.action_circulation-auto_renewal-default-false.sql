96c47dc7b8f9392c5d770bd8efff7ab5175495a0
Jason Stephenson <jason@sigio.com>
2019-08-29 11:06:56 -0400

    Lp 1839002: Default circulation auto_renewal field to FALSE
    Change the definition of the action.circulation table so that the
    auto_renewal field is not null, default false.
    
    Add an upgrade script to update the field values and alter the field
    definition in both the action.circulation and action.aged_circulation
    tables.
    
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.action_circulation-auto_renewal-default-false.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+UPDATE action.circulation SET auto_renewal = FALSE WHERE auto_renewal IS NULL;
+
+UPDATE action.aged_circulation SET auto_renewal = FALSE WHERE auto_renewal IS NULL;
+
+COMMIT;
+
+-- The following two changes cannot occur in a transaction with the
+-- above updates because we will get an error about not being able to
+-- alter a table with pending transactions.  They also need to occur
+-- after the above updates or the SET NOT NULL change will fail.
+
+ALTER TABLE action.circulation ALTER COLUMN auto_renewal SET DEFAULT FALSE;
+ALTER TABLE action.circulation ALTER COLUMN auto_renewal SET NOT NULL;
+
+ALTER TABLE action.aged_circulation ALTER COLUMN auto_renewal SET DEFAULT FALSE;
+ALTER TABLE action.aged_circulation ALTER COLUMN auto_renewal SET NOT NULL;
