e8507394878a873920710cb1c979dcf8dd36661f
Galen Charlton <gmc@equinoxinitiative.org>
2019-09-06 17:13:30 -0400

    LP#1817645: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1179', :eg_version); -- dpearl/Dycrona/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1180', :eg_version); -- jeffdavis/gmcharlt
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1180.schema.remoteauth.sql
+++ Open-ILS/src/sql/Pg/upgrade/1180.schema.remoteauth.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+SELECT evergreen.upgrade_deps_block_check('1180', :eg_version);
+
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 615, 'ADMIN_REMOTEAUTH', oils_i18n_gettext( 615,
     'Administer remote patron authentication', 'ppl', 'description' ));
