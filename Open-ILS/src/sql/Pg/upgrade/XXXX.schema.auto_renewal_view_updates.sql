4b70112dc86f8f64d2c2469b45b6486906a72087
Jason Stephenson <jason@sigio.com>
2019-09-10 11:27:07 -0400

    Lp 1835577: Add more missing auto_renewal fields
    Add missing auto_renewal and auto_renewal_remaining fields to the
    following additional IDL classes:
    
    * aacs
    * aoc
    * circbyyr
    * robcirc
    
    Add database updates to add auto_renewal fields to the following
    views:
    
    * action.billable_circulations
    * action.open_circulation
    * reporter.circ_type
    * reporter.overdue_circs
    
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.auto_renewal_view_updates.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+CREATE OR REPLACE VIEW action.open_circulation AS
+	SELECT	*
+	  FROM	action.circulation
+	  WHERE	checkin_time IS NULL
+	  ORDER BY due_date;
+
+CREATE OR REPLACE VIEW action.billable_circulations AS
+	SELECT	*
+	  FROM	action.circulation
+	  WHERE	xact_finish IS NULL;
+
+CREATE OR REPLACE VIEW reporter.overdue_circs AS
+SELECT  *
+  FROM  "action".circulation
+  WHERE checkin_time is null
+        AND (stop_fines NOT IN ('LOST','CLAIMSRETURNED') OR stop_fines IS NULL)
+        AND due_date < now();
+
+CREATE OR REPLACE VIEW reporter.circ_type AS
+SELECT	id,
+	CASE WHEN opac_renewal OR phone_renewal OR desk_renewal OR auto_renewal
+		THEN 'RENEWAL'
+		ELSE 'CHECKOUT'
+	END AS "type"
+  FROM	action.circulation;
+
+COMMIT;
