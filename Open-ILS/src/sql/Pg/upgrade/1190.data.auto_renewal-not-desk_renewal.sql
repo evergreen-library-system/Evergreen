474831f9f2102b6de9fd825a7e1799826f11bc0c
Galen Charlton <gmc@equinoxinitiative.org>
2019-10-01 17:24:16 -0400

    LP#1835085: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1190.data.auto_renewal-not-desk_renewal.sql
+++ Open-ILS/src/sql/Pg/upgrade/1190.data.auto_renewal-not-desk_renewal.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1190', :eg_version);
 
 UPDATE action.circulation SET desk_renewal = FALSE WHERE auto_renewal IS TRUE;
 
