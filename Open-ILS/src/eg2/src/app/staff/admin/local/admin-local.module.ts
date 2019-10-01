d2446dd1015f3551123be9a21222cfe2fe6cdcd6
Julian Clementson <51331324+oajulianclementson@users.noreply.github.com>
2019-10-01 16:17:53 +0100

    LP#1842297: Implements patron sign-on to the OpenAthens service.
    Allows global or local administrators to configure a connection to the
    OpenAthens cloud-based single sign-on service. Allows patrons to connect
    seamlessly to third party resources that use OpenAthens authentication.
    For more detailed feature description visit
    https://wiki.evergreen-ils.org/doku.php?id=dev%3Aproposal%3Aopenathens_integration
    
    Signed-off-by: Julian Clementson <julian.clementson@openathens.net>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/eg2/src/app/staff/admin/local/admin-local.module.ts
+++ Open-ILS/src/eg2/src/app/staff/admin/local/admin-local.module.ts
@@ -6,6 +6,7 @@ import {AdminCommonModule} from '@eg/staff/admin/common.module';
 import {AdminLocalSplashComponent} from './admin-local-splash.component';
 import {AddressAlertComponent} from './address-alert.component';
 import {AdminCarouselComponent} from './admin-carousel.component';
+import {OpenAthensIdentityComponent} from './openathens-identity.component';
 import {StandingPenaltyComponent} from './standing-penalty.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import {StandingPenaltyComponent} from './standing-penalty.component';
       AdminLocalSplashComponent,
       AddressAlertComponent,
       AdminCarouselComponent,
+      OpenAthensIdentityComponent,
       StandingPenaltyComponent
   ],
   imports: [
