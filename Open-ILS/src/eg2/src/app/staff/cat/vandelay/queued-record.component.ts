e52ade3eda336bbe71da95202313528a2ac40b37
Bill Erickson <berickxx@gmail.com>
2019-10-17 17:59:08 -0400

    LP1830923 Vandelay queued record MARC edit
    Allow editing of Vandelay queued bib records (from the queue
    interface) via the Angular MARC editor.
    
    Note only the flat text editor is currently supportd in the
    Angular MARC editor.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/eg2/src/app/staff/cat/vandelay/queued-record.component.ts
+++ Open-ILS/src/eg2/src/app/staff/cat/vandelay/queued-record.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router, ActivatedRoute, ParamMap} from '@angular/router';
 import {NgbTabset, NgbTabChangeEvent} from '@ng-bootstrap/ng-bootstrap';
+import {PcrudService} from '@eg/core/pcrud.service';
+import {IdlObject} from '@eg/core/idl.service';
+import {ToastService} from '@eg/share/toast/toast.service';
+import {StringComponent} from '@eg/share/string/string.component';
 
 @Component({
   templateUrl: 'queued-record.component.html'
@@ -11,16 +15,24 @@ export class QueuedRecordComponent {
     queueType: string;
     recordId: number;
     recordTab: string;
+    queuedRecord: IdlObject;
+
+    @ViewChild('updateSuccess') updateSuccess: StringComponent;
 
     constructor(
         private router: Router,
-        private route: ActivatedRoute) {
+        private route: ActivatedRoute,
+        private toast: ToastService,
+        private pcrud: PcrudService) {
 
         this.route.paramMap.subscribe((params: ParamMap) => {
             this.queueId = +params.get('id');
             this.recordId = +params.get('recordId');
             this.queueType = params.get('qtype');
             this.recordTab = params.get('recordTab');
+            if (this.recordTab === 'edit') {
+                this.loadRecord();
+            }
         });
     }
 
@@ -37,6 +49,24 @@ export class QueuedRecordComponent {
           `/record/${this.recordId}/${this.recordTab}`;
 
         this.router.navigate([url]);
+    }
+
+    loadRecord() {
+        this.queuedRecord = null;
+        this.pcrud.retrieve('vqbr', this.recordId)
+        .subscribe(rec => this.queuedRecord = rec);
+    }
+
+    handleMarcRecordSaved(saveEvent: any) {
+        this.queuedRecord.marc(saveEvent.marcXml);
+        this.queuedRecord.bib_source(saveEvent.bibSource);
+        this.pcrud.update(this.queuedRecord).subscribe(
+            response => this.toast.success(this.updateSuccess.text),
+            error => {
+                console.error(error);
+                this.toast.danger(error);
+            }
+        );
     }
 }
 
--- Open-ILS/src/eg2/src/app/staff/cat/vandelay/vandelay.module.ts
+++ Open-ILS/src/eg2/src/app/staff/cat/vandelay/vandelay.module.ts
@@ -23,6 +23,7 @@ import {MatchSetExpressionComponent} from './match-set-expression.component';
 import {MatchSetQualityComponent} from './match-set-quality.component';
 import {MatchSetNewPointComponent} from './match-set-new-point.component';
 import {RecentImportsComponent} from './recent-imports.component';
+import {MarcEditModule} from '@eg/staff/share/marc-edit/marc-edit.module';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import {RecentImportsComponent} from './recent-imports.component';
   imports: [
     TreeModule,
     StaffCommonModule,
+    MarcEditModule,
     CatalogCommonModule,
     VandelayRoutingModule,
     HttpClientModule,
