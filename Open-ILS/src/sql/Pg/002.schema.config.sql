11868e97844a47d57489062197ac6809d9473366
Jason Boyer <jboyer@equinoxinitiative.org>
2019-10-23 12:01:15 -0400

    Stamp upgrade script for LP 1849152
    Signed-off-by: Jason Boyer <jboyer@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1195', :eg_version); -- dbwells/khuckins/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1196', :eg_version); -- rhamby/gcollum/jboyer
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
