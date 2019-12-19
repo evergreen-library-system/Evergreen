da3fff6cf5f075ab02ea991ebe38f8600f848b7e
Mike Risher <mrisher@catalyte.io>
2019-12-19 17:56:41 +0000

    LP#1855780 Angular Notification/Action Triggers port
    Port Notification Action Triggers from DOJO to Angular.  This consists of 4 grids, each
    navigated to by its corresponding tab. The Trigger Event Definitions grid allows
    cloning of records.  When editing an event defintion one can also edit parameters,
    environments, and run tests.
    
    Signed-off-by: Mike Risher <mrisher@catalyte.io>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

---
+++ Open-ILS/src/eg2/src/app/staff/admin/local/triggers/triggers.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {AdminCommonModule} from '@eg/staff/admin/common.module';
+import {TriggersComponent} from './triggers.component';
+import {TriggersRoutingModule} from './triggers_routing.module';
+import {EditEventDefinitionComponent} from './trigger-edit.component';
+
+@NgModule({
+  declarations: [
+    TriggersComponent,
+    EditEventDefinitionComponent
+  ],
+  imports: [
+    AdminCommonModule,
+    TriggersRoutingModule,
+  ],
+  exports: [
+  ],
+  providers: [
+  ]
+})
+
+export class TriggersModule {
+}
---
+++ Open-ILS/src/eg2/src/app/staff/admin/local/triggers/triggers_routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TriggersComponent} from './triggers.component';
+import {EditEventDefinitionComponent} from './trigger-edit.component';
+
+const routes: Routes = [{
+    path: '',
+    component: TriggersComponent
+}, {
+    path: ':id',
+    component: EditEventDefinitionComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class TriggersRoutingModule {
+}
