20ac557e84d7477906c4bd502a46bc91bcacfa5e
Galen Charlton <gmc@equinoxinitiative.org>
2020-02-04 19:52:25 -0500

    LP#1862022: add more permissions to stock Acquisitions and Acq Admin profiles
    This patch makes the stock Acquisitions and Acquisitions Administrator
    profiles better able to use the acquisitions modules out of the box.
    
    The permissions added are:
    
    Acquisitions Administrator Profile
    -----------------------------------------
    VIEW_FUND
    VIEW_FUNDING_SOURCE
    VIEW_FUND_ALLOCATION
    VIEW_PICKLIST
    VIEW_PROVIDER
    VIEW_PURCHASE_ORDER
    VIEW_INVOICE
    CREATE_PICKLIST
    ACQ_ADD_LINEITEM_IDENTIFIER
    ACQ_SET_LINEITEM_IDENTIFIER
    MANAGE_FUND
    CREATE_INVOICE
    CREATE_PURCHASE_ORDER
    IMPORT_ACQ_LINEITEM_BIB_RECORD
    IMPORT_ACQ_LINEITEM_BIB_RECORD_UPLOAD
    MANAGE_CLAIM
    MANAGE_PROVIDER
    MANAGE_FUNDING_SOURCE
    RECEIVE_PURCHASE_ORDER
    ADMIN_ACQ_LINEITEM_ALERT_TEXT
    UPDATE_FUNDING_SOURCE
    UPDATE_PROVIDER
    VIEW_IMPORT_MATCH_SET
    VIEW_MERGE_PROFILE
    IMPORT_MARC
    
    Acquisitions Profile
    -----------------------------------------
    ACQ_ADD_LINEITEM_IDENTIFIER
    ACQ_SET_LINEITEM_IDENTIFIER
    ADMIN_ACQ_FUND
    ADMIN_FUND
    ACQ_INVOICE-REOPEN
    ADMIN_ACQ_DISTRIB_FORMULA
    ADMIN_INVOICE
    IMPORT_ACQ_LINEITEM_BIB_RECORD_UPLOAD
    VIEW_IMPORT_MATCH_SET
    VIEW_MERGE_PROFILE
    
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>
    Signed-off-by: Tiffany Little <tlittle@georgialibraries.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/YYYY.data.acq_permissions.sql
@@ -0,0 +1,65 @@
+BEGIN;
+
+INSERT INTO permission.grp_perm_map (grp, perm, depth, grantable)
+        SELECT
+                pgt.id, perm.id, aout.depth, TRUE
+        FROM
+                permission.grp_tree pgt,
+                permission.perm_list perm,
+                actor.org_unit_type aout
+        WHERE
+                pgt.name = 'Acquisitions Administrator' AND
+                aout.name = 'Consortium' AND
+                perm.code IN (
+                    'VIEW_FUND',
+                    'VIEW_FUNDING_SOURCE',
+                    'VIEW_FUND_ALLOCATION',
+                    'VIEW_PICKLIST',
+                    'VIEW_PROVIDER',
+                    'VIEW_PURCHASE_ORDER',
+                    'VIEW_INVOICE',
+                    'CREATE_PICKLIST',
+                    'ACQ_ADD_LINEITEM_IDENTIFIER',
+                    'ACQ_SET_LINEITEM_IDENTIFIER',
+                    'MANAGE_FUND',
+                    'CREATE_INVOICE',
+                    'CREATE_PURCHASE_ORDER',
+                    'IMPORT_ACQ_LINEITEM_BIB_RECORD',
+                    'IMPORT_ACQ_LINEITEM_BIB_RECORD_UPLOAD',
+                    'MANAGE_CLAIM',
+                    'MANAGE_PROVIDER',
+                    'MANAGE_FUNDING_SOURCE',
+                    'RECEIVE_PURCHASE_ORDER',
+                    'ADMIN_ACQ_LINEITEM_ALERT_TEXT',
+                    'UPDATE_FUNDING_SOURCE',
+                    'UPDATE_PROVIDER',
+                    'VIEW_IMPORT_MATCH_SET',
+                    'VIEW_MERGE_PROFILE',
+                    'IMPORT_MARC'
+                );
+
+
+INSERT INTO permission.grp_perm_map (grp, perm, depth, grantable)
+        SELECT
+                pgt.id, perm.id, aout.depth, FALSE
+        FROM
+                permission.grp_tree pgt,
+                permission.perm_list perm,
+                actor.org_unit_type aout
+        WHERE
+                pgt.name = 'Acquisitions' AND
+                aout.name = 'Consortium' AND
+                perm.code IN (
+                    'ACQ_ADD_LINEITEM_IDENTIFIER',
+                    'ACQ_SET_LINEITEM_IDENTIFIER',
+                    'ADMIN_ACQ_FUND',
+                    'ADMIN_FUND',
+                    'ACQ_INVOICE-REOPEN',
+                    'ADMIN_ACQ_DISTRIB_FORMULA',
+                    'ADMIN_INVOICE',
+                    'IMPORT_ACQ_LINEITEM_BIB_RECORD_UPLOAD',
+                    'VIEW_IMPORT_MATCH_SET',
+                    'VIEW_MERGE_PROFILE'
+                );
+
+COMMIT;
