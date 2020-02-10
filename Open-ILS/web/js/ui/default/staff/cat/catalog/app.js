738348fb69b9e3297484f5db4e83c26a33f07c5f
Josh Stompro <stompro@stompro.org>
2020-02-10 10:15:02 -0600

    LP#2031114 - Create MARC Record - focus on item add and call number
    This change sets the focus on the "Add Item" checkbox when the marc editor
    is opened with fast item add enabled.  This allows the user to just press space
    to enable Add item.
    
    When the add item checkbox is enabled, the focus is moved to the call number field.
    
    This adds a directive to app.js called focusOnShow that makes it easy
    to mark an element to have focus when it becomes visible when using ng-show.
    
    I had to add in a bit of redundancy in t_marcedit.tt2, the Add Item checkbox
    needs to have it's own ng-show directive for the focus-on-show to work.
    
    Includes release note entry.
    
    Signed-off-by: Josh Stompro <stompro@stompro.org>
    Signed-off-by: Beth Willis <willis@noblenet.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

--- Open-ILS/web/js/ui/default/staff/cat/catalog/app.js
+++ Open-ILS/web/js/ui/default/staff/cat/catalog/app.js
@@ -271,6 +271,33 @@ function($scope , $routeParams , $location , $window , $q , egCore) {
     };
 })
 
+.directive('focusOnShow', function($timeout) {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attr) {
+            if ($attr.ngShow){
+                $scope.$watch($attr.ngShow, function(newValue){
+                    if(newValue){
+                        $timeout(function(){
+                            $element[0].focus();
+                        }, 0);
+                    }
+                })
+            }
+            if ($attr.ngHide){
+                $scope.$watch($attr.ngHide, function(newValue){
+                    if(!newValue){
+                        $timeout(function(){
+                            $element[0].focus();
+                        }, 0);
+                    }
+                })
+            }
+
+        }
+    };
+})
+
 .controller('CatalogCtrl',
        ['$scope','$routeParams','$location','$window','$q','egCore','egHolds','egCirc','egConfirmDialog','ngToast',
         'egGridDataProvider','egHoldGridActions','egProgressDialog','$timeout','$uibModal','holdingsSvc','egUser','conjoinedSvc',
