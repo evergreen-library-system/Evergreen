4f5846551b45d53dd34664a6616c86695860c385
Jeff Davis <jdavis@sitka.bclibraries.ca>
2020-05-28 09:17:11 -0700

    LP#1096209: add check_sms_notify parameter to all relevant event defs
    Signed-off-by: Jeff Davis <jdavis@sitka.bclibraries.ca>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.use_check_sms_notify.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+INSERT INTO action_trigger.event_params (event_def, param, value)
+SELECT id, 'check_sms_notify', 1
+FROM action_trigger.event_definition
+WHERE reactor = 'SendSMS'
+AND validator IN ('HoldIsAvailable', 'HoldIsCancelled', 'HoldNotifyCheck')
+AND NOT EXISTS (
+    SELECT * FROM action_trigger.event_params
+    WHERE param = 'check_sms_notify'
+);
+
+--COMMIT;
