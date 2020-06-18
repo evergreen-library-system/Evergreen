be3de646ad7c8b7ef34c0bcad169d2912ccb4c50
Galen Charlton <gmc@equinoxinitiative.org>
2020-06-18 16:27:19 -0400

    LP#1851306: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1204', :eg_version); -- jeffdavis/sandbergja/csharp
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1205', :eg_version); -- sandbergja/tmcanna/gmcharlt
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1205.data.port_capture_reservations_to_angular.sql
+++ Open-ILS/src/sql/Pg/upgrade/1205.data.port_capture_reservations_to_angular.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1205', :eg_version);
 
 INSERT INTO config.print_template 
     (id, name, locale, active, owner, label, template) 
