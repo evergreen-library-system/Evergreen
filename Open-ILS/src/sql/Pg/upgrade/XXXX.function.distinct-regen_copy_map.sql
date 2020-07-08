a78747a609f3db4b557bb2305cb85d9c0adc9b79
Mike Rylander <mrylander@gmail.com>
2020-07-08 13:18:52 -0400

    LP#1886852: Protect against dupes in hold-copy map function
    If the DB function that regenerates the hold-copy maps for a hold
    receives duplicate copies in its second parameter, it blows up.  This
    commit prevents that by making the list unique at use time.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Jason Stephenson <jason@sigio.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.function.distinct-regen_copy_map.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+CREATE OR REPLACE FUNCTION
+    action.hold_request_regen_copy_maps(
+        hold_id INTEGER, copy_ids INTEGER[]) RETURNS VOID AS $$
+    DELETE FROM action.hold_copy_map WHERE hold = $1;
+    INSERT INTO action.hold_copy_map (hold, target_copy) SELECT DISTINCT $1, UNNEST($2);
+$$ LANGUAGE SQL;
+
+COMMIT;
