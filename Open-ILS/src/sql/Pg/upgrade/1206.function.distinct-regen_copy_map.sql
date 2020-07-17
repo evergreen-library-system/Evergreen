09e7d384b0523be53c64e736889c141fd94108d0
Jason Stephenson <jason@sigio.com>
2020-07-17 07:09:29 -0400

    LP#1886852: Stamping Upgrade Script
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/upgrade/1206.function.distinct-regen_copy_map.sql
+++ Open-ILS/src/sql/Pg/upgrade/1206.function.distinct-regen_copy_map.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1206', :eg_version);
 
 CREATE OR REPLACE FUNCTION
     action.hold_request_regen_copy_maps(
