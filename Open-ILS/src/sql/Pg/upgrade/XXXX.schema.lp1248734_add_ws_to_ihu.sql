ebbdca4cde8eb4480266562a675545570d137ea4
Galen Charlton <gmc@equinoxinitiative.org>
2020-07-27 10:16:37 -0400

    LP#1248734: (follow-up) add new indexes to schema update script
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.schema.lp1248734_add_ws_to_ihu.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.lp1248734_add_ws_to_ihu.sql
@@ -5,4 +5,7 @@ BEGIN;
 ALTER TABLE action.in_house_use ADD COLUMN workstation INT REFERENCES actor.workstation (id) DEFERRABLE INITIALLY DEFERRED;
 ALTER TABLE action.non_cat_in_house_use ADD COLUMN workstation INT REFERENCES actor.workstation (id) DEFERRABLE INITIALLY DEFERRED;
 
+CREATE INDEX action_in_house_use_ws_idx ON action.in_house_use ( workstation );
+CREATE INDEX non_cat_in_house_use_ws_idx ON action.non_cat_in_house_use ( workstation );
+
 COMMIT;
