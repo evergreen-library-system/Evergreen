70c3cbeba6f3a754effcc7a3be6a17ebb8f005b5
Galen Charlton <gmc@equinoxinitiative.org>
2020-07-27 10:19:29 -0400

    LP#1248734: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1209', :eg_version); -- csharp/gmcharlt/JBoyer
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1210', :eg_version); -- csharp/rhamby/sandbergja/gmcharlt
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1210.schema.lp1248734_add_ws_to_ihu.sql
+++ Open-ILS/src/sql/Pg/upgrade/1210.schema.lp1248734_add_ws_to_ihu.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1210', :eg_version); -- csharp/rhamby/sandbergja/gmcharlt
 
 ALTER TABLE action.in_house_use ADD COLUMN workstation INT REFERENCES actor.workstation (id) DEFERRABLE INITIALLY DEFERRED;
 ALTER TABLE action.non_cat_in_house_use ADD COLUMN workstation INT REFERENCES actor.workstation (id) DEFERRABLE INITIALLY DEFERRED;
