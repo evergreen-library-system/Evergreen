3bfb9f22291df09ffe0e860624e507b79a451d45
Bill Erickson <berickxx@gmail.com>
2020-07-27 14:21:09 -0400

    LP1889113 Staff catalog record holds sticky org select
    Teaches the record holds grid to use a persistkey for making the pickup
    lib selector sticky.  Additionaly, teach the holds retrieval code to
    wait until the org select has settled on a value before collecting any
    data.
    
    Also adds the new workstation setting type
    'eg.orgselect.cat.catalog.wide_holds'
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -20414,4 +20414,13 @@ VALUES (
     )
 );
 
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.orgselect.cat.catalog.wide_holds', 'gui', 'integer',
+    oils_i18n_gettext(
+        'eg.orgselect.cat.catalog.wide_holds',
+        'Default org unit for catalog holds org unit selector',
+        'cwst', 'label'
+    )
+);
 
