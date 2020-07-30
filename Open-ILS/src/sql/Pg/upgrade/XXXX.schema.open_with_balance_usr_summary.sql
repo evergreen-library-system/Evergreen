388608cd4d66581c886d7bd782ca3acf57458b69
Mike Rylander <mrylander@gmail.com>
2020-07-30 11:48:46 -0400

    LP#1772955: Only include xacts with balance in summary
    This commit adds a new view and IDL definition that shows the total
    owed/paid/balance for transactions that have a balance other than 0.
    This new view is used in the patron summary bar, and access is provided
    wherever the all-open-transactions version is available in case we want
    to move to the new one elsewhere.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Ruth Frasur <rfrasur@library.in.gov>
    Signed-off-by: Jason Boyer <JBoyer@EquinoxInitiative.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.open_with_balance_usr_summary.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+CREATE OR REPLACE VIEW money.open_with_balance_usr_summary AS
+    SELECT
+        usr,
+        sum(total_paid) AS total_paid,
+        sum(total_owed) AS total_owed,
+        sum(balance_owed) AS balance_owed
+    FROM money.materialized_billable_xact_summary
+    WHERE xact_finish IS NULL AND balance_owed <> 0.0
+    GROUP BY usr;
+
+COMMIT;
