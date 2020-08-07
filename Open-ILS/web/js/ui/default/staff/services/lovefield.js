908f32f794b9c0df8e7c167bcea67ceb0b7ebd06
Mike Risher <mrisher@catalyte.io>
2020-08-07 20:14:40 +0000

    LP#1724019 Offline Circulation Orgs Alphabetized
    On the offline circulation interface adjust the orgs so that they're
    sorted alphabetically.  Preserve the org hierarchy.
    
    Signed-off-by: Mike Risher <mrisher@catalyte.io>
    
     Changes to be committed:
    	modified:   Open-ILS/web/js/ui/default/staff/services/env.js
    	modified:   Open-ILS/web/js/ui/default/staff/services/lovefield.js
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/web/js/ui/default/staff/services/lovefield.js
+++ Open-ILS/web/js/ui/default/staff/services/lovefield.js
@@ -439,6 +439,11 @@ angular.module('egCoreMod')
                     }
                 });
 
+                if (type == 'aou') {
+                    // Sort the org tree before absorbing
+                    egCore.env.sort_aou(top);
+                }
+
                 egCore.env.absorbTree(top, type, true)
                 return $q.when(true)
             });
