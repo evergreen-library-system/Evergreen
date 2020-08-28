4075a1214db39514a6e2bfc61f2d104725c81d25
Jason Boyer <JBoyer@equinoxinitiative.org>
2020-08-28 17:13:14 -0400

    LP1893463: Prevent duplicated report outputs
    When re-running recurring reports that are interrupted,
    it's possible to end up with a duplicate copy of the next
    recurrance of that report. This branch adds a unique index
    to reporter.schedule and tells clark-kent.pl not to worry
    about it.
    
    For the purposes of this change a duplicate report is
    one where report.schedule has identical values for report,
    (output) folder, runner, run_time, and email.
    
    Signed-off-by: Jason Boyer <JBoyer@equinoxinitiative.org>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/reporter/clark-kent.pl
+++ Open-ILS/src/reporter/clark-kent.pl
@@ -250,7 +250,7 @@ for my $r ( @reports ) {
 			  WHERE	id = ?;
 		SQL
 
-	    $logger->debug('Report SQL: ' . $r->{resultset}->toSQL);
+		$logger->debug('Report SQL: ' . $r->{resultset}->toSQL);
 		$sth = $data_dbh->prepare($r->{resultset}->toSQL);
 
 		$sth->execute;
@@ -314,7 +314,9 @@ for my $r ( @reports ) {
 					VALUES ( ?, ?, ?, ?::TIMESTAMPTZ + ?, ?, ?, ?, ?, ?, ?, ? );
 			SQL
 
-			$state_dbh->do(
+			my $prevP = $state_dbh->{PrintError};
+			$state_dbh->{PrintError} = 0;
+			if (!$state_dbh->do(
 				$sql,
 				{},
 				$r->{report}->{id},
@@ -329,7 +331,11 @@ for my $r ( @reports ) {
 				$r->{chart_pie},
 				$r->{chart_bar},
 				$r->{chart_line},
-			);
+			)) {
+				# Ignore duplicate key errors on reporter.schedule (err 7 is a fatal query error). Just look for the constraint name in the message to avoid l10n issues.
+				warn($state_dbh->errstr()) unless $state_dbh->err() == 7 && $state_dbh->errstr() =~ m/rpt_sched_recurrence_once_idx/;
+			}
+			$state_dbh->{PrintError} = $prevP;
 		}
 
 		$state_dbh->do(<<'		SQL',{}, $r->{id});
