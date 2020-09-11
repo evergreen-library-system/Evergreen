ab774e44eb803d106a62f551209ac2b1257e12b0
Galen Charlton <gmc@equinoxinitiative.org>
2020-09-11 15:54:58 -0400

    LP#1777677: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1229.data.lp1777677-action-triggers-test-notification.sql
+++ Open-ILS/src/sql/Pg/upgrade/1229.data.lp1777677-action-triggers-test-notification.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1229', :eg_version);
 
 
 INSERT into action_trigger.hook (key, core_type, description) VALUES (
