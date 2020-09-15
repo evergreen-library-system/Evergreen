6e52783e0cac279eb4d9c97820269d01ebb68d7e
Jason Boyer <JBoyer@equinoxinitiative.org>
2020-09-15 08:50:26 -0400

    LP1895660: Silence Perl Warnings
    There are many perl warnings in make check and the day-to-day
    logs when using Evergreen. This branch aims to address the
    warnings and to make reviewing easier each file will be
    individually committed with a small rationale for the changes
    though it is assumed that this branch will be largely squashed
    away if / when committed.
    
    Booking.pm: Possible precedence issue with control flow operator
    The precedence of 'or' is significantly lower than '||', so much so
    that it can be less than the precedence for assignments.
    Replace 'or' with '||' since they're equivalent in this case anyway,
    and also add parens for flavor.
    
    Signed-off-by: Jason Boyer <JBoyer@equinoxinitiative.org>
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Booking.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Booking.pm
@@ -1274,7 +1274,7 @@ sub get_captured_reservations {
             return $patron;
         },
         "ready" => sub {
-            return $e->search_booking_reservation([
+            return ($e->search_booking_reservation([
                 {
                     "usr" => $patron->id,
                     "capture_time" => {"!=" => undef},
@@ -1283,10 +1283,10 @@ sub get_captured_reservations {
                     "cancel_time" => undef
                 },
                 $bresv_flesh
-            ]) or $e->die_event;
+            ]) || $e->die_event);
         },
         "out" => sub {
-            return $e->search_booking_reservation([
+            return ($e->search_booking_reservation([
                 {
                     "usr" => $patron->id,
                     "pickup_time" => {"!=" => undef},
@@ -1294,17 +1294,17 @@ sub get_captured_reservations {
                     "cancel_time" => undef
                 },
                 $bresv_flesh
-            ]) or $e->die_event;
+            ]) || $e->die_event);
         },
         "in" => sub {
-            return $e->search_booking_reservation([
+            return ($e->search_booking_reservation([
                 {
                     "usr" => $patron->id,
                     "return_time" => {">=" => "today"},
                     "cancel_time" => undef
                 },
                 $bresv_flesh
-            ]) or $e->die_event;
+            ]) || $e->die_event);
         }
     };
 
