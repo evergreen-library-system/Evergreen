1d6705d5aea46a58b9f03c9b0967348ff27169e7
Chris Sharp <csharp@georgialibraries.org>
2020-09-23 16:33:06 -0400

    LP#1835127 - Stamp upgrade script
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1236', :eg_version); -- csharp/rhamby/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1238', :eg_version); -- jeffdavis/csharp
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
