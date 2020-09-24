fbf828293e04df6aa248111fef857f1640a0c7b2
Galen Charlton <gmc@equinoxinitiative.org>
2020-09-24 11:58:08 -0400

    LP#1835127: (follow-up) fixes to update script
    - include the version number stamp
    - adjust the permission IDs
    
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1238.data.permission.booking_reservation.sql
+++ Open-ILS/src/sql/Pg/upgrade/1238.data.permission.booking_reservation.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1238', :eg_version);
 
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
- ( 625, 'VIEW_BOOKING_RESERVATION', oils_i18n_gettext(623,
+ ( 625, 'VIEW_BOOKING_RESERVATION', oils_i18n_gettext(625,
     'View booking reservations', 'ppl', 'description')),
- ( 626, 'VIEW_BOOKING_RESERVATION_ATTR_MAP', oils_i18n_gettext(624,
+ ( 626, 'VIEW_BOOKING_RESERVATION_ATTR_MAP', oils_i18n_gettext(626,
     'View booking reservation attribute maps', 'ppl', 'description'))
 ;
 
