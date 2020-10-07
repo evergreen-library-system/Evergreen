a7a1ed39257504914dcc25fcb4969a135f8421c0
Chris Sharp <csharp@georgialibraries.org>
2020-10-07 14:54:41 -0400

    LP1893463: stamp upgrade script
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/sql/Pg/upgrade/1241.schema.reporter_schedule_uniqueness.sql
+++ Open-ILS/src/sql/Pg/upgrade/1241.schema.reporter_schedule_uniqueness.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1241', :eg_version);
 
 -- Explicitly supply the name because it is referenced in clark-kent.pl
 CREATE UNIQUE INDEX rpt_sched_recurrence_once_idx ON reporter.schedule (report,folder,runner,run_time,email);
