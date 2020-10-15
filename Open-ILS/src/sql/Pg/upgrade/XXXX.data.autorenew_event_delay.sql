3c26b261dc18a86ef7210cd95f25402db061823a
Jason Stephenson <jason@sigio.com>
2020-10-15 14:28:03 -0400

    Lp 1899976: Adjust AutoRenew Delay
    Modify the AutoRenew event delay so that it covers a complete 24-hour
    period.
    
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.autorenew_event_delay.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+UPDATE action_trigger.event_definition
+SET delay = '-24:01:00'::INTERVAL
+WHERE reactor = 'Circ::AutoRenew'
+AND delay = '-23 hours'::INTERVAL
+AND max_delay = '-1 minute'::INTERVAL;
+
+
+COMMIT;
