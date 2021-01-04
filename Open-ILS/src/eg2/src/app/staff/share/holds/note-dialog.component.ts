aa3dbaee4b8c34aec79e752a6d94534a14394175
Bill Erickson <berickxx@gmail.com>
2021-01-04 16:12:50 -0500

    LP1910145 Angular Hold Detail Notes & Notifications
    The hold detail view now displays hold notes and hold notification
    records.  Notes and Notifications may be created by staff.  Notes may be
    deleted by staff.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Garry Collum <gcollum@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/eg2/src/app/staff/share/holds/note-dialog.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit, Input, Output, ViewChild, EventEmitter} from '@angular/core';
+import {Observable, Observer, of} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {IdlObject, IdlService} from '@eg/core/idl.service';
+import {NetService} from '@eg/core/net.service';
+import {PcrudService} from '@eg/core/pcrud.service';
+import {OrgService} from '@eg/core/org.service';
+import {AuthService} from '@eg/core/auth.service';
+import {NgbModal, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
+import {DialogComponent} from '@eg/share/dialog/dialog.component';
+
+/** New hold note dialog */
+
+@Component({
+  selector: 'eg-hold-note-dialog',
+  templateUrl: 'note-dialog.component.html'
+})
+export class HoldNoteDialogComponent extends DialogComponent {
+    pub = false;
+    slip = false;
+    title: string;
+    body: string;
+
+    @Input() holdId: number;
+
+    constructor(
+        private modal: NgbModal,
+        private idl: IdlService,
+        private pcrud: PcrudService
+    ) { super(modal); }
+
+    createNote() {
+        const note = this.idl.create('ahrn');
+        note.staff('t');
+        note.hold(this.holdId);
+        note.title(this.title);
+        note.body(this.body);
+        note.slip(this.slip ? 't' : 'f');
+        note.pub(this.pub ? 't' : 'f');
+
+        this.pcrud.create(note).toPromise().then(
+            resp => this.close(resp), // new note object
+            err => console.error('Could not create note', err)
+        );
+    }
+}
+
+
