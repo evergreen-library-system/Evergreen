32e38d0d56ef0af8b9bf98f16ee6cc922032d74c
Jane Sandberg <sandbej@linnbenton.edu>
2021-01-09 22:20:29 -0800

    LP1910891: Add new booking perms to appropriate groups
    Steps to test:
    
    1) Without this patch, create a reservation for a BR1 resource at BR1.
    2) Log in as a circulator at BR1.
    3) Go to Booking > Manage Reservations.  Note that you can't see your reservation.
    4) Run this upgrade script.
    5) Repeat steps 2-3.  Notice that you can now see the reservation.
    6) Wipe your database and re-install it with the additions to the seed data
    in this commit.  Repeat steps 1-3 and notice that you can see the reservation
    as expected.
    
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.new_booking_perms_in_map.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+WITH perms_to_add AS
+    (SELECT id FROM
+    permission.perm_list
+    WHERE code IN ('VIEW_BOOKING_RESERVATION', 'VIEW_BOOKING_RESERVATION_ATTR_MAP'))
+
+INSERT INTO permission.grp_perm_map (grp, perm, depth, grantable)
+    SELECT grp, perms_to_add.id as perm, depth, grantable
+        FROM perms_to_add,
+        permission.grp_perm_map
+        
+        --- Don't add the permissions if they have already been assigned
+        WHERE grp NOT IN
+            (SELECT DISTINCT grp FROM permission.grp_perm_map
+            INNER JOIN perms_to_add ON perm=perms_to_add.id)
+            
+        --- Anybody who can view resources should also see reservations
+        --- at the same level
+        AND perm = (
+            SELECT id
+                FROM permission.perm_list
+                WHERE code = 'VIEW_BOOKING_RESOURCE'
+        );
+
+
+COMMIT;
