d57d75242f4d16be1fc5394d72826b0465ed429d
Galen Charlton <gmc@equinoxinitiative.org>
2021-03-10 16:26:56 -0500

    LP#1917826: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1251', :eg_version); -- khuckins/Dyrcona/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1252', :eg_version); -- sandbergja/rhamby/gmcharlt
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1252.data.all_video_search_format.sql
+++ Open-ILS/src/sql/Pg/upgrade/1252.data.all_video_search_format.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1252', :eg_version);
 
 INSERT INTO config.coded_value_map
     (id, ctype, code, opac_visible, value, search_label)
