e98a3759a0578fa58ce27b5dfc289764b8d8e29b
Galen Charlton <gmc@equinoxinitiative.org>
2021-03-11 13:45:55 -0500

    LP#1815815: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/sql/Pg/upgrade/1254.schema.lassos.sql
+++ Open-ILS/src/sql/Pg/upgrade/1254.schema.lassos.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1254', :eg_version);
 
 -- XXX Committer: confirm ID below is the next available!
 INSERT INTO permission.perm_list (id, code, description)
