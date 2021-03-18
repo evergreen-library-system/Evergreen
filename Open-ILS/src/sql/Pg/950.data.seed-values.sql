e2db1fb80955e1ec8b8d8b5bc9da96a7b3f52193
Shula Link <slink@gchrl.org>
2021-03-18 10:27:02 -0400

    Lp 1826759: Spelling correction: oustanding to outstanding
    Correct the spelling of "oustanding" to "outstanding" in the
    descriptions of 3 config.org_unit_setting_type entries:
    
      * ui.circ.items_out.longoverdue
      * ui.circ.items_out.lost
      * ui.circ.items_out.claimsreturned
    
    Signed-off-by: Shula Link <slink@gchrl.org>
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -17002,7 +17002,7 @@ VALUES (
 'or "Other/Special Circulations") the circulation '||
 'should appear while checked out, and B. Whether the circulation should '||
 'continue to appear in the "Other" tab when checked in with '||
-'oustanding fines.  '||
+'outstanding fines.  '||
 '1 = (A) "Items", (B) "Other".  2 = (A) "Other", (B) "Other".  ' ||
 '5 = (A) "Items", (B) do not display.  6 = (A) "Other", (B) do not display.',
         'coust',
@@ -17022,7 +17022,7 @@ VALUES (
 'or "Other/Special Circulations") the circulation '||
 'should appear while checked out, and B. Whether the circulation should '||
 'continue to appear in the "Other" tab when checked in with '||
-'oustanding fines.  '||
+'outstanding fines.  '||
 '1 = (A) "Items", (B) "Other".  2 = (A) "Other", (B) "Other".  ' ||
 '5 = (A) "Items", (B) do not display.  6 = (A) "Other", (B) do not display.',
         'coust',
@@ -17042,7 +17042,7 @@ VALUES (
 'or "Other/Special Circulations") the circulation '||
 'should appear while checked out, and B. Whether the circulation should '||
 'continue to appear in the "Other" tab when checked in with '||
-'oustanding fines.  '||
+'outstanding fines.  '||
 '1 = (A) "Items", (B) "Other".  2 = (A) "Other", (B) "Other".  ' ||
 '5 = (A) "Items", (B) do not display.  6 = (A) "Other", (B) do not display.',
         'coust',
