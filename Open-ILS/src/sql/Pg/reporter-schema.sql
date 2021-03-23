4cce82764668745c49b18f08f6210e1d75793d26
Chris Sharp <csharp@georgialibraries.org>
2021-03-23 19:03:32 -0400

    LP#1921057 - Expand reporter.demographic to include detailed age breakdown
    Add "age_division" column to reporter.demographic.  Expose the new
    column to the reporter as "Detailed Age Division" resulting in the following
    options:
    
    Child 0-5 Years Old
    Child 6-12 Years Old
    Teen 13-17 Years Old
    Adult 18-25 Years Old
    Adult 50-59 Years Old
    Adult 50-59 Years Old
    Adult 60-69 Years Old
    Adult 70+
    
    If no DOB is present, say so.
    
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Ruth Frasur <rfrasur@library.in.gov>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/reporter-schema.sql
+++ Open-ILS/src/sql/Pg/reporter-schema.sql
@@ -257,16 +257,37 @@ CREATE OR REPLACE VIEW reporter.asset_call_number_dewey AS
   WHERE call_number_dewey(label) ~ '^[0-9]'::text;
 
 CREATE OR REPLACE VIEW reporter.demographic AS
-SELECT	u.id,
-	u.dob,
-	CASE
-		WHEN u.dob IS NULL
-			THEN 'Adult'
-		WHEN AGE(u.dob) > '18 years'::INTERVAL
-			THEN 'Adult'
-		ELSE 'Juvenile'
-	END AS general_division
-  FROM	actor.usr u;
+SELECT  u.id,
+    u.dob,
+    CASE
+        WHEN u.dob IS NULL
+            THEN 'Adult'
+        WHEN AGE(u.dob) > '18 years'::INTERVAL
+            THEN 'Adult'
+        ELSE 'Juvenile'
+    END AS general_division,
+    CASE
+        WHEN u.dob IS NULL
+            THEN 'No Date of Birth Entered'::text
+        WHEN age(u.dob::timestamp with time zone) >= '0 years'::interval and age(u.dob::timestamp with time zone) < '6 years'::interval
+            THEN 'Child 0-5 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '6 years'::interval and age(u.dob::timestamp with time zone) < '13 years'::interval
+            THEN 'Child 6-12 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '13 years'::interval and age(u.dob::timestamp with time zone) < '18 years'::interval
+            THEN 'Teen 13-17 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '18 years'::interval and age(u.dob::timestamp with time zone) < '26 years'::interval
+            THEN 'Adult 18-25 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '26 years'::interval and age(u.dob::timestamp with time zone) < '50 years'::interval
+            THEN 'Adult 26-49 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '50 years'::interval and age(u.dob::timestamp with time zone) < '60 years'::interval
+            THEN 'Adult 50-59 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '60 years'::interval and age(u.dob::timestamp with time zone) < '70  years'::interval
+            THEN 'Adult 60-69 Years Old'::text
+        WHEN age(u.dob::timestamp with time zone) >= '70 years'::interval
+            THEN 'Adult 70+'::text
+        ELSE NULL::text
+    END AS age_division
+    FROM actor.usr u;
 
 CREATE OR REPLACE VIEW reporter.circ_type AS
 SELECT	id,
