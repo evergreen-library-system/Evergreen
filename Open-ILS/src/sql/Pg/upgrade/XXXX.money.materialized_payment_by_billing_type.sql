9e9786d71b3d149bd819f8c1f53ac0ab41e43ba0
Galen Charlton <gmc@equinoxinitiative.org>
2021-03-30 11:28:33 -0400

    Revert "LP#1174498: stamp schema update"
    This reverts commit 1e60ff5c23cd496e7f3069c71cb407959f9d9e1a.

--- Open-ILS/src/sql/Pg/upgrade/XXXX.money.materialized_payment_by_billing_type.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.money.materialized_payment_by_billing_type.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('1257', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 
 
 CREATE TABLE money.materialized_payment_by_billing_type (
