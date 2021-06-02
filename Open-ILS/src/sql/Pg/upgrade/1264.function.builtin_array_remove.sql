e817dea2e0f5f0e04ee40b17450df909cb322f6c
Galen Charlton <gmc@equinoxOLI.org>
2021-06-02 11:24:00 -0400

    LP#1778955: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1264.function.builtin_array_remove.sql
+++ Open-ILS/src/sql/Pg/upgrade/1264.function.builtin_array_remove.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1264', :eg_version);
 
 CREATE OR REPLACE FUNCTION unapi.bre (
     obj_id BIGINT,
