223ed4091a709ba910202ce665ea47f96916a02b
blake <blake@mobiusconsortium.org>
2021-06-24 15:46:38 -0500

    Translation updates - po files
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/eg2/karma.conf.js
+++ Open-ILS/src/eg2/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless','FirefoxHeadless'],
+    browsers: ['FirefoxHeadless'],
     customLaunchers: {
         'FirefoxHeadless': {
             base: 'Firefox',
