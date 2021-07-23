ed7bdb048484567d85dc63754fc7ae2ed9929cfe
Jane Sandberg <sandbej@linnbenton.edu>
2021-07-23 10:15:13 -0700

    LP1906058: Course-term map interface only allow reasonable mappings
    Different libraries will use different sets of course terms.  In an
    academic library context, for example, a consortium may include
    libraries with very different start and end dates to their terms/semesters,
    different numbers of terms/year, etc.
    
    This commit changes the interface to only allow users to associate
    a course with a term from the same library or one of its ancestors.
    
    To test:
    1. Create many courses and course terms and many different OUs.
    2. On the course list, click "Terms taught".  Associate some courses
    and course terms.  Make sure that you aren't able to associate your
    course with any course terms that would not be reasonable for the
    course's library.
    3. Edit a course, and choose the Course terms tab.  Continue to associate
    courses and terms, and make sure the mappings are reasonable.
    
    Signed-off-by: Jane Sandberg <sandbej@linnbenton.edu>
    Signed-off-by: Beth Willis <willis@noblenet.org>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/eg2/src/app/staff/admin/local/course-reserves/course-term-map-grid.component.ts
@@ -0,0 +1,98 @@
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
+import {PcrudService} from '@eg/core/pcrud.service';
+import {Pager} from '@eg/share/util/pager';
+import {GridDataSource} from '@eg/share/grid/grid';
+import {GridComponent} from '@eg/share/grid/grid.component';
+import {ComboboxEntry} from '@eg/share/combobox/combobox.component';
+import {IdlObject, IdlService} from '@eg/core/idl.service';
+import {OrgService} from '@eg/core/org.service';
+import {FmRecordEditorComponent} from '@eg/share/fm-editor/fm-editor.component';
+
+@Component({
+    selector: 'eg-course-term-map-grid',
+    templateUrl: './course-term-map-grid.component.html',
+})
+export class CourseTermMapGridComponent implements OnInit {
+    @Input() courseId: number;
+    @ViewChild('grid') private grid: GridComponent;
+    @ViewChild('editDialog') private editDialog: FmRecordEditorComponent;
+
+    readonlyFields: string;
+
+    defaultNewAcmtcm: IdlObject;
+    gridDataSource: GridDataSource;
+    createNew: () => void;
+    deleteSelected: (rows: IdlObject[]) => void;
+    termEntryGenerator: (course: number) => (query: string) => Observable<ComboboxEntry>;
+    termEntries: (query: string) => Observable<ComboboxEntry>;
+
+    constructor(
+        private idl: IdlService,
+        private org: OrgService,
+        private pcrud: PcrudService,
+    ) {
+        this.gridDataSource = new GridDataSource();
+        this.defaultNewAcmtcm = this.idl.create('acmtcm');
+    }
+
+    ngOnInit() {
+
+        if (this.courseId) {
+            this.defaultNewAcmtcm.course(this.courseId);
+            this.readonlyFields = 'course';
+        }
+
+        this.gridDataSource.getRows = (pager: Pager, sort: any[]) => {
+            const orderBy: any = {};
+
+            const searchOps = {
+                offset: pager.offset,
+                limit: pager.limit,
+                order_by: orderBy
+            };
+
+            const criteria = this.courseId ? {course: this.courseId} : {};
+
+            return this.pcrud.search('acmtcm',
+                criteria, searchOps, {fleshSelectors: true});
+        };
+
+        // Produce a bespoke callback for the combobox search, which
+        // limits the results to course terms that make sense for the
+        // selected course.  This prevents users from associating a
+        // course at their library to a term from a completely different
+        // academic calendar.
+        this.termEntryGenerator = (courseId: number) => {
+            return (query: string) => {
+                return this.pcrud.retrieve('acmc', courseId).pipe(switchMap(fullCourseObject => {
+                    return this.pcrud.search(
+                        'acmt', {
+                            name: {'ilike': `%${query}`},
+                            owning_lib: this.org.ancestors(fullCourseObject.owning_lib(), true)
+                        },
+                        {order_by: {'acmt': 'name'}}
+                       );
+                }), map(courseTerm => {
+                    return {id: courseTerm.id(), label: courseTerm.name()};
+                }));
+            };
+        };
+
+        this.createNew = () => {
+            this.editDialog.mode = 'create';
+            this.editDialog.open({size: 'lg'})
+                .subscribe(() => this.grid.reload());
+        };
+
+        this.deleteSelected = (termMaps: IdlObject[]) => {
+            termMaps.forEach(termMap => termMap.isdeleted(true));
+            this.pcrud.autoApply(termMaps).subscribe(
+                val => console.debug('deleted: ' + val),
+                err => {},
+                ()  => this.grid.reload()
+            );
+        };
+    }
+}
