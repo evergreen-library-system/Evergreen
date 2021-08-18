128f4d5664979881d5a64bb3f6b213f0c176a002
Tiffany Little <tlittle@georgialibraries.org>
2021-08-18 08:18:31 -0400

    LP#1737019 Add an active flag to funding sources
    This adds an active flag to funding sources. It also adds limits the
    available funding sources in the Create Allocation dropdown to only
    active funding sources.
    
    Signed-off-by: Tiffany Little <tlittle@georgialibraries.org>
    Signed-off-by: Lindsay Stratton <lstratton@wlsmail.org>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/200.schema.acq.sql
+++ Open-ILS/src/sql/Pg/200.schema.acq.sql
@@ -156,6 +156,7 @@ CREATE TABLE acq.funding_source (
 	owner		INT	NOT NULL REFERENCES actor.org_unit (id) DEFERRABLE INITIALLY DEFERRED,
 	currency_type	TEXT	NOT NULL REFERENCES acq.currency_type (code) DEFERRABLE INITIALLY DEFERRED,
 	code		TEXT	NOT NULL,
+	active		BOOL	NOT NULL DEFAULT TRUE,
 	CONSTRAINT funding_source_code_once_per_owner UNIQUE (code,owner),
 	CONSTRAINT funding_source_name_once_per_owner UNIQUE (name,owner)
 );
---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.acq-active-flag.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('TODO', :eg_version);
+
+-- Add an active flag column
+
+ALTER TABLE acq.funding_source ADD COLUMN active BOOL;
+
+UPDATE acq.funding_source SET active = 't';
+
+ALTER TABLE acq.funding_source ALTER COLUMN active SET NOT NULL DEFAULT TRUE;
+
+COMMIT;
