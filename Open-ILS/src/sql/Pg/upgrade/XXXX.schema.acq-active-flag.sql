1f25f132a0ebb50127588dab3f5d4ec2bf98d3b2
Galen Charlton <gmc@equinoxOLI.org>
2021-09-21 10:10:38 -0400

    LP#1737019: (follow-up) fix issue in schema update
    Looks like 'SET DEFAULT ...' and 'SET NOT NULL' cannot be
    combined in a single ALTER TABLE statement.
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.schema.acq-active-flag.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.acq-active-flag.sql
@@ -8,6 +8,7 @@ ALTER TABLE acq.funding_source ADD COLUMN active BOOL;
 
 UPDATE acq.funding_source SET active = 't';
 
-ALTER TABLE acq.funding_source ALTER COLUMN active SET NOT NULL DEFAULT TRUE;
+ALTER TABLE acq.funding_source ALTER COLUMN active SET DEFAULT TRUE;
+ALTER TABLE acq.funding_source ALTER COLUMN active SET NOT NULL;
 
 COMMIT;
