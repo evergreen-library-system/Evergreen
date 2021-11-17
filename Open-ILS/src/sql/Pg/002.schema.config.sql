654b02f1e9c1ee6f14a5d43233ef675b5684f1be
Jane Sandberg <sandbergja@gmail.com>
2021-11-17 11:53:52 -0800

    lp1855780 stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1307', :eg_version); -- jeffdavis/JBoyer/gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1308', :eg_version); -- mrisher/rhamby/gmcharlt/sandbergja
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
