4d667aa09ff61496c9f60a0cfae34c13566a5a61
Lynn Floyd <alynn2671@gmail.com>
2021-12-30 11:38:07 -0500

    LP1956003 Hold Group Workstation Settings to Server Settings.
    add Group Holds to grid settings in Seeded Values.
    
    Signed-off-by: Lynn Floyd <alynn2671@gmail.com>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -22049,3 +22049,34 @@ VALUES (
         'cwst', 'label'
     )
 );
+
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.cat.bucket.batch_hold.view', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.view',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.view',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.cat.bucket.batch_hold.pending', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.pending',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.pending',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.cat.bucket.batch_hold.events', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.events',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.events',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.cat.bucket.batch_hold.list', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.list',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.list',
+        'cwst', 'label'
+    )
+);
---
+++ Open-ILS/src/sql/Pg/upgrade/xxxx.data.hold_group_ws_settings.sql
@@ -0,0 +1,36 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version); -- alynn26
+
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.cat.bucket.batch_hold.view', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.view',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.view',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.cat.bucket.batch_hold.pending', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.pending',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.pending',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.cat.bucket.batch_hold.events', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.events',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.events',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.cat.bucket.batch_hold.list', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.cat.bucket.batch_hold.list',
+        'Grid Config: eg.grid.cat.bucket.batch_hold.list',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
