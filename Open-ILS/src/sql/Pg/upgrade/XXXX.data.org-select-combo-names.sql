b45e311ff395678b2a84a789da2d3604bd0ef221
Bill Erickson <berickxx@gmail.com>
2022-01-03 12:54:43 -0500

    LP1771636 Org Select Combined Org Names Option
    Adds an option to the staff client workstation settings page for
    "Library Selector Shows Combined Names?".  When enabled, Angular org
    unit selectors display the full branch in addition to the branch short
    code.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: John Amundson <jamundson@cwmars.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.org-select-combo-names.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('TODO', :eg_version);
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.orgselect.show_combined_names', 'gui', 'bool',
+    oils_i18n_gettext(
+        'eg.orgselect.show_combined_names',
+        'Library Selector Show Combined Names',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
