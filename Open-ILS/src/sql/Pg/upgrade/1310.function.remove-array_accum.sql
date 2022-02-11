9582ea41ef57dbc6068a5c9eb6a4c9400f8c3c89
Jane Sandberg <sandbergja@gmail.com>
2022-02-10 19:30:06 -0800

    LP1947595: stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1310.function.remove-array_accum.sql
+++ Open-ILS/src/sql/Pg/upgrade/1310.function.remove-array_accum.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1310', :eg_version);
 
 DROP AGGREGATE IF EXISTS array_accum(anyelement) CASCADE;
 
