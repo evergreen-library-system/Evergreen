3863ed4177da51362969ca4c99d421a2afdf135c
Mike Rylander <mrylander@gmail.com>
2022-03-25 17:03:06 -0400

    Stamping upgrade script for JQuery YAOUS
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1321', :eg_version); -- Dyrcona/miker
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1322', :eg_version); -- rhamby/miker
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
