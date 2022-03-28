1a07bfb3bdfca6a10db36e2c92ae7c4ea7af21c3
Jane Sandberg <sandbergja@gmail.com>
2022-03-27 20:09:54 -0700

    Stamping upgrade script for OAI-PMH

--- Open-ILS/src/sql/Pg/upgrade/1323.schema.oai.sql
+++ Open-ILS/src/sql/Pg/upgrade/1323.schema.oai.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1323', :eg_version);
 
 -- VIEWS for the oai service
 CREATE SCHEMA oai;
