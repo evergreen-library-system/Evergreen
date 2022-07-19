5edb9d1bb36f6da61ab39bafdc31e8221acd2194
Bill Erickson <berickxx@gmail.com>
2022-07-19 10:19:29 -0400

    LP1910546 Prevent Copy Location Delete With Active Copies
    Raise a database exception when any attempt is made to marke a copy
    location as deleted when the location contains non-deleted copies.
    
    To Test:
    
    1. Navigate to /eg2/staff/admin/local/asset/copy_location
    
    2. Attempt to delete a copy location that is known to contain
       non-deleted copies.
    
    3. Confirm an error message is displayed and the location is not marked
       as deleted.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Tiffany Little <tlittle@georgialibraries.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.reject-active-acpl-delete.sql
@@ -0,0 +1,30 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('TODO', :eg_version);
+
+CREATE OR REPLACE FUNCTION asset.check_delete_copy_location(acpl_id INTEGER)
+    RETURNS VOID AS $FUNK$
+BEGIN
+    PERFORM TRUE FROM asset.copy WHERE location = acpl_id AND NOT deleted LIMIT 1;
+
+    IF FOUND THEN
+        RAISE EXCEPTION
+            'Copy location % contains active copies and cannot be deleted', acpl_id;
+    END IF;
+END;
+$FUNK$ LANGUAGE plpgsql;
+
+DROP RULE protect_copy_location_delete ON asset.copy_location;
+
+CREATE RULE protect_copy_location_delete AS
+    ON DELETE TO asset.copy_location DO INSTEAD (
+        SELECT asset.check_delete_copy_location(OLD.id);
+        UPDATE asset.copy_location SET deleted = TRUE WHERE OLD.id = asset.copy_location.id;
+        UPDATE acq.lineitem_detail SET location = NULL WHERE location = OLD.id;
+        DELETE FROM asset.copy_location_order WHERE location = OLD.id;
+        DELETE FROM asset.copy_location_group_map WHERE location = OLD.id;
+        DELETE FROM config.circ_limit_set_copy_loc_map WHERE copy_loc = OLD.id;
+    );
+
+COMMIT;
+
