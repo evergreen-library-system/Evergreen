56709d6d7c8de4d2f6c0659c464b79c7da1e29f1
Jason Boyer <JBoyer@equinoxOLI.org>
2022-07-27 18:07:59 -0400

    LP1982887: Repair translations for eg2 client
    Previously following the build instructions for the Czech localization
    placed the results in eg2/cs-CZ/cs-CZ and also broke the en-US build.
    These changes bring the staff client a little more in line with current
    Angular localization methods.
    
    Note that currently both en-US and cs-CZ are always built. This can be
    limited to en-US by using "--localize false" on the ng build command line.
    Until an alternative method to determine the number of available locales is
    implemented the only way to hide the locale selector is to edit environment.prod.ts.
    
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>
    Signed-off-by: Linda Jansova <skolkova@chello.cz>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/eg2/src/environments/environment.prod.ts
+++ Open-ILS/src/eg2/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // TODO: a way to pass these in at build time.
   // locales: ['en-US', 'fr-CA', 'cs-CZ']
-  locales: ['en-US']
+  locales: ['en-US', 'cs-CZ']
 };
