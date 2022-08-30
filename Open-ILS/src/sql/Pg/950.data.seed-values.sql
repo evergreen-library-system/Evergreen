48ff38d435b078dcf7d16c51d7932e2deb540199
Galen Charlton <gmc@equinoxOLI.org>
2022-08-30 12:06:55 -0400

    LP#1942220: (follow-up) fix issue with seed data
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>
    Signed-off-by: Ruth Frasur <rfrasur@library.in.gov>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -21978,12 +21978,12 @@ VALUES (
     )
 );
 
-INSERT INTO config.org_unit_setting_type (name, grp, datatype, label)
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
 VALUES (
-    'ui.staff.acq.show_deprecated_links', 'gui', 'bool',
+    'acq.lineitem.sort_order', 'gui', 'integer',
     oils_i18n_gettext(
-        'ui.staff.acq.show_deprecated_links',
-        'Display Links to Deprecated Acquisitions Interfaces',
+        'acq.lineitem.sort_order',
+        'ACQ Lineitem List Sort Order',
         'cwst', 'label'
     )
 );
