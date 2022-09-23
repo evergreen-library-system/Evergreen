1b4c040cd75941b6b47c6b1247c2d8fc7df02e10
Jane Sandberg <sandbergja@gmail.com>
2022-09-22 21:16:55 -0700

    LP1842297: stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1329.schema.openathens_identity.sql
+++ Open-ILS/src/sql/Pg/upgrade/1329.schema.openathens_identity.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1329', :eg_version);
 
 CREATE TABLE config.openathens_uid_field (
     id      SERIAL  PRIMARY KEY,
