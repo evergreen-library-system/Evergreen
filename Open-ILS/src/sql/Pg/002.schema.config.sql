58bd3eb1af4e89764315ccfb769e8f1d7d137faf
Michele Morgan <mmorgan@noblenet.org>
2022-10-13 14:00:09 -0400

    LP1821950, LP198049 - Stamping upgrade script
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1336', :eg_version); -- khuckins/berick/mrylander/tmccanna/sandbergja
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1337', :eg_version); -- khuckins/tmccanna/cmorgan/willis/mmorgan
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
