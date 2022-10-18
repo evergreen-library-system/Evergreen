2d4bbf394ba44c057210c7fa1759eea88116cb2b
Michele Morgan <mmorgan@noblenet.org>
2022-10-18 17:05:29 -0400

    LP1913604 - Stamping upgrade script
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

--- Open-ILS/src/sql/Pg/upgrade/1339.schema.course_original_circ_lib.sql
+++ Open-ILS/src/sql/Pg/upgrade/1339.schema.course_original_circ_lib.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1339', :eg_version);
 
 ALTER TABLE asset.course_module_course_materials
     ADD COLUMN original_circ_lib INT REFERENCES actor.org_unit (id);
