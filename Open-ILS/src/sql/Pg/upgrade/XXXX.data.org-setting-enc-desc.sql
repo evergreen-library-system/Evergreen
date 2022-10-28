47d7294f6582dddfaa2048b770cd53908a8b08c5
Galen Charlton <gmc@equinoxOLI.org>
2022-10-27 21:15:13 -0400

    LP#1982031: (follow-up) corrections to DB update
    - fix syntax error
    - update the label and description only on exact match
      with the legacy value; this prevents overwritting any
      local tweaking of the library setting
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.data.org-setting-enc-desc.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.org-setting-enc-desc.sql
@@ -1,8 +1,10 @@
 BEGIN;
 
 UPDATE config.org_unit_setting_type
-    SET label = 'Rollover encumbrances only'
-    SET description = 'Rollover encumbrances only when doing fiscal year end.  This makes money left in the old fund disappear, modeling its return to some outside entity.'
-    WHERE name= 'acq.fund.allow_rollover_without_money';
+    SET label = 'Rollover encumbrances only',
+        description = 'Rollover encumbrances only when doing fiscal year end.  This makes money left in the old fund disappear, modeling its return to some outside entity.'
+    WHERE name = 'acq.fund.allow_rollover_without_money'
+    AND label = 'Allow funds to be rolled over without bringing the money along'
+    AND description = 'Allow funds to be rolled over without bringing the money along.  This makes money left in the old fund disappear, modeling its return to some outside entity.';
 
 COMMIT;
