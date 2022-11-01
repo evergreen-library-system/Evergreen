b4e52b4d24e9b47a771fd1845e40d854b52bb631
Galen Charlton <gmc@equinoxOLI.org>
2022-11-01 15:42:29 -0400

    LP#1982887: (follow-up) set l10n list back to en-US
    This works around an issue where the Angular staff client's
    navbar langauge selector selects all provided locales, but
    that can break things without additional configuration.
    
    To enable the Czech translation, edit
    
    Open-ILS/src/eg2/src/environments/environment.prod.ts
    
    to add 'cs-CZ' to the list of locales, then rebuild the
    Angular app using
    
    ng build --configuration=production
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/eg2/src/environments/environment.prod.ts
+++ Open-ILS/src/eg2/src/environments/environment.prod.ts
@@ -2,5 +2,10 @@ export const environment = {
   production: true,
   // TODO: a way to pass these in at build time.
   // locales: ['en-US', 'fr-CA', 'cs-CZ']
-  locales: ['en-US', 'cs-CZ']
+  // FIXME: The language selector in the Angular staff
+  //        client's navbar goes strictly by the list
+  //        here, but that's not ideal: the list of localizations
+  //        shipped with Evergreen (and maybe pre-built) aren't
+  //        necessarily the list you want to turn on.
+  locales: ['en-US']
 };
