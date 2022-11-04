2e1ae9e2e72373af1b917709b12731461ce965eb
Jane Sandberg <js7389@princeton.edu>
2022-11-03 20:00:11 -0700

    lp1856097 stamp and small fixes to upgrade script
    Don't apply the upgrade if it would overwrite existing customizations
    to the description, also add a missing semicolon.
    
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/upgrade/1353.data.default_classification-description-update.sql
+++ Open-ILS/src/sql/Pg/upgrade/1353.data.default_classification-description-update.sql
@@ -1,10 +1,12 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1353', :eg_version);
 
 UPDATE config.org_unit_setting_type SET description = oils_i18n_gettext('cat.default_classification_scheme',
         'Defines the default classification scheme for new call numbers.',
-         'coust', 'description')
+        'coust', 'description')
     WHERE name = 'cat.default_classification_scheme'
+    AND description =
+        'Defines the default classification scheme for new call numbers: 1 = Generic; 2 = Dewey; 3 = LC';
 
 COMMIT;
