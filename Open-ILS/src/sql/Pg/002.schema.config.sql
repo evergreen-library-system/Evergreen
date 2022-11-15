d7282ae6b0be91df26305468d1d77ba79efef42d
Galen Charlton <gmc@equinoxOLI.org>
2022-11-15 18:48:53 -0500

    update version stamps and ChangeLog
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,7 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1353', :eg_version); -- khuckins/smorrison/sandbergja
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.10-rc', :eg_version);
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.10.0', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
