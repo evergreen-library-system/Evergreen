31eee80a8c2521d8efe2253e3985265d2b31562d
Mike Rylander <mrylander@gmail.com>
2022-11-22 16:38:03 -0500

    LP#1361782: Add DoS protection
    This commit adds two types of simple DoS protection:
    
     * Limit concurrent search requests per client IP address, regardless of
     the searches being performed.  This helps address issues of accidental
     spamming from a malfunctioning OPAC workstation, or crawlers of various
     types.  The limit is controlled by a global flag called
     "opac.max_concurrent_search.ip".
    
     * Limit the global concurrent search requests for the same query.  This
     helps address both simple and distributed DoS that send the same search
     request over and over.  The limit is controlled by a global flag called
     "opac.max_concurrent_search.query", and defaults to 20.
    
    When the limit is exceeded in either case the client receives an HTTP
    429 "Too many requests" response from the web server, and the connection
    is ended.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.concurrent_search_global_flags.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+INSERT INTO config.global_flag (name, value, enabled, label)
+VALUES (
+    'opac.max_concurrent_search.query',
+    '20',
+    TRUE,
+    oils_i18n_gettext(
+        'opac.max_concurrent_search.query',
+        'Limit the number of global concurrent matching search queries',
+        'cgf', 'label'
+    )
+);
+
+INSERT INTO config.global_flag (name, value, enabled, label)
+VALUES (
+    'opac.max_concurrent_search.ip',
+    '0',
+    TRUE,
+    oils_i18n_gettext(
+        'opac.max_concurrent_search.ip',
+        'Limit the number of global concurrent searches per client IP address',
+        'cgf', 'label'
+    )
+);
+
+COMMIT;
+
