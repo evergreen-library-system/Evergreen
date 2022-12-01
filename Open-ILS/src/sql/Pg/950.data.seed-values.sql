8b611110df540fd86d87d4f160bd5b9f6d44c8d6
Mike Rylander <mrylander@gmail.com>
2022-12-01 14:13:35 -0500

    LP#1908576: Restrict login redirection
    This commit implements a new global flag: opac.login_redirect_domains
    When this flag is enabled, redirection from login via redirect_to will
    be restricted to local URLs.  For local URLs, they must either start
    with a / (provide an absolute path) or the hostname in the URL must
    match the current hostname and have a scheme of http, https, ftp, or
    ftps.
    
    The value for the global flag can be set to a list of comma-separated
    domain names.  Redirection to these domains, and subdomains/hosts
    thereof, will also be allowed.  For all non-local URLs allowed by the
    global flag value, the scheme must be one of http, https, ftp, or ftps.
    
    Signed-off-by: Mike Rylander <mrylander@gmail.com>
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -22218,3 +22218,15 @@ VALUES (
     )
 );
 
+INSERT INTO config.global_flag (name, value, enabled, label)
+VALUES (
+    'opac.login_redirect_domains',
+    '',
+    TRUE,
+    oils_i18n_gettext(
+        'opac.login_redirect_domains',
+        'Restrict post-login redirection to local URLs, or those that match the supplied comma-separated list of foreign domains or host names.',
+        'cgf', 'label'
+    )
+);
+
---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.login_redirect_regexp.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+-- check whether patch can be applied
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+-- 950.data.seed-values.sql
+
+INSERT INTO config.global_flag (name, value, enabled, label)
+VALUES (
+    'opac.login_redirect_domains',
+    '',
+    TRUE,
+    oils_i18n_gettext(
+        'opac.login_redirect_domains',
+        'Restrict post-login redirection to local URLs, or those that match the supplied comma-separated list of foreign domains or host names.',
+        'cgf', 'label'
+    )
+);
+
+COMMIT;
+
