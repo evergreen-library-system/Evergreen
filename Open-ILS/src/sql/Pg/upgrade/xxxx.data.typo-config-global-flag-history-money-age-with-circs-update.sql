9dada41bc8dc10c8c93813c6ac198774c30e37bd
Scott Angel <scottangel@mobiusconsortium.org>
2022-12-06 16:03:55 -0600

    LP1987908: Fixing typo in seed data for history.money.age_with_circs
    Created upgrade sql script to accompany Gina's original commit.
    
    Signed-off-by: Scott Angel <scottangel@mobiusconsortium.org>
    Signed-off-by: Mary Llewellyn <mllewell@biblio.org>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/xxxx.data.typo-config-global-flag-history-money-age-with-circs-update.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+
+UPDATE config.global_flag
+SET label = 'Age billings and payments when circulations are aged.'
+WHERE name = 'history.money.age_with_circs'
+  AND label = 'Age billings and payments when cirulcations are aged.';
+
+COMMIT;
