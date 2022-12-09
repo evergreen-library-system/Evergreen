e5771b849b5bf729501f036ee8e7b6f0f2f1c5e9
Stephanie Leary <stephanie.leary@equinoxOLI.org>
2022-12-09 11:22:13 -0600

    LP1999955 AccessKeys info style updates
    Adds styles to the keyboard shortcuts to make them look more like
    keys. Also adds striping to each row and makes the headers bold.
    
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxOLI.org>
    Signed-off-by: Josh Stompro <stomproj@larl.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/eg2/src/app/share/accesskey/accesskey-info.component.ts
+++ Open-ILS/src/eg2/src/app/share/accesskey/accesskey-info.component.ts
@@ -1,13 +1,15 @@
 /**
  */
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {AccessKeyService} from '@eg/share/accesskey/accesskey.service';
 import {DialogComponent} from '@eg/share/dialog/dialog.component';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'eg-accesskey-info',
-  templateUrl: './accesskey-info.component.html'
+  templateUrl: './accesskey-info.component.html',
+  styleUrls: [ 'accesskey-info.component.css' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class AccessKeyInfoComponent extends DialogComponent {
 
