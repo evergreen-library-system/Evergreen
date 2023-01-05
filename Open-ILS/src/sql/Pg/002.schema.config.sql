1cfbe9c8e6c66f91645cc18fae9f1ea7f5440744
Jason Boyer <JBoyer@equinoxOLI.org>
2023-01-05 16:45:31 -0500

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1341', :eg_version); -- miker/sandbergja/dyrcona/blake
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.8.2', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
