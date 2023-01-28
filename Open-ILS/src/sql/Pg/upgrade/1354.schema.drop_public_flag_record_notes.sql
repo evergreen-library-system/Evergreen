d9c24951f096950a1dc802cf49277b61ba7dd0b0
Galen Charlton <gmc@equinoxOLI.org>
2023-01-28 14:24:26 -0500

    LP#1978978: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1354.schema.drop_public_flag_record_notes.sql
+++ Open-ILS/src/sql/Pg/upgrade/1354.schema.drop_public_flag_record_notes.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1354', :eg_version);
 
 ALTER TABLE biblio.record_note DROP COLUMN pub;
 
