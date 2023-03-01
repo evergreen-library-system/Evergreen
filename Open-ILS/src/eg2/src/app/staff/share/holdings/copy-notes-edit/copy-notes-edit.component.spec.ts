0a7e1c2ca0d4c7c2023964af079b94da7f4c6d04
Jane Sandberg <js7389@princeton.edu>
2023-03-01 04:58:07 -0800

    LP1983628: Add editor for item notes
    Test plan:
    1. Open your favorite bib record in the staff catalog
    2. On the item table tab, find a barcode and click "Edit"
    3. Press the Item Notes button.
    4. Add a note with a title and value.
    5. Press Apply Changes
    6. Press Apply All and Save
    7. Press the Item Notes button again.
    8. Without this commit, you will not have a way to edit
    these notes.  With this commit, you will have an edit
    button.
    9. Confirm that you can Back out of the editor without
    making changes
    10. Confirm that you can make changes and they persist.
    
    This commit also adds a test to confirm that this fm-editor
    won't inadvertently fetch every single row in asset.copy
    (as a linked field).
    
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Elaine Hardy <ehardy@georgialibraries.org>
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>

---
+++ Open-ILS/src/eg2/src/app/staff/share/holdings/copy-notes-edit/copy-notes-edit.component.spec.ts
@@ -0,0 +1,37 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CopyNotesEditComponent } from './copy-notes-edit.component';
+
+describe('CopyNotesEditComponent', () => {
+  let component: CopyNotesEditComponent;
+  let fixture: ComponentFixture<CopyNotesEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CopyNotesEditComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CopyNotesEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  describe('back button', () => {
+    it('emits an event on click', () => {
+      spyOn(component.doneWithEdits, 'emit');
+      const generatedElement: HTMLElement = fixture.nativeElement;
+      const buttonElement: HTMLButtonElement = generatedElement.querySelector('button');
+      buttonElement.dispatchEvent(new Event('click'));
+      fixture.detectChanges();
+      expect(component.doneWithEdits.emit).toHaveBeenCalled();
+    })
+  })
+});
