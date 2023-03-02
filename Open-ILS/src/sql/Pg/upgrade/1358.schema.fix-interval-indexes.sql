cce04dcc77309d86ee6a241e8487750e0ef43c60
Bill Erickson <berickxx@gmail.com>
2023-03-02 11:46:39 -0500

    LP1997098 Stamping DB Upgrade: PG 15
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1358.schema.fix-interval-indexes.sql
+++ Open-ILS/src/sql/Pg/upgrade/1358.schema.fix-interval-indexes.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1358', :eg_version);
 
 DROP INDEX config.ccmm_once_per_paramset;
 
