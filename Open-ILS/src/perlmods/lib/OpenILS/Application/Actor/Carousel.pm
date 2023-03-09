79080a199753a2240767d5ff8036af2fa18445a4
Jeff Davis <jdavis@sitka.bclibraries.ca>
2023-03-09 12:46:54 -0800

    LP#2009891: add item author to open-ils.actor.carousel.get_contents
    Signed-off-by: Jeff Davis <jdavis@sitka.bclibraries.ca>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Actor/Carousel.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Actor/Carousel.pm
@@ -37,7 +37,7 @@ sub get_carousel_contents {
         name => $carousel->name
     };
     my $q = {
-        select => { bre => ['id'], mfde => [{ column => 'value', alias => 'title' }] },
+        select => { bre => ['id'], mfde1 => [{ column => 'value', alias => 'title' }], mfde2 => [{ column => 'value', alias => 'author' }] },
         from   => {
             bre => {
                 cbrebi => {
@@ -47,13 +47,15 @@ sub get_carousel_contents {
                         }
                     }
                 },
-                mfde => {}
+                mfde1 => { class => 'mfde' },
+                mfde2 => { class => 'mfde' }
             }
         },
         where  => {
             '+cc' => { id => $id },
             '+bre' => { deleted => 'f' },
-            '+mfde' => { name => 'title' }
+            '+mfde1' => { name => 'title' },
+            '+mfde2' => { name => 'author' }
         },
         order_by => {cbrebi => ['pos','create_time']}
     };
