221560179c04dc475533401fa7b4258fd2788e6a
Jason Etheridge <jason@EquinoxOLI.org>
2023-03-12 09:33:27 -0400

    lp1993824: linkchecker; sql
    Signed-off-by: Jason Etheridge <jason@EquinoxOLI.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -23578,3 +23578,95 @@ VALUES (
         'coust', 'description'
     )
 );
+-- XXXX.data.schema.url_verify.sql
+-- Do we need to distribute these changes? (workstation setting types to that one big INSERT,
+-- table changes made without using ALTER, etc.)
+
+ALTER TABLE url_verify.url_selector
+    DROP CONSTRAINT url_selector_session_fkey,
+    ADD CONSTRAINT url_selector_session_fkey 
+        FOREIGN KEY (session) 
+        REFERENCES url_verify.session(id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE url_verify.url
+    DROP CONSTRAINT url_session_fkey,
+    DROP CONSTRAINT url_redirect_from_fkey,
+    ADD CONSTRAINT url_session_fkey 
+        FOREIGN KEY (session) 
+        REFERENCES url_verify.session(id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED,
+    ADD CONSTRAINT url_redirect_from_fkey
+        FOREIGN KEY (redirect_from)
+        REFERENCES url_verify.url(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE url_verify.verification_attempt
+    DROP CONSTRAINT verification_attempt_session_fkey,
+    ADD CONSTRAINT verification_attempt_session_fkey 
+        FOREIGN KEY (session) 
+        REFERENCES url_verify.session(id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE url_verify.url_verification
+    DROP CONSTRAINT url_verification_url_fkey,
+    ADD CONSTRAINT url_verification_url_fkey
+        FOREIGN KEY (url)
+        REFERENCES url_verify.url(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.catalog.link_checker', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.link_checker',
+        'Grid Config: catalog.link_checker',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.catalog.link_checker.attempt', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.link_checker.attempt',
+        'Grid Config: catalog.link_checker.attempt',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.catalog.link_checker.url', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.link_checker.url',
+        'Grid Config: catalog.link_checker.url',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.filters.catalog.link_checker', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.filters.catalog.link_checker',
+        'Grid Filter Sets: catalog.link_checker',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.filters.catalog.link_checker.attempt', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.filters.catalog.link_checker.attempt',
+        'Grid Filter Sets: catalog.link_checker.attempt',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.filters.catalog.link_checker.url', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.filters.catalog.link_checker.url',
+        'Grid Filter Sets: catalog.link_checker.url',
+        'cwst', 'label'
+    )
+);
+
---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.schema.url_verify.sql
@@ -0,0 +1,94 @@
+
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+ALTER TABLE url_verify.url_selector
+    DROP CONSTRAINT url_selector_session_fkey,
+    ADD CONSTRAINT url_selector_session_fkey 
+        FOREIGN KEY (session) 
+        REFERENCES url_verify.session(id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE url_verify.url
+    DROP CONSTRAINT url_session_fkey,
+    DROP CONSTRAINT url_redirect_from_fkey,
+    ADD CONSTRAINT url_session_fkey 
+        FOREIGN KEY (session) 
+        REFERENCES url_verify.session(id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED,
+    ADD CONSTRAINT url_redirect_from_fkey
+        FOREIGN KEY (redirect_from)
+        REFERENCES url_verify.url(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE url_verify.verification_attempt
+    DROP CONSTRAINT verification_attempt_session_fkey,
+    ADD CONSTRAINT verification_attempt_session_fkey 
+        FOREIGN KEY (session) 
+        REFERENCES url_verify.session(id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE url_verify.url_verification
+    DROP CONSTRAINT url_verification_url_fkey,
+    ADD CONSTRAINT url_verification_url_fkey
+        FOREIGN KEY (url)
+        REFERENCES url_verify.url(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED;
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.catalog.link_checker', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.link_checker',
+        'Grid Config: catalog.link_checker',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.catalog.link_checker.attempt', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.link_checker.attempt',
+        'Grid Config: catalog.link_checker.attempt',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.catalog.link_checker.url', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.link_checker.url',
+        'Grid Config: catalog.link_checker.url',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.filters.catalog.link_checker', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.filters.catalog.link_checker',
+        'Grid Filter Sets: catalog.link_checker',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.filters.catalog.link_checker.attempt', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.filters.catalog.link_checker.attempt',
+        'Grid Filter Sets: catalog.link_checker.attempt',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.filters.catalog.link_checker.url', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.filters.catalog.link_checker.url',
+        'Grid Filter Sets: catalog.link_checker.url',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
