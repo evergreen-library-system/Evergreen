9e98ef97bfa5c1d0c4d01a00bfd2221adfa27f77
Jeff Davis <jdavis@sitka.bclibraries.ca>
2023-05-02 22:33:43 -0700

    LP#1979345: Stamping upgrade script
    Signed-off-by: Jeff Davis <jdavis@sitka.bclibraries.ca>

--- Open-ILS/src/sql/Pg/upgrade/1372.data.permission.hold_pull_list.sql
+++ Open-ILS/src/sql/Pg/upgrade/1372.data.permission.hold_pull_list.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1372', :eg_version);
 
 INSERT INTO permission.perm_list (id, code, description) VALUES
  ( 643, 'VIEW_HOLD_PULL_LIST', oils_i18n_gettext(643,
