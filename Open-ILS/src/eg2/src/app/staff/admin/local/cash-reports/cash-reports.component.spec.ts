16cd7e1e495dbe761a72669eb9ef13e8caaea0c2
Jane Sandberg <js7389@princeton.edu>
2023-05-14 00:58:17 -0700

    LP2002343: cash reports end date cannot come before start date
    To test:
    1. Log in as a staff member with a branch-level home library (global admin account will work only if you change it from CONS to, say, BR1)
    2. As that staff member, bill some patrons and then accept some payments from them
    3. Go to admin > local admin > cash reports
    4. Try to set the end date before the start date.
    5. Note that you get an alert, and cannot press the Submit button.
    6. Confirm that when you enter valid dates and select your staff member's home ou, the reports display as usual.
    
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ Open-ILS/src/eg2/src/app/staff/admin/local/cash-reports/cash-reports.component.spec.ts
@@ -0,0 +1,60 @@
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from '@eg/core/auth.service';
+import { IdlService } from '@eg/core/idl.service';
+import { NetService } from '@eg/core/net.service';
+import { OrgService } from '@eg/core/org.service';
+import { CashReportsComponent } from './cash-reports.component';
+import { DateSelectComponent } from '@eg/share/date-select/date-select.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgbDatepickerModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { DatesInOrderValidatorDirective } from '@eg/share/validators/dates_in_order_validator.directive';
+
+const mockIdlObject = {a: null,
+    classname: null,
+    _isfieldmapper: null,
+    id: () => {null},
+    ws_ou: () => {null}};
+const mockNet = jasmine.createSpyObj<NetService>(['request']);
+mockNet.request.and.returnValue(of());
+const mockOrg = jasmine.createSpyObj<OrgService>(['get', 'filterList']);
+const mockAuth = jasmine.createSpyObj<AuthService>(['user', 'token']);
+mockAuth.user.and.returnValue(mockIdlObject);
+
+describe('CashReportsComponent', () => {
+  it('alerts the user if end date is before start date', async () => {
+    await TestBed.configureTestingModule({
+        declarations: [
+            CashReportsComponent,
+            DateSelectComponent,
+            DatesInOrderValidatorDirective
+        ],
+        providers: [
+            {provide: IdlService, useValue: {}},
+            {provide: NetService, useValue: mockNet},
+            {provide: OrgService, useValue: mockOrg},
+            {provide: AuthService, useValue: mockAuth}
+        ],
+        imports: [
+            NgbNavModule,
+            NgbDatepickerModule,
+            FormsModule
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(CashReportsComponent);
+    const component = fixture.componentInstance;
+    const element = fixture.nativeElement;
+    component.selectedOrg = mockIdlObject;
+    fixture.detectChanges();
+    
+    element.querySelector('#start-date').value = '2022-01-01';
+    element.querySelector('#end-date').value = '2021-01-01';
+    component.criteria.form.setErrors({datesOutOfOrder: true});
+    component.criteria.form.markAsDirty();
+    fixture.detectChanges();
+    expect(element.querySelector('#dateOutOfOrderAlert').innerText).toContain('Start date must be before end date');
+  });
+});
