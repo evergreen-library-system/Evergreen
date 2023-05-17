202374d8ef70a34fbea1f1e63a66fb464b503fe1
Jason Stephesnon <jason@sigio.com>
2023-05-17 18:29:22 -0400

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: Jason Stephesnon <jason@sigio.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1378', :eg_version); -- miker / gmcharlt / JBoyer
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.10.2', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
