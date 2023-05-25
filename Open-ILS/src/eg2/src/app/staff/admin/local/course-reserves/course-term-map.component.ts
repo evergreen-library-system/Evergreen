1f2530636e3a428a222ec2ca6bcb89fed9a6180b
Stephanie Leary <stephanie.leary@equinoxoli.org>
2023-05-25 18:24:09 +0000

    LP1615714 Accessible names for Material Icons
    Adds ARIA labels or hidden text, as appropriate, to ensure that Material
    Icons are announced to screen reader users in a sensible way rather than
    having the icon keyword read as if it were normal text.
    
    In buttons containing an icon with no text, both ARIA labels and title
    attributes are used so that the tooltip matches the button's accessible
    name. This helps sighted users of dictation software identify the
    button's name. The icon keyword is hidden from screen readers with the
    aria-hidden attribute.
    
    In buttons with both an icon and text, the icon keyword is hidden from
    screen readers.
    
    Where icons appeared in inline text rather than a button or link, a
    visually-hidden span has been added to mirror the existing title
    attribute (tooltip), unless the icon was purely decorative.
    
    As part of this cleanup, several <a click()> elements were identified
    that had not already been addressed in other bug reports related to
    keyboard navigation support. These have been changed to <button>, and
    button types have been specified throughout. Other small role and alt
    text errors identified by the Axe linter have also been corrected.
    
    Note that this branch does not include labels for  the grid flair icons
    (bug 1818086) or the grid actions toolbar (bug 1833726), which are
    being addressed separately.
    
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Garry Collum <gcollum@gmail.com>
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>

--- Open-ILS/src/eg2/src/app/staff/admin/local/course-reserves/course-term-map.component.ts
+++ Open-ILS/src/eg2/src/app/staff/admin/local/course-reserves/course-term-map.component.ts
@@ -14,7 +14,7 @@ import {ActivatedRoute} from '@angular/router';
         <div class="row">
             <div class="col text-right">
                 <a class="btn btn-warning ml-3" routerLink="/staff/admin/local/asset/course_list" i18n>
-                    <i class="material-icons align-middle">keyboard_return</i>
+                    <i class="material-icons align-middle" aria-hidden="true">keyboard_return</i>
                     <span class="align-middle">Return to Course List</span>
                 </a>
             </div>
--- Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
+++ Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit, Input} from '@angular/core';
     <div class="lead alert alert-primary text-center pt-1 pb-1"
       [ngClass]="bannerStyle ? bannerStyle : 'alert-primary'">
       <h1>
-        <i class="material-icons align-middle text-left" *ngIf="bannerIcon">{{bannerIcon}}</i>
+        <i class="material-icons align-middle text-left" aria-hidden="true" *ngIf="bannerIcon">{{bannerIcon}}</i>
         <span i18n>{{bannerText}}</span>
       </h1>
     </div>
