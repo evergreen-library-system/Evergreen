b2987a2ffbc7610dde6bc787a44d6fa740401e6e
Galen Charlton <gmc@equinoxOLI.org>
2023-06-06 12:22:34 -0400

    LP#2004058: fix SQL injection flaw in several functions
    This is part of of cleaning up EXECUTEs in stored functions that don't
    do enough quoting.
    
    The functions that were fixed are:
    
    * asset.label_normalizer()
    * evergreen.extract_marc_field()
    
    As part of this patch, the versions of extract_marc_field()
    that were present in the public schema have been removed.
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.lp2004058_fix_sql_injections.sql
@@ -0,0 +1,61 @@
+BEGIN;
+
+CREATE OR REPLACE FUNCTION asset.label_normalizer() RETURNS TRIGGER AS $func$
+DECLARE
+    sortkey        TEXT := '';
+BEGIN
+    sortkey := NEW.label_sortkey;
+
+    IF NEW.label_class IS NULL THEN
+            NEW.label_class := COALESCE(
+            (   
+                SELECT substring(value from E'\\d+')::integer
+                FROM actor.org_unit_ancestor_setting('cat.default_classification_scheme', NEW.owning_lib)
+            ), 1
+        );
+    END IF;
+
+    EXECUTE FORMAT('SELECT %s(%L)', acnc.normalizer::REGPROC, NEW.label)
+       FROM asset.call_number_class acnc
+       WHERE acnc.id = NEW.label_class
+       INTO sortkey;
+    NEW.label_sortkey = sortkey;
+    RETURN NEW;
+END;
+$func$ LANGUAGE PLPGSQL;
+
+DROP FUNCTION IF EXISTS public.extract_marc_field(TEXT, BIGINT, TEXT, TEXT);
+DROP FUNCTION IF EXISTS public.extract_marc_field(TEXT, BIGINT, TEXT);
+
+CREATE OR REPLACE FUNCTION evergreen.extract_marc_field ( TEXT, BIGINT, TEXT, TEXT ) RETURNS TEXT AS $$
+DECLARE
+    query TEXT;
+    output TEXT;
+BEGIN
+    query := FORMAT($q$
+        SELECT  regexp_replace(
+                    oils_xpath_string(
+                        %L,
+                        marc,
+                        ' '
+                    ),
+                    %L,
+                    '',
+                    'g')
+          FROM %s
+          WHERE id = %L
+    $q$, $3, $4, $1::REGCLASS, $2);
+
+    EXECUTE query INTO output;
+
+    -- RAISE NOTICE 'query: %, output; %', query, output;
+
+    RETURN output;
+END;
+$$ LANGUAGE PLPGSQL IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION evergreen.extract_marc_field ( TEXT, BIGINT, TEXT ) RETURNS TEXT AS $$
+    SELECT extract_marc_field($1,$2,$3,'');
+$$ LANGUAGE SQL IMMUTABLE;
+
+COMMIT;
