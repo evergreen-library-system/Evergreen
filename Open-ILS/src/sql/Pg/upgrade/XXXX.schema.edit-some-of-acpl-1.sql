37d18ce9a249df51d072823efdbc5e0cfa0aa6a2
Jason Boyer <JBoyer@equinoxOLI.org>
2023-06-08 16:01:16 -0400

    LP2023314: Allow Some Edits to Copy Location 1, as a Treat
    Prevent deletion of locations with items attached and allow
    edits to the 'Stacks' location, barring owning_lib and
    deletion (regardless of active item count).
    
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>
    Signed-off-by: Susan Morrison <smorrison@georgialibraries.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.edit-some-of-acpl-1.sql
@@ -0,0 +1,47 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('TODO', :eg_version); -- JBoyer /  / 
+
+-- Remove previous acpl 1 protection
+DROP RULE protect_acl_id_1 ON asset.copy_location;
+
+-- Ensure that the owning_lib is set to CONS (equivalent), *should* be a noop.
+UPDATE asset.copy_location SET owning_lib = (SELECT id FROM actor.org_unit_ancestor_at_depth(owning_lib,0)) WHERE id = 1;
+
+CREATE OR REPLACE FUNCTION asset.check_delete_copy_location(acpl_id integer)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    PERFORM TRUE FROM asset.copy WHERE location = acpl_id AND NOT deleted LIMIT 1;
+
+    IF FOUND THEN
+        RAISE EXCEPTION
+            'Copy location % contains active copies and cannot be deleted', acpl_id;
+    END IF;
+    
+    IF acpl_id = 1 THEN
+        RAISE EXCEPTION
+            'Copy location 1 cannot be deleted';
+    END IF;
+END;
+$function$;
+
+CREATE OR REPLACE FUNCTION asset.copy_location_validate_edit()
+  RETURNS trigger
+  LANGUAGE plpgsql
+AS $function$
+BEGIN
+    IF OLD.id = 1 THEN
+        IF OLD.owning_lib != NEW.owning_lib OR NEW.deleted THEN
+            RAISE EXCEPTION 'Copy location 1 cannot be moved or deleted';
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$function$;
+
+CREATE TRIGGER acpl_validate_edit BEFORE UPDATE ON asset.copy_location FOR EACH ROW EXECUTE FUNCTION asset.copy_location_validate_edit();
+
+COMMIT;
+
