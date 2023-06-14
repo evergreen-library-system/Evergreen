9f4c0853db8a0e261f787041dc73549e361e93f9
Galen Charlton <gmc@equinoxOLI.org>
2023-06-14 10:12:43 -0400

    LP#1956510: add grid settings
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Garry Collum <gcollum@gmail.com>
    Signed-off-by: Andrea Buntz Neiman <abneiman@equinoxinitiative.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Christine Morgan <cmorgan@noblenet.org>
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -22047,6 +22047,20 @@ VALUES (
         'Grid Config: eg.grid.acq.funding_source.credit',
         'cwst', 'label'
     )
+), (
+    'eg.grid.admin.acq.fiscal_calendar', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.acq.fiscal_calendar',
+        'Grid Config: eg.grid.admin.acq.fiscal_calendar',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.acq.fiscal_year', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.acq.fiscal_year',
+        'Grid Config: eg.grid.admin.acq.fiscal_year',
+        'cwst', 'label'
+    )
 );
 
 INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.lp1956510_grid_settings.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.acq.fiscal_calendar', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.acq.fiscal_calendar',
+        'Grid Config: eg.grid.admin.acq.fiscal_calendar',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.acq.fiscal_year', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.acq.fiscal_year',
+        'Grid Config: eg.grid.admin.acq.fiscal_year',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
