8654b1041ac036a4d2679c4dfb43a08982687fe3
Steven Mayo <smayo@georgialibraries.org>
2023-10-12 14:42:01 -0400

    LP#1818912: Single Day Emergency Closings Fail to Update Due Dates Correctly
    The original version of evergreen.find_next_open_time() was only
    checking the hours of operation of the organization after it recursed at least
    once - meaning it was just taking the first day it looked at even if it
    closed. The variable making it only check after a recursion had no other
    uses afterwards, so I've removed it as an argument. I also made sure it
    doesn't get stuck in a loop if all the hours of operation of the org it's
    checking are closed, since that was on the mind.
    
    There may still be timezone chicanery left, judging from launchpad
    comments, but hopefully this is at least better. Gunkier databases that
    are copies of your productions may be better for testing.
    
    Steps to test:
    [1] Have some circulations, loans, and expire times that fall on a
    certain day.
    [2] Have some days where your org is closed.
    [3] Add an emergency closure that lasts until the day before your org is
    closed, and process that closure.
    [4] Check that the circs, loans and whatnot are now due the first day
    your org is open again.
    [5] Have some more circs and whatnot due the day before another closure
    (emergency or otherwise)
    [6] Add an emergency closure that ends the day before the other closure
    and process the new closure.
    [7] Check that the circs and whatnots are updated to the correct day.
    
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Andrea Buntz Neiman <abneiman@equinoxOLI.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/096.schema.emergency_closing.sql
+++ Open-ILS/src/sql/Pg/096.schema.emergency_closing.sql
@@ -73,7 +73,7 @@ CREATE OR REPLACE VIEW action.emergency_closing_status AS
             LEFT JOIN (SELECT emergency_closing, count(*) count, SUM((process_time IS NOT NULL)::INT) completed FROM action.emergency_closing_hold GROUP BY 1) h ON (h.emergency_closing = e.id)
 ;
 
-CREATE OR REPLACE FUNCTION evergreen.find_next_open_time ( circ_lib INT, initial TIMESTAMPTZ, hourly BOOL DEFAULT FALSE, initial_time TIME DEFAULT NULL, dow_count INT DEFAULT 0 )
+CREATE OR REPLACE FUNCTION evergreen.find_next_open_time ( circ_lib INT, initial TIMESTAMPTZ, hourly BOOL DEFAULT FALSE, initial_time TIME DEFAULT NULL, has_hoo BOOL DEFAULT TRUE )
     RETURNS TIMESTAMPTZ AS $$
 DECLARE
     day_number      INT;
@@ -86,10 +86,6 @@ DECLARE
     breakout        INT := 0;
 BEGIN
 
-    IF dow_count > 6 THEN
-        RETURN initial;
-    END IF;
-
     IF initial_time IS NULL THEN
         initial_time := initial::TIME;
     END IF;
@@ -100,9 +96,11 @@ BEGIN
 
         time_adjusted := FALSE;
 
-        IF dow_count > 0 THEN -- we're recursing, so check for HOO closing
-            day_number := EXTRACT(ISODOW FROM final_time::TIMESTAMPTZ) - 1;
+        IF has_hoo THEN -- Don't check hours if they have no hoo. I think the behavior in that case is that we act like they're always open? Better than making things due in 2 years.
+                        -- Don't expect anyone to call this with it set to false; it's just for our own recursive use.
+            day_number := EXTRACT(ISODOW FROM final_time::TIMESTAMPTZ) - 1; --Get which day of the week  it is from which it started on.
             plus_days := 0;
+            has_hoo := FALSE; -- set has_hoo to false to check if any days are open (for the first recursion where it's always true)
             FOR i IN 1..7 LOOP
                 EXECUTE 'SELECT dow_' || day_number || '_open, dow_' || day_number || '_close FROM actor.hours_of_operation WHERE id = $1'
                     INTO hoo_open, hoo_close
@@ -115,6 +113,8 @@ BEGIN
                     plus_days := plus_days + 1;
                     time_adjusted := TRUE;
                     CONTINUE;
+                ELSE
+                    has_hoo := TRUE; --We do have hours open sometimes, yay!
                 END IF;
 
                 IF hoo_close IS NULL THEN -- no hours of operation ... assume no closing?
@@ -124,6 +124,10 @@ BEGIN
                 EXIT;
             END LOOP;
 
+            IF NOT has_hoo THEN -- If always closed then forget the extra days - just determine based on closures.
+                plus_days := 0;
+            END IF;
+
             final_time := DATE(final_time::TIMESTAMPTZ + (plus_days || ' days')::INTERVAL)::TEXT;
             IF hoo_close <> '00:00:00' AND hourly THEN -- Not a day-granular circ
                 final_time := final_time||' '|| hoo_close;
@@ -132,13 +136,15 @@ BEGIN
             END IF;
         END IF;
 
+        --RAISE NOTICE 'final_time: %',final_time;
+
         -- Loop through other closings
-        LOOP
+        LOOP 
             SELECT * INTO adjacent FROM actor.org_unit_closed WHERE org_unit = circ_lib AND final_time::TIMESTAMPTZ between close_start AND close_end;
             EXIT WHEN adjacent.id IS NULL;
             time_adjusted := TRUE;
             -- RAISE NOTICE 'recursing for closings with final_time: %',final_time;
-            final_time := evergreen.find_next_open_time(circ_lib, adjacent.close_end::TIMESTAMPTZ, hourly, initial_time, dow_count + 1)::TEXT;
+            final_time := evergreen.find_next_open_time(circ_lib, adjacent.close_end::TIMESTAMPTZ, hourly, initial_time, has_hoo)::TEXT;
         END LOOP;
 
         EXIT WHEN breakout > 100;
---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.function.emergency-closing-due-date-fix.sql
@@ -0,0 +1,87 @@
+BEGIN;
+
+DROP FUNCTION IF EXISTS evergreen.find_next_open_time(INT, TIMESTAMPTZ, BOOL, TIME, INT); --Get rid of the last version of this function with different arguments so it doesn't cause conflicts when calling it
+CREATE OR REPLACE FUNCTION evergreen.find_next_open_time ( circ_lib INT, initial TIMESTAMPTZ, hourly BOOL DEFAULT FALSE, initial_time TIME DEFAULT NULL, has_hoo BOOL DEFAULT TRUE )
+    RETURNS TIMESTAMPTZ AS $$
+DECLARE
+    day_number      INT;
+    plus_days       INT;
+    final_time      TEXT;
+    time_adjusted   BOOL;
+    hoo_open        TIME WITHOUT TIME ZONE;
+    hoo_close       TIME WITHOUT TIME ZONE;
+    adjacent        actor.org_unit_closed%ROWTYPE;
+    breakout        INT := 0;
+BEGIN
+
+    IF initial_time IS NULL THEN
+        initial_time := initial::TIME;
+    END IF;
+
+    final_time := (initial + '1 second'::INTERVAL)::TEXT;
+    LOOP
+        breakout := breakout + 1;
+
+        time_adjusted := FALSE;
+
+        IF has_hoo THEN -- Don't check hours if they have no hoo. I think the behavior in that case is that we act like they're always open? Better than making things due in 2 years.
+                        -- Don't expect anyone to call this with it set to false; it's just for our own recursive use.
+            day_number := EXTRACT(ISODOW FROM final_time::TIMESTAMPTZ) - 1; --Get which day of the week  it is from which it started on.
+            plus_days := 0;
+            has_hoo := FALSE; -- set has_hoo to false to check if any days are open (for the first recursion where it's always true)
+            FOR i IN 1..7 LOOP
+                EXECUTE 'SELECT dow_' || day_number || '_open, dow_' || day_number || '_close FROM actor.hours_of_operation WHERE id = $1'
+                    INTO hoo_open, hoo_close
+                    USING circ_lib;
+
+                -- RAISE NOTICE 'initial time: %; dow: %; close: %',initial_time,day_number,hoo_close;
+
+                IF hoo_close = '00:00:00' THEN -- bah ... I guess we'll check the next day
+                    day_number := (day_number + 1) % 7;
+                    plus_days := plus_days + 1;
+                    time_adjusted := TRUE;
+                    CONTINUE;
+                ELSE
+                    has_hoo := TRUE; --We do have hours open sometimes, yay!
+                END IF;
+
+                IF hoo_close IS NULL THEN -- no hours of operation ... assume no closing?
+                    hoo_close := '23:59:59';
+                END IF;
+
+                EXIT;
+            END LOOP;
+
+            IF NOT has_hoo THEN -- If always closed then forget the extra days - just determine based on closures.
+                plus_days := 0;
+            END IF;
+
+            final_time := DATE(final_time::TIMESTAMPTZ + (plus_days || ' days')::INTERVAL)::TEXT;
+            IF hoo_close <> '00:00:00' AND hourly THEN -- Not a day-granular circ
+                final_time := final_time||' '|| hoo_close;
+            ELSE
+                final_time := final_time||' 23:59:59';
+            END IF;
+        END IF;
+
+        --RAISE NOTICE 'final_time: %',final_time;
+
+        -- Loop through other closings
+        LOOP 
+            SELECT * INTO adjacent FROM actor.org_unit_closed WHERE org_unit = circ_lib AND final_time::TIMESTAMPTZ between close_start AND close_end;
+            EXIT WHEN adjacent.id IS NULL;
+            time_adjusted := TRUE;
+            -- RAISE NOTICE 'recursing for closings with final_time: %',final_time;
+            final_time := evergreen.find_next_open_time(circ_lib, adjacent.close_end::TIMESTAMPTZ, hourly, initial_time, has_hoo)::TEXT;
+        END LOOP;
+
+        EXIT WHEN breakout > 100;
+        EXIT WHEN NOT time_adjusted;
+
+    END LOOP;
+
+    RETURN final_time;
+END;
+$$ LANGUAGE PLPGSQL;
+
+COMMIT;
