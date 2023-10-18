96b4074ef684de71ccf334726bd9dd4e4f909acf
Jane Sandberg <js7389@princeton.edu>
2023-10-18 16:01:02 -0700

    LP2039612: regression test for creating carousels
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

---
+++ Open-ILS/src/eg2/nightwatch/src/staff/admin/local/carousels.spec.ts
@@ -0,0 +1,28 @@
+import { NightwatchBrowser } from 'nightwatch';
+import {randomString} from '../../../utils';
+
+module.exports = {
+  before: (browser: NightwatchBrowser) => {
+    browser.page.login().loginToWebClient(browser, 'br1csmith', 'cathys1234');
+  },
+
+  after: (browser: NightwatchBrowser) => {
+    browser.end();
+  },
+
+  'Can create new carousel': (browser: NightwatchBrowser) => {
+    const carouselName = randomString();
+    browser.navigateTo('eg2/en-US/staff/admin/local/container/carousel')
+           .click('xpath', '//button[contains(text(),"New Carousels")]')
+           // click the owner org-unit select to show the options
+           .click('xpath', '//input[contains(@id, "owner")]')
+           // then click the option we want from the dropdown
+           .click('xpath', '//button[@class = "dropdown-item active" and contains(./span/text(), "BR1")]')
+           .setValue('xpath', '//input[contains(@id, "name")]', carouselName)
+           .click('xpath', '//input[contains(@placeholder, "Carousel Type")]')
+           .click('xpath', '//button[@class = "dropdown-item" and contains(./span/text(), "Top Circulated Items")]')
+           .setValue('xpath', '//input[contains(@id, "max_items")]', '25')
+           .click('xpath', '//button[contains(text(), "Save")]')
+           .assert.textContains('eg-grid', carouselName.slice(0, 5), 'Grid has refreshed and is now showing the first part of the carousel name');
+  }
+};
