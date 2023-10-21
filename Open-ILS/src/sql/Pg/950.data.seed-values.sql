1985fc5a70c6044e6fb85650b40d1b53c8f3a5c4
Dan Briem <dbriem@wlsmail.org>
2023-10-21 19:16:26 +0000

    LP#2019207 Patron View Discovery Layer URL
    A new org unit setting configures the discovery layer URL opened
    by the Patron View button on a staff catalog record.
    
    The placeholder to include for the record ID is {eg_record_id}.
    
    Example format: https://example.com/Record/{eg_record_id}
    
    Signed-off-by: Dan Briem <dbriem@wlsmail.org>
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -23661,3 +23661,23 @@ VALUES (
         'cwst', 'label'
     )
 );
+
+INSERT INTO config.org_unit_setting_type (
+  name, grp, label, description, datatype
+) VALUES (
+  'cat.patron_view_discovery_layer_url',
+  'cat',
+  oils_i18n_gettext(
+    'cat.patron_view_discovery_layer_url',
+    'Patron view discovery layer URL',
+    'coust',
+    'label'
+  ),
+  oils_i18n_gettext(
+    'cat.patron_view_discovery_layer_url',
+    'URL for displaying an item in the "patron view" discovery layer with a placeholder for the bib record ID: {eg_record_id}. For example: https://example.com/Record/{eg_record_id}',
+    'coust',
+    'description'
+  ),
+  'string'
+);
