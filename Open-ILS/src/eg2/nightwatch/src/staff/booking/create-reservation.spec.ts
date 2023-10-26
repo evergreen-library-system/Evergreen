ab002191b8343d47821ee7ff2b51b7cf3ab20c89
Jane Sandberg <js7389@princeton.edu>
2023-10-26 15:42:29 -0700

    LP2032717: fix endless observable loop in booking create reservation
    Previously, this pipe/tap had an unusual behavior when a resourceId
    was passed in via url params.  `from` created an Observable from
    the this.resources array, but `tap` tried to add to that same array
    that was under observation.  This apparently worked before the Angular
    15 migration, but afterwards caused an infinite loop.
    
    If the resourceId is passed in, we know there will only be a single
    resource in this.resources.  Therefore, we can just observe the first
    element of the array in these cases.
    
    This commit also eliminates one unnecessary trip through fetchData().
    
    To test:
    * Go to Booking > Create Reservation
    * Choose the "Choose resource by barcode" tab
    * Type in ROOM1231 (the barcode of a non-cataloged resource in the concerto data set)
    * Confirm that you can place the reservation
    
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Christine Burns <christine.burns@bc.libraries.coop>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

---
+++ Open-ILS/src/eg2/nightwatch/src/staff/booking/create-reservation.spec.ts
@@ -0,0 +1,27 @@
+import {NightwatchBrowser} from 'nightwatch';
+
+
+module.exports = {
+    before: (browser: NightwatchBrowser) => {
+        browser.page.login().loginToWebClient(browser, 'br1mneal', 'melvinn1234'); // circulation admin
+    },
+
+    after: (browser: NightwatchBrowser) => {
+        browser.end();
+    },
+    'Can create a new reservation for a specific resource': (browser: NightwatchBrowser) => {
+        browser.page.navbar().click('@bookingMenu')
+               .click('@bookingMenuCreateReservations')
+               .click('xpath', '//a[contains(text(), "Choose resource by barcode")]')
+               .setValue('#ideal-resource-barcode', 'ROOM1231')
+               // Select a day
+               .click('xpath', '//button[contains(./span/text(), "Next day")]')
+               // Select a time
+               .click('.eg-grid-body .eg-grid-checkbox-cell input')
+               .click('xpath', '//button[contains(text(), "Create Reservation")]')
+               .setValue('#create-patron-barcode', '99999372586')
+               .click('xpath', '//button[contains(text(), "Confirm and show patron reservations")]')
+               .assert.textContains('eg-grid', 'ROO', 'Grid has refreshed and is showing the first part of the resource barcode');
+    }
+
+};
