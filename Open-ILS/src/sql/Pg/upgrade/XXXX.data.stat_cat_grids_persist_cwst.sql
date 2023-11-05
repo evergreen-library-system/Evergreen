5bef90979832bd4fe9250fea0a367c7ecba28d9b
Dan Briem <dbriem@wlsmail.org>
2023-11-05 21:20:23 +0000

    LP#2039750 Save Stat Cat Grid Settings
    Adds workstation settings to persist item and user stat cat
    grid preferences.
    
    Signed-off-by: Dan Briem <dbriem@wlsmail.org>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.stat_cat_grids_persist_cwst.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.local.actor.stat_cat', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.local.actor.stat_cat',
+        'Grid Config: admin.local.actor.stat_cat',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.local.asset.stat_cat', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.local.asset.stat_cat',
+        'Grid Config: admin.local.asset.stat_cat',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
