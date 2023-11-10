c274805fec8aaaa76063b274a7ddc1292ad75f77
Chris Sharp <csharp@georgialibraries.org>
2023-11-10 10:26:08 -0500

    LP#1930753: Stamping upgrade script
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/sql/Pg/upgrade/1389.schema.acq-buyer-san.sql
+++ Open-ILS/src/sql/Pg/upgrade/1389.schema.acq-buyer-san.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('TODO', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1389', :eg_version);
 
 ALTER TABLE acq.provider ADD COLUMN buyer_san TEXT;
 
