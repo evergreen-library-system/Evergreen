b3958d4c4fcd6c3442193c30884840e3803767f4
Bill Erickson <berickxx@gmail.com>
2023-11-10 17:02:50 -0500

    LP1993815 DB Stamp: Ang Custom Org Trees
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1390.schema.org_unit_custom_node.sql
+++ Open-ILS/src/sql/Pg/upgrade/1390.schema.org_unit_custom_node.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1390', :eg_version);
 
 ALTER TABLE actor.org_unit_custom_tree_node
 DROP CONSTRAINT org_unit_custom_tree_node_parent_node_fkey;
