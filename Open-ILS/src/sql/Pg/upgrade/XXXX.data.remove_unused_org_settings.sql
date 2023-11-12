a5ecec1d81cf248610774e08a6a481a115eca78d
Chris Sharp <csharp@georgialibraries.org>
2023-11-12 12:10:18 -0500

    LP#2023692: Remove unused DYM settings from existing installations
    Remove 6 unused org settings from existing installations:
    
    'opac.did_you_mean.low_result_threshold',
    'opac.did_you_mean.max_suggestions',
    'search.symspell.keyboard_distance.weight',
    'search.symspell.min_suggestion_use_threshold',
    'search.symspell.pg_trgm.weight',
    'search.symspell.soundex.weight'
    
    Harmless to run if the settings do not exist.
    
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.remove_unused_org_settings.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+DELETE FROM actor.org_unit_setting WHERE name IN (
+    'opac.did_you_mean.low_result_threshold',
+    'opac.did_you_mean.max_suggestions',
+    'search.symspell.keyboard_distance.weight',
+    'search.symspell.min_suggestion_use_threshold',
+    'search.symspell.pg_trgm.weight',
+    'search.symspell.soundex.weight'
+);
+
+DELETE FROM config.org_unit_setting_type WHERE name IN (
+    'opac.did_you_mean.low_result_threshold',
+    'opac.did_you_mean.max_suggestions',
+    'search.symspell.keyboard_distance.weight',
+    'search.symspell.min_suggestion_use_threshold',
+    'search.symspell.pg_trgm.weight',
+    'search.symspell.soundex.weight'
+);
+
+COMMIT;
