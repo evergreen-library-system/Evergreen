21156cd3a3df1b5384460da0527032319ee25caa
Terran McCanna <tmccanna@georgialibraries.org>
2023-11-13 17:33:29 -0500

    LP1895699 Stamping upgrade script
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1392', :eg_version); -- tlittle/cmorgan/sandbergja
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1393', :eg_version); -- sandbergja, abneiman
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
