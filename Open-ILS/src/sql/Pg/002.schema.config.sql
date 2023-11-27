99919aa0938b5f4882c4693ca7476bf4cb578fbe
Galen Charlton <gmc@equinoxOLI.org>
2023-11-27 13:36:26 -0500

    LP#2038472: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1397', :eg_version); -- dbriem / tmccanna
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1398', :eg_version); -- miker/gmcharlt/jeffdavis
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
