14ca07693531ce57409ac5b13246c9e1e7a27c43
Steven Mayo <smayo@georgialibraries.org>
2023-11-30 11:30:49 -0500

    LP#2028095: Fix typo in Library Setting Description for acq.default_owning_lib_for_auto_lids_strategy
    Updated the description for acq.default_owning_lib_for_auto_lids_strategy to the text suggested in the ticket.
    
    Release-note: Improves description of the "How to set default owning library for auto-created line item items" Library Setting
    
    Steps to test:
    [1] Run XXXX.data.default_owning_lib_for_auto_lids_strategy_typo_fix.sql or install the seed data anew
    [2] Go to Administration -> Local Admin -> Library Settings Editor
    [3] Search for acq.default_owning_lib_for_auto_lids_strategy
    [4] Read description
    
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Christine Morgan <cmorgan@noblenet.org>
    Signed-off-by: Susan Morrison <smorrison@georgialibraries.org>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -3166,7 +3166,7 @@ INSERT into config.org_unit_setting_type
         'How to set default owning library for auto-created line item items',
         'coust', 'label'),
     oils_i18n_gettext('acq.default_owning_lib_for_auto_lids_strategy',
-        'Stategy to use to set default owning library to set when line item items are auto-created because the provider''s default copy count has been set. Valid values are "workstation" to use the workstation library, "blank" to leave it blank, and "use_setting" to use the "Default owning library for auto-created line item items" setting. If not set, the workstation library will be used.',
+        'Strategy to use when setting the default owning library for line item items that are auto-created due to the provider''s default copy count being set. Valid values are "workstation" to use the workstation library, "blank" to leave it blank, and "use_setting" to use the "Default owning library for auto-created line item items" setting. If not set, the workstation library will be used.',
         'coust', 'description'),
     'string', null)
 ,( 'acq.default_owning_lib_for_auto_lids', 'acq',
