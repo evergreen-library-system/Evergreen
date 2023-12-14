dc9d7201121f1f4f11fed247c14874c92c8e407f
Jason Boyer <JBoyer@equinoxOLI.org>
2023-12-14 15:12:28 -0500

    Correct Pg10 Syntax Error in Trigger Creation (version upgrade)
    We need to use EXECUTE PROCEDURE at least util we no longer support Pg10.
    
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/version-upgrade/3.11.1-3.12.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/3.11.1-3.12.0-upgrade-db.sql
@@ -75,7 +75,7 @@ BEGIN
 END;
 $function$;
 
-CREATE TRIGGER acpl_validate_edit BEFORE UPDATE ON asset.copy_location FOR EACH ROW EXECUTE FUNCTION asset.copy_location_validate_edit();
+CREATE TRIGGER acpl_validate_edit BEFORE UPDATE ON asset.copy_location FOR EACH ROW EXECUTE PROCEDURE asset.copy_location_validate_edit();
 
 
 
