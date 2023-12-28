03a9c45ae10992118cf3132a631de9afbba1316a
Jason Stephenson <jason@sigio.com>
2023-12-28 10:29:49 -0500

    LP1384796: Fix asset.copy_template valid_fine_level constraint
    The "valid_fine_level" constraint on the asset.copy_template has been
    erroneously defined to check the load_duration column.  This commit
    fixes it so that the constraint looks at the fine_level column as it
    should.
    
    This commit adds the database upgrade, a miscellaneous release note,
    as well as the main schema change.
    
    To test the patch, simply look at the asset.copy_template table in the
    database before and after applying the database upgrade.  The
    valid_fine_level constraint should reference the fine_level column
    after the patch.
    
    The code is based on a patch submitted by Tim Faile.
    
    Signed-off-by: Jason Stephenson <jason@sigio.com>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.copy_template_fine_level_constaint.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+--SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+ALTER TABLE asset.copy_template DROP CONSTRAINT valid_fine_level;
+ALTER TABLE asset.copy_template ADD CONSTRAINT valid_fine_level
+      CHECK (fine_level IS NULL OR fine_level IN (1,2,3));
+
+COMMIT;
