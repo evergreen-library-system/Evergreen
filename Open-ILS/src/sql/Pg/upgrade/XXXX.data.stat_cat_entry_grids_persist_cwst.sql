a1f93fda6d0c7c6aaf9493ff9bdeb21d630a1588
Terran McCanna <tmccanna@georgialibraries.org>
2024-01-02 15:01:15 -0500

    LP2046575 Angular Stat Cat Entries Grid Settings Do Not Save
    Adds ability to save the column settings on the patron and item
    stat cat entries. (The main stat cat save grid options were already
    in place, but the entries grid options were not.)
    
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Ruth Frasur Davis <rfrasur@library.in.gov>
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.stat_cat_entry_grids_persist_cwst.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.local.actor.stat_cat_entry', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.local.actor.stat_cat_entry',
+        'Grid Config: admin.local.actor.stat_cat_entry',
+        'cwst', 'label'
+    )
+), (
+    'eg.grid.admin.local.asset.stat_cat_entry', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.local.asset.stat_cat_entry',
+        'Grid Config: admin.local.asset.stat_cat_entry',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
