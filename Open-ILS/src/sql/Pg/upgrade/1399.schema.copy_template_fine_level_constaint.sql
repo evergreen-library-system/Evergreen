20050c64c1cb53e0983f4912ca65f35ba7023b2b
Galen Charlton <gmc@equinoxOLI.org>
2024-01-03 09:58:55 -0500

    LP#1384796: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1399.schema.copy_template_fine_level_constaint.sql
+++ Open-ILS/src/sql/Pg/upgrade/1399.schema.copy_template_fine_level_constaint.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1399', :eg_version);
 
 ALTER TABLE asset.copy_template DROP CONSTRAINT valid_fine_level;
 ALTER TABLE asset.copy_template ADD CONSTRAINT valid_fine_level
