ed3b3111bcbde994dc18dfacbe250941e7b20070
Llewellyn Marshall <llewellyn.marshall@ncdcr.gov>
2024-01-24 11:30:31 -0500

    LP2051140: Adding missing bib bucket IDL permissions
    Release-Note: Adds missing bib bucket IDL permissions, fixes carousel admin interface
    
    Signed-off-by: Llewellyn Marshall <llewellyn.marshall@ncdcr.gov>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: blake <blake@mobiusconsortium.org>

---
+++ Open-ILS/src/sql/Pg/upgrade/xxxx.data.lp2051140_missing_bucket_perms.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+INSERT INTO permission.perm_list ( id, code, description ) SELECT DISTINCT
+  648,
+  'ADMIN_BIB_BUCKET',
+  oils_i18n_gettext(648,
+    'Administer bibliographic record buckets', 'ppl', 'description'
+  )
+  FROM permission.perm_list
+  WHERE NOT EXISTS (SELECT 1 FROM permission.perm_list WHERE code = 'ADMIN_BIB_BUCKET');
+ 
+INSERT INTO permission.perm_list ( id, code, description )  SELECT DISTINCT
+  649,
+  'CREATE_BIB_BUCKET',
+  oils_i18n_gettext(649,
+    'Create bibliographic record buckets', 'ppl', 'description'
+  )
+  FROM permission.perm_list
+  WHERE NOT EXISTS (SELECT 1 FROM permission.perm_list WHERE code = 'CREATE_BIB_BUCKET');
+
+COMMIT;
