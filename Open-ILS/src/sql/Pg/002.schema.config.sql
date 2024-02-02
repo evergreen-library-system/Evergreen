d59d5173f8406660a8b80c4ce2ffd9e6e2190058
Jane Sandberg <js7389@princeton.edu>
2024-02-02 11:55:57 -0600

    Bumping version numbers, adding Upgrade Script and Changelog
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1400', :eg_version); -- terranm/sleary/redavis/smayo/sandbergja
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.12.1', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
