c6eacbf7b10d84673555f391d3f9df5df5af6476
blake <blake@mobiusconsortium.org>
2024-02-19 16:21:49 -0600

    LP2054349 3.11.1-3.12.0 upgrade script 1395 portion
    The 1395 portion needed to address the
    config.org_unit_setting_type_log table as well.
    
    If an installation has history for any of those settings,
    PG will throw a foriegn key error upon commit;
    
    Signed-off-by: blake <blake@mobiusconsortium.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/upgrade/1395.data.remove_unused_org_settings.sql
+++ Open-ILS/src/sql/Pg/upgrade/1395.data.remove_unused_org_settings.sql
@@ -20,4 +20,13 @@ DELETE FROM config.org_unit_setting_type WHERE name IN (
     'search.symspell.soundex.weight'
 );
 
+DELETE FROM config.org_unit_setting_type_log WHERE field_name IN (
+    'opac.did_you_mean.low_result_threshold',
+    'opac.did_you_mean.max_suggestions',
+    'search.symspell.keyboard_distance.weight',
+    'search.symspell.min_suggestion_use_threshold',
+    'search.symspell.pg_trgm.weight',
+    'search.symspell.soundex.weight'
+);
+
 COMMIT;
