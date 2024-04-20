6963fb3afdb33fb9bd7d6772073171f75f4ecb18
Jane Sandberg <sandbergja@gmail.com>
2024-04-19 18:51:39 -0700

    LP2062944: Running angular tests with code coverage should not error
    To test:
    1) On a running Evergreen system, cd to the eg2 directory
    2) Run this command: npm run test -- --code-coverage
    3) Note that the eg2 directory now has a coverage directory
    4) Open one of the index.html files within this directory in your browser.
    5) Note that you get a visual view of the code, with untested code in red.
    
    Release-note: Developers no longer get an error message while checking how much of Evergreen's staff client code is covered by automated tests
    
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>
    Signed-off-by: Shula Link <slink@gchrl.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>
    Signed-off-by: Ruth Frasur Davis <rdavis@evergreencdi.org>
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/eg2/karma.conf.js
+++ Open-ILS/src/eg2/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
