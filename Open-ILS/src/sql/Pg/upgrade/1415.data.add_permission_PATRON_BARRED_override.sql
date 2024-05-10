237de137088b89283d5ca91252585c4dbd2574e9
Galen Charlton <gmc@equinoxOLI.org>
2024-05-10 16:47:39 -0400

    LP#2062023: stamp DB update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1415.data.add_permission_PATRON_BARRED_override.sql
+++ Open-ILS/src/sql/Pg/upgrade/1415.data.add_permission_PATRON_BARRED_override.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1415', :eg_version);
 INSERT INTO permission.perm_list ( id, code, description ) SELECT DISTINCT
-   652,
+   656,
    'PATRON_BARRED.override',
-   oils_i18n_gettext(652,
+   oils_i18n_gettext(656,
      'Override the PATRON_BARRED event', 'ppl', 'description'
    )
    FROM permission.perm_list
