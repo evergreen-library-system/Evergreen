a5acca481a090a73d1f4c49e521cdfbc4916a4cc
blake <blake@mobiusconsortium.org>
2024-05-12 22:59:38 -0500

    LP#2006969: stamp DB update
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1415', :eg_version); -- smayo / sandbergja / gmcharlt
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1416', :eg_version); -- phasefx / sleary /blake
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1416.data.updated_marc_tag_tables.sql
+++ Open-ILS/src/sql/Pg/upgrade/1416.data.updated_marc_tag_tables.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1416', :eg_version);
 
 \set ON_ERROR_STOP on
 
