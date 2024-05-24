88bfb41e62d0d9bb9bf12aeeba92f13a2720710c
Jane Sandberg <js7389@princeton.edu>
2024-05-24 15:06:57 -0700

    LP2067115: Fieldmapper should set combobox domId, not id
    To test:
    1. Install an accessibility checking browser plugin like WAVE or Axe (or just use lighthouse on Chrome or Edge)
    2. Go to Administration > Local > Carousels
    3. Press the New Carousels Button
    4. Run your accessibility checker.
    5. Note that you no longer get an error about form field
       accessible names
    
    Alternatively, you can run the supplied nightwatch test
    from the eg2 directory with:
    `MOZ_HEADLESS=1 npx nightwatch nightwatch/src/staff/admin/local/booking/resource.spec.ts`
    
    Release-note: Fix an accessibility issue in staff client forms.
    
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: blake <blake@mobiusconsortium.org>

---
+++ Open-ILS/src/eg2/nightwatch/src/staff/admin/local/booking/resource.spec.ts
@@ -0,0 +1,14 @@
+import {NightwatchBrowser} from 'nightwatch';
+
+module.exports = {
+    before: (browser: NightwatchBrowser) => {
+        // log in as circ admin
+        browser.page.login().loginToWebClient(browser, 'br3kwright', 'kayw1234');
+        browser.navigateTo('eg2/en-US/staff/admin/booking/resource')
+    },
+
+    'Form is accessible': (browser: NightwatchBrowser) => {
+            browser.click('xpath', '//button[contains(text(), "New Resource")]')
+                   .axeInject().axeRun('form');
+    },
+};
