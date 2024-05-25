0aa236185f065fb934888bcb1920ddc72f73c5c0
Jane Sandberg <js7389@princeton.edu>
2024-05-24 23:42:14 -0700

    LP2058256: Update eg-marc-editor selectors in nightwatch tests
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>

--- Open-ILS/src/eg2/nightwatch/pages/staff/share/marcEdit.ts
+++ Open-ILS/src/eg2/nightwatch/pages/staff/share/marcEdit.ts
@@ -1,20 +1,7 @@
 import {PageObjectModel} from 'nightwatch';
 
-// Create a bunch of selectors for all the MARC tags
-let marcElements = {};
-// An array of 010-999
-const marcTags = [...Array(990).keys()].map((x: number) => (x + 10).toString().padStart(3, '0'));
-for (const tag of marcTags) {
-    marcElements['marcTag' + tag] = {
-        selector: '//input[text() = "' + tag + '" and @aria-label = "Data Field Tag"]',
-        locateStrategy: 'xpath'
-    };
-}
-
-
 const marcEditPage: PageObjectModel = {
     elements: {
-        ...marcElements,
         dataFieldTag: {
             selector: 'input[aria-label = "Data Field Tag"]'
         },
