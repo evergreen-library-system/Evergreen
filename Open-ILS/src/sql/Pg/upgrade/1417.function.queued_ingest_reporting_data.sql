289da315bd3f09556a96df80fb645dd235aab4e5
blake <blake@mobiusconsortium.org>
2024-05-28 16:04:01 -0500

    LP#2066981 Stamping upgrade 1417
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/upgrade/1417.function.queued_ingest_reporting_data.sql
+++ Open-ILS/src/sql/Pg/upgrade/1417.function.queued_ingest_reporting_data.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1417', :eg_version);
 
 CREATE OR REPLACE FUNCTION action.process_ingest_queue_entry (qeid BIGINT) RETURNS BOOL AS $func$
 DECLARE
