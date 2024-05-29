726a3a140eb791db6e75b19c0d72c283eada9892
blake <blake@mobiusconsortium.org>
2024-05-29 13:02:48 -0500

    LP#2059974: stamp DB update
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/upgrade/1418.data.restrict_suggestions.sql
+++ Open-ILS/src/sql/Pg/upgrade/1418.data.restrict_suggestions.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1418', :eg_version);
 
 INSERT INTO config.global_flag (name, enabled, value, label) 
     VALUES (
