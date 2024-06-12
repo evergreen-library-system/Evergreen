6d99c8b8fba481f59fd24c3931b193d27e5a0a9a
Jane Sandberg <sandbergja@gmail.com>
2024-06-11 21:51:47 -0400

    LP2069098: get Angular tests compiling and passing again
    Two Angular constructors got a new parameter, but the
    tests that called those constructors weren't updated
    to include the new parameter.  This commit adds those
    missing parameters (just using null as the value for
    now, since the actual values aren't exercised in
    the current tests)
    
    Release-note: Update automated tests for the staff client.
    
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Scott Angel <scottangel@mobiusconsortium.org>

--- Open-ILS/src/eg2/src/app/staff/catalog/prefs.spec.ts
+++ Open-ILS/src/eg2/src/app/staff/catalog/prefs.spec.ts
@@ -12,13 +12,13 @@ describe('prefs.component', () => {
     describe('#showCoursePreferences', () => {
         it('when work_ou is using the course module', async () => {
             mockStore.getItemBatch.and.returnValue(Promise.resolve({'circ.course_materials_opt_in': true}));
-            component = new PreferencesComponent(mockStore, mockToast, mockStaffCat);
+            component = new PreferencesComponent(mockStore, mockToast, mockStaffCat, null);
             await component.ngOnInit();
             expect(component.showCoursePreferences()).toBe(true);
         });
         it('when work_ou is not using the course module', async () => {
             mockStore.getItemBatch.and.returnValue(Promise.resolve({'circ.course_materials_opt_in': false}));
-            component = new PreferencesComponent(mockStore, mockToast, mockStaffCat);
+            component = new PreferencesComponent(mockStore, mockToast, mockStaffCat, null);
             await component.ngOnInit();
             expect(component.showCoursePreferences()).toBe(false);
         });
