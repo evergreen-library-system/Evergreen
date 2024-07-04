6d29285adbf11cd62136b5b46ef25def7c99e6f0
Dan Briem <dbriem@harrisonpl.org>
2024-07-04 15:30:26 +0000

    LP#2069472 Conjoined Items Grid Config Not Saving
    Adds eg.grid.catalog.record.conjoined workstation setting type.
    
    Release-note: Allow users to save grid settings in Conjoined Items grid.
    
    Signed-off-by: Dan Briem <dbriem@harrisonpl.org>
    Signed-off-by: Ruth Davis <rfrasur@gmail.com>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.catalog_record_conjoined_grid_cwst.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.catalog.record.conjoined', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.catalog.record.conjoined',
+        'Grid Config: catalog.record.conjoined',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
