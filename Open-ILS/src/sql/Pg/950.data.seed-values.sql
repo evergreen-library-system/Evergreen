259d769a5077de40eae884eb7dc8f745d2a288cc
Dan Briem <dbriem@harrisonpl.org>
2024-07-04 14:09:40 +0000

    LP#2069750 Save Acqusitions Distribution Formulas Grid Config
    Adds the eg.grid.admin.acq.distribution_formula workstation
    setting type to the DB.
    
    Release-note: Allow users to save grid settings in Acquisitions Distribution Formulas administrative interface.
    
    Signed-off-by: Dan Briem <dbriem@harrisonpl.org>
    Signed-off-by: Mary Llewellyn <mllewell@biblio.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -24215,3 +24215,13 @@ VALUES (
         'cwst', 'label'
     )
 );
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.acq.distribution_formula', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.acq.distribution_formula',
+        'Grid Config: admin.acq.distribution_formula',
+        'cwst', 'label'
+    )
+);
