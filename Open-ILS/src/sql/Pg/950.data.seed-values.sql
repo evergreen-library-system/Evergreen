3a27eb8243e650ff768279d231f6e1e24620d23b
Steven Mayo <smayo@georgialibraries.org>
2024-07-15 15:14:09 -0400

    lp2018014 Holdings Require Parts If Parts On Record
    Updated the description of 'circ.holds.ui_require_monographic_part_when_present'
    to include its new effect on the holdings editor.
    
    Release-note: The setting to require a monographic part when placing a hold now also requires that all copies have parts on a record with parts.
    
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -21668,7 +21668,7 @@ VALUES (
         'coust', 'label'),
     'circ',
     oils_i18n_gettext('circ.holds.ui_require_monographic_part_when_present',
-        'Normally the selection of a monographic part during hold placement is optional if there is at least one copy on the bib without a monographic part.  A true value for this setting will require part selection even under this condition.',
+        'Normally the selection of a monographic part during hold placement is optional if there is at least one copy on the bib without a monographic part.  A true value for this setting will require part selection even under this condition.  A true value for this setting will also require a part to be added before saving any changes or creating a new item in the holdings editor, if there are parts on the bib.',
         'coust', 'description'),
     'bool'
 );
