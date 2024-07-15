671159fba663dec58a4bbe2196f10dbe69f592e7
Jason Stephenson <jason@sigio.com>
2024-07-15 15:45:34 -0400

    LP2018014: Stamp upgrade script
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/upgrade/1422.data.update_require_parts_desc.sql
+++ Open-ILS/src/sql/Pg/upgrade/1422.data.update_require_parts_desc.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1422', :eg_version);
 
-UPDATE config.org_unit_setting_type 
+UPDATE config.org_unit_setting_type
 SET description = oils_i18n_gettext('circ.holds.ui_require_monographic_part_when_present',
         'Normally the selection of a monographic part during hold placement is optional if there is at least one copy on the bib without a monographic part.  A true value for this setting will require part selection even under this condition.  A true value for this setting will also require a part to be added before saving any changes or creating a new item in the holdings editor, if there are parts on the bib.',
         'coust', 'description')
