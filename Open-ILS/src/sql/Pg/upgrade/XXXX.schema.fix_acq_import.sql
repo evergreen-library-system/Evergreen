450de74660a7e79165e76ef14641a4d78bd12e0b
Jane Sandberg <js7389@princeton.edu>
2024-07-17 13:27:17 -0700

    LP924952: Make acq import resilient against empty subfields
    To test:
    1. Find or create an acquisitions provider with a holdings tag and several holdings subfields.
    2. Create a MARC record that has the holdings field you configured.
    3. In the holdings field, add some of the subfields you configured in step 1.  Add data to some
       of them, but leave some empty.
    4. Go to Acquisitions > Load MARC Order records.
    5. Choose the provider you configured in step 1.
    6. Choose an ordering agency, fiscal year, and queue name.
    7. Check Create Purchase Order.
    8. Press the Upload button.
    9. Without this patch, these steps will give an ACQ_IMPORT_ERROR.  With this patch, the steps
       should give no error, and give "Go to: Queue, Purchase Order" links at the bottom.
    
    Release-note: Allow acquisitions vendor MARC Order record load even if some subfields are empty.
    
    Co-authored-by: Ian Skelskey <ianskelskey@gmail.com>
    Co-authored-by: Steven Mayo <smayo@georgialibraries.org>
    Co-authored-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.fix_acq_import.sql
@@ -0,0 +1,44 @@
+BEGIN;
+
+-- INSERT INTO config.upgrade_log (version) VALUES ('XXXX');
+
+-- Use oils_xpath_table instead of pgxml's xpath_table
+CREATE OR REPLACE FUNCTION acq.extract_holding_attr_table (lineitem int, tag text) RETURNS SETOF acq.flat_lineitem_holding_subfield AS $$
+DECLARE
+    counter INT;
+    lida    acq.flat_lineitem_holding_subfield%ROWTYPE;
+BEGIN
+
+    SELECT  COUNT(*) INTO counter
+      FROM  oils_xpath_table(
+                'id',
+                'marc',
+                'acq.lineitem',
+                '//*[@tag="' || tag || '"]',
+                'id=' || lineitem
+            ) as t(i int,c text);
+
+    FOR i IN 1 .. counter LOOP
+        FOR lida IN
+            SELECT  *
+              FROM  (   SELECT  id,i,t,v
+                          FROM  oils_xpath_table(
+                                    'id',
+                                    'marc',
+                                    'acq.lineitem',
+                                    '//*[@tag="' || tag || '"][position()=' || i || ']/*[text()]/@code|' ||
+                                        '//*[@tag="' || tag || '"][position()=' || i || ']/*[@code and text()]',
+                                    'id=' || lineitem
+                                ) as t(id int,t text,v text)
+                    )x
+        LOOP
+            RETURN NEXT lida;
+        END LOOP;
+    END LOOP;
+
+    RETURN;
+END;
+$$ LANGUAGE PLPGSQL;
+
+
+COMMIT;
