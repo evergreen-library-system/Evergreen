9e96edff7f1eae383a0e44c0b6cd3affa0acf53d
Terran McCanna <tmccanna@georgialibraries.org>
2024-08-26 19:08:42 -0400

    LP1910580 Org Setting Typo in Course Materials
    Corrects the spelling of 'transcendant' to 'transcendent' in the
    Library Setting for 'Bib source for brief records created in the
    course materials module'
    
    Release-note: Fixes typo in 'Bib source for brief records' library setting
    
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Susan Morrison <smorrison@georgialibraries.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

---
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.data.org-setting-brief-record-typo.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+
+UPDATE config.org_unit_setting_type
+SET description = oils_i18n_gettext('circ.course_materials_brief_record_bib_source',
+    'The course materials module will use this bib source for any new brief bibliographic records made inside that module. For best results, use a transcendent bib source.',
+    'coust', 'description')
+WHERE name='circ.course_materials_brief_record_bib_source';
+
+COMMIT;
