8f0643389d0ffb964bda1cdeaa5ed9cb89f29586
Jane Sandberg <js7389@princeton.edu>
2024-08-28 06:37:48 -0700

    LP#2012669 stamping upgrade script
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/upgrade/1426.hold_reset_reasons.sql
+++ Open-ILS/src/sql/Pg/upgrade/1426.hold_reset_reasons.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1426', :eg_version);
 
 
 CREATE TABLE action.hold_request_reset_reason (
