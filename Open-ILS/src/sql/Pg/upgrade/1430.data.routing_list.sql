99beac7c58f9725c7c763a3f6ac1ba304160eb4d
Jason Stephenson <jason@sigio.com>
2024-09-06 10:48:59 -0400

    LP1906462: Stamp db upgrade
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/upgrade/1430.data.routing_list.sql
+++ Open-ILS/src/sql/Pg/upgrade/1430.data.routing_list.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1430', :eg_version);
 
 INSERT INTO config.print_template
     (name, label, owner, active, locale, content_type, template)
