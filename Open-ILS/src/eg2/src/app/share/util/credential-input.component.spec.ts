d7b2194676e773dd014d93dc55a73688337a47d0
Stephanie Leary <stephanie.leary@equinoxoli.org>
2024-09-13 18:35:46 +0000

    LP1840773: Test followup for self-checkout
    Corrects a focused test (fdescribe) that was accidentally left in, which
    restricts the Angular tests to those contained in this file. This
    correction allows 'npm run test' to run all of our tests again
    (currently 190) instead of running only the 6 tests in this file.
    
    Thanks to Jane Sandberg for spotting the error.
    
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/eg2/src/app/share/util/credential-input.component.spec.ts
+++ Open-ILS/src/eg2/src/app/share/util/credential-input.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CredentialInputComponent } from './credential-input.component';
 
-fdescribe('CredentialInputComponent', () => {
+describe('CredentialInputComponent', () => {
     let component: CredentialInputComponent;
     let fixture: ComponentFixture<CredentialInputComponent>;
 
