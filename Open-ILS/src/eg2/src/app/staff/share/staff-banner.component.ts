04f4f33f6325de07d9d8fe1381c645759374cd7f
Jason Etheridge <jason@equinoxOLI.org>
2024-09-16 13:05:20 -0700

    LP2063146: angular record buckets, Angular StaffBannerComponent changes
    * CSS tweaks
    * bannerTemplateRef property
    
    Co-authored-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Jason Etheridge <jason@equinoxOLI.org>
    Signed-off-by: Ruth Davis <rdavis@evergreenecdi.org>
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
+++ Open-ILS/src/eg2/src/app/staff/share/staff-banner.component.ts
@@ -1,24 +1,29 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, TemplateRef, OnInit, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'eg-staff-banner',
     template: `
     <eg-title i18n-prefix [prefix]="bannerText"></eg-title>
 
-    <div class="lead alert alert-primary text-center pt-1 pb-1"
-      [ngClass]="bannerStyle ? bannerStyle : 'alert-primary'">
-      <h1 id="staff-banner" tabindex="0">
+    <div class="staff-banner" [ngClass]="bannerStyle">
+      <h1 id="staff-banner" tabindex="0" *ngIf="bannerText || bannerIcon">
         <i class="material-icons align-middle text-left" aria-hidden="true" *ngIf="bannerIcon">{{bannerIcon}}</i>
         <span i18n>{{bannerText}}</span>
       </h1>
+      <ng-container *ngIf="bannerTemplateRef">
+        <ng-container *ngTemplateOutlet="bannerTemplateRef"></ng-container>
+      </ng-container>
     </div>
-    `
+    `,
+    styleUrls: ['staff-banner.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class StaffBannerComponent {
     @Input() public bannerText: string;
     @Input() public bannerIcon: string;
     @Input() public bannerStyle: string;
+    @Input() public bannerTemplateRef: TemplateRef<any>; // replaces bannerText in the heading, but not the title
 }
 
 
