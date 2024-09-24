39ad820d86f8c062bd1f977ca277a305ce274464
Jason Stephenson <jason@sigio.com>
2024-09-24 16:07:36 -0400

    LP2073988: Stamp database upgrade
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1441', :eg_version); -- phasefx/redavis/dyrcona
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1442', :eg_version); -- sleary/dyrcona
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
