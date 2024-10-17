04cdaf9ae4c7e432857e8fc3611dcbab0499b081
Jason Stephenson <jason@sigio.com>
2024-10-17 14:35:58 -0400

    LP1848398: Stamp DB upgrade script
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1443', :eg_version); -- sleary/tmccanna/dyrcona
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1444', :eg_version); -- JBoyer/sleary/dyrcona
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1444.data.really-reprecate-format-date.sql
+++ Open-ILS/src/sql/Pg/upgrade/1444.data.really-reprecate-format-date.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version); -- JBoyer
+SELECT evergreen.upgrade_deps_block_check('1444', :eg_version); -- JBoyer
 
 -- If these settings have not been marked Deprecated go ahead and do so now
 UPDATE config.org_unit_setting_type SET label = 'Deprecated: ' || label WHERE name IN ('format.date', 'format.time') AND NOT label ILIKE 'deprecated: %';
