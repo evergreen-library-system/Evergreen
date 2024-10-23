7a75a194c1120e679093aacedd5323717152cbc4
blake <blake@mobiusconsortium.org>
2024-10-23 09:08:57 -0500

    Forward-port 3.13.4-3.13.5 upgrade script
    Signed-off-by: blake <blake@mobiusconsortium.org>

---
+++ Open-ILS/src/sql/Pg/version-upgrade/3.13.4-3.13.5-upgrade-db.sql
@@ -0,0 +1,51 @@
+--Upgrade Script for 3.13.4 to 3.13.5
+\set eg_version '''3.13.5'''
+BEGIN;
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.13.5', :eg_version);
+
+INSERT INTO config.upgrade_log (version) VALUES ('1439');
+
+-- Use oils_xpath_table instead of pgxml's xpath_table
+CREATE OR REPLACE FUNCTION acq.extract_holding_attr_table (lineitem int, tag text) RETURNS SETOF acq.flat_lineitem_holding_subfield AS $$
+DECLARE
+    counter INT;
+    lida    acq.flat_lineitem_holding_subfield%ROWTYPE;
+BEGIN
+
+    SELECT  COUNT(*) INTO counter
+      FROM  oils_xpath_table(
+                'id',
+                'marc',
+                'acq.lineitem',
+                '//*[@tag="' || tag || '"]',
+                'id=' || lineitem
+            ) as t(i int,c text);
+
+    FOR i IN 1 .. counter LOOP
+        FOR lida IN
+            SELECT  *
+              FROM  (   SELECT  id,i,t,v
+                          FROM  oils_xpath_table(
+                                    'id',
+                                    'marc',
+                                    'acq.lineitem',
+                                    '//*[@tag="' || tag || '"][position()=' || i || ']/*[text()]/@code|' ||
+                                        '//*[@tag="' || tag || '"][position()=' || i || ']/*[@code and text()]',
+                                    'id=' || lineitem
+                                ) as t(id int,t text,v text)
+                    )x
+        LOOP
+            RETURN NEXT lida;
+        END LOOP;
+    END LOOP;
+
+    RETURN;
+END;
+$$ LANGUAGE PLPGSQL;
+
+
+COMMIT;
+
+-- Update auditor tables to catch changes to source tables.
+--   Can be removed/skipped if there were no schema changes.
+SELECT auditor.update_auditors();
