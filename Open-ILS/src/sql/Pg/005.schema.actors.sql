5b90c95f19b83bebc266ab44918ccc62bb9ae5ef
Bill Erickson <berickxx@gmail.com>
2024-10-31 10:27:05 -0400

    LP2086106 SMS/Default phone search performance improvements
    Move the patron phone search part of the query to a CTE/WITH clause and
    add an index on phone-related user settings to improve performance.
    
    Now the query looks more like this:
    
    WITH has_phone_number AS (
        SELECT id FROM actor.usr
        WHERE evergreen.lowercase(day_phone) ~ '^333-333-3333' OR
            evergreen.lowercase(evening_phone) ~ '^333-333-3333' OR
            evergreen.lowercase(other_phone) ~ '^333-333-3333'
        UNION
        SELECT usr AS id FROM actor.usr_setting
        WHERE
            name IN ('opac.default_phone', 'opac.default_sms_notify')
            AND evergreen.lowercase(value) ~ '^"?333-333-3333'
        )
    SELECT ...
    FROM  actor.usr AS users
    JOIN actor.org_unit_descendants(1) d ON (d.id = users.home_ou)
    JOIN has_phone_number hpn ON hpn.id = users.id
    WHERE users.deleted = FALSE
    AND users.active = TRUE
    ...
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Llewellyn Marshall <llewellyn.marshall@dncr.nc.gov>
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/005.schema.actors.sql
+++ Open-ILS/src/sql/Pg/005.schema.actors.sql
@@ -208,6 +208,15 @@ $$;
 
 CREATE INDEX actor_usr_setting_usr_idx ON actor.usr_setting (usr);
 
+-- special indexes for phone number searches
+CREATE INDEX actor_usr_setting_phone_values_idx
+    ON actor.usr_setting (evergreen.lowercase(value))
+    WHERE name IN ('opac.default_phone', 'opac.default_sms_notify');
+
+CREATE INDEX actor_usr_setting_phone_values_numeric_idx
+    ON actor.usr_setting (evergreen.lowercase(REGEXP_REPLACE(value, '[^0-9]', '', 'g')))
+    WHERE name IN ('opac.default_phone', 'opac.default_sms_notify');
+
 CREATE TABLE actor.stat_cat_sip_fields (
     field   CHAR(2) PRIMARY KEY,
     name    TEXT    NOT NULL,
