735f056a62951263e997f59c960267f335ce55e6
Jane Sandberg <js7389@princeton.edu>
2024-12-09 12:09:57 -0800

    lp1980294 - stamp upgrade script
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/upgrade/1445.data.at_event_def_money_payment_receipt_print.sql
+++ Open-ILS/src/sql/Pg/upgrade/1445.data.at_event_def_money_payment_receipt_print.sql
@@ -1,6 +1,6 @@
 BEGIN;
 -- check whether patch can be applied
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1445', :eg_version);
 
 UPDATE action_trigger.event_definition
 SET template =
