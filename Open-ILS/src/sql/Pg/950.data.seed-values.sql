e0078f49d6fd75ae812b67ac5e694a2abdbcb67f
Terran McCanna <tmccanna@georgialibraries.org>
2025-01-16 11:43:15 -0500

    LP1903873 Move OPAC alert banner to library setting
    Moves the control of the OPAC alert banner away from the config.tt2
    file and into a trio of "OPAC Alert Banner" library settings for
    ease of use.
    
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: blake <blake@mobiusconsortium.org>
    Signed-off-by: Gina Monti <gmonti@biblio.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -24906,3 +24906,37 @@ VALUES (
         'coust', 'description'),
     'string'
 );
+
+INSERT into config.org_unit_setting_type
+         (name, grp, label, description, datatype)
+VALUES (
+         'opac.alert_banner_show',
+         'opac',
+         oils_i18n_gettext('opac.alert_banner_show',
+         'OPAC Alert Banner: Display',
+         'coust', 'label'),
+         oils_i18n_gettext('opac.alert_message_show',
+         'Show an alert banner in the OPAC. Default is false.',
+         'coust', 'description'),
+         'bool'
+), (
+         'opac.alert_banner_type',
+         'opac',
+         oils_i18n_gettext('opac.alert_banner_type',
+         'OPAC Alert Banner: Type',
+         'coust', 'label'),
+         oils_i18n_gettext('opac.alert_message_type',
+         'Determine the display of the banner. Options are: success, info, warning, danger.',
+         'coust', 'description'),
+         'string'
+), (
+         'opac.alert_banner_text',
+         'opac',
+         oils_i18n_gettext('opac.alert_banner_text',
+         'OPAC Alert Banner: Text',
+         'coust', 'label'),
+         oils_i18n_gettext('opac.alert_message_text',
+         'Text that will display in the alert banner.',
+         'coust', 'description'),
+         'string'
+);
