57c8610a7f3cc72106f0e4e1d835ba713a24c1c9
Jane Sandberg <sandbergja@gmail.com>
2025-01-28 19:27:39 -0800

    LP1975852: stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1451.data.monograph_parts_grid.sql
+++ Open-ILS/src/sql/Pg/upgrade/1451.data.monograph_parts_grid.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1451', :eg_version);
 
 INSERT into config.workstation_setting_type (name, grp, datatype, label)
 VALUES (
