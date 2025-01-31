adcd453039bd9d2844d94ee13aba2b65f7789b06
Terran McCanna <tmccanna@georgialibraries.org>
2025-01-31 12:48:29 -0500

    LP1949392 Stamp Upgrade Script
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

--- Open-ILS/src/sql/Pg/upgrade/1453.data.top_of_queue_perms.sql
+++ Open-ILS/src/sql/Pg/upgrade/1453.data.top_of_queue_perms.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1453', :eg_version);
 
 INSERT INTO permission.perm_list ( id, code, description ) SELECT DISTINCT
    676,
