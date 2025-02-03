f62f71d6c876242c6394cb2a33ad5ef10633b896
Jane Sandberg <js7389@princeton.edu>
2025-02-03 12:12:03 -0800

    LP 2009066: stamp upgrade script
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/upgrade/1454.schema.acq-invoice_item_fund_debit_fkey-deferrable.sql
+++ Open-ILS/src/sql/Pg/upgrade/1454.schema.acq-invoice_item_fund_debit_fkey-deferrable.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1454', :eg_version);
 
 ALTER TABLE acq.invoice_item ALTER CONSTRAINT invoice_item_fund_debit_fkey DEFERRABLE INITIALLY DEFERRED;
 
