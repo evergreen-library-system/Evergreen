5505de1263e1ce3e9b710a45e851910b49f6824c
Stephanie Leary <stephanie.leary@equinoxoli.org>
2025-02-03 19:50:07 +0000

    LP2089178 Restore OPAC Shelving Location filter
    The #adv_copy_location_selector_new div was accidentally removed during
    the accordion and fieldset refactoring done in bug 2048682. This patch
    ensures that a fieldset with the required ID exists so that it can be
    filled in by the render_adv_copy_locations_new() function in copyloc.js,
    and adds a comment to alert future developers of the relationship.
    
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

--- Open-ILS/web/js/ui/default/opac/copyloc.js
+++ Open-ILS/web/js/ui/default/opac/copyloc.js
@@ -96,7 +96,7 @@ function render_adv_copy_locations_new(locations) {
         
         ulist.appendChild(dojo.create('li')).appendChild(dojo.create('div', {class: "form-check"})).appendChild(dojo.create('label', {innerHTML : loc.name, class: "form-check-label"})).prepend(dojo.create('input', attrs));
     });
-    sel.appendChild(dojo.create("div", {class: "card-body"})).appendChild(ulist);}
+    sel.appendChild(ulist);}
 }
 
    
