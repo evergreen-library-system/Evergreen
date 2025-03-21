48bdcabf928785e0db9aa649c0b6a4714c3ffcdc
Mike Rylander <mrylander@gmail.com>
2025-03-21 11:25:38 -0400

    Stamping upgrade scripts for new-tab/no-new-tabs
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1465.schema.staff-portal-urls-newtab.sql
+++ Open-ILS/src/sql/Pg/upgrade/1465.schema.staff-portal-urls-newtab.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1465', :eg_version);
 
 ALTER TABLE config.ui_staff_portal_page_entry
 ADD COLUMN url_newtab boolean;
--- Open-ILS/src/sql/Pg/upgrade/1466.data.ws-setting-no-newtabs.sql
+++ Open-ILS/src/sql/Pg/upgrade/1466.data.ws-setting-no-newtabs.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('YYYY', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1466', :eg_version);
 
 INSERT into config.workstation_setting_type
     (name, grp, label, description, datatype)
