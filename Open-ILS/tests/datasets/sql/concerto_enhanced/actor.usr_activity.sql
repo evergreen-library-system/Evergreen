55698546cda7cdef7bbce5408e4f964eb2302cf4
blake <blake@mobiusconsortium.org>
2025-04-09 14:57:27 -0500

    Regenerate and update the enhanced concerto dataset
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/tests/datasets/sql/concerto_enhanced/actor.usr_activity.sql
+++ Open-ILS/tests/datasets/sql/concerto_enhanced/actor.usr_activity.sql
@@ -1,6 +1,6 @@
-COPY actor.usr_activity (id, usr, etype, event_time) FROM stdin;
-23	1	17	2021-08-04 09:27:19.495139-05
-110	1	26	2022-06-17 15:51:18.092677-05
+COPY actor.usr_activity (id, usr, etype, event_time, event_data) FROM stdin;
+23	1	17	2021-08-04 09:27:19.495139-05	\N
+110	1	26	2022-06-17 15:51:18.092677-05	\N
 \.
 
 \echo sequence update column: id
