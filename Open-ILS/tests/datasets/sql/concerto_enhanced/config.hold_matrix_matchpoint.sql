f49b0d80645bcd4def76cc0ea43ca289de065a20
blake <blake@mobiusconsortium.org>
2025-04-09 12:50:00 -0500

    Fixing enhanced concerto
    Skipping inclusion of sip.setting
    The logic attempts to ORDER BY a json column which isn't allowed
    
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/tests/datasets/sql/concerto_enhanced/config.hold_matrix_matchpoint.sql
+++ Open-ILS/tests/datasets/sql/concerto_enhanced/config.hold_matrix_matchpoint.sql
@@ -1,5 +1,5 @@
 COPY config.hold_matrix_matchpoint (id, active, strict_ou_match, user_home_ou, request_ou, pickup_ou, item_owning_ou, item_circ_ou, usr_grp, requestor_grp, circ_modifier, marc_type, marc_form, marc_bib_level, marc_vr_format, juvenile_flag, ref_flag, item_age, holdable, distance_is_from_owner, transit_range, max_holds, include_frozen_holds, stop_blocked_user, age_hold_protect_rule, description) FROM stdin;
-3	1	0	\N	\N	\N	\N	\N	\N	18	\N	\N	\N	\N	\N	\N	0	\N	0	0	\N	\N	0	0	\N	No holds for Digital Patrons
+3	1	0	\N	\N	\N	\N	\N	\N	1018	\N	\N	\N	\N	\N	\N	0	\N	0	0	\N	\N	0	0	\N	No holds for Digital Patrons
 4	1	0	\N	\N	\N	\N	\N	\N	1	bestseller no hold	\N	\N	\N	\N	\N	0	\N	0	0	\N	\N	0	0	\N	\N
 \.
 
