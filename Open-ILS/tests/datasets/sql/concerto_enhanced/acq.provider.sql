9c37d3479fe4755332cea974eb1261f8d47ee36b
blake <blake@mobiusconsortium.org>
2025-04-09 14:57:27 -0500

    Regenerate and update the enhanced concerto dataset
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/tests/datasets/sql/concerto_enhanced/acq.provider.sql
+++ Open-ILS/tests/datasets/sql/concerto_enhanced/acq.provider.sql
@@ -1,9 +1,9 @@
-COPY acq.provider (id, name, owner, currency_type, code, holding_tag, san, edi_default, active, prepayment_required, url, email, phone, fax_phone, default_copy_count, default_claim_policy, primary_contact, buyer_san) FROM stdin;
-1	Ingram	1	USD	INGRAM	970	1697978	\N	1	0	\N	\N	\N	\N	0	\N	\N	\N
-2	Brodart	2	USD	BRODART	970	1697684	1	1	0	\N	\N	\N	\N	0	\N	\N	\N
-3	Baker & Taylor	1	USD	BT	970	3349659	\N	1	0	\N	\N	\N	\N	0	\N	\N	\N
-4	Initech	4	USD	IT	970	1001001	2	1	0	\N	\N	\N	\N	0	\N	\N	\N
-5	Overdrive	105	USD	OVERDRIVE	\N	\N	\N	1	0	\N	\N	\N	\N	1	\N	\N	\N
+COPY acq.provider (id, name, owner, currency_type, code, holding_tag, san, buyer_san, edi_default, active, prepayment_required, url, email, phone, fax_phone, default_copy_count, default_claim_policy, primary_contact) FROM stdin;
+1	Ingram	1	USD	INGRAM	970	1697978	\N	\N	1	0	\N	\N	\N	\N	0	\N	\N
+2	Brodart	2	USD	BRODART	970	1697684	\N	1	1	0	\N	\N	\N	\N	0	\N	\N
+3	Baker & Taylor	1	USD	BT	970	3349659	\N	\N	1	0	\N	\N	\N	\N	0	\N	\N
+4	Initech	4	USD	IT	970	1001001	\N	2	1	0	\N	\N	\N	\N	0	\N	\N
+5	Overdrive	105	USD	OVERDRIVE	\N	\N	\N	\N	1	0	\N	\N	\N	\N	1	\N	\N
 \.
 
 \echo sequence update column: id
--- Open-ILS/tests/datasets/sql/concerto_enhanced/permission.grp_tree.sql
+++ Open-ILS/tests/datasets/sql/concerto_enhanced/permission.grp_tree.sql
@@ -1,8 +1,8 @@
-COPY permission.grp_tree (id, name, parent, usergroup, perm_interval, description, application_perm, hold_priority) FROM stdin;
-1016	Patron - Student	2	1	1 year	No overdue fines	\N	0
-1017	Patron - Homebound	2	1	2 years	No overdue fines	\N	0
-1018	Patron - Digital Only	2	1	5 years	No checkouts of physical materials	\N	0
-1019	Patron - Restricted	2	1	3 years	Limited to 2 concurrent checkouts	\N	0
+COPY permission.grp_tree (id, name, parent, usergroup, perm_interval, description, application_perm, hold_priority, mfa_allowed, mfa_required) FROM stdin;
+1016	Patron - Student	2	1	1 year	No overdue fines	\N	0	0	0
+1017	Patron - Homebound	2	1	2 years	No overdue fines	\N	0	0	0
+1018	Patron - Digital Only	2	1	5 years	No checkouts of physical materials	\N	0	0	0
+1019	Patron - Restricted	2	1	3 years	Limited to 2 concurrent checkouts	\N	0	0	0
 \.
 
 \echo sequence update column: id
