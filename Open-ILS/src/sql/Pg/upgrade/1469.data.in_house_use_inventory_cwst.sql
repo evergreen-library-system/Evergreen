6c34d66d7185d9b7d583a3c262b01c08dcb5a8ce
Terran McCanna <tmccanna@georgialibraries.org>
2025-05-15 11:46:33 -0400

    LP2095128 Stamp upgrade script
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>

--- Open-ILS/src/sql/Pg/upgrade/1469.data.in_house_use_inventory_cwst.sql
+++ Open-ILS/src/sql/Pg/upgrade/1469.data.in_house_use_inventory_cwst.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1469', :eg_version);
 
 INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
 VALUES ('eg.circ.in_house.do_inventory_update', 'circ', 'bool',
