636bf685ff4f01272eb074b96ec85bfff3b2c2fa
Jane Sandberg <sandbergja@gmail.com>
2025-05-25 08:19:09 -0700

    LP#1289651: stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1470.hard_due_date_values_perms.sql
+++ Open-ILS/src/sql/Pg/upgrade/1470.hard_due_date_values_perms.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1470', :eg_version);
 
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
  ( 688, 'UPDATE_HARD_DUE_DATE', oils_i18n_gettext(688,
