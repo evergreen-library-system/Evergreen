871f74731c58558f84160cbe090d8e9aa40b78f2
Josh Stompro <stomproj@larl.org>
2025-05-29 12:23:26 -0500

    LP#2111291 - Allow opac.hold_notify to be an empty string when placing holds in Angular interface.
    Follow up to LP#2073896 which ignored notification user settings if they were set to an empty
    string in the format '""'.
    
    In the case where a user doesn't want to be notified for holds, they have no hold
    notification methods enabled, so their opac.hold_notify user setting is just set
    to '""' and that change causes that to not be evaluated in applyUserSettings().
    So they get the default settings from the resetRecipient function. (Email and Phone).
    
    Release-Note: Allow opac.hold_notify to be an empty string in Angular hold interface.
    
    Testing Plan:
    
    Before the fix to confirm the issue:
    
    1. Pick a user to edit.
    2. Edit user and set an email and default hold phone number.
    3. Unselect all forms of hold notification checkboxes and save user.
    4. Attempt to place a hold for the user.
    5. Note that Email and Phone notifications are selected.  So it isn't possible for
       the user to choose to have no forms of notification.
    
    After the fix.
    1. Pick a user to edit.
    2. Edit user and set an email and default hold phone number.
    3. Unselect all forms of hold notification checkboxes and save user.
    4. Attempt to place a hold for the user.
    5. Note that none of the notification preferences are selected, which matches
       the users settings.
    
    Signed-off-by: Josh Stompro <stomproj@larl.org>
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

--- Open-ILS/src/eg2/src/app/staff/catalog/hold/hold.component.ts
+++ Open-ILS/src/eg2/src/app/staff/catalog/hold/hold.component.ts
@@ -490,7 +490,11 @@ export class HoldComponent implements OnInit {
             const name = setting.name();
             let value = setting.value();
 
-            if (value === '' || value === null || value === '""') { return; }
+            // Exclude hold notification from empty quote check because
+            // a user may wish to have no hold notification options which
+            // is stored as '""'
+            if (value === '' || value === null || 
+                (value === '""' && name != 'opac.hold_notify' )) { return; }
 
             // When fleshing 'settings' on the actor.usr object,
             // we're grabbing the raw JSON values.
