56ea9d76098116427451c5de7a3c9fd5bfe845ca
Terran McCanna <tmccanna@georgialibraries.org>
2025-06-12 12:01:52 -0400

    LP1927510 Coded Value Map Save Grid Settings
    Adds a workstation setting for eg.grid.admin.config.coded_value_map
    
    To test, go to: Administration > Server Administration > MARC Coded Value
    Maps. Open the browser console (F12), change the visible columns
    and click 'Save Grid Settings.' Without the patch, a console error
    appears indicating that no settings exists, and refreshing the page
    shows that the column changes did not save.
    
    Release-note: Fixes 'Save Grid Settings' on MARC Coded Value Maps grid.
    
    Signed-off-by: Terran McCanna <tmccanna@georgialibraries.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

---
+++ Open-ILS/src/sql/Pg/XXXX.data.coded_value_map_grid.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+-- SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+
+INSERT into config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.config.coded_value_map', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.config.coded_value_map',
+        'Grid Config: eg.grid.admin.config.coded_value_map',
+        'cwst', 'label'
+    )
+);
+
+COMMIT;
