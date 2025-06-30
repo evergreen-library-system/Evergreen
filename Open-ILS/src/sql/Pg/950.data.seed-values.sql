dee5c0d621c0ca046b71aef3a6588a03f3baed0d
Michele Morgan <mmorgan@noblenet.org>
2025-06-30 11:32:58 -0400

    LP#1721131: Add upgrade script and seed data for workstation setting
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>
    Signed-off-by: Susan Morrison <smorrison@georgialibraries.org>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -24862,3 +24862,18 @@ VALUES (
     'object'
 );
 
+INSERT INTO config.workstation_setting_type (name, grp, label, description, datatype, fm_class)
+VALUES (
+    'eg.circ.patron.search.ou',
+    'circ',
+    oils_i18n_gettext(
+        'eg.circ.patron.search.ou',
+        'Staff Client patron search: home organization unit',
+        'cwst', 'label'),
+    oils_i18n_gettext(
+        'eg.circ.patron.search.ou',
+        'Specifies the home organization unit for patron search',
+        'cwst', 'description'),
+    'link',
+    'aou'
+);
