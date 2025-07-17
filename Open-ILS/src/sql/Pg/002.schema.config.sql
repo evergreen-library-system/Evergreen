02889b2bca033d4cc61664dd8b4a2181774faf18
Mike Rylander <mrylander@gmail.com>
2025-07-17 09:47:25 -0400

    Stamp baseline db version for LP#1648276
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1476', :eg_version); -- csharp/elidavis 
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1477', :eg_version); -- csharp/elidavis 
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
