a4925b9001d0bd50549d113ae319c29571dc4c91
Steven Mayo <smayo@georgialibraries.org>
2025-07-17 13:10:39 -0400

    lp2069220 Search_Filter_Group_Config_Save_Grid_Settings
    Adds a persistKey and SQL to allow saving grid settings in the search
    filter group config screen.
    
    Release-Note: Adds grid settings for Search Filter Group Configuration
    Signed-off-by: Steven Mayo <smayo@georgialibraries.org>
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -25050,6 +25050,16 @@ VALUES (
        )
 );
 
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.local.config.search_filter_group', 'gui', 'object',
+    oils_i18n_gettext(
+        'eg.grid.admin.local.config.search_filter_group',
+        'Grid Config: admin.local.config.search_filter_group',
+        'cwst', 'label'
+    )
+);
+
 INSERT into config.workstation_setting_type
     (name, grp, label, description, datatype)
 VALUES (
