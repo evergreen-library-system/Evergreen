47ce7f7f05e7b899d36107df00860574d4eece2b
Stephanie Leary <stephanie.leary@equinoxoli.org>
2025-08-09 03:59:35 +0000

    LP1958076 Grid settings for EDI Attribute Sets admin
    Adds grid settings persistKey and seed data for the EDI Attribute Sets
    administration screen.
    
    Release-note: Adds grid settings for EDI Attribute Sets administration.
    
    Signed-off-by: Stephanie Leary <stephanie.leary@equinoxoli.org>
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -26078,3 +26078,15 @@ VALUES (
     'link',
     'aou'
 );
+
+
+INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
+VALUES (
+    'eg.grid.admin.acq.edi_attr_set', 'gui', 'object', 
+    oils_i18n_gettext(
+        'eg.grid.admin.acq.edi_attr_set',
+        'EDI Attribute Sets Grid Settings',
+        'cwst', 'label'
+    )
+);
+
