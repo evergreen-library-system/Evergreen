71888629419b4efd6ba47a0df469aeb551292aa8
Jane Sandberg <sandbergja@gmail.com>
2025-08-09 17:14:22 -0700

    lp1908764: stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1482.data.survey-save-grid-settings.sql
+++ Open-ILS/src/sql/Pg/upgrade/1482.data.survey-save-grid-settings.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1482', :eg_version);
 
 
 INSERT INTO config.workstation_setting_type (name, grp, datatype, label)
