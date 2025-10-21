397b4abaa811f80e8c11925e55c24394ba021dde
Michele Morgan <mmorgan@noblenet.org>
2025-10-21 14:42:48 -0400

    LP2120713 Stamp upgrade script
    Signed-off-by: Michele Morgan <mmorgan@noblenet.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1487', :eg_version); -- miker
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1492', :eg_version); -- stevecallender/tkunesh/mmorgan
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
--- Open-ILS/src/sql/Pg/upgrade/1492.functions_bib_merge_update_for_deleted_parts.sql
+++ Open-ILS/src/sql/Pg/upgrade/1492.functions_bib_merge_update_for_deleted_parts.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1492', :eg_version);
 
 CREATE OR REPLACE FUNCTION asset.merge_record_assets( target_record BIGINT, source_record BIGINT ) RETURNS INT AS $func$
 DECLARE
