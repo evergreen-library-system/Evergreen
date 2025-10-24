b48d7dbf25f186f99687285583188962f15645b0
Galen Charlton <gmc@equinoxOLI.org>
2025-10-24 15:43:46 -0400

    LP2089564: stamp database update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1493.schema.staff_only_library_groups.sql
+++ Open-ILS/src/sql/Pg/upgrade/1493.schema.staff_only_library_groups.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('xxxx', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1493', :eg_version);
 ALTER TABLE actor.org_lasso ADD COLUMN IF NOT EXISTS opac_visible BOOL NOT NULL DEFAULT TRUE;
 
 COMMIT;
