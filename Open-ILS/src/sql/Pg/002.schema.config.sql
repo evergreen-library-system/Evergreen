42f55fb10fb77beb01df2fd58d1fefba995938cb
Galen Charlton <gmc@equinoxOLI.org>
2025-10-29 12:53:21 -0400

    Bumping version numbers
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1500', :eg_version); -- sleary/miker/mmorgan/mccanna/sandbergja
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.16-beta', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
