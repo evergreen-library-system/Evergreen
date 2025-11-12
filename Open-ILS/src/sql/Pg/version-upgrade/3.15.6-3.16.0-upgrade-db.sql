4063c9b4a9174eb3fe2c1258890295cc5833845e
Galen Charlton <gmc@equinoxOLI.org>
2025-11-12 15:29:26 +0000

    update DB update script for 3.16.0 release
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/version-upgrade/3.15.6-3.16.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/3.15.6-3.16.0-upgrade-db.sql
@@ -1,7 +1,7 @@
---Upgrade Script for 3.15.6 to 3.16-rc
-\set eg_version '''3.16-rc'''
+--Upgrade Script for 3.15.6 to 3.16.0
+\set eg_version '''3.16.0'''
 BEGIN;
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.16-beta', :eg_version);
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.16.0', :eg_version);
 
 SELECT evergreen.upgrade_deps_block_check('1469', :eg_version);
 
