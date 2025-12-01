e1b9384c4381a2ac9bcaddc1eb8cf9e59ad938d1
Galen Charlton <gmc@equinoxOLI.org>
2025-12-01 10:17:31 -0500

    LP#2132031: stamp database updated
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1506.schema.anti_delete_rule_normalization.sql
+++ Open-ILS/src/sql/Pg/upgrade/1506.schema.anti_delete_rule_normalization.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1506', :eg_version);
 
 CREATE OR REPLACE FUNCTION evergreen.setup_delete_protect_rule (
     t_schema TEXT,
