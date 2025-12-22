ad34d7fc999c72e54ddd0b9ce81b7a224ea32d7a
Galen Charlton <gmc@equinoxOLI.org>
2025-12-22 16:44:05 -0500

    LP#2137016: control strictness of address checks when offering e-renewal
    This patch adds a new library setting, "Number of valid addresses needed to offer patron
    account e-renewal via Quipu", is added to specify the number of valid
    addresses that a patron record must have in order to be eligible for
    e-renewal.
    
    This setting takes a value of 0, 1, or 2:
    
     * 0: Do not consider the patron's addresses when checking whether to
       offer e-renewal
     * 1: Either the mailing address or the billing/physical address must
       be set and marked as valid for e-renewal to be offered.
     * 2: Both the mailing address and the billing/physical address must be
       set and marked as valid.
    
    If the library setting is not set, the default value used is 2, following
    the behavior before the setting was added.
    
    This patch also does a bit of fixing and refactoring:
    
    * When checking whether to offer e-renewal on the My Account home page,
      always fetch the patron record from the database. This fixes a crash
      that can occur (especiallly when mixing patron record edits and OPAC
      account eaccess) if the "au" object in the login session is out of date.
    * Set the valid_mailing_address and valid_billing_address context variables
      to idiomatic Perl Boolean values.
    
    To test
    -------
    Note that testing does not require that a full Quipu configuation be in
    place.
    
    [0] Apply the patch.
    [1] Turn on the "Enable eCard request renewal feature in the OPAC" library
        setting.
    [2] Set up a patron record whose expiration date is in the past and
        that has both a valid mailing address and a valid physical address. The
        patron must be active, not barred, and not have a standing penalty that
        blocks e-renewal.
    [3] Log into the OPAC and verify that a link to renew the account is offered
        on the My Account home page.
    [4] Mark one of the addresses as not invalid and refresh the My Account page,
        This time, a message saying that e-renewal cannot be offered should be
        displayed.
    [5] Set the ""Number of valid addresses needed to offer patron account e-renewal via Quipu""
        library setting to zero and refresh the My Account Page. This time,
        e-renewal should be offered.
    [6] As with step 5, but using 1 as the setting value. E-renewal should still be
        offered.
    [7] As with step 5, but using 2 as the value. E-renewal should not be offered.
    [8] Test various permutations of the setting and whether or not the the patron
        has valid mailing and/or physical addresses (or no addresses at all). Verify
        that the setting behaves as described.
    [9] As with step 8, but this time check the state of the e-renewal offer link
        under My Account -> Preferences -> Personal Information in the OPAC.
    
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>
    Signed-off-by: Elizabeth Davis <elizabeth.davis@sparkpa.org>
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -4880,6 +4880,14 @@ INSERT into config.org_unit_setting_type
         'Organizational unit used by the Evergreen admin user of the Quipu eCard feature',
         'coust', 'description'),
     'link', 'aou')
+,( 'vendor.quipu.erenew.num_addresses_required', 'lib',
+    oils_i18n_gettext('vendor.quipu.erenew.num_addresses_required',
+        'Number of valid addresses needed to offer patron account e-renewal via Quipu',
+        'coust', 'label'),
+    oils_i18n_gettext('vendor.quipu.erenew.num_addresses_required',
+        'Number of valid addresses that a patron record must have in order to offer e-renewal via Quipu. Zero means that the patron record address are not considered; one means that either the mailing or billing/physical address must be set and marked as valid; two means that both the mailing and billing/physical address must be set and marked as valid. Default value is two (2).',
+        'coust', 'description'),
+    'integer', null)
 
 ,( 'lib.info_url', 'lib',
     oils_i18n_gettext('lib.info_url',
