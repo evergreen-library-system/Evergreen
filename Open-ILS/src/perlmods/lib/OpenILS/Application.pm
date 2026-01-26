29629a3c2ecd6d2d07dfd4521f33f882a336a603
Galen Charlton <gmc@equinoxOLI.org>
2026-01-26 13:50:49 -0500

    Revert "Bumping version numbers and adding Upgrade Script"
    This reverts commit c2a84896123f61231126a31baf89844b0d3acdcd.
    
    Committed in error; this type of build commit belongs only in the tag
    branches.

--- Open-ILS/src/perlmods/lib/OpenILS/Application.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application.pm
@@ -26,7 +26,7 @@ sub use_authoritative {
 sub ils_version {
     # version format is "x-y-z", for example "2-0-0" for Evergreen 2.0.0
     # For branches, format is "x-y"
-    return "3-16-3";
+    return "HEAD";
 }
 
 __PACKAGE__->register_method(
