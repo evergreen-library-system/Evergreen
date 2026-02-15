b6efdb5aa8abc875469788b11cdade4754771ebd
Jane Sandberg <js7389@princeton.edu>
2026-02-15 08:26:14 -0800

    LP#2137016: stamp upgrade script
    Signed-off-by: Jane Sandberg <js7389@princeton.edu>

--- Open-ILS/src/sql/Pg/upgrade/1510.data.quipu-erenew-address-check-setting.sql
+++ Open-ILS/src/sql/Pg/upgrade/1510.data.quipu-erenew-address-check-setting.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1510', :eg_version);
 
 INSERT into config.org_unit_setting_type
 ( name, grp, label, description, datatype, fm_class ) VALUES
