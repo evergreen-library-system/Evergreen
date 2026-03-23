4ad96114f5ee05bc629f4dad889266889cb5994a
Jane Sandberg <sandbergja@gmail.com>
2026-03-23 12:04:46 -0700

    LP#885270: stamp upgrade script
    Signed-off-by: Jane Sandberg <sandbergja@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1512.function.lp885270_usr_purge_shared_address.sql
+++ Open-ILS/src/sql/Pg/upgrade/1512.function.lp885270_usr_purge_shared_address.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1512', :eg_version);
 
 CREATE OR REPLACE FUNCTION actor.usr_purge_data(
 	src_usr  IN INTEGER,
