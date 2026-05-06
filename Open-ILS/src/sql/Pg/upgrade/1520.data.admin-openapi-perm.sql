d61358a995d63cab191cd9a895c1c9289f12b024
Galen Charlton <gmc@equinoxOLI.org>
2026-05-06 17:22:51 -0400

    LP#2148133: stamp schema update
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- Open-ILS/src/sql/Pg/upgrade/1520.data.admin-openapi-perm.sql
+++ Open-ILS/src/sql/Pg/upgrade/1520.data.admin-openapi-perm.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1520', :eg_version);
 
 INSERT INTO permission.perm_list (code)
 VALUES ('ADMIN_OPENAPI')
