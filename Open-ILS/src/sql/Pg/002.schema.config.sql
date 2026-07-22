a38f935398c75b4160dc522db30f2b17c5d1f90d
Gina Monti <gmonti@biblio.org>
2026-07-22 14:24:32 -0400

    3.16.9 upload
    Signed-off-by: Gina Monti <gmonti@biblio.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -93,6 +93,7 @@ CREATE TRIGGER no_overlapping_deps
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1522', :eg_version); -- Dyrcona/miker
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.16.9', :eg_version);
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
