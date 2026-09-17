5db31b9849eb2ee9d9e7d2177a95fbb96b5f124f
blake <blake@mobiusconsortium.org>
2026-09-17 14:38:04 -0500

    LP#2146576: stamp upgrade script Retro-fixing
    Fixing a commit mistake, moving 1525 to 1527
    this ensures that 1525/1527 is going to get ran
    by our server admins at some point
    
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1526', :eg_version); -- ianskelskey/Bmagic
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1527', :eg_version); -- stompro/smorrison/sandbergja
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.17.5', :eg_version);
 
 CREATE TABLE config.bib_source (
--- Open-ILS/src/sql/Pg/version-upgrade/3.17.4-3.17.5-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/3.17.4-3.17.5-upgrade-db.sql
@@ -3,16 +3,6 @@
 BEGIN;
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.17.5', :eg_version);
 
-SELECT evergreen.upgrade_deps_block_check('1525', :eg_version);
-
-UPDATE config.org_unit_setting_type
-SET grp='holds'
-WHERE
-name='circ.holds.calculated_age_proximity'
-and grp='circ'
-;
-
-
 SELECT evergreen.upgrade_deps_block_check('1526', :eg_version);
 
 -- Fix broken JOIN in the 'ateo' case of evergreen.hint_opt_in_check.
@@ -48,6 +38,15 @@ BEGIN
     END CASE;
 END;
 $f$ STABLE LANGUAGE PLPGSQL;
+
+SELECT evergreen.upgrade_deps_block_check('1527', :eg_version);
+
+UPDATE config.org_unit_setting_type
+SET grp='holds'
+WHERE
+name='circ.holds.calculated_age_proximity'
+and grp='circ'
+;
 
 COMMIT;
 
