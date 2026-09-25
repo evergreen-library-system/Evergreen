a42b6d09ca9158e682c10b83e5ede9af1ed06e60
Mike Rylander <mrylander@gmail.com>
2026-09-25 15:05:57 -0400

    Stamping upgrade script
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/1529.data.openapi_search_endpoint.sql
+++ Open-ILS/src/sql/Pg/upgrade/1529.data.openapi_search_endpoint.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('1529', :eg_version);
 
 INSERT INTO openapi.endpoint_set
 (name, description, active, rate_limit)
